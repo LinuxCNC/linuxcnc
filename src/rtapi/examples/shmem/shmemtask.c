8b68b631bd4023f20eb587974ff66687794fea6e
Paul Corner <paul_c@users.sourceforge.net>
2003-12-23 22:52:46 +0000

    Minor oops....

--- src/rtapi/examples/shmem/shmemtask.c
+++ src/rtapi/examples/shmem/shmemtask.c
@@ -38,7 +38,7 @@ int rtapi_app_main(void)
     int shmem_prio;
     long period;
 
-    module = rtapi_init("SHMMTASK");
+    module = rtapi_init("SHMEMTASK");
     if (module < 0) {
 	rtapi_print("shmemtask init: rtapi_init returned %d\n", module);
 	return -1;
