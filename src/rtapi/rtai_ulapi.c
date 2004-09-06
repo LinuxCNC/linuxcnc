f23650c067962691573eff81b82e5becd7d8745b
Paul Corner <paul_c@users.sourceforge.net>
2004-09-06 21:09:11 +0000

    Some of the code used has been taken from GPL sources, so GPL MUST be used. See section about 'derived code' - This of course means that any 3rd party code linked to rtapi must also be GPL

--- src/rtapi/rtai_ulapi.c
+++ src/rtapi/rtai_ulapi.c
@@ -18,7 +18,7 @@
 */
 
 /* This library is free software; you can redistribute it and/or
-   modify it under the terms of version 2.1 of the GNU Lesser General
+   modify it under the terms of version 2.1 of the GNU General
    Public License as published by the Free Software Foundation.
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
