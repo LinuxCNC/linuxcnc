f64f4febea278ef775ad1642eb9859bddcbf0802
Paul Corner <paul_c@users.sourceforge.net>
2005-01-01 17:48:50 +0000

    Most of the sources in place now... Just the Makefiles to fix up..

--- src/emc/kinematics/mmxavg.c
+++ src/emc/kinematics/mmxavg.c
@@ -1,112 +1,135 @@
-/********************************************************************
-* Description: mmxavg.c
-*   Min-max-average code
-*
-*   Derived from a work by Fred Proctor & Will Shackleford
-*
-* Author:
-* License: GPL Version 2
-* System: Linux
-*    
-* Copyright (c) 2004 All rights reserved.
-*
-* Last change:
-* $Revision$
-* $Author$
-* $Date$
-********************************************************************/
+/*
+  mmxavg.c
 
-#include "mmxavg.h"		/* these declarations */
+  Min-max-average code
+
+  Modification history:
+
+  14-Jun-2001 WPS put in checks for NULL pointers and paranoid index checks.
+  13-Mar-2000 WPS added unused attribute to pid_h to avoid 'defined but not used' compiler warning.
+  10-Oct-1997  FMP created
+  */
+
+#include "mmxavg.h"             /* these declarations */
+
+/* ident tag */
+#ifndef __GNUC__
+#ifndef __attribute__
+#define __attribute__(x)
+#endif
+#endif
+
+static char __attribute__((unused)) ident[] = "$Id$";
 
 #ifndef MAIN
 /* MAIN not defined-- compile functions only, no main() */
 
-int mmxavgInit(MMXAVG_STRUCT * mmxavg, double *space, int size)
+int mmxavgInit(MMXAVG_STRUCT *mmxavg, double *space, int size)
 {
-    if (0 == mmxavg) {
-	return -1;
+  if(0 == mmxavg)
+    {
+      return -1;
     }
-    mmxavg->nums = space;
-    mmxavg->size = size;
+  mmxavg->nums = space;
+  mmxavg->size = size;
 
-    return mmxavgReset(mmxavg);
+  return mmxavgReset(mmxavg);
 }
 
-int mmxavgReset(MMXAVG_STRUCT * mmxavg)
+int mmxavgReset(MMXAVG_STRUCT *mmxavg)
 {
-    if (0 == mmxavg) {
-	return -1;
+  if(0 == mmxavg)
+    {
+      return -1;
     }
-    mmxavg->sum = 0;
-    mmxavg->index = 0;
-    mmxavg->in = 0;
-    mmxavg->inited = 0;
-    mmxavg->min = 0.0;
-    mmxavg->max = 0.0;
+  mmxavg->sum = 0;
+  mmxavg->index = 0;
+  mmxavg->in = 0;
+  mmxavg->inited = 0;
+  mmxavg->min = 0.0;
+  mmxavg->max = 0.0;
 
-    return 0;
+  return 0;
 }
 
-int mmxavgAdd(MMXAVG_STRUCT * mmxavg, double num)
+int mmxavgAdd(MMXAVG_STRUCT *mmxavg, double num)
 {
-    if (0 == mmxavg) {
-	return -1;
+  if(0 == mmxavg)
+    {
+      return -1;
     }
-    if (mmxavg->index >= mmxavg->size || mmxavg->index < 0) {
-	return -1;
+  if(mmxavg->index >= mmxavg->size ||
+     mmxavg->index < 0 )
+    {
+      return -1;
     }
-    if (mmxavg->in == mmxavg->size) {
-	/* already full-- knock off oldest */
-	mmxavg->sum -= mmxavg->nums[mmxavg->index];
-    } else {
-	mmxavg->in++;
+  if (mmxavg->in == mmxavg->size)
+    {
+      /* already full-- knock off oldest */
+      mmxavg->sum -= mmxavg->nums[mmxavg->index];
     }
-
-    mmxavg->nums[mmxavg->index] = num;
-    mmxavg->sum += num;
-    mmxavg->index++;
-    if (mmxavg->index == mmxavg->size) {
-	mmxavg->index = 0;
-    }
-
-    if (!mmxavg->inited) {
-	mmxavg->min = num;
-	mmxavg->max = num;
-	mmxavg->inited = 1;
-    } else if (num < mmxavg->min) {
-	mmxavg->min = num;
-    } else if (num > mmxavg->max) {
-	mmxavg->max = num;
+  else
+    {
+      mmxavg->in++;
     }
 
-    return 0;
+  mmxavg->nums[mmxavg->index] = num;
+  mmxavg->sum += num;
+  mmxavg->index++;
+  if (mmxavg->index == mmxavg->size)
+    {
+      mmxavg->index = 0;
+    }
+
+  if (! mmxavg->inited)
+    {
+      mmxavg->min = num;
+      mmxavg->max = num;
+      mmxavg->inited = 1;
+    }
+  else if (num < mmxavg->min)
+    {
+      mmxavg->min = num;
+    }
+  else if (num > mmxavg->max)
+    {
+      mmxavg->max = num;
+    }
+
+  return 0;
 }
 
-double mmxavgMin(MMXAVG_STRUCT * mmxavg)
+double mmxavgMin(MMXAVG_STRUCT *mmxavg)
 {
-    if (0 == mmxavg) {
-	return -1;
+  if(0 == mmxavg)
+    {
+      return -1;
     }
-    return mmxavg->min;
+  return mmxavg->min;
 }
 
-double mmxavgMax(MMXAVG_STRUCT * mmxavg)
+double mmxavgMax(MMXAVG_STRUCT *mmxavg)
 {
-    if (0 == mmxavg) {
-	return -1;
+  if(0 == mmxavg)
+    {
+      return -1;
     }
-    return mmxavg->max;
+  return mmxavg->max;
 }
 
-double mmxavgAvg(MMXAVG_STRUCT * mmxavg)
+double mmxavgAvg(MMXAVG_STRUCT *mmxavg)
 {
-    if (0 == mmxavg) {
-	return -1;
+  if(0 == mmxavg)
+    {
+      return -1;
     }
-    if (mmxavg->in > 0) {
-	return mmxavg->sum / mmxavg->in;
-    } else {
-	return 0.0;
+  if (mmxavg->in > 0)
+    {
+      return mmxavg->sum / mmxavg->in;
+    }
+  else
+    {
+      return 0.0;
     }
 }
 
@@ -124,60 +147,72 @@ double mmxavgAvg(MMXAVG_STRUCT * mmxavg)
   */
 int main(int argc, char *argv[])
 {
-    MMXAVG_STRUCT mmxavg;
-    int size;
-    double *space;
-    double num;
-    int count = 0;
-    int which = 3;		/* 1=min, 2=max, 3=avg printed */
+  MMXAVG_STRUCT mmxavg;
+  int size;
+  double *space;
+  double num;
+  int count = 0;
+  int which = 3;                /* 1=min, 2=max, 3=avg printed */
 
-    /* check args */
-    if (argc < 2) {
-	fprintf(stderr, "syntax: %s <size> {\"min\" | \"max\"}\n", argv[0]);
-	exit(1);
+  /* check args */
+  if (argc < 2)
+    {
+      fprintf(stderr, "syntax: %s <size> {\"min\" | \"max\"}\n",
+              argv[0]);
+      exit(1);
     }
 
-    /* read first mandatory arg */
-    if (1 != sscanf(argv[1], "%d", &size) || size < 1) {
-	fprintf(stderr, "size arg must be > 0\n");
-	exit(1);
+  /* read first mandatory arg */
+  if (1 != sscanf(argv[1], "%d", &size) ||
+      size < 1)
+    {
+      fprintf(stderr, "size arg must be > 0\n");
+      exit(1);
     }
 
-    /* read optional args */
-    if (argc > 2) {
-	if (!strcmp(argv[2], "min")) {
-	    which = 1;
-	} else if (!strcmp(argv[2], "max")) {
-	    which = 2;
-	} else {
-	    fprintf(stderr, "printing arg must be \"min\" or \"max\"\n");
-	    exit(1);
-	}
+  /* read optional args */
+  if (argc > 2)
+    {
+      if (!strcmp(argv[2], "min"))
+        {
+          which = 1;
+        }
+      else if (!strcmp(argv[2], "max"))
+        {
+          which = 2;
+        }
+      else
+        {
+          fprintf(stderr, "printing arg must be \"min\" or \"max\"\n");
+          exit(1);
+        }
     }
 
-    /* get space */
-    space = (double *) malloc(size * sizeof(double));
-    mmxavgInit(&mmxavg, space, size);
+  /* get space */
+  space = (double *) malloc(size * sizeof(double));
+  mmxavgInit(&mmxavg, space, size);
 
-    while (!feof(stdin)) {
-	if (1 != scanf("%lf", &num)) {
-	    break;
-	}
+  while (! feof(stdin))
+    {
+      if (1 != scanf("%lf", &num))
+        {
+          break;
+        }
 
-	mmxavgAdd(&mmxavg, num);
+      mmxavgAdd(&mmxavg, num);
 
-	if (which == 1)
-	    printf("%d\t%f\n", count++, mmxavgMin(&mmxavg));
-	else if (which == 2)
-	    printf("%d\t%f\n", count++, mmxavgMax(&mmxavg));
-	else
-	    printf("%d\t%f\n", count++, mmxavgAvg(&mmxavg));
+      if (which == 1)
+        printf("%d\t%f\n", count++, mmxavgMin(&mmxavg));
+      else if (which == 2)
+        printf("%d\t%f\n", count++, mmxavgMax(&mmxavg));
+      else
+        printf("%d\t%f\n", count++, mmxavgAvg(&mmxavg));
     }
 
-    /* release space */
-    free(space);
+  /* release space */
+  free(space);
 
-    exit(0);
+  exit(0);
 }
 
 #endif /* MAIN defined */
--- src/emc/kinematics/mmxavg.h
+++ src/emc/kinematics/mmxavg.h
@@ -1,56 +1,60 @@
-/********************************************************************
-* Description: mmxavg.h
-*   Min-max-average code
-*
-*   Derived from a work by Fred Proctor & Will Shackleford
-*
-* Author:
-* License: GPL Version 2
-* System: Linux
-*    
-* Copyright (c) 2004 All rights reserved.
-*
-* Last change:
-* $Revision$
-* $Author$
-* $Date$
-********************************************************************/
 #ifndef MMXAVG_H
 #define MMXAVG_H
+
+/*
+  mmxavg.h
+
+  Min-max-average code
+
+Modifications:
+
+  13-Mar-2000 WPS added unused attribute to mmxavg_h to avoid 'defined but not used' compiler warning.
+  */
+
+/* ident tag */
+#ifndef __GNUC__
+#ifndef __attribute__
+#define __attribute__(x)
+#endif
+#endif
+
+static char __attribute__((unused)) mmxavg_h[] = "$Id$";
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    typedef struct {
-	double sum;		/* total */
-	double *nums;		/* array for stored values */
-	int index;		/* index into array for next spot */
-	int in;			/* how many in array */
-	int size;		/* size of array; 0 <= index < max */
-	int inited;		/* non-zero means at least one value in */
-	double min, max;	/* saved min, max values */
-    } MMXAVG_STRUCT;
+typedef struct
+{
+  double sum;                   /* total */
+  double *nums;                 /* array for stored values */
+  int index;                    /* index into array for next spot */
+  int in;                       /* how many in array */
+  int size;                     /* size of array; 0 <= index < max */
+  int inited;                   /* non-zero means at least one value in */
+  double min, max;              /* saved min, max values */
+} MMXAVG_STRUCT;
 
 /* initializes vars */
-    extern int mmxavgInit(MMXAVG_STRUCT * mmxavg, double *space, int size);
+extern int mmxavgInit(MMXAVG_STRUCT *mmxavg, double *space, int size);
 
 /* re-initializes vars, leaves space alone */
-    extern int mmxavgReset(MMXAVG_STRUCT * mmxavg);
+extern int mmxavgReset(MMXAVG_STRUCT *mmxavg);
 
 /* adds a point to the mmxavg structure */
-    extern int mmxavgAdd(MMXAVG_STRUCT * mmxavg, double num);
+extern int mmxavgAdd(MMXAVG_STRUCT *mmxavg, double num);
 
 /* returns the min */
-    extern double mmxavgMin(MMXAVG_STRUCT * mmxavg);
+extern double mmxavgMin(MMXAVG_STRUCT *mmxavg);
 
 /* returns the max */
-    extern double mmxavgMax(MMXAVG_STRUCT * mmxavg);
+extern double mmxavgMax(MMXAVG_STRUCT *mmxavg);
 
 /* returns the average */
-    extern double mmxavgAvg(MMXAVG_STRUCT * mmxavg);
+extern double mmxavgAvg(MMXAVG_STRUCT *mmxavg);
 
 #ifdef __cplusplus
 }
 #endif
-#endif				/* MMXAVG_H */
+
+#endif /* MMXAVG_H */
