6d48a53b97bf67a0ccb4109660c162537b868fa0
Paul Corner <paul_c@users.sourceforge.net>
2005-03-23 00:22:10 +0000

    Overloaded some of the operators so that we can do array addressing and simple math on EmcPose.

--- src/emc/nml_intf/emcpos.h
+++ src/emc/nml_intf/emcpos.h
@@ -4,10 +4,26 @@
 
 #include "posemath.h"		/* PmCartesian */
 
+#ifdef __cplusplus
+class EmcPose 
+{
+    public:
+
+    EmcPose() {tran.x = tran.y = tran.z = a = b = c = 0;}
+
+    PmCartesian tran;
+    double a,b,c;
+    double & operator[](int i);
+    EmcPose operator - (EmcPose v1);
+    EmcPose operator + (EmcPose v1);
+    bool operator == (EmcPose v1);
+    bool operator != (EmcPose v1);
+};
+#else
+
 typedef struct _EmcPose {
-  PmCartesian tran;
-  double a,b,c;
+    PmCartesian tran;
+    double a,b,c;
 } EmcPose;
-
-
+#endif
 #endif
