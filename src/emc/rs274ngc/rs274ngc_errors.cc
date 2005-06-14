572b82c3fbf322627b1d870992546211c2ea0299
Matt Shaver <matt@mattshaver.com>
2005-06-14 06:08:36 +0000

    Forgot to update this file with changed position for FILE_NOT_OPEN error code value. This method of getting strings to represent error codes seems very hokey, especially since the errors and their values are declared one place and these strings are stored in hopefully the right order in this altogether different file.

--- src/emc/rs274ngc/rs274ngc_errors.cc
+++ src/emc/rs274ngc/rs274ngc_errors.cc
@@ -28,86 +28,86 @@ char * _rs274ngc_errors[] = {
 /*   1 */ _("No error"),
 /*   2 */ _("No error"),
 /*   3 */ _("No error"),
-/*   4 */ _("A file is already open"), // Interp::open
-/*   5 */ _("All axes missing with g92"), // enhance_block
-/*   6 */ _("All axes missing with motion code"), // enhance_block
-/*   7 */ _("Arc radius too small to reach end point"), // arc_data_r
-/*   8 */ _("Argument to acos out of range"), // execute_unary
-/*   9 */ _("Argument to asin out of range"), // execute_unary
-/*  10 */ _("Attempt to divide by zero"), // execute_binary1
-/*  11 */ _("Attempt to raise negative to non integer power"), // execute_binary1
-/*  12 */ _("Bad character used"), // read_one_item
-/*  13 */ _("Bad format unsigned integer"), // read_integer_unsigned
-/*  14 */ _("Bad number format"), // read_real_number
-/*  15 */ _("Bug bad g code modal group 0"), // check_g_codes
-/*  16 */ _("Bug code not g0 or g1"), // convert_straight, convert_straight_comp1, convert_straight_comp2
-/*  17 */ _("Bug code not g17 g18 or g19"), // convert_set_plane
-/*  18 */ _("Bug code not g20 or g21"), // convert_length_units
-/*  19 */ _("Bug code not g28 or g30"), // convert_home
-/*  20 */ _("Bug code not g2 or g3"), // arc_data_comp_ijk, arc_data_ijk
-/*  21 */ _("Bug code not g40 g41 or g42"), // convert_cutter_compensation
-/*  22 */ _("Bug code not g43 or g49"), // convert_tool_length_offset
-/*  23 */ _("Bug code not g4 g10 g28 g30 g53 or g92 series"), // convert_modal_0
-/*  24 */ _("Bug code not g61 g61 1 or g64"), // convert_control_mode
-/*  25 */ _("Bug code not g90 or g91"), // convert_distance_mode
-/*  26 */ _("Bug code not g93 or g94"), // convert_feed_mode
-/*  27 */ _("Bug code not g98 or g99"), // convert_retract_mode
-/*  28 */ _("Bug code not in g92 series"), // convert_axis_offsets
-/*  29 */ _("Bug code not in range g54 to g593"), // convert_coordinate_system
-/*  30 */ _("Bug code not m0 m1 m2 m30 m60"), // convert_stop
-/*  31 */ _("Bug distance mode not g90 or g91"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
-/*  32 */ _("Bug function should not have been called"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx, read_a, read_b, read_c, read_comment, read_d, read_f, read_g, read_h, read_i, read_j, read_k, read_l, read_line_number, read_m, read_p, read_parameter, read_parameter_setting, read_q, read_r, read_real_expression, read_s, read_t, read_x, read_y, read_z
-/*  33 */ _("Bug in tool radius comp"), // arc_data_comp_r
-/*  34 */ _("Bug plane not xy yz or xz"), // convert_arc, convert_cycle
-/*  35 */ _("Bug side not right or left"), // convert_straight_comp1, convert_straight_comp2
-/*  36 */ _("Bug unknown motion code"), // convert_motion
-/*  37 */ _("Bug unknown operation"), // execute_binary1, execute_binary2, execute_unary
-/*  38 */ _("Cannot change axis offsets with cutter radius comp"), // convert_axis_offsets
-/*  39 */ _("Cannot change units with cutter radius comp"), // convert_length_units
-/*  40 */ _("Cannot create backup file"), // Interp::save_parameters
-/*  41 */ _("Cannot do g1 with zero feed rate"), // convert_straight
-/*  42 */ _("Cannot do zero repeats of cycle"), // convert_cycle
-/*  43 */ _("Cannot make arc with zero feed rate"), // convert_arc
-/*  44 */ _("Cannot move rotary axes during probing"), // convert_probe
-/*  45 */ _("Cannot open backup file"), // Interp::save_parameters
-/*  46 */ _("Cannot open variable file"), // Interp::save_parameters
-/*  47 */ _("Cannot probe in inverse time feed mode"), // convert_probe
-/*  48 */ _("Cannot probe with cutter radius comp on"), // convert_probe
-/*  49 */ _("Cannot probe with zero feed rate"), // convert_probe
-/*  50 */ _("Cannot put a b in canned cycle"), // check_other_codes
-/*  51 */ _("Cannot put a c in canned cycle"), // check_other_codes
-/*  52 */ _("Cannot put an a in canned cycle"), // check_other_codes
-/*  53 */ _("Cannot turn cutter radius comp on out of xy plane"), // convert_cutter_compensation_on
-/*  54 */ _("Cannot turn cutter radius comp on when on"), // convert_cutter_compensation_on
-/*  55 */ _("Cannot use a word"), // read_a
-/*  56 */ _("Cannot use axis values with g80"), // enhance_block
-/*  57 */ _("Cannot use axis values without a g code that uses them"), // enhance_block
-/*  58 */ _("Cannot use b word"), // read_b
-/*  59 */ _("Cannot use c word"), // read_c
-/*  60 */ _("Cannot use g28 or g30 with cutter radius comp"), // convert_home
-/*  61 */ _("Cannot use g53 incremental"), // check_g_codes
-/*  62 */ _("Cannot use g53 with cutter radius comp"), // convert_straight
-/*  63 */ _("Cannot use two g codes that both use axis values"), // enhance_block
-/*  64 */ _("Cannot use xz plane with cutter radius comp"), // convert_set_plane
-/*  65 */ _("Cannot use yz plane with cutter radius comp"), // convert_set_plane
-/*  66 */ _("Command too long"), // read_text, Interp::open
-/*  67 */ _("Concave corner with cutter radius comp"), // convert_arc_comp2, convert_straight_comp2
-/*  68 */ _("Coordinate system index parameter 5220 out of range"), // Interp::init
-/*  69 */ _("Current point same as end point of arc"), // arc_data_r
-/*  70 */ _("Cutter gouging with cutter radius comp"), // convert_arc_comp1, convert_straight_comp1
-/*  71 */ _("D word with no g41 or g42"), // check_other_codes
-/*  72 */ _("Dwell time missing with g4"), // check_g_codes
-/*  73 */ _("Dwell time p word missing with g82"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
-/*  74 */ _("Dwell time p word missing with g86"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
-/*  75 */ _("Dwell time p word missing with g88"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
-/*  76 */ _("Dwell time p word missing with g89"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
-/*  77 */ _("Equal sign missing in parameter setting"), // read_parameter_setting
-/*  78 */ _("F word missing with inverse time arc move"), // convert_arc
-/*  79 */ _("F word missing with inverse time g1 move"), // convert_straight
-/*  80 */ _("File ended with no percent sign"), // read_text, Interp::open
-/*  81 */ _("File ended with no percent sign or program end"), // read_text
-/*  82 */ _("File name too long"), // Interp::open
-/*  83 */ _("File not open"), // Interp::read
+/*   4 */ _("File not open"), // Interp::read
+/*   5 */ _("A file is already open"), // Interp::open
+/*   6 */ _("All axes missing with g92"), // enhance_block
+/*   7 */ _("All axes missing with motion code"), // enhance_block
+/*   8 */ _("Arc radius too small to reach end point"), // arc_data_r
+/*   9 */ _("Argument to acos out of range"), // execute_unary
+/*  10 */ _("Argument to asin out of range"), // execute_unary
+/*  11 */ _("Attempt to divide by zero"), // execute_binary1
+/*  12 */ _("Attempt to raise negative to non integer power"), // execute_binary1
+/*  13 */ _("Bad character used"), // read_one_item
+/*  14 */ _("Bad format unsigned integer"), // read_integer_unsigned
+/*  15 */ _("Bad number format"), // read_real_number
+/*  16 */ _("Bug bad g code modal group 0"), // check_g_codes
+/*  17 */ _("Bug code not g0 or g1"), // convert_straight, convert_straight_comp1, convert_straight_comp2
+/*  18 */ _("Bug code not g17 g18 or g19"), // convert_set_plane
+/*  19 */ _("Bug code not g20 or g21"), // convert_length_units
+/*  20 */ _("Bug code not g28 or g30"), // convert_home
+/*  21 */ _("Bug code not g2 or g3"), // arc_data_comp_ijk, arc_data_ijk
+/*  22 */ _("Bug code not g40 g41 or g42"), // convert_cutter_compensation
+/*  23 */ _("Bug code not g43 or g49"), // convert_tool_length_offset
+/*  24 */ _("Bug code not g4 g10 g28 g30 g53 or g92 series"), // convert_modal_0
+/*  25 */ _("Bug code not g61 g61 1 or g64"), // convert_control_mode
+/*  26 */ _("Bug code not g90 or g91"), // convert_distance_mode
+/*  27 */ _("Bug code not g93 or g94"), // convert_feed_mode
+/*  28 */ _("Bug code not g98 or g99"), // convert_retract_mode
+/*  29 */ _("Bug code not in g92 series"), // convert_axis_offsets
+/*  30 */ _("Bug code not in range g54 to g593"), // convert_coordinate_system
+/*  31 */ _("Bug code not m0 m1 m2 m30 m60"), // convert_stop
+/*  32 */ _("Bug distance mode not g90 or g91"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
+/*  33 */ _("Bug function should not have been called"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx, read_a, read_b, read_c, read_comment, read_d, read_f, read_g, read_h, read_i, read_j, read_k, read_l, read_line_number, read_m, read_p, read_parameter, read_parameter_setting, read_q, read_r, read_real_expression, read_s, read_t, read_x, read_y, read_z
+/*  34 */ _("Bug in tool radius comp"), // arc_data_comp_r
+/*  35 */ _("Bug plane not xy yz or xz"), // convert_arc, convert_cycle
+/*  36 */ _("Bug side not right or left"), // convert_straight_comp1, convert_straight_comp2
+/*  37 */ _("Bug unknown motion code"), // convert_motion
+/*  38 */ _("Bug unknown operation"), // execute_binary1, execute_binary2, execute_unary
+/*  39 */ _("Cannot change axis offsets with cutter radius comp"), // convert_axis_offsets
+/*  40 */ _("Cannot change units with cutter radius comp"), // convert_length_units
+/*  41 */ _("Cannot create backup file"), // Interp::save_parameters
+/*  42 */ _("Cannot do g1 with zero feed rate"), // convert_straight
+/*  43 */ _("Cannot do zero repeats of cycle"), // convert_cycle
+/*  44 */ _("Cannot make arc with zero feed rate"), // convert_arc
+/*  45 */ _("Cannot move rotary axes during probing"), // convert_probe
+/*  46 */ _("Cannot open backup file"), // Interp::save_parameters
+/*  47 */ _("Cannot open variable file"), // Interp::save_parameters
+/*  48 */ _("Cannot probe in inverse time feed mode"), // convert_probe
+/*  49 */ _("Cannot probe with cutter radius comp on"), // convert_probe
+/*  50 */ _("Cannot probe with zero feed rate"), // convert_probe
+/*  51 */ _("Cannot put a b in canned cycle"), // check_other_codes
+/*  52 */ _("Cannot put a c in canned cycle"), // check_other_codes
+/*  53 */ _("Cannot put an a in canned cycle"), // check_other_codes
+/*  54 */ _("Cannot turn cutter radius comp on out of xy plane"), // convert_cutter_compensation_on
+/*  55 */ _("Cannot turn cutter radius comp on when on"), // convert_cutter_compensation_on
+/*  56 */ _("Cannot use a word"), // read_a
+/*  57 */ _("Cannot use axis values with g80"), // enhance_block
+/*  58 */ _("Cannot use axis values without a g code that uses them"), // enhance_block
+/*  59 */ _("Cannot use b word"), // read_b
+/*  60 */ _("Cannot use c word"), // read_c
+/*  61 */ _("Cannot use g28 or g30 with cutter radius comp"), // convert_home
+/*  62 */ _("Cannot use g53 incremental"), // check_g_codes
+/*  63 */ _("Cannot use g53 with cutter radius comp"), // convert_straight
+/*  64 */ _("Cannot use two g codes that both use axis values"), // enhance_block
+/*  65 */ _("Cannot use xz plane with cutter radius comp"), // convert_set_plane
+/*  66 */ _("Cannot use yz plane with cutter radius comp"), // convert_set_plane
+/*  67 */ _("Command too long"), // read_text, Interp::open
+/*  68 */ _("Concave corner with cutter radius comp"), // convert_arc_comp2, convert_straight_comp2
+/*  69 */ _("Coordinate system index parameter 5220 out of range"), // Interp::init
+/*  70 */ _("Current point same as end point of arc"), // arc_data_r
+/*  71 */ _("Cutter gouging with cutter radius comp"), // convert_arc_comp1, convert_straight_comp1
+/*  72 */ _("D word with no g41 or g42"), // check_other_codes
+/*  73 */ _("Dwell time missing with g4"), // check_g_codes
+/*  74 */ _("Dwell time p word missing with g82"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
+/*  75 */ _("Dwell time p word missing with g86"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
+/*  76 */ _("Dwell time p word missing with g88"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
+/*  77 */ _("Dwell time p word missing with g89"), // convert_cycle_xy, convert_cycle_yz, convert_cycle_zx
+/*  78 */ _("Equal sign missing in parameter setting"), // read_parameter_setting
+/*  79 */ _("F word missing with inverse time arc move"), // convert_arc
+/*  80 */ _("F word missing with inverse time g1 move"), // convert_straight
+/*  81 */ _("File ended with no percent sign"), // read_text, Interp::open
+/*  82 */ _("File ended with no percent sign or program end"), // read_text
+/*  83 */ _("File name too long"), // Interp::open
 /*  84 */ _("G code out of range"), // read_g
 /*  85 */ _("H word with no g43"), // check_other_codes
 /*  86 */ _("I word given for arc in yz plane"), // convert_arc
