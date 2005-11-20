b2c083bd7e3306e8f17bf5279c00496c3f280df8
Peter G. Vavaroutsos <pete@vavaroutsos.com>
2005-11-20 02:39:36 +0000

    Added GPL header.

--- src/hal/utils/m5i20cfg.c
+++ src/hal/utils/m5i20cfg.c
@@ -1,3 +1,42 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2005 Peter C. Wallace <pcw AT mesanet DOT com>
+ *
+ * $RCSfile$
+ * $Author$
+ * $Locker$
+ * $Revision$
+ * $State$
+ * $Date$
+ *
+ ******************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111 USA
+ *
+ * THE AUTHORS OF THIS LIBRARY ACCEPT ABSOLUTELY NO LIABILITY FOR
+ * ANY HARM OR LOSS RESULTING FROM ITS USE.  IT IS _EXTREMELY_ UNWISE
+ * TO RELY ON SOFTWARE ALONE FOR SAFETY.  Any machinery capable of
+ * harming persons must have provisions for completely removing power
+ * from all motors, etc, before persons enter any danger area.  All
+ * machinery must be designed to comply with local and national safety
+ * codes, and the authors of this software can not, and do not, take
+ * any responsibility for such compliance.
+ *
+ * This code was written as part of the EMC HAL project.  For more
+ * information, go to www.linuxcnc.org.
+ *
+ ******************************************************************************/
+
 /*---------------------------------------------------------------------------*/
 /*
    sc5i20l.c
