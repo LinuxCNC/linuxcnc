5e11436cc8059ee8ff62cb04e581941f9f38c273
Jeff Epler <jepler@unpythonic.net>
2006-01-30 03:37:07 +0000

    factor out the common "check string against whitelist" code and use it
    check the extension of the module name when inserting, and mention the
    permitted extensions in the help

--- src/module_helper/module_helper.c
+++ src/module_helper/module_helper.c
@@ -40,6 +40,12 @@ char *path_whitelist[] = {
     NULL
 };
 
+/* module extension must be one of these */
+
+char *ext_whitelist[] = {
+    ".o", ".ko", NULL
+};
+
 void error(int argc, char **argv) {
     int i;
     char *prog = argv[0];
@@ -51,14 +57,18 @@ void error(int argc, char **argv) {
     for(i=1; i<argc; i++) {
         fprintf(stderr, " %s", argv[i]);
     }
-    fprintf(stderr, "\n\nUsage: %s insert /path/to/module [param1=value1 ...]\n", prog);
+    fprintf(stderr, "\n\nUsage: %s insert /path/to/module.ext [param1=value1 ...]\n", prog);
     fprintf(stderr, "\nwhere module is one of:\n");
     for(i=0; module_whitelist[i]; i++) {
         fprintf(stderr, "\t%s\n", module_whitelist[i]);
-    }
-    fprintf(stderr, "\nand the path starts with one of:\n");
+    } 
+    fprintf(stderr, "\nthe path starts with one of:\n");
     for(i=0; path_whitelist[i]; i++) {
         fprintf(stderr, "\t%s\n", path_whitelist[i]);
+    }
+    fprintf(stderr, "\nand the extension is one of:\n");
+    for(i=0; ext_whitelist[i]; i++) {
+        fprintf(stderr, "\t%s\n", ext_whitelist[i]);
     }
 
     fprintf(stderr, "OR\n\n%s remove module\n\nwhere module is one of"
@@ -66,12 +76,31 @@ void error(int argc, char **argv) {
     exit(1);
 }
 
+/* Check that the next characters in target match one of the items
+ * in table.  Failure is indicated by NULL, success is indicated by returning
+ * a pointer to just after the end of the string matched from the whitelist.
+ * If target was NULL, then the result is NULL.
+ *
+ * This means that it is easy to check for what comes after the whitelisted
+ * characters (e.g., module extension) even when the table entries have
+ * different lengths.
+ */
+char *check_whitelist(char *target, char *table[]) {
+    int i;
+    if(!target) return 0;
+    for(i=0; table[i]; i++) {
+        int l = strlen(table[i]);
+        if(!strncmp(target, table[i], l)) return target + l;
+    }
+    return 0;
+}
+
 int main(int argc, char **argv) {
     char *mod;
     char *last_slash;
+    char *ext;
     int i;
     int inserting = 0;
-    int module_ok = 0, path_ok = 0;
     char **exec_argv;
 
     /* drop root privs temporarily */
@@ -91,16 +120,13 @@ int main(int argc, char **argv) {
 
         if(!last_slash || strstr(mod, "..")) error(argc, argv);
 
-        for(i=0; module_whitelist[i]; i++)
-            if(!strncmp(last_slash+1, module_whitelist[i], 
-                        strlen(module_whitelist[i])))
-                module_ok = 1;
+        ext = check_whitelist(last_slash + 1, module_whitelist);
+        if(!ext) error(argc, argv);
 
-        for(i=0; path_whitelist[i]; i++)
-            if(!strncmp(mod, path_whitelist[i], strlen(path_whitelist[i])))
-                path_ok = 1;
+        if(!check_whitelist(ext, ext_whitelist)) error(argc, argv);
 
-        if(!path_ok || !module_ok) error(argc, argv);
+        if(!check_whitelist(mod, path_whitelist)) error(argc, argv);
+
         exec_argv[0] = "/sbin/insmod";
         exec_argv[1] = mod;
 
@@ -109,11 +135,10 @@ int main(int argc, char **argv) {
         }
         exec_argv[argc-1] = NULL;
     } else {
-        for(i=0; module_whitelist[i]; i++)
-            if(!strcmp(module_whitelist[i], mod)) 
-                module_ok = 1;
+        char *end = check_whitelist(mod, module_whitelist);
 
-        if(!module_ok) error(argc, argv);
+        /* Check that end is not NULL (whitelist succeeded) and that it is the end of the string */
+        if(!end || *end) error(argc, argv);
 
         exec_argv[0] = "/sbin/rmmod";
         exec_argv[1] = mod;
@@ -127,3 +152,6 @@ int main(int argc, char **argv) {
     perror("execv failed");
     return 1;
 }
+
+/* vim:sts=4:et:sw=4
+ */
