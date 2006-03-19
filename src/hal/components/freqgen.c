2612d49dccc274a1a348d6b79c3255f22b9533d2
John Kasunich <jmkasunich@fastmail.fm>
2006-03-19 22:29:47 +0000

    fixed a typo

--- src/hal/components/freqgen.c
+++ src/hal/components/freqgen.c
@@ -438,7 +438,7 @@ int rtapi_app_main(void)
      for (n = 0; n < MAX_CHAN && step_type[n] != -1 ; n++) {
 	if (step_type[n] > MAX_STEP_TYPE) {
 	    rtapi_print_msg(RTAPI_MSG_ERR,
-			    "STEPGEN: ERROR: bad stepping type '%i', axes %i\n",
+			    "FREQGEN: ERROR: bad stepping type '%i', axes %i\n",
 			    step_type[n], n);
 	    return -1;
 	} else {
