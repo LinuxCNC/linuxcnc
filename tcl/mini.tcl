b4614ef25159659611e0d879e8934a9bf75537c6
Ray Henry <rehenry@copper.net>
2006-05-02 19:44:32 +0000

    added halshow to menus

--- tcl/mini.tcl
+++ tcl/mini.tcl
@@ -1011,6 +1011,8 @@ $settingsmenu add radiobutton -label [msgcat::mc "Relative Position"] \
 $settingsmenu add separator
 $settingsmenu add command -label [msgcat::mc "Calibration..."] \
     -command "exec $TCLBIN/emccalib.tcl -- -ini $EMC_INIFILE &"
+$settingsmenu add command -label [msgcat::mc "HAL Show..."] \
+    -command "exec $TCLBIN/halshow.tcl -- -ini $EMC_INIFILE &"
 $settingsmenu add command -label [msgcat::mc "HAL Config..."] \
     -command "exec $TCLBIN/halconfig.tcl -- -ini $EMC_INIFILE &"
 
