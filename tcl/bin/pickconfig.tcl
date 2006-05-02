3d16066d782838e404d0534f273f1ad755a975b1
Chris Radek <chris@timeguy.com>
2006-05-02 14:57:23 +0000

    default to previously-run config: copy of head v1.35

--- tcl/bin/pickconfig.tcl
+++ tcl/bin/pickconfig.tcl
@@ -76,6 +76,8 @@ if ([info exists env(LANG)]) {
     msgcat::mcload $LANGDIR
 }
 
+# use initialize_config for bwidget and .emcrc
+
 proc initialize_config {} {
     # need bwidget
     set result [catch {package require BWidget 1.7}]
@@ -84,6 +86,39 @@ proc initialize_config {} {
         tk_messageBox -icon error -type ok -message [msgcat::mc "Can't find the bwidget 1.7 package.  There is a debian bwidget package; install \nit with sudo apt-get install bwidget."]
         exit
         destroy .
+    } else {
+        if {[catch {open ~/.emcrc} programin]} {
+            return 
+        } else {
+            set rcstring [read $programin]
+            catch {close $programin}
+            set ret [getVal $rcstring PICKCONFIG LAST_CONFIG ]
+            return $ret
+        }
+    }
+}
+
+
+# FIXME add trap for comment on the first of a line.
+proc getVal {stringa sect var} {
+    set x [regexp -indices -- "$sect.*$var *= *" $stringa  indexes]
+    if {$x } {
+        set startindex [lindex $indexes 1]
+        set x [regexp -start $startindex -linestop -- ".*" \
+            $stringa varval ]
+        set varval [string trim $varval]
+    }
+    return $varval
+}
+
+proc setVal {stringb sect var newval} {
+    set x [regexp -indices -- "$sect.*$var *= *" $stringb  start ]
+    if {$x } {
+        set startindex [expr [lindex $start 1] +1]
+        set x [regexp -indices -start $startindex -linestop -- ".*" $stringb end ]
+        set endindex [lindex $end 1]
+        set newstring [string replace $stringb $startindex $endindex $newval]
+        return $newstring
     }
 }
 
@@ -106,7 +141,7 @@ proc node_clicked {} {
 
     set node [$tree selection get]
     if {$node == ""} return
-    
+
     $tree selection set $node
     if { [ regexp {.*\.ini$} $node ] == 1 } {
 	# an ini node, acceptable
@@ -177,7 +212,8 @@ pack $top -side left -expand yes -fill both
 wm geo . 780x480
 wm resiz . 0 0
 
-initialize_config
+set last_ini "none"
+set last_ini [initialize_config]
 
 proc SW { args } {
     set res [eval ScrolledWindow $args]
@@ -197,10 +233,10 @@ pack $lbl -anchor w
 set f2 [ frame $f1.f2 -borderwidth 0 -relief flat -padx 15 ]
 
 # Let the tree scroll
-set s1 [ SW $f2.f3 -scrollbar vertical]
+set s1 [ SW $f2.f3 -auto both]
 $s1 configure -relief sunken -borderwidth 2
 # the tree
-set tree [Tree $s1.tree -highlightthickness 0 -width 25 -relief flat -padx 4]
+set tree [Tree $s1.tree -highlightthickness 0 -width 25 -relief flat -padx 4 ]
 $s1 setwidget $tree
 pack $s1 -fill y -expand n -side left
 
@@ -263,7 +299,8 @@ foreach dir $configs_dir_list {
 	    $tree insert end $dir $inifile -text [lindex $parts end-1] -open 1
 	    incr config_count
 	} elseif { [ llength $inifile_list ] > 1 } {
-	    # multiples, use second level
+	    # multiples, use second level and sort
+  	    set inifile_list [ lsort $inifile_list ]
 	    # add dir to tree if not already
 	    if { $dir_in_tree == 0 } {
 		$tree insert end root $dir -text $dir/ -open 1
@@ -292,13 +329,63 @@ bind . <Escape> {button_pushed Cancel}
 bind . <Return> ""
 wm protocol . WM_DELETE_WINDOW {button_pushed Cancel}
 
+proc wait_and_see {node {wait 10}} {
+    global tree
+    set yv [$tree yview]
+    if {![winfo viewable $tree] || [lindex $yv 0] == [lindex $yv 1]} {
+        after $wait wait_and_see $node [expr $wait*2]
+    } else {
+        $tree see $node
+    }
+}
+
+# add the selection set if a last_ini has been found in ~/.emcrc
+
+if {$last_ini != -1 && [file exists $last_ini] } {
+    $tree selection set $last_ini
+    wait_and_see $last_ini
+    $tree xview moveto 0.0
+    node_clicked
+}
+
 focus $tree
 vwait choice
 
 if { $choice == "OK" } {
     puts $inifile
+
+    # test for ~/.emcrc file and modify if needed.
+    # or make this file and add the var.
+
+    if {[file exists ~/.emcrc]} {
+        if {$inifile == $last_ini} {
+            exit
+        } else {
+            if {[catch {open ~/.emcrc} programin]} {
+                return 
+            } else {
+                set rcstring [read $programin]
+                catch {close $programin}
+            }
+            set ret [setVal $rcstring PICKCONFIG LAST_CONFIG $inifile ]
+            catch {file copy -force $name $name.bak}
+            if {[catch {open ~/.emcrc w} fileout]} {
+                puts stdout [msgcat::mc "can't save %s" ~/.emcrc ]
+                exit
+            }
+            puts $fileout $ret
+            catch {close $fileout}
+            exit
+        }
+    }
+    set newfilestring "# .emcrc is a startup configuration file for EMC2. \n# format is INI like. \n# \[SECTION_NAME\] \n# VARNAME = varvalue \n# where section name is the name of the system writing to this file \n\n# written by pickconfig.tcl \n\[PICKCONFIG\]\nLAST_CONFIG = $inifile\n"
+            
+    if {[catch {open ~/.emcrc w+} fileout]} {
+        puts stdout [msgcat::mc "can't save %s" ~/.emcrc ]
+        exit
+    }
+    puts $fileout $newfilestring
+    catch {close $fileout}
 }
 
 exit
-
-
