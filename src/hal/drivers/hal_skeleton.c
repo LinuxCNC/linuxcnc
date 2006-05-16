428ae72a8c5a863f22b4b69a0530970f2c0ece34
Jeff Epler <jepler@unpythonic.net>
2006-05-16 21:32:47 +0000

    remove the userspace portion of hal_skeleton.  perhaps it should be resurrected as user_comps/hal_skeleton.

--- src/hal/drivers/hal_skeleton.c
+++ src/hal/drivers/hal_skeleton.c
@@ -26,8 +26,7 @@
  to a hardware adress, here we use the parallel port (0x378).
 
  This driver support no configuration strings so installing is easy:
- user: hal_skeleton
- realtime: insmod hal_skeleton
+ realtime: halcmd loadrt hal_skeleton
 
  The driver creates a HAL pin and if it run in realtime a function
  as follows:
@@ -35,22 +34,6 @@
  Pin: 'skeleton.<portnum>.pin-<pinnum>-out'
  Function: 'skeleton.<portnum>.write'
 
-    The user space version of the driver cannot export functions,
-    instead it exports a parameters with the same name.  The main()
-    function sits in a loop checking the parameters.  If it is
-    zero, it does nothing.  If the parameter is greater than zero,
-    the function runs once, then the parameter is reset to zero.
-    If the parameter is less than zero, the function runs on every
-    pass through the loop.
-    The driver will loop forever, until it receives either
-    SIGINT (ctrl-C) or SIGTERM, at which point it cleans up and
-    exits.
-
- You can see that you need at minimum four steps for installing the driver.
- The steps 1-3 are in realtime and userspace exactly the same, so it is highly
- recommended that you put this steps into a function.
- Here this is not done because so the code is easier to read.
- 
  This skeleton driver also doesn't use arguments you can pass to the driver
  at startup. Please look at the parport driver how to implement this if you need
  this for your driver.
@@ -88,21 +71,13 @@
     information, go to www.linuxcnc.org.
 */
 
-#if ( !defined RTAPI ) && ( !defined ULAPI )
-#error parport needs RTAPI/ULAPI, check makefile and flags
+#if ( !defined RTAPI )
+#error parport needs RTAPI, check makefile and flags
 #endif
 
-#ifdef RTAPI			/* realtime */
 #include <linux/ctype.h>	/* isspace() */
 #include "rtapi.h"		/* RTAPI realtime OS API */
 #include "rtapi_app.h"		/* RTAPI realtime module decls */
-#else /* user space */
-#include <ctype.h>		/* isspace() */
-#include <signal.h>		/* signal() */
-#include <sched.h>		/* sched_yield() */
-#include <sys/io.h>		/* iopl() */
-#include "rtapi.h"		/* RTAPI realtime OS API */
-#endif
 
 #include "hal.h"		/* HAL public API decls */
 
@@ -120,7 +95,6 @@
 #endif
 #endif
 
-#ifdef RTAPI			/* realtime */
 #ifdef MODULE
 /* module information */
 MODULE_AUTHOR("Martin Kuhnle");
@@ -133,7 +107,6 @@ MODULE_LICENSE("GPL");
 MODULE_PARM(cfg, "s");
 MODULE_PARM_DESC(cfg, "config string"); */
 #endif /* MODULE */
-#endif /* RTAPI */
 
 /***********************************************************************
 *                STRUCTURES AND GLOBAL VARIABLES                       *
@@ -167,8 +140,6 @@ static void write_port(void *arg, long period);
 ************************************************************************/
 
 #define MAX_PORTS 8
-
-#ifdef RTAPI			/* realtime part of skeleton driver */
 
 #define MAX_TOK ((MAX_PORTS*2)+3)
 
@@ -231,112 +202,6 @@ void rtapi_app_exit(void)
 {
     hal_exit(comp_id);
 }
-
-#else
-/******************** user space part of skeleton driver ********************/
-
-static int done = 0;
-static void quit(int sig)
-{
-    done = 1;
-}
-
-int main()
-{
-    char name[HAL_NAME_LEN + 2];
-    int n, retval;
-    hal_s8_t *write_funct_flags;
-    num_ports = 1;
-    n = 0;
-
-    /* ask linux for permission to use the I/O ports */
-    retval = iopl(3);
-    if (retval != 0) {
-	rtapi_print_msg(RTAPI_MSG_ERR,
-	    "SKELETON: ERROR: could not get I/O permission\n");
-	return -1;
-    }
-
-    /* STEP 1: initialise the driver */
-    comp_id = hal_init("SKELETON");
-    if (comp_id < 0) {
-	rtapi_print_msg(RTAPI_MSG_ERR,
-	    "SKELETON: ERROR: hal_init() failed\n");
-	return -1;
-    }
-
-    /* STEP 2: allocate shared memory for skeleton data */
-    port_data_array = hal_malloc(num_ports * sizeof(skeleton_t));
-    if (port_data_array == 0) {
-	rtapi_print_msg(RTAPI_MSG_ERR,
-	    "SKELETON: ERROR: hal_malloc() failed\n");
-	hal_exit(comp_id);
-	return -1;
-    }
-
-    /* STEP 3: export the pin(s) */
-    rtapi_snprintf(name, HAL_NAME_LEN, "skeleton.%d.pin-%02d-out", n, 1);
-    retval =
-	hal_pin_u8_new(name, HAL_RD, &(port_data_array->data_out), comp_id);
-    if (retval != HAL_SUCCESS) {
-	rtapi_print_msg(RTAPI_MSG_ERR,
-	    "SKELETON: ERROR: port %d var export failed with err=%i\n", n,
-	    retval);
-	hal_exit(comp_id);
-	return -1;
-    }
-
-    /* STEP 4a: allocate space for function run/stop parameter(s) */
-    write_funct_flags = hal_malloc((num_ports + 1) * sizeof(hal_s8_t));
-    if (write_funct_flags == 0) {
-	rtapi_print_msg(RTAPI_MSG_ERR,
-	    "SKELETON: ERROR: hal_malloc() failed\n");
-	hal_exit(comp_id);
-	return -1;
-    }
-
-    /* STEP 4b: export read function parameter */
-    rtapi_snprintf(name, HAL_NAME_LEN, "skeleton.%d.write", n);
-    retval =
-	hal_param_s8_new(name, HAL_WR, &write_funct_flags[n + 1], comp_id);
-    if (retval != HAL_SUCCESS) {
-	rtapi_print_msg(RTAPI_MSG_ERR,
-	    "SKELETON: ERROR: port %d write funct param failed\n", n);
-	hal_exit(comp_id);
-	return -1;
-    }
-
-    rtapi_print_msg(RTAPI_MSG_INFO,
-	"SKELETON: installed driver for %d ports\n", num_ports);
-
-    /* capture INT (ctrl-C) and TERM signals */
-    signal(SIGINT, quit);
-    signal(SIGTERM, quit);
-
-    /*********************************************/
-    /* STEP 6: main loop - loops forever until */
-    /* SIGINT (ctrl-C) or SIGTERM (kill) */
-    /*********************************************/
-    while (!done) {
-
-	n = 0;
-	if (write_funct_flags[n + 1]) {
-	    /* run the function */
-	    write_port(&(port_data_array[n]), 0);
-	    /* if flag is positive, reset it */
-	    if (write_funct_flags[n + 1] > 0) {
-		write_funct_flags[n + 1] = 0;
-	    }
-	}
-
-	/* give up the CPU so other processes can run */
-	sched_yield();
-    }
-    hal_exit(comp_id);
-    return 0;
-}
-
-#endif
 
 /**************************************************************
 * REALTIME PORT WRITE FUNCTION                                *
