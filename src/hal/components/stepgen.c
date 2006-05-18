5687674042220c266a840b86d0d759bd52a73d33
Stephen Wille Padnos <spadnos@sover.net>
2006-05-18 19:34:25 +0000

    Add extra error checking to step_types:  only allow types between 0 and MAX_STEP_TYPE

--- src/hal/components/stepgen.c
+++ src/hal/components/stepgen.c
@@ -437,7 +437,7 @@ int rtapi_app_main(void)
     int n, retval;
 
     for (n = 0; n < MAX_CHAN && step_type[n] != -1 ; n++) {
-	if (step_type[n] > MAX_STEP_TYPE) {
+	if ((step_type[n] > MAX_STEP_TYPE) || (step_type[n] < 0)) {
 	    rtapi_print_msg(RTAPI_MSG_ERR,
 			    "STEPGEN: ERROR: bad stepping type '%i', axes %i\n",
 			    step_type[n], n);
