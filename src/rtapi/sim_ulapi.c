807a9e4ba95682a2b36f89b98de15b0d5218ecd0
Jeff Epler <jepler@unpythonic.net>
2006-09-30 03:37:19 +0000

    make the shared memory portion of the userland-only rtapi simulator work

--- src/rtapi/sim_ulapi.c
+++ src/rtapi/sim_ulapi.c
@@ -16,22 +16,26 @@
 ********************************************************************/
 
 #include <stddef.h>		/* NULL */
+#include <stdio.h>		/* printf */
 #include <sys/ipc.h>		/* IPC_* */
 #include <sys/shm.h>		/* shmget() */
 #include <malloc.h>		/* malloc(), free() */
+#include <sys/time.h>
+#include <time.h>
 #include "rtapi.h"
 
 /* These structs hold data associated with objects like tasks, etc. */
 /* Task handles are pointers to these structs.                      */
 
-struct rtapi_shmem {
+typedef struct {
   int magic;			/* to check for valid handle */
   int key;			/* key to shared memory area */
   int id;			/* OS identifier for shmem */
   unsigned long int size;	/* size of shared memory area */
   void *mem;			/* pointer to the memory */
-};
+} rtapi_shmem_handle;
 
+static int msg_level = RTAPI_MSG_INFO;	/* message printing level */
 
 #define SHMEM_MAGIC   25453	/* random numbers used as signatures */
 
@@ -39,7 +43,7 @@ struct rtapi_shmem {
 int rtapi_init(char *modname)
 {
   /* does nothing, for now */
-  return RTAPI_SUCCESS;
+  return getpid();
 }
 
 
@@ -52,14 +56,8 @@ int rtapi_exit(int module_id)
 
 int rtapi_shmem_new(int key, int module_id, unsigned long int size)
 {
-  rtapi_shmem_handle shmem;
-
-  /* validate shmemptr */
-  if (shmemptr == NULL)
-    return RTAPI_INVAL;
-
   /* alloc space for shmem structure */
-  shmem = malloc(sizeof(struct rtapi_shmem));
+  rtapi_shmem_handle *shmem = malloc(sizeof(rtapi_shmem_handle));
   if (shmem == NULL)
     return RTAPI_NOMEM;
 
@@ -83,18 +81,19 @@ int rtapi_shmem_new(int key, int module_id, unsigned long int size)
   shmem->key = key;
 
   /* return handle to the caller */
-  *shmemptr = shmem;
-  return RTAPI_SUCCESS;
+  return (int)shmem;
 }
 
 
-int rtapi_shmem_getptr(rtapi_shmem_handle shmem, void **ptr)
+int rtapi_shmem_getptr(int handle, void **ptr)
 {
+  rtapi_shmem_handle *shmem = (rtapi_shmem_handle*) handle;
+
   /* validate shmem handle */
   if (shmem == NULL)
-    return RTAPI_BADH;
+    return RTAPI_BADID;
   if (shmem->magic != SHMEM_MAGIC)
-    return RTAPI_BADH;
+    return RTAPI_BADID;
 
   /* pass memory address back to caller */
   *ptr = shmem->mem;
@@ -102,22 +101,28 @@ int rtapi_shmem_getptr(rtapi_shmem_handle shmem, void **ptr)
 }
 
 
-int rtapi_shmem_delete(rtapi_shmem_handle shmem)
+int rtapi_shmem_delete(int handle, int module_id)
 {
   struct shmid_ds d;
   int r1, r2;
 
+  rtapi_shmem_handle *shmem = (rtapi_shmem_handle*) handle;
+
   /* validate shmem handle */
   if (shmem == NULL)
-    return RTAPI_BADH;
+    return RTAPI_BADID;
   if (shmem->magic != SHMEM_MAGIC)
-    return RTAPI_BADH;
+    return RTAPI_BADID;
 
   /* unmap the shared memory */
   r1 = shmdt(shmem->mem);
 
   /* destroy the shared memory */
-  r2 = shmctl(shmem->id, IPC_RMID, &d);
+  r2 = shmctl(shmem->id, IPC_STAT, &d);
+  fprintf(stderr, "r2=%d d.shm_nattch=%d\n", r2, (int)d.shm_nattch);
+  if(r2 == 0 && d.shm_nattch == 0) {
+      r2 = shmctl(shmem->id, IPC_RMID, &d);
+  }
   /* FIXME - Fred had the first two arguments reversed.  I changed
      them to match the shmctl man page on my machine.  Since his way
      worked, maybe there is difference between different libs, or
@@ -135,24 +140,96 @@ int rtapi_shmem_delete(rtapi_shmem_handle shmem)
 
 /* FIXME - no support for fifos */
 
-int rtapi_fifo_new(int key, unsigned long int size, char mode,
-		   rtapi_fifo_handle * fifoptr)
+int rtapi_fifo_new(int key, int module_id, unsigned long int size, char mode)
+
 {
   return RTAPI_UNSUP;
 }
 
-int rtapi_fifo_delete(rtapi_fifo_handle fifo)
+int rtapi_fifo_delete(int fifo_id, int module_id)
 {
   return RTAPI_UNSUP;
 }
 
-int rtapi_fifo_read(rtapi_fifo_handle fifo, char *buf, unsigned long int size)
+int rtapi_fifo_read(int fifo_id, char *buf, unsigned long size)
 {
   return RTAPI_UNSUP;
 }
 
-int rtapi_fifo_write(rtapi_fifo_handle fifo,
-		     char *buf, unsigned long int size)
+int rtapi_fifo_write(int fifo_id, char *buf, unsigned long int size)
 {
   return RTAPI_UNSUP;
 }
+
+#define BUFFERLEN 1024
+
+void rtapi_print(const char *fmt, ...)
+{
+    char buffer[BUFFERLEN + 1];
+    va_list args;
+
+    va_start(args, fmt);
+    /* call the normal library vnsprintf() */
+    vsnprintf(buffer, BUFFERLEN, fmt, args);
+    fputs(buffer, stdout);
+    va_end(args);
+}
+
+
+void rtapi_print_msg(int level, const char *fmt, ...)
+{
+    char buffer[BUFFERLEN + 1];
+    va_list args;
+
+    if ((level <= msg_level) && (msg_level != RTAPI_MSG_NONE)) {
+	va_start(args, fmt);
+	/* call the normal library vnsprintf() */
+	vsnprintf(buffer, BUFFERLEN, fmt, args);
+	fputs(buffer, stdout);
+	va_end(args);
+    }
+}
+
+int rtapi_snprintf(char *buffer, unsigned long int size, const char *msg, ...) {
+    va_list args;
+    int result;
+
+    va_start(args, msg);
+    /* call the normal library vnsprintf() */
+    result = vsnprintf(buffer, size, msg, args);
+    va_end(args);
+    return result;
+}
+
+int rtapi_vsnprintf(char *buffer, unsigned long int size, const char *fmt,
+	va_list args) {
+    return vsnprintf(buffer, size, fmt, args);
+}
+
+int rtapi_set_msg_level(int level) {
+    msg_level = level;
+    return RTAPI_SUCCESS;
+}
+
+int rtapi_get_msg_level() { 
+    return msg_level;
+}
+
+long long rtapi_get_time(void) {
+    struct timeval tv;
+    gettimeofday(&tv, 0);
+    return tv.tv_sec * 1000 * 1000 * 1000 + tv.tv_usec * 1000;
+}
+
+#define rdtscll(val) \
+         __asm__ __volatile__("rdtsc" : "=A" (val))
+
+long long rtapi_get_clocks(void)
+{
+    long long int retval;
+
+    rdtscll(retval);
+    return retval;    
+}
+
+
