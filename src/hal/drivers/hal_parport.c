b1096f3f7ed3c2d0912b5cf13657b5dad16081d1
Jeff Epler <jepler@unpythonic.net>
2006-11-01 15:27:25 +0000

    fix references to "version 2.1 of the GNU GPL"; should have read "version 2"

--- src/hal/drivers/hal_parport.c
+++ src/hal/drivers/hal_parport.c
@@ -70,7 +70,7 @@
 */
 
 /** This program is free software; you can redistribute it and/or
-    modify it under the terms of version 2.1 of the GNU General
+    modify it under the terms of version 2 of the GNU General
     Public License as published by the Free Software Foundation.
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
