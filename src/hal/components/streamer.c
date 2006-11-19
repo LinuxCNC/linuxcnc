e755c5b2cdfea0dc7eff40e53a27153a3fa2f929
John Kasunich <jmkasunich@fastmail.fm>
2006-11-19 22:22:07 +0000

    fix a pin name

--- src/hal/components/streamer.c
+++ src/hal/components/streamer.c
@@ -321,7 +321,7 @@ static int init_streamer(int num, fifo_t *tmp_fifo)
 	return -EIO;
     }
     retval = hal_pin_s32_newf(HAL_OUT, &(str->curr_depth), comp_id,
-	"streamer.%d.curr_depth", num);
+	"streamer.%d.curr-depth", num);
     if (retval != 0 ) {
 	rtapi_print_msg(RTAPI_MSG_ERR,
 	    "STREAMER: ERROR: 'curr_depth' pin export failed\n");
