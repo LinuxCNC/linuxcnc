180d93de2700474cac100e302a05836221266bfd
Jeff Epler <jepler@unpythonic.net>
2006-12-21 19:45:56 +0000

    get rid of debug options that are useless to set after startup make the labels use the same font as axis make the debug options appear in the order shown in the source code

--- src/emc/usr_intf/axis/scripts/debuglevel.py
+++ src/emc/usr_intf/axis/scripts/debuglevel.py
@@ -23,20 +23,33 @@ t = Tkinter.Tk(className="EmcDebugLevel")
 t.wm_title("Debug Level")
 t.wm_resizable(0, 0)
 
-bits = {
-    emc.DEBUG_INVALID: _('Configuration: Invalid items'),
-    emc.DEBUG_CONFIG: _('Configuration: Default values'),
-    emc.DEBUG_DEFAULTS: _('Configuration: Other'),
-    emc.DEBUG_VERSIONS: _('Version Numbers'),
-    emc.DEBUG_TASK_ISSUE: _('Task Issue'),
-    emc.DEBUG_IO_POINTS: _('IO Points'),
-    emc.DEBUG_NML: _('NML'),
-    emc.DEBUG_MOTION_TIME: _('Motion Time'),
-    emc.DEBUG_INTERP: _('Interpreter'),
-    emc.DEBUG_RCS: _('RCS'),
-    emc.DEBUG_TRAJ: _('Trajectory'),
-    emc.DEBUG_INTERP_LIST: _('Interpreter List'),
-}
+import rs274.options
+rs274.options.install(t)
+
+# The configuration-related items are commented out because
+# it only makes sense to enable them during startup.
+#
+# From inspection of the source, and because no messages were seen
+# for those flags, it looks like the state of DEBUG_NML and DEBUG_RCS
+# is only checked early in startup, so changing them later has no effect.
+#
+# There's apparently only one message for EMC_DEBUG_IO_POINTS and hardly
+# any messages for EMC_DEBUG_TRAJ (all from tkemc/mini), but I'll leave them
+# enabled anyway
+bits = [
+    #(emc.DEBUG_INVALID, _('Configuration: Invalid items')),
+    #(emc.DEBUG_CONFIG, _('Configuration: Default values')),
+    #(emc.DEBUG_DEFAULTS, _('Configuration: Other')),
+    #(emc.DEBUG_VERSIONS, _('Version Numbers')),
+    (emc.DEBUG_TASK_ISSUE, _('Task Issue')),
+    (emc.DEBUG_IO_POINTS, _('IO Points')),
+    #(emc.DEBUG_NML, _('NML')),
+    (emc.DEBUG_MOTION_TIME, _('Motion Time')),
+    (emc.DEBUG_INTERP, _('Interpreter')),
+    #(emc.DEBUG_RCS, _('RCS')),
+    (emc.DEBUG_TRAJ, _('Trajectory')),
+    (emc.DEBUG_INTERP_LIST, _('Interpreter List')),
+]
 
 vars = {}
 blackout = 0
@@ -60,7 +73,7 @@ def setdebug():
         value = value | v.get()
     c.debug(value)
 
-for k, v in bits.items():
+for k, v in bits:
     vv = Tkinter.IntVar(t)
     vars[k] = vv
     b = Tkinter.Checkbutton(text = v, onvalue = k, offvalue = 0,
