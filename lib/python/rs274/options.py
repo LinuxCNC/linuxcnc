d3e208032c5f069617e2a9beb8444be86b1b18b9
Jeff Epler <jepler@unpythonic.net>
2007-01-14 19:43:01 +0000

    merge rev 1.4: make the background of . match

--- lib/python/rs274/options.py
+++ lib/python/rs274/options.py
@@ -17,6 +17,8 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 options = '''
+. configure -bg #d9d9d9
+
 set BASE_FONT {Helvetica -12}
 
 proc option_nont {args} {
