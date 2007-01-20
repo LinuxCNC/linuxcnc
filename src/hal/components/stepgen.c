abb755fae26bec02517fba2f76820b1682f391dd
Jeff Epler <jepler@unpythonic.net>
2007-01-20 01:48:51 +0000

    get rid of rtapi_print_msg from the HAL realtime function

--- src/hal/components/stepgen.c
+++ src/hal/components/stepgen.c
@@ -345,7 +345,6 @@ typedef struct {
     hal_float_t freq;		/* param: frequency command */
     hal_float_t maxvel;		/* param: max velocity, (pos units/sec) */
     hal_float_t maxaccel;	/* param: max accel (pos units/sec^2) */
-    int printed_error;          /* Has the error message been logged? */
 } stepgen_t;
 
 /* ptr to array of stepgen_t structs in shared memory, 1 per channel */
@@ -760,15 +759,6 @@ static void update_freq(void *arg, long period)
 	} else {
 	    /* parameter is non-zero, compare to max_freq */
 	    if ((stepgen->maxvel * fabs(stepgen->pos_scale)) > max_freq) {
-                if(!stepgen->printed_error) {
-                    rtapi_print_msg(RTAPI_MSG_ERR,
-"STEPGEN: Channel %d: The requested step rate of %dHz is not attainable.  "
-"The maximum step rate is %dHz.  You must reduce MAX_VELOCITY, INPUT_SCALE, "
-"or BASE_PERIOD.  Otherwise, following errors will result.\n",
-                        n, (int) (stepgen->maxvel * fabs(stepgen->pos_scale)),
-                        (int)max_freq);
-                    stepgen->printed_error = 1;
-                }
 		/* parameter is too high, lower it */
 		stepgen->maxvel = max_freq * fabs(stepgen->scale_recip);
 	    } else {
@@ -982,8 +972,6 @@ static int export_stepgen(int num, stepgen_t * addr, int step_type)
     addr->newaddval = 0;
     addr->deltalim = 0;
     addr->rawcount = 0;
-    /* set other default values */
-    addr->printed_error = 0;
 
     /* init the position controller */
 
