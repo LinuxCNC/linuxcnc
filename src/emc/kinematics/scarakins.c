e982b6a2c390dabf7a3ab1303b28cd6845be1ce7
John Kasunich <jmkasunich@fastmail.fm>
2007-02-05 01:10:10 +0000

    documented kins parameters, rewrote and commented code for clarity, and to properly handle tool rotation and tooltip not centered on end-effector

--- src/emc/kinematics/scarakins.c
+++ src/emc/kinematics/scarakins.c
@@ -8,7 +8,7 @@
 * Author: Sagar Behere 
 * License: GPL Version 2
 * System: Linux
-*    
+*
 * Copyright (c) 2003 All rights reserved.
 *
 * Last change:
@@ -27,37 +27,76 @@
 #include "rtapi_app.h"		/* RTAPI realtime module decls */
 #include "hal.h"
 
-struct {
-    hal_float_t a1, a2, d1, d4;
+struct scara_data {
+    hal_float_t d1, d2, d3, d4, d5, d6;
 } *haldata = 0;
 
+/* key dimensions
 
-#define SCARA_A1 (haldata->a1)
-#define SCARA_A2 (haldata->a2)
-#define SCARA_D1 (haldata->d1)
-#define SCARA_D4 (haldata->d4)
-
-/*
-assuming that joint[0], joint[1] and joint[3] come in degrees, and joint[2] comes in mm
+   joint[0] = Entire arm rotates around a vertical axis at its inner end
+		which is attached to the earth.  A value of zero means the
+		inner arm is pointing along the X axis.
+   D1 = Vertical distance from the ground plane to the center of the inner
+		arm.
+   D2 = Horizontal distance between joint[0] axis and joint[1] axis, ie.
+		the length of the inner arm.
+   joint[1] = Outer arm rotates around a vertical axis at its inner end
+		which is attached to the outer end of the inner arm.  A
+		value of zero means the outer arm is parallel to the
+		inner arm (and extending outward).
+   D3 = Vertical distance from the center of the inner arm to the center
+		of the outer arm.  May be positive or negative depending
+		on the structure of the robot.
+   joint[2] = End effector slides along a vertical axis at the outer end
+		of the outer arm.  A value of zero means the end effector
+		is at the same height as the center of the outer arm, and
+		positive values mean downward movement.
+   D4 = Horizontal distance between joint[1] axis and joint[2] axis, ie.
+		the length of the outer arm
+   joint[3] = End effector rotates around the same vertical axis that it
+		slides along.  A value of zero means that the tooltip (if
+		offset from the axis) is pointing in the same direction
+		as the centerline of the outer arm.
+   D5 = Vertical distance from the end effector to the tooltip.  Positive
+		means the tooltip is lower than the end effector, and is
+		the normal case.
+   D6 = Horizontal distance from the centerline of the end effector (and
+		the joints 2 and 3 axis) and the tooltip.  Zero means the
+		tooltip is on the centerline.  Non-zero values should be
+		positive, if negative they introduce a 180 degree offset
+		on the value of joint[3].
 */
+
+#define D1 (haldata->d1)
+#define D2 (haldata->d2)
+#define D3 (haldata->d3)
+#define D4 (haldata->d4)
+#define D5 (haldata->d5)
+#define D6 (haldata->d6)
+
+/* joint[0], joint[1] and joint[3] are in degrees and joint[2] is in length units */
 int kinematicsForward(const double * joint,
                       EmcPose * world,
                       const KINEMATICS_FORWARD_FLAGS * fflags,
                       KINEMATICS_INVERSE_FLAGS * iflags)
 {
-    double q0, q1;
+    double a0, a1, a3;
     double x, y, z, c;
 
-/* 
-	convert joint[0] and joint[1] to radian, since sin() and cos() requires them so.
-*/
-    q0 = joint[0] / 180 * PM_PI;
-    q1 = joint[1] / 180 * PM_PI;
-	
-    x = SCARA_A1*cos(q0) + SCARA_A2*cos((q0+q1));
-    y = SCARA_A1*sin(q0) + SCARA_A2*sin((q0+q1));
-    z = SCARA_D1 - joint[2];
-    c = joint[3];
+/* convert joint angles to radians for sin() and cos() */
+
+    a0 = joint[0] * ( PM_PI / 180 );
+    a1 = joint[1] * ( PM_PI / 180 );
+    a3 = joint[3] * ( PM_PI / 180 );
+/* convert angles into world coords */
+
+    a1 = a1 + a0;
+    a3 = a3 + a1;
+
+    x = D2*cos(a0) + D4*cos(a1) + D6*cos(a3);
+    y = D2*sin(a0) + D4*sin(a1) + D6*sin(a3);
+    z = D1 + D3 - joint[2] - D5;
+    c = a3;
 	
     *iflags = 0;
     if (joint[1] < 90)
@@ -79,30 +118,53 @@ int kinematicsInverse(const EmcPose * world,
                       const KINEMATICS_INVERSE_FLAGS * iflags,
                       KINEMATICS_FORWARD_FLAGS * fflags)
 {
+    double a3;
     double q0, q1;
-    double x,y;
+    double xt, yt, rsq;
+    double x, y, z, c;
 
     x = world->tran.x;
     y = world->tran.y;
+    z = world->tran.z;
+    c = world->c;
+
+    /* convert degrees to radians */
+    a3 = c * ( PM_PI / 180 );
+
+    /* center of end effector (correct for D6) */
+    xt = x - D6*cos(a3);
+    yt = y - D6*cos(a3);
+
+    /* horizontal distance (squared) from end effector centerline
+	to main column centerline */
+    rsq = xt*xt + yt*yt;
+    /* joint 1 angle needed to make arm length match sqrt(rsq) */
+    q1 = acos((rsq - D2*D2 - D4*D4) / (2*D2*D4));
 
     if (*iflags)
-	q1 = (-1)*acos( (x*x + y*y - SCARA_A1*SCARA_A1 - SCARA_A2*SCARA_A2) / (2*SCARA_A1*SCARA_A2) );
-    else
-	q1 = acos( (x*x + y*y - SCARA_A1*SCARA_A1 - SCARA_A2*SCARA_A2) / (2*SCARA_A1*SCARA_A2) );
-	
-    q0 = atan2(y, x) - atan2( (sin(q1) * SCARA_A2), (cos(q1) * SCARA_A2 + SCARA_A1) );
-	
-    /* q1 and a2 are still in radians. convert them to degree now */
-    q0 = q0 / PM_PI * 180;
-    q1 = q1 / PM_PI * 180;
-	
+	q1 = -q1;
+
+    /* angle to end effector */
+    q0 = atan2(yt, xt);
+
+    /* end effector coords in inner arm coord system */
+    xt = D2 + D4*cos(q1);
+    yt = D4*sin(q1);
+
+    /* inner arm angle */
+    q0 = q0 - atan2(yt, xt);
+
+    /* q0 and q1 are still in radians. convert them to degrees */
+    q0 = q0 * (180 / PM_PI);
+    q1 = q1 * (180 / PM_PI);
+
     joint[0] = q0;
     joint[1] = q1;
-    joint[2] = SCARA_D1 - world->tran.z;
-    joint[3] = world->c;
-	
+    joint[2] = D1 + D3 - D5 - z;
+    joint[3] = c - ( q0 + q1);
+
     *fflags = 0;
-	
+
     return (0);
 }
 
@@ -120,10 +182,12 @@ KINEMATICS_TYPE kinematicsType()
   return KINEMATICS_BOTH;
 }
 
-#define DEFAULT_SCARA_A1 340
-#define DEFAULT_SCARA_A2 250
-#define DEFAULT_SCARA_D1 489
-#define DEFAULT_SCARA_D4 197
+#define DEFAULT_D1 490
+#define DEFAULT_D2 340
+#define DEFAULT_D3  50
+#define DEFAULT_D4 250
+#define DEFAULT_D5  50
+#define DEFAULT_D6  50
 
 EXPORT_SYMBOL(kinematicsType);
 EXPORT_SYMBOL(kinematicsForward);
@@ -140,15 +204,19 @@ int rtapi_app_main(void) {
     
     haldata = hal_malloc(sizeof(*haldata));
     if (!haldata) goto error;
-    SCARA_A1 = DEFAULT_SCARA_A1;
-    SCARA_A2 = DEFAULT_SCARA_A2;
-    SCARA_D1 = DEFAULT_SCARA_D1;
-    SCARA_D4 = DEFAULT_SCARA_D4;
+    D1 = DEFAULT_D1;
+    D2 = DEFAULT_D2;
+    D3 = DEFAULT_D3;
+    D4 = DEFAULT_D4;
+    D5 = DEFAULT_D5;
+    D6 = DEFAULT_D6;
 
-    if((res = hal_param_float_new("scarakins.A1", HAL_RW, &haldata->a1, comp_id)) != HAL_SUCCESS) goto error;
-    if((res = hal_param_float_new("scarakins.A2", HAL_RW, &haldata->a2, comp_id)) != HAL_SUCCESS) goto error;
     if((res = hal_param_float_new("scarakins.D1", HAL_RW, &haldata->d1, comp_id)) != HAL_SUCCESS) goto error;
+    if((res = hal_param_float_new("scarakins.D2", HAL_RW, &haldata->d2, comp_id)) != HAL_SUCCESS) goto error;
+    if((res = hal_param_float_new("scarakins.D3", HAL_RW, &haldata->d3, comp_id)) != HAL_SUCCESS) goto error;
     if((res = hal_param_float_new("scarakins.D4", HAL_RW, &haldata->d4, comp_id)) != HAL_SUCCESS) goto error;
+    if((res = hal_param_float_new("scarakins.D5", HAL_RW, &haldata->d5, comp_id)) != HAL_SUCCESS) goto error;
+    if((res = hal_param_float_new("scarakins.D6", HAL_RW, &haldata->d6, comp_id)) != HAL_SUCCESS) goto error;
     
     hal_ready(comp_id);
     return 0;
