a3451536890acc1bb47349cf791511e1aae4e6d7
Jeff Epler <jepler@unpythonic.net>
2007-02-10 04:11:34 +0000

    allow the scara table to be tilted two ways, giving a full 6 joints

--- src/emc/usr_intf/axis/scripts/scaragui.py
+++ src/emc/usr_intf/axis/scripts/scaragui.py
@@ -27,6 +27,7 @@ c.newpin("joint1", hal.HAL_FLOAT, hal.HAL_IN)
 c.newpin("joint2", hal.HAL_FLOAT, hal.HAL_IN)
 c.newpin("joint3", hal.HAL_FLOAT, hal.HAL_IN)
 c.newpin("joint4", hal.HAL_FLOAT, hal.HAL_IN)
+c.newpin("joint5", hal.HAL_FLOAT, hal.HAL_IN)
 c.ready()
 
 # parameters that define the geometry see scarakins.c for definitions these
@@ -172,8 +173,9 @@ table = Collection([
 	work,
 	Box(-0.35*reach,-0.5*reach, -0.1*d1, 0.35*reach, 0.5*reach, 0.0)])
 
-# for testing, make the table moveable (tilting)
+# make the table moveable (tilting)
 table = HalRotate([table],c,"joint4",1,0,1,0)
+table = HalRotate([table],c,"joint5",1,1,0,0)
 
 # put the table into its proper place
 table = Translate([table],0.5*reach,0.0,table_height)
