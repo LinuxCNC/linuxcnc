1291cac082b30bd543e968152be45e1766bd45a1
Jeff Epler <jepler@unpythonic.net>
2007-02-27 01:42:47 +0000

    make hal_input's items act more like the HAL canonical interfaces they resemble

--- src/hal/user_comps/hal_input.py
+++ src/hal/user_comps/hal_input.py
@@ -18,90 +18,144 @@ import linux_event, sys, os, fcntl, hal, select, time, glob, fnmatch
 
 def tohalname(s): return str(s).lower().replace("_", "-")
 
+class HalWrapper:
+    def __init__(self, comp):
+	self._comp = comp
+	self._drive = {}
+	self._pins = set()
+	self._params = set()
+
+    def drive(self):
+	for k, v in self._drive.items(): setattr(self._comp, k, v)
+
+    def newpin(self, *args):
+	self._pins.add(args[0])
+	return self._comp.newpin(*args)
+    def newparam(self, *args):
+	print args[0]
+	self._params.add(args[0])
+ 	return self._comp.newparam(*args)
+
+    def __getitem__(self, k):
+	if k in self._drive: return self._drive[k]
+	return self._comp[k]
+
+    def __setitem__(self, k, v):
+	if k in self._params:
+	    self._comp[k] = v; return
+	if k not in self._pins: raise KeyError, k
+	self._drive[k] = v
+
 class HalInputDevice:
     def __init__(self, comp, idx, name):
 	self.device = linux_event.InputDevice(name)
 
 	self.idx = idx
-	self.drive = {}
-	self.abs = {}
+	self.codes = set()
 	self.last = {}
 	self.rel_items = []
 	self.comp = comp
 
 	for key in self.device.get_bits('EV_KEY'):
 	    key = tohalname(key)
+	    self.codes.add(key)
 	    comp.newpin("%s.%s" % (idx, key), hal.HAL_BIT, hal.HAL_OUT)
-	    self.drive[key] = 0
+	    comp.newpin("%s.%s-not" % (idx, key), hal.HAL_BIT, hal.HAL_OUT)
+	    self.set(key + "-not", 1)
 
 	for axis in self.device.get_bits('EV_REL'):
 	    name = tohalname(axis)
-	    comp.newpin("%s.%s" % (idx, name), hal.HAL_FLOAT, hal.HAL_OUT)
+	    self.codes.add(name)
+	    comp.newpin("%s.%s-position" % (idx, name), hal.HAL_FLOAT, hal.HAL_OUT)
+	    comp.newpin("%s.%s-counts" % (idx, name), hal.HAL_S32, hal.HAL_OUT)
 	    comp.newpin("%s.%s-reset" % (idx, name), hal.HAL_BIT, hal.HAL_IN)
+	    comp.newparam("%s.%s-position-scale" % (idx, name), hal.HAL_FLOAT, hal.HAL_RW)
+	    self.set(name + '-position-scale', 1.)
 	    self.rel_items.append(name)
-	    self.drive[name] = 0
 
 	for axis in self.device.get_bits('EV_ABS'):
 	    name = tohalname(axis)
-	    self.abs[name] = self.device.get_absinfo(axis)
-	    comp.newpin("%s.%s" % (idx, name), hal.HAL_FLOAT, hal.HAL_OUT)
-	    self.drive[name] = 0
+	    self.codes.add(name)
+	    absinfo = self.device.get_absinfo(axis)
+	    comp.newpin("%s.%s-position" % (idx, name), hal.HAL_FLOAT, hal.HAL_OUT)
+	    comp.newpin("%s.%s-counts" % (idx, name), hal.HAL_S32, hal.HAL_OUT)
+	    comp.newparam("%s.%s-position-scale" % (idx, name), hal.HAL_FLOAT, hal.HAL_RW)
+	    comp.newparam("%s.%s-fuzz" % (idx, name), hal.HAL_FLOAT, hal.HAL_RW)
+	    comp.newparam("%s.%s-flat" % (idx, name), hal.HAL_FLOAT, hal.HAL_RW)
+	    self.set(name + "-position-scale", float(max(-absinfo.minimum, absinfo.maximum)))
+	    self.set(name + "-fuzz", absinfo.fuzz)
+	    self.set(name + "-flat", absinfo.flat)
 
+	self.ledmap = {}
 	for led in self.device.get_bits('EV_LED'):
 	    name = tohalname(led)
+	    self.ledmap[name] = led
 	    comp.newpin("%s.%s" % (idx, name), hal.HAL_BIT, hal.HAL_IN)
+	    comp.newparam("%s.%s-invert" % (idx, name), hal.HAL_BIT, hal.HAL_RW)
 	    self.last[name] = 0
 	    self.device.write_event('EV_LED', led, 0)
 	
+    def get(self, name):
+	name = "%s.%s" % (self.idx, name)
+	return self.comp[name]
+
+    def set(self, name, value):
+	name = "%s.%s" % (self.idx, name)
+	self.comp[name] = value
+
     def update(self):
 	while self.device.readable():
 	    ev = self.device.read_event()
+	    if ev.type == 'EV_SYN': continue
 	    code = tohalname(ev.code)
+	    print "event", ev.type, ev.code, code
+	    if code not in self.codes:
+		print >>sys.stderr, "Unexpected event", ev.type, ev.code
+		continue
 	    if ev.type == 'EV_KEY':
-		if code not in self.drive:
-		    print >>sys.stderr, "Unexpcted event EV_KEY", code, ev.code
-		    continue
 		if ev.value:
-		    self.drive[code] = 1
+		    self.set(code, 1)
+		    self.set(code + "-not", 0)
 		else:
-		    self.drive[code] = 0
+		    self.set(code, 0)
+		    self.set(code + "-not", 1)
 	    elif ev.type == 'EV_REL':
-		if code not in self.drive:
-		    print >>sys.stderr, "Unexpcted event EV_REL", code, ev.code
-		    continue
-		self.drive[code] += ev.value
+		self.set(code + "-counts", self.get(code + "-counts") + ev.value)
 	    elif ev.type == 'EV_ABS':
-		if code not in self.drive:
-		    print >>sys.stderr, "Unexpcted event EV_ABS", code, ev.code
-		    continue
-		absinfo = self.abs[code]
-		if abs(ev.value) < absinfo.flat: ev.value = 0
-		scale = max(-absinfo.minimum, absinfo.maximum)
-		self.drive[code] = ev.value * 1. / scale
+		flat = self.get(code + "-flat")
+		fuzz = self.get(code + "-fuzz")
+		if ev.value < -flat: value = ev.value + flat
+		elif ev.value > flat: value = ev.value - flat
+		else: value = 0
+		if abs(value - self.get(code + "-counts")) > fuzz:
+		    self.set(code + "-counts", ev.value)
+		    self.set(code + "-position", ev.value / self.get(code + "-position-scale"))
 
 	for r in self.rel_items:
-	    reset = self.comp["%s.%s-reset" % (self.idx, r)]
-	    if reset: self.drive[r] = 0
-
-	for k, v in self.drive.items():
-	    self.comp["%s.%s" % (self.idx, k)] = v
+	    reset = self.get(r + "-reset")
+	    if reset: self.set(r + "-counts", 0)
+	    self.set(r + "-position", self.get(r + "-counts") / self.get(r + "-position-scale"))
 
 	for k, v in self.last.items():
-	    u = self.comp["%s.%s" % (self.idx, k)]
+	    # Note: this is OK because the hal module always returns True or False for HAL_BIT values
+	    u = self.comp["%s.%s" % (self.idx, k)] != self.comp["%s.%s-invert" % (self.idx, k)]
 	    if u != self.last[k]:
-		self.device.write_event('EV_LED', k.upper(), u)
+		led = self.ledmap[k]
+		self.device.write_event('EV_LED', led, u)
 		self.last[k] = u
 
 h = hal.component("hal_input")
+w = HalWrapper(h)
 h.setprefix("input")
 d = []
 for i, f in enumerate(sys.argv[1:]):
-    d.append(HalInputDevice(h, i, f))
+    d.append(HalInputDevice(w, i, f))
 h.ready()
 
 try:
     while 1:
 	time.sleep(.01)
 	for i in d: i.update()
+	w.drive()
 except KeyboardInterrupt:
     pass
