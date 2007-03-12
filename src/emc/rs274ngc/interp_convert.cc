e1a94f91e669864d646befd3e6a6c9a90e04407c
Chris Radek <chris@timeguy.com>
2007-03-12 05:18:29 +0000

    fix zero-radius tool comp

--- src/emc/rs274ngc/interp_convert.cc
+++ src/emc/rs274ngc/interp_convert.cc
@@ -1181,7 +1181,7 @@ int Interp::convert_cutter_compensation_off(setup_pointer settings)      //!< po
 #ifdef DEBUG_EMC
   COMMENT("interpreter: cutter radius compensation off");
 #endif
-  if(settings->cutter_comp_side != OFF) {
+  if(settings->cutter_comp_side != OFF && settings->cutter_comp_radius > 0.0) {
       settings->current_x = xtrans(settings, settings->program_x);
       settings->current_y = settings->program_y;
       settings->current_z = ztrans(settings, settings->program_z);
