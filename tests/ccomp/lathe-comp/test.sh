565de0b7503383ecec4d2f846f59ccc62e92abef
Chris Radek <chris@timeguy.com>
2007-03-16 01:25:02 +0000

    finish converting scripts to new scheme of running sai

--- tests/ccomp/lathe-comp/test.sh
+++ tests/ccomp/lathe-comp/test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-rs274 test.ngc <<EOF
-2
-test.var
-3
-test.tbl
-5
-2
-1
-1
-EOF
+rs274 -v test.var -t test.tbl -g test.ngc
 result=$?
 [ -f test.var.bak ] && mv test.var.bak test.var
 exit $result
