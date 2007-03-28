526613270415354c693f30d2ff149206c77318c3
Jeff Epler <jepler@unpythonic.net>
2007-03-28 18:22:20 +0000

    remove unused files

--- src/hal/classicladder/protocol_modbus_slave.h
+++
@@ -1,3 +0,0 @@
-
-int ModbusRequestToRespond(unsigned char *Question, int LgtQuestion,
-    unsigned char *Response);
--- src/hal/classicladder/socket_server.h
+++
@@ -1,4 +0,0 @@
-
-void InitSocketServer(int UseUdpMode, int PortNbr);
-void SocketServerTcpMainLoop(void);
-void CloseSocketServer(void);
