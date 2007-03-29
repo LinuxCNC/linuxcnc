cebfd046c2994ef38f455458dfcfdee0042a522d
Jeff Epler <jepler@unpythonic.net>
2007-03-29 17:59:25 +0000

    revert merge of 1.67: the jog button bugfix caused more serious problems than the original problem

--- src/emc/usr_intf/axis/scripts/axis.py
+++ src/emc/usr_intf/axis/scripts/axis.py
@@ -72,13 +72,10 @@ lathe = 0
 feedrate_blackout = 0
 spindlerate_blackout = 0
 jogincr_index_last = 1
-
 from math import hypot, atan2, sin, cos, pi, sqrt
 from rs274 import ArcsToSegmentsMixin
 import emc
 import hal
-
-button_jog_axis = None
 
 homeicon = array.array('B', 
         [0x2, 0x00,   0x02, 0x00,   0x02, 0x00,   0x0f, 0x80,
@@ -2326,29 +2323,21 @@ class TclCommands(nf.TclCommands):
     # The next three don't have 'manual_ok' because that's done in jog_on /
     # jog_off
     def jog_plus(event=None):
-        global button_jog_axis
-        if button_jog_axis is not None: commands.jog_stop()
         a = vars.current_axis.get()
-        button_jog_axis = a
         if isinstance(a, (str, unicode)):
             a = "xyzabc".index(a)
         if a < 3: speed = vars.jog_speed.get()
         else: speed = vars.jog_aspeed.get()
         jog_on(a, speed/60.)
     def jog_minus(event=None):
-        global button_jog_axis
-        if button_jog_axis is not None: commands.jog_stop()
         a = vars.current_axis.get()
-        button_jog_axis = a
         if isinstance(a, (str, unicode)):
             a = "xyzabc".index(a)
         if a < 3: speed = vars.jog_speed.get()
         else: speed = vars.jog_aspeed.get()
         jog_on(a, -speed/60.)
     def jog_stop(event=None):
-        global button_jog_axis
-        jog_off(button_jog_axis)
-        button_jog_axis = None
+        jog_off(vars.current_axis.get())
 
     def home_all_axes(event=None):
         if not manual_ok(): return
