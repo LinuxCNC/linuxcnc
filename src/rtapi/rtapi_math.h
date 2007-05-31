9a030da90071bc16599317a38170a6806abd5d7d
Jeff Epler <jepler@unpythonic.net>
2007-05-31 22:27:17 +0000

    the defintion of huge_val should be portable to all systems with IEEE maths, so move it out of the CPU-specific header file

--- src/rtapi/rtapi_math.h
+++ src/rtapi/rtapi_math.h
@@ -16,6 +16,34 @@ extern double floor(double);
 
 #define M_PIl		3.1415926535897932384626433832795029L  /* pi */
 
+#ifndef __GNUC_PREREQ
+/* Convenience macro to test the versions of glibc and gcc. */
+/*  taken from include/features.h */
+#if defined __GNUC__ && defined __GNUC_MINOR__
+# define __GNUC_PREREQ(maj, min) \
+	((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#else
+# define __GNUC_PREREQ(maj, min) 0
+#endif
+#endif /* __GNUC_PREREQ */
+
+
+#ifndef HUGE_VAL
+/* IEEE positive infinity (-HUGE_VAL is negative infinity).  */
+/*  taken from include/bits/huge_val.h */
+#if __GNUC_PREREQ(3,3)
+# define HUGE_VAL	(__builtin_huge_val())
+#elif __GNUC_PREREQ(2,96)
+# define HUGE_VAL	(__extension__ 0x1.0p2047)
+#else
+# define HUGE_VAL \
+  (__extension__							      \
+   ((union { unsigned __l __attribute__((__mode__(__DI__))); double __d; })   \
+    { __l: 0x7ff0000000000000ULL }).__d)
+#endif
+#endif /* HUGE_VAL */
+
+
 #ifdef __i386__
 #include "rtapi_math_i386.h"
 #endif
--- src/rtapi/rtapi_math_i386.h
+++ src/rtapi/rtapi_math_i386.h
@@ -23,32 +23,4 @@ extern __inline double atan2 (double __y, double __x) {
 extern __inline double acos (double __x) {
     return atan2(sqrt(1.0 - __x * __x), __x);
 }
-
-#ifndef __GNUC_PREREQ
-/* Convenience macro to test the versions of glibc and gcc. */
-/*  taken from include/features.h */
-#if defined __GNUC__ && defined __GNUC_MINOR__
-# define __GNUC_PREREQ(maj, min) \
-	((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
-#else
-# define __GNUC_PREREQ(maj, min) 0
-#endif
-#endif /* __GNUC_PREREQ */
-
-
-#ifndef HUGE_VAL
-/* IEEE positive infinity (-HUGE_VAL is negative infinity).  */
-/*  taken from include/bits/huge_val.h */
-#if __GNUC_PREREQ(3,3)
-# define HUGE_VAL	(__builtin_huge_val())
-#elif __GNUC_PREREQ(2,96)
-# define HUGE_VAL	(__extension__ 0x1.0p2047)
-#else
-# define HUGE_VAL \
-  (__extension__							      \
-   ((union { unsigned __l __attribute__((__mode__(__DI__))); double __d; })   \
-    { __l: 0x7ff0000000000000ULL }).__d)
-#endif
-#endif /* HUGE_VAL */
-
 #endif /* RTAPI_MATH_I386_H */
