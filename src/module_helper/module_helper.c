4f3fd99d06f687ff2303e7ff7f8df26c5da4ee03
Jeff Epler <jepler@unpythonic.net>
2007-05-31 22:28:11 +0000

    this module is needed on some systems

--- src/module_helper/module_helper.c
+++ src/module_helper/module_helper.c
@@ -24,7 +24,7 @@ think of a better way.
  * "rtai_math") then put the shorter name last.
  */
 char *module_whitelist[] = {
-    "rtai_math", "rtai_sem", "rtai_shm", "rtai_fifos", "rtai_up", 
+    "rtai_math", "rtai_sem", "rtai_shm", "rtai_fifos", "rtai_up", "rtai_lxrt",
     "rtai_hal", "rtai_sched", "rtai_smi", "rtai", "rt_mem_mgr", "adeos",
     "rtl_time", "rtl_sched", "rtl_posixio", "rtl_fifo", "rtl", "mbuff",
 
