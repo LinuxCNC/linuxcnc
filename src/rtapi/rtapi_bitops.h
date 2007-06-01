d669af1d3806b8917abc2cbf06c4127b063af6d1
Jeff Epler <jepler@unpythonic.net>
2007-06-01 00:07:28 +0000

    for userspace, always use the bundled version of bitops -- the current compromise caused problems on x86 2.6.17-rtai3.5, which passed the current configure test but didn't actually have a usable asm/bitops.h augment the x86-64 set of bitops because a few more are used in rtai_ulapi that aren't in sim_ulapi

--- src/rtapi/rtapi_bitops.h
+++ src/rtapi/rtapi_bitops.h
@@ -1,6 +1,6 @@
 #ifndef RTAPI_BITOPS_H
 #define RTAPI_BITOPS_H
-#if (defined(__MODULE__) && !defined(SIM)) || defined(ASM_BITOPS_H_USABLE)
+#if (defined(__MODULE__) && !defined(SIM))
 #include <asm/bitops.h>
 #elif defined(__i386__)
 /* From <asm/bitops.h>
@@ -125,6 +125,66 @@ static __inline__ int test_and_clear_bit(int nr, volatile void * addr)
 #define ADDR (*(volatile long *) addr)
 
 /**
+ * set_bit - Atomically set a bit in memory
+ * @nr: the bit to set
+ * @addr: the address to start counting from
+ *
+ * This function is atomic and may not be reordered.  See __set_bit()
+ * if you do not require the atomic guarantees.
+ * Note that @nr may be almost arbitrarily large; this function is not
+ * restricted to acting on a single-word quantity.
+ */
+static __inline__ void set_bit(int nr, volatile void * addr)
+{
+	__asm__ __volatile__( LOCK_PREFIX
+		"btsl %1,%0"
+		:"=m" (ADDR)
+		:"dIr" (nr) : "memory");
+}
+
+
+/**
+ * clear_bit - Clears a bit in memory
+ * @nr: Bit to clear
+ * @addr: Address to start counting from
+ *
+ * clear_bit() is atomic and may not be reordered.  However, it does
+ * not contain a memory barrier, so if it is used for locking purposes,
+ * you should call smp_mb__before_clear_bit() and/or smp_mb__after_clear_bit()
+ * in order to ensure changes are visible on other processors.
+ */
+static __inline__ void clear_bit(int nr, volatile void * addr)
+{
+	__asm__ __volatile__( LOCK_PREFIX
+		"btrl %1,%0"
+		:"=m" (ADDR)
+		:"dIr" (nr));
+}
+
+
+static __inline__ int constant_test_bit(int nr, const volatile void * addr)
+{
+	return ((1UL << (nr & 31)) & (((const volatile unsigned int *) addr)[nr >> 5])) != 0;
+}
+
+static __inline__ int variable_test_bit(int nr, volatile const void * addr)
+{
+	int oldbit;
+
+	__asm__ __volatile__(
+		"btl %2,%1\n\tsbbl %0,%0"
+		:"=r" (oldbit)
+		:"m" (ADDR),"dIr" (nr));
+	return oldbit;
+}
+
+#define test_bit(nr,addr) \
+(__builtin_constant_p(nr) ? \
+ constant_test_bit((nr),(addr)) : \
+ variable_test_bit((nr),(addr)))
+
+
+/**
  * test_and_set_bit - Set a bit and return its old value
  * @nr: Bit to set
  * @addr: Address to count from
@@ -161,6 +221,7 @@ static __inline__ int test_and_clear_bit(int nr, volatile void * addr)
 		:"dIr" (nr) : "memory");
 	return oldbit;
 }
+#undef ADDR
 #elif defined(__powerpc__)
 
 #define BITS_PER_LONG 32
