f99176c67783ff7645cefa2ee64d69fb459a6763
Chris Radek <chris@timeguy.com>
2007-06-01 13:55:46 +0000

    remove line numbers from output to make diff work

--- tests/ccomp/mill-zchanges/test.sh
+++ tests/ccomp/mill-zchanges/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rs274 -v test.var -t test.tbl -g test.ngc
+rs274 -v test.var -t test.tbl -g test.ngc | awk '{$1=""; print}'
 result=$?
 [ -f test.var.bak ] && mv test.var.bak test.var
 exit $result
