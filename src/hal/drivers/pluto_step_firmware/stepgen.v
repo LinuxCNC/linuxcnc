7ea2783032cac2a03b6df57ca0e9659f6b13fb8f
Jeff Epler <jepler@unpythonic.net>
2007-10-21 23:00:52 +0000

    note license for this file

--- src/hal/drivers/pluto_step_firmware/stepgen.v
+++ src/hal/drivers/pluto_step_firmware/stepgen.v
@@ -1,3 +1,21 @@
+//    This is a component of pluto_servo, a PWM servo driver and quadrature
+//    counter for emc2
+//    Copyright 2007 Jeff Epler <jepler@unpythonic.net>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 module stepgen(clk, enable, position, velocity, dirtime, steptime, step, dir, tap);
 `define STATE_STEP 0
 `define STATE_DIRCHANGE 1
