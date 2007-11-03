f94ea5d2f910cf12cf9d99a5a617f126e2eceaf7
Chris Radek <chris@timeguy.com>
2007-11-03 01:23:49 +0000

    not used for anything, but called now

--- src/emc/rs274ngc/gcodemodule.cc
+++ src/emc/rs274ngc/gcodemodule.cc
@@ -283,12 +283,14 @@ void SET_FEED_MODE(int mode) {
 
 
 void SET_FEED_RATE(double rate) {
+#if 0
     maybe_new_line();   
     if(interp_error) return;
     PyObject *result =
         PyObject_CallMethod(callback, "set_feed_rate", "f", rate);
     if(result == NULL) interp_error ++;
     Py_XDECREF(result);
+#endif
 }
 
 void DWELL(double time) {
