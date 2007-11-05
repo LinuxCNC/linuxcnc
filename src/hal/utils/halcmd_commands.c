de1225f7abe2b9ebbf049a1e9d67a3541ace1154
Ben Lipkowitz <fenn@sdf.lonestar.org>
2007-11-05 19:40:23 +0000

    clear up ambiguous error messages

--- src/hal/utils/halcmd_commands.c
+++ src/hal/utils/halcmd_commands.c
@@ -362,7 +362,7 @@ static int preflight_net_cmd(char *signal, hal_sig_t *sig, char *pins[]) {
         }
         if(pin->dir == HAL_OUT) {
             if(writers || bidirs) {
-                halcmd_error("Signal '%s' may not add OUT pin '%s'\n",
+                halcmd_error("Signal '%s' can not add OUT pin '%s'\n",
                         signal, pin->name);
                 return HAL_INVAL;
             }
@@ -370,7 +370,7 @@ static int preflight_net_cmd(char *signal, hal_sig_t *sig, char *pins[]) {
         }
 	if(pin->dir == HAL_IO) {
             if(writers) {
-                halcmd_error("Signal '%s' may not add I/O pin '%s'\n",
+                halcmd_error("Signal '%s' can not add I/O pin '%s'\n",
                         signal, pin->name);
                 return HAL_INVAL;
             }
@@ -402,7 +402,7 @@ int do_net_cmd(char *signal, char *pins[]) {
     {
 	hal_pin_t *pin = halpr_find_pin_by_name(signal);
 	if(pin) {
-	    halcmd_error("Signal name '%s' may not be the same as a pin.\n",
+	    halcmd_error("Signal name '%s' must not be the same as a pin.\n",
 		signal);
 	    rtapi_mutex_give(&(hal_data->mutex));
 	    return HAL_BADVAR;
