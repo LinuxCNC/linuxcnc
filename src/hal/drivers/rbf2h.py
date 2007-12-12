cdad5e3cd5d7675a94a84fb45bf3b83b3b3d8d22
Sebastian Kuzminsky <seb@highlab.com>
2007-12-12 22:10:23 +0000

    Make LGPL work, and a minor cosmetic fix for GPL.
    (I discovered these when shamelessly stealing rbf2h.py for mesa7i43's
    to-header.py.)

--- src/hal/drivers/rbf2h.py
+++ src/hal/drivers/rbf2h.py
@@ -37,12 +37,10 @@ if GPL:
 //    You should have received a copy of the GNU General Public License
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-
 """
 
 if LGPL:
-    print """"\
-
+    print """\
 //    This library is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU Lesser General Public
 //    License as published by the Free Software Foundation; either
