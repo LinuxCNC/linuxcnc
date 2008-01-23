c23f739deb1977852c5124903791ecf000a9d8ba
Alex Joni <alex_joni@users.sourceforge.net>
2008-01-23 21:57:57 +0000

    hackish fix to prevent segfaults with bad M66 index numbers. the real fix is in r1.126

--- src/emc/task/emccanon.cc
+++ src/emc/task/emccanon.cc
@@ -2409,14 +2409,15 @@ CANON_PLANE GET_EXTERNAL_PLANE()
 int GET_EXTERNAL_DIGITAL_INPUT(int index)
 {
 /* returns current value of the digital input selected by index.*/
-//#ifdef INPUT_DEBUG
-    printf("GET_EXTERNAL_DIGITAL_INPUT called\n di[%d]=%d \n timeout=%d \n",index,emcStatus->motion.synch_di[index],emcStatus->task.input_timeout);
-//#endif
     if ((index < 0) || (index >= EMC_MAX_DIO))
 	return -1;
 
     if (emcStatus->task.input_timeout == 1)
 	return -1;
+
+//#ifdef INPUT_DEBUG
+    printf("GET_EXTERNAL_DIGITAL_INPUT called\n di[%d]=%d \n timeout=%d \n",index,emcStatus->motion.synch_di[index],emcStatus->task.input_timeout);
+//#endif
 
     return emcStatus->motion.synch_di[index];
 }
@@ -2620,6 +2621,12 @@ void WAIT(int index, /* index of the motion exported input */
 	 int wait_type, /* 0 - rise, 1 - fall, 2 - be high, 3 - be low */
 	 int timeout) /* time to wait [in seconds], if the input didn't change the value -1 is returned */
 {
+
+ if ((index < 0) || (index >= EMC_MAX_DIO)) {
+    printf("WAIT(): index %d out of bounds\n", index);
+    return ;
+ }
+
  EMC_AUX_INPUT_WAIT wait_msg;
  
  flush_segments();
