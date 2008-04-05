84d475136e453a051701d8a1324fbaf39a5a5589
Alex Joni <alex_joni@users.sourceforge.net>
2008-04-05 22:57:23 +0000

    remove *teleop_vector*, those will be handled by regular jog messages (with motion controller in TELEOP mode, not in joint mode)

--- src/emc/task/taskintf.cc
+++ src/emc/task/taskintf.cc
@@ -740,13 +740,6 @@ int emcTrajSetMode(int mode)
     }
 }
 
-int emcTrajSetTeleopVector(EmcPose vel)
-{
-    emcmotCommand.command = EMCMOT_SET_TELEOP_VECTOR;
-    emcmotCommand.pos = vel;
-    return usrmotWriteEmcmotCommand(&emcmotCommand);
-}
-
 int emcTrajSetVelocity(double vel, double ini_maxvel)
 {
     int retval;
