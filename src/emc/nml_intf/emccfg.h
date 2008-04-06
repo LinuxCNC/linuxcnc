c091f5e7eea0f86252851696828e4ea4494ad458
Alex Joni <alex_joni@users.sourceforge.net>
2008-04-06 09:20:37 +0000

    add default values for AXIS vel & acc limits

--- src/emc/nml_intf/emccfg.h
+++ src/emc/nml_intf/emccfg.h
@@ -51,6 +51,12 @@ extern "C" {
 /* default joint acceleration, in user units per second per second */
 #define DEFAULT_JOINT_MAX_ACCELERATION 1.0
 
+/* default axis velocity, in user units per second */
+#define DEFAULT_AXIS_MAX_VELOCITY 1.0
+
+/* default axis acceleration, in user units per second per second */
+#define DEFAULT_AXIS_MAX_ACCELERATION 1.0
+
 #ifdef __cplusplus
 }				/* matches extern "C" at top */
 #endif
