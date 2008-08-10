82a3f5a1e091e2a0298d6d3df6b6af98e23fc407
Jeff Epler <jepler@unpythonic.net>
2008-08-10 18:11:40 +0000

    note 2008 copyright ight

--- share/axis/tcl/axis.tcl
+++ share/axis/tcl/axis.tcl
@@ -1,6 +1,6 @@
 #    This is a component of AXIS, a front-end for emc
-#    Copyright 2004, 2005, 2006 Jeff Epler <jepler@unpythonic.net> and
-#    Chris Radek <chris@timeguy.com>
+#    Copyright 2004, 2005, 2006, 2007, 2008 Jeff Epler <jepler@unpythonic.net>
+#    and Chris Radek <chris@timeguy.com>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -1609,7 +1609,7 @@ text .about.message \
 	.about.message configure -cursor hand2
 	.about.message tag configure link -foreground red}
 .about.message tag bind link <ButtonPress-1><ButtonRelease-1> {launch_website}
-.about.message insert end [subst [_ "EMC2/AXIS version \$version\n\nCopyright (C) 2004, 2005, 2006, 2007 Jeff Epler and Chris Radek.\n\nThis is free software, and you are welcome to redistribute it under certain conditions.  See the file COPYING, included with EMC2.\n\nVisit the EMC web site: "]] {} {http://www.linuxcnc.org/} link
+.about.message insert end [subst [_ "EMC2/AXIS version \$version\n\nCopyright (C) 2004, 2005, 2006, 2007, 2008 Jeff Epler and Chris Radek.\n\nThis is free software, and you are welcome to redistribute it under certain conditions.  See the file COPYING, included with EMC2.\n\nVisit the EMC web site: "]] {} {http://www.linuxcnc.org/} link
 .about.message configure -state disabled
 
 button .about.ok \
