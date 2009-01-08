16a901623efec2661dee4c34022c83c8859d5488
Chris Radek <chris@timeguy.com>
2009-01-08 17:44:38 +0000

    avoid "cannot do G1 with no feed rate" after switching units

--- src/emc/rs274ngc/gcodemodule.cc
+++ src/emc/rs274ngc/gcodemodule.cc
@@ -531,7 +531,7 @@ int GET_EXTERNAL_QUEUE_EMPTY() { return true; }
 CANON_DIRECTION GET_EXTERNAL_SPINDLE() { return 0; }
 int GET_EXTERNAL_TOOL_SLOT() { return 0; }
 int GET_EXTERNAL_SELECTED_TOOL_SLOT() { return 0; }
-double GET_EXTERNAL_FEED_RATE() { return 0; }
+double GET_EXTERNAL_FEED_RATE() { return 1; }
 double GET_EXTERNAL_TRAVERSE_RATE() { return 0; }
 int GET_EXTERNAL_FLOOD() { return 0; }
 int GET_EXTERNAL_MIST() { return 0; }
