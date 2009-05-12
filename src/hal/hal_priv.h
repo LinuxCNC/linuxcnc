8eeaf57fb843c739ff833f0439a4766dfe531f04
Alex Joni <alex_joni@users.sourceforge.net>
2009-05-12 09:14:05 +0000

    bump HAL size, as it's needed for extended configs, and hopefully doesn't cause issues - 2.3 backport candidate

--- src/hal/hal_priv.h
+++ src/hal/hal_priv.h
@@ -325,8 +325,8 @@ typedef struct {
 */
 
 #define HAL_KEY   0x48414C32	/* key used to open HAL shared memory */
-#define HAL_VER   0x0000000A	/* version code */
-#define HAL_SIZE  131000
+#define HAL_VER   0x0000000B	/* version code */
+#define HAL_SIZE  262000
 
 /* These pointers are set by hal_init() to point to the shmem block
    and to the master data structure. All access should use these
