d55005c9aa4ae98a927c3986cd6fa7eb079b0cc9
Chris Radek <cradek@dsndata.com>
2009-06-24 11:31:30 -0500

    additional changes to make it RUN now

--- src/emc/usr_intf/emcsh.cc
+++ src/emc/usr_intf/emcsh.cc
@@ -3003,7 +3003,7 @@ static int emc_joint_backlash(ClientData clientdata,
 	    return TCL_ERROR;
 	}
 	// write it out
-	sendAxisSetBacklash(joint, backlash);
+	sendJointSetBacklash(joint, backlash);
 	return TCL_OK;
     }
 }
@@ -3044,7 +3044,7 @@ static int emc_joint_enable(ClientData clientdata,
 	return TCL_ERROR;
     }
 
-    sendAxisEnable(joint, val);
+    sendJointEnable(joint, val);
     return TCL_OK;
 }
 
@@ -3076,7 +3076,7 @@ static int emc_joint_load_comp(ClientData clientdata,
     }
 
     // now write it out
-    sendAxisLoadComp(joint, file, type);
+    sendJointLoadComp(joint, file, type);
     return TCL_OK;
 }
 
