df5c85a65c926e847d29ca637932a5231e0e1412
Michael Geszkiewicz <micges@wp.pl>
2009-07-02 14:59:17 +0200

    Enable analog_input code in motion * create HAL pins for analog inputs * only immediate inputs works for now * all aio values are copied to status struct Signed-off-by: Michael Geszkiewicz <micges@wp.pl>

--- src/emc/motion/mot_priv.h
+++ src/emc/motion/mot_priv.h
@@ -109,6 +109,7 @@ typedef struct {
     hal_bit_t *synch_do[EMCMOT_MAX_DIO]; /* WPI array: output pins for motion synched IO */
     hal_bit_t *synch_di[EMCMOT_MAX_DIO]; /* RPI array: input pins for motion synched IO */
     hal_float_t *analog_input[EMCMOT_MAX_AIO]; /* RPI array: input pins for analog Inputs */
+    hal_float_t *analog_output[EMCMOT_MAX_AIO]; /* RPI array: output pins for analog Inputs */
 
 
     // creating a lot of pins for spindle control to be very flexible
