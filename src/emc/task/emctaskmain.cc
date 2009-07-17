e1d0fb26106cb307654f350ff0e3e702aaddf537
Michael Geszkiewicz <micges@wp.pl>
2009-07-18 01:21:21 +0200

    Checking homed joints only when they exists Signed-off-by: Michael Geszkiewicz <micges@wp.pl>

--- src/emc/task/emctaskmain.cc
+++ src/emc/task/emctaskmain.cc
@@ -128,9 +128,8 @@ static int emctask_shutdown(void);
 static int pseudoMdiLineNumber = -1;
 
 static int all_homed(void) {
-    for(int i=0; i<9; i++) {
-        unsigned int mask = 1<<i;
-        if((emcStatus->motion.traj.axis_mask & mask) && !emcStatus->motion.joint[i].homed) // XXX
+    for (int i = 0; i < emcStatus->motion.traj.joints; i++) {
+        if(!emcStatus->motion.joint[i].homed) // XXX
             return 0;
     }
     return 1;
