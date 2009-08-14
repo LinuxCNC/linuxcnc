846da70574188233f8a41d0717c9deafb4d15dae
Alex Joni <juve@juve-desktop.(none)>
2009-08-15 02:11:47 +0300

    take out cbrt function, not used, and not GPL compliant

--- src/libnml/posemath/gomath.c
+++ src/libnml/posemath/gomath.c
@@ -25,22 +25,12 @@ extern int printf(const char * fmt, ...);
 
 #include "sincos.h"
 
-
 #include <math.h>		/* atan2(), sin(), cos(), etc. */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 #include "gotypes.h"		/* GoResult ,Real, etc */
 #include "gomath.h"		/* these decls, GoCartesian, etc. */
-
-/*
-  If the local implementation of cbrt() below is inadequate due to the
-  ldexp, frexp calls that may not exist, use pow(x,1/3) instead. Define
-  CBRT_IS_POW in this case.
-*/
-#define CBRT_IS_POW
-
-#if defined(CBRT_IS_POW)
 
 go_real go_cbrt(go_real x)
 {
@@ -50,98 +40,6 @@ go_real go_cbrt(go_real x)
   return (go_real) pow((double) x, 1.0/3.0);
 }
 
-#else
-
-/*
-  cbrt.c
-
-  Cube root function
-
-  Adapted from Cephes Math Library Release 2.8:  June, 2000
-  Copyright 1984, 1991, 2000 by Stephen L. Moshier
-*/
-
-static double CBRT2 = 1.2599210498948731647672;
-static double CBRT4 = 1.5874010519681994747517;
-static double CBRT2I = 0.79370052598409973737585;
-static double CBRT4I = 0.62996052494743658238361;
-
-#include <math.h>		/* frexp(), ldexp() */
-
-static double cbrt(double x)
-{
-  int e, rem, sign;
-  double z;
-
-  if (x == 0) {
-    return x;
-  }
-
-  if (x > 0) {
-    sign = 1;
-  } else {
-    sign = -1;
-    x = -x;
-  }
-
-  z = x;
-
-  /* extract power of 2, leaving mantissa between 0.5 and 1 */
-  x = frexp(x, &e);
-
-  /* Approximate cube root of number between .5 and 1,
-     peak relative error = 9.2e-6 */
-  x = (((-1.3466110473359520655053e-1 * x
-	 + 5.4664601366395524503440e-1) * x
-	- 9.5438224771509446525043e-1) * x
-       + 1.1399983354717293273738e0) * x + 4.0238979564544752126924e-1;
-
-  /* exponent divided by 3 */
-  if (e >= 0) {
-    rem = e;
-    e /= 3;
-    rem -= 3 * e;
-    if (rem == 1) {
-      x *= CBRT2;
-    } else if (rem == 2) {
-      x *= CBRT4;
-    }
-  } else {
-    /* argument less than 1 */
-    e = -e;
-    rem = e;
-    e /= 3;
-    rem -= 3 * e;
-    if (rem == 1)
-      x *= CBRT2I;
-    else if (rem == 2)
-      x *= CBRT4I;
-    e = -e;
-  }
-
-  /* multiply by power of 2 */
-  x = ldexp(x, e);
-
-  /* Newton iteration */
-  x -= (x - (z / (x * x))) * 0.33333333333333333333;
-  x -= (x - (z / (x * x))) * 0.33333333333333333333;
-
-  if (sign < 0) {
-    x = -x;
-  }
-
-  return x;
-}
-
-go_real go_cbrt(go_real x)
-{
-  if (x < 0.0) {
-    return (go_real) -cbrt((double) -x);
-  }
-  return (go_real) cbrt((double) x);
-}
-
-#endif
 
 /* Translation rep conversion functions */
 
