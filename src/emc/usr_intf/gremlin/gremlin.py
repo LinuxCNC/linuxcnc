8c7faf073f59802ee493ed46f57a0a3d96f16091
Jeff Epler <jepler@unpythonic.net>
2009-08-25 18:15:48 -0500

    use antialised font for DRO

--- src/emc/usr_intf/gremlin/gremlin.py
+++ src/emc/usr_intf/gremlin/gremlin.py
@@ -22,9 +22,63 @@ import os
 import thread
 
 from minigl import *
+import pango
+import cairo
+import pangocairo
 
 def use_font(fontDesc, start, count, base):
-    return gtk.gdkgl.font_use_pango_font(fontDesc, start, count, base)
+    surface = cairo.ImageSurface(cairo.FORMAT_A8, 256, 256)
+    context = pangocairo.CairoContext(cairo.Context(surface))
+    layout = context.create_layout()
+    fontmap = pangocairo.cairo_font_map_get_default()
+    font = fontmap.load_font(fontmap.create_context(), fontDesc)
+    layout.set_font_description(fontDesc)
+    metrics = font.get_metrics()
+    descent = metrics.get_descent()
+    d = pango.PIXELS(descent)
+    linespace = metrics.get_ascent() + metrics.get_descent()
+    width = metrics.get_approximate_char_width()
+
+    glPushClientAttrib(GL_CLIENT_PIXEL_STORE_BIT)
+    glPixelStorei(GL_UNPACK_SWAP_BYTES, 0)
+    glPixelStorei(GL_UNPACK_LSB_FIRST, 1)
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 256)
+    glPixelStorei(GL_UNPACK_IMAGE_HEIGHT, 256)
+    glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0)
+    glPixelStorei(GL_UNPACK_SKIP_ROWS, 0)
+    glPixelStorei(GL_UNPACK_SKIP_IMAGES, 0)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glPixelZoom(1, -1)
+
+    for i in range(count):
+        ch = unichr(start+i)
+        layout.set_text(ch)
+        w, h = layout.get_size()
+        context.save()
+        context.new_path()
+        context.rectangle(0, 0, 256, 256)
+        context.set_source_rgba(0., 0., 0., 0.)
+        context.set_operator (cairo.OPERATOR_SOURCE);
+        context.paint()
+        context.restore()
+
+        context.save()
+        context.set_source_rgba(1., 1., 1., 1.)
+        context.set_operator (cairo.OPERATOR_SOURCE);
+        context.move_to(0, 0)
+        context.update_layout(layout)
+        context.show_layout(layout)
+        context.restore()
+
+        w, h = pango.PIXELS(w), pango.PIXELS(h)
+        glNewList(base+i, GL_COMPILE)
+        glBitmap(0, 0, 0, 0, 0, h-d, '');
+        if w and h: glDrawPixels(w, h, GL_LUMINANCE, GL_UNSIGNED_BYTE, surface.get_data())
+        glBitmap(0, 0, 0, 0, w, -h+d, '');
+        glEndList()
+
+    glPopClientAttrib()
+    return pango.PIXELS(width), pango.PIXELS(linespace)
 
 class DummyProgress:
     def nextphase(self, unused): pass
@@ -167,14 +221,9 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
 
         fontDesc = pango.FontDescription('courier bold 16')
         self.font_base = glGenLists(128)
-        font = use_font(fontDesc, 0, 128, self.font_base)
-        metrics = font.get_metrics()
-        linespace = metrics.get_ascent() + metrics.get_descent()
-        print 'line', linespace, pango.PIXELS(linespace)
-        self.font_linespace = pango.PIXELS(linespace)
-        width = metrics.get_approximate_char_width()
-        print 'width', width, pango.PIXELS(width)
-        self.font_charwidth = pango.PIXELS(width)
+        width, linespace = use_font(fontDesc, 0, 128, self.font_base)
+        self.font_linespace = linespace
+        self.font_charwidth = width
         rs274.glcanon.GlCanonDraw.realize(self)
 
     def load(self, filename):
