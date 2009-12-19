0d7845915848f8bb24d5481543bf2a72146e7b15
Michael Geszkiewicz <micges@wp.pl>
2009-12-20 00:04:21 +0100

    Remove not needed tests from touch off window
    Signed-off-by: Michael Geszkiewicz <micges@wp.pl>

--- src/emc/usr_intf/axis/scripts/axis.py
+++ src/emc/usr_intf/axis/scripts/axis.py
@@ -2315,11 +2315,6 @@ all_systems = ['P1  G54', 'P2  G55', 'P3  G56', 'P4  G57', 'P5  G58',
 
 class _prompt_touchoff(_prompt_float):
     def __init__(self, title, text, default, defaultsystem):
-        systems = all_systems[:]
-        tool_offset_axes = "xyzabcuvw"
-        if vars.current_axis.get() not in tool_offset_axes:
-            del systems[-1]
-            if defaultsystem.startswith("T"): defaultsystem = systems[0]
         linear_axis = vars.current_axis.get() in "xyzuvw"
         if linear_axis:
             if vars.metric.get(): unit_str = " " + _("mm")
@@ -2336,7 +2331,7 @@ class _prompt_touchoff(_prompt_float):
         self.c = c = StringVar(t)
         c.set(defaultsystem)
         l = Label(f, text=_("Coordinate System:"))
-        mb = OptionMenu(f, c, *systems)
+        mb = OptionMenu(f, c, *all_systems)
         mb.tk.call("size_menubutton_to_entries", mb)
         mb.configure(takefocus=1)
         l.pack(side="left") 
@@ -2344,10 +2339,10 @@ class _prompt_touchoff(_prompt_float):
         f.pack(side="top") 
         self.buttons.tkraise()
         for i in [1,2,3,4,5,6,7,8,9]:
-            t.bind("<Alt-KeyPress-%s>" % i, lambda event, system=systems[i-1]: c.set(system))
-        if not (current_tool is None or vars.current_axis.get() not in tool_offset_axes):
-            t.bind("<Alt-t>", lambda event: c.set(systems[9]))
-            t.bind("<Alt-0>", lambda event: c.set(systems[9]))
+            t.bind("<Alt-KeyPress-%s>" % i, lambda event, system=all_systems[i-1]: c.set(system))
+        if current_tool != None:
+            t.bind("<Alt-t>", lambda event: c.set(all_systems[9]))
+            t.bind("<Alt-0>", lambda event: c.set(all_systems[9]))
 
     def result(self):
         if self.u.get(): return self.v.get(), self.c.get()
