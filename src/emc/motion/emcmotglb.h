388721f2c7b66a32ac254f05f9c14ae5db8f2821
Michael Geszkiewicz <micges@wp.pl>
2010-01-03 02:56:41 +0100

    Remove leavings of changelogs in motion source
    Signed-off-by: Michael Geszkiewicz <micges@wp.pl>

--- src/emc/motion/emcmotglb.h
+++ src/emc/motion/emcmotglb.h
@@ -9,8 +9,6 @@
 * System: Linux
 *    
 * Copyright (c) 2004 All rights reserved.
-*
-* Last change:
 ********************************************************************/
 #ifndef EMCMOTGLB_H
 #define EMCMOTGLB_H
