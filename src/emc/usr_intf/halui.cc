9fe8ca9a580fc7defaf7f743abfff96300b813f3
Michael Geszkiewicz <micges@wp.pl>
2010-01-23 00:40:36 +0100

    Fix halui to look for new sections names in inifile
    Signed-off-by: Michael Geszkiewicz <micges@wp.pl>

--- src/emc/usr_intf/halui.cc
+++ src/emc/usr_intf/halui.cc
@@ -1517,7 +1517,7 @@ static int iniLoad(const char *filename)
 	}
     }
 
-    if (NULL != inifile.Find("HOME_SEQUENCE", "AXIS_0")) {
+    if (NULL != inifile.Find("HOME_SEQUENCE", "JOINT_0")) {
         have_home_all = 1;
     }
 
