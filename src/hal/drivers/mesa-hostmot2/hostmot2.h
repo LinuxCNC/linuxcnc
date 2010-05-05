2fa0df4ddf37651b6a303ca0ba66c38fcbe5c2d2
Sebastian Kuzminsky <seb@highlab.com>
2010-05-05 09:18:41 -0600

    fix pwmgen memory leaks, & simplify pwmgen a bit
    pwmgen leaked memory (pwmgen.instance and pwmgen.hal) in error paths
    in initialization, and also in the cleanup function.  This commit fixes
    those leaks.
    
    This commit also simplifies the pwmgen code a bit by making pwmgen.hal
    be statically allocated.

--- src/hal/drivers/mesa-hostmot2/hostmot2.h
+++ src/hal/drivers/mesa-hostmot2/hostmot2.h
@@ -344,15 +344,6 @@ typedef struct {
 } hm2_pwmgen_instance_t;
 
 
-// these hal params affect all pwmgen instances
-typedef struct {
-    struct {
-        hal_u32_t pwm_frequency;
-        hal_u32_t pdm_frequency;
-    } param;
-} hm2_pwmgen_module_global_t;
-
-
 typedef struct {
     int num_instances;
     hm2_pwmgen_instance_t *instance;
@@ -361,8 +352,13 @@ typedef struct {
     u8 version;
 
 
-    // module-global HAL objects...
-    hm2_pwmgen_module_global_t *hal;
+    // these hal params affect all pwmgen instances
+    struct {
+        struct {
+            hal_u32_t pwm_frequency;
+            hal_u32_t pdm_frequency;
+        } param;
+    } hal;
 
     // these keep track of the most recent hal->param.p{d,w}m_frequency
     // that we've told the FPGA about, so we know if we need to update it
