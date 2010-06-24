167ec44a7a9c491568c82fed11c801e64d17a466
Jeff Epler <jepler@unpythonic.net>
2010-06-24 10:21:20 -0400

    make desktop files executable to placate lucid
    while we agree it's utterly insane to require .desktop files to be
    executable (because they won't do the right thing when executed
    from the shell), it's apparently some kind of security measure.  Without
    this, double clicking a desktop emc icon doesn't work.

--- tcl/bin/pickconfig.tcl
+++ tcl/bin/pickconfig.tcl
@@ -415,6 +415,7 @@ proc make_shortcut {inifile} {
 	set filename $filename0${i}.desktop
     }
     exec emcmkdesktop $inifile $name > $filename
+    file attributes $filename -permissions +x
     tk_dialog .d [msgcat::mc "Shortcut Created"] [msgcat::mc "A shortcut to this configuration file has been created on your desktop.  You can use it to automatically launch this configuration."] info 0 [msgcat::mc "OK"]
 }
 
