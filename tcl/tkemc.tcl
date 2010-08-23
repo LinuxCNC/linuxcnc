87f95107927b8b19921060fbb7992b6bd5f4fa63
Chris Radek <chris@timeguy.com>
2010-08-23 15:08:00 -0500

    remove unused/mistaken code

--- tcl/tkemc.tcl
+++ tcl/tkemc.tcl
@@ -82,58 +82,47 @@ set axiscoordmap ""
 set numaxes [emc_ini "AXES" "TRAJ"]
 set coordnames [ emc_ini "COORDINATES" "TRAJ"]
 
-set numcoords 0
-
 if { [ string first "X" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0 } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist X]
     set axiscoordmap [ concat $axiscoordmap 0  ]
 }
 
 if { [ string first "Y" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist Y]
     set axiscoordmap [ concat $axiscoordmap  1 ]
 }
 
 if { [ string first "Z" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist Z]
     set axiscoordmap [ concat $axiscoordmap  2 ]
 }
 
 if { [ string first "A" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist A]
     set axiscoordmap [ concat $axiscoordmap  3 ]
 }
 
 if { [ string first "B" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist B]
     set axiscoordmap [ concat $axiscoordmap  4 ]
 }
 
 if { [ string first "C" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist C]
     set axiscoordmap [ concat $axiscoordmap  5 ]
 }
 
 if { [ string first "U" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist U]
     set axiscoordmap [ concat $axiscoordmap  6 ]
 }
 
 if { [ string first "V" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist V]
     set axiscoordmap [ concat $axiscoordmap  7 ]
 }
 
 if { [ string first "W" $coordnames ] >= 0 | [ string first " " $coordnames ] >= 0  } {
-    set numcoords $numcoords+1
     set worldlabellist [ concat $worldlabellist W]
     set axiscoordmap [ concat $axiscoordmap  8 ]
 }
