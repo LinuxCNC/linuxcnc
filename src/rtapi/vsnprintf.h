9a4f7645938671d81051bae424c7551c55201e98
Jeff Epler <jepler@unpythonic.net>
2010-11-23 20:11:30 +0000

    vsnprintf: let others use small_digits, large_digits
    .. which the floating-point formatter will soon want to do

--- src/rtapi/vsnprintf.h
+++ src/rtapi/vsnprintf.h
@@ -61,6 +61,9 @@ static int skip_atoi(const char **s)
 #define SPECIAL	32		/* 0x */
 #define LARGE	64		/* use 'ABCDEF' instead of 'abcdef' */
 
+static const char small_digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+static const char large_digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 static char *ch(char *buf, char *end, char ch)
 {
     if(buf <= end) *buf = ch;
@@ -73,8 +76,6 @@ static char *number(char *buf, char *end, long long numll, int base,
     unsigned long num;
     char c, sign, tmp[66];
     const char *digits;
-    const char small_digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-    const char large_digits[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int i;
 
     digits = (type & LARGE) ? large_digits : small_digits;
