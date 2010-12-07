d5114846cffc11a7204488d70679c0fe70d844c6
Pavel Shramov <psha@kamba.psha.org.ru>
2010-12-07 22:14:24 +0300

    gladevcp: Added license headers to new files

--- lib/python/gladevcp/hal_bar.py
+++ lib/python/gladevcp/hal_bar.py
@@ -1,4 +1,18 @@
 # vim: sts=4 sw=4 et
+# GladeVcp Widgets
+#
+# Copyright (c) 2010  Pavel Shramov <shramov@mexmat.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import gtk
 import gobject
 import cairo
