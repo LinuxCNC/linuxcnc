5e0e875e038ee0a9c1b7deb3db5a5a368f9d82a1
Michael Haberler <git@mah.priv.at>
2011-04-05 07:24:28 +0200

    interp/o_word: fix oword sub return & endsub errors when called from MDI and no file open
    this removes the assumption that all o-word subs need to return/endsub to an
    open .ngc file at the top level, even if in MDI mode (so the top level
    ngc file isnt executing anyway).
    
    This fix is a prerequisite for procedure-based toolchange to properly work
    when no .ngc file is loaded.

--- src/emc/rs274ngc/interp_o_word.cc
+++ src/emc/rs274ngc/interp_o_word.cc
@@ -82,7 +82,6 @@ int Interp::findFile( // ARGUMENTS
     ERS(NCE_FILE_NOT_OPEN);
 }
 
-
 /************************************************************************/
 /*
    In the long run, this function will use a hash table or other
@@ -181,10 +180,10 @@ int Interp::control_back_to( /* ARGUMENTS                       */
       // if(settings->oword_offset[i].o_word == line)
       if(0 == strcmp(settings->oword_offset[i].o_word_name, block->o_name))
 	{
-          if(settings->file_pointer == NULL)
-          {
-            ERS(NCE_FILE_NOT_OPEN);
-          }
+	    if ((settings->filename[0] != 0) &
+		(settings->file_pointer == NULL))  {
+		ERS(NCE_FILE_NOT_OPEN);
+	    }
           if(0 != strcmp(settings->filename,
                          settings->oword_offset[i].filename))
           {
@@ -200,7 +199,8 @@ int Interp::control_back_to( /* ARGUMENTS                       */
               if(newFP)
               {
                   // close the old file...
-                  fclose(settings->file_pointer);
+		  if (settings->file_pointer) // only close if it was open
+		      fclose(settings->file_pointer);
                   settings->file_pointer = newFP;
               }
               else
@@ -209,8 +209,9 @@ int Interp::control_back_to( /* ARGUMENTS                       */
                   ERS(NCE_UNABLE_TO_OPEN_FILE,settings->filename);
               }
           }
-	  fseek(settings->file_pointer,
-		settings->oword_offset[i].offset, SEEK_SET);
+	  if (settings->file_pointer) // only seek if it was open
+	      fseek(settings->file_pointer,
+		    settings->oword_offset[i].offset, SEEK_SET);
 
 	  settings->sequence_number =
 	    settings->oword_offset[i].sequence_number;
@@ -269,7 +270,8 @@ int Interp::control_back_to( /* ARGUMENTS                       */
       logDebug("fopen: |%s| OK", newFileName);
 
       // close the old file...
-      fclose(settings->file_pointer);
+      if (settings->file_pointer)
+	  fclose(settings->file_pointer);
       settings->file_pointer = newFP;
 
       strcpy(settings->filename, newFileName);
@@ -452,34 +454,39 @@ int Interp::convert_control_functions( /* ARGUMENTS           */
       if(settings->call_level != 0) {
 	  unwind(settings); // common code for return and endsub
 
-	  logDebug("seeking to: %ld",
-		   settings->sub_context[settings->call_level].position);
+	  // file at this level was marked as closed, so dont reopen.
+	  if (settings->sub_context[settings->call_level].position == -1) {
+	      settings->file_pointer = NULL;
+	      strcpy(settings->filename,"");
+	  } else {
+	      logDebug("seeking to: %ld",
+		       settings->sub_context[settings->call_level].position);
 
-          if(settings->file_pointer == NULL)
-          {
-            ERS(NCE_FILE_NOT_OPEN);
-          }
+	      if(settings->file_pointer == NULL)
+		  {
+		      ERS(NCE_FILE_NOT_OPEN);
+		  }
 
-          //!!!KL must open the new file, if changed
+	      //!!!KL must open the new file, if changed
 
-          if(0 != strcmp(settings->filename,
-                         settings->sub_context[settings->call_level].filename))
-          {
-              fclose(settings->file_pointer);
-              settings->file_pointer = 
-              fopen(settings->sub_context[settings->call_level].filename, "r");
+	      if(0 != strcmp(settings->filename,
+			     settings->sub_context[settings->call_level].filename))
+		  {
+		      fclose(settings->file_pointer);
+		      settings->file_pointer = 
+			  fopen(settings->sub_context[settings->call_level].filename, "r");
 
-              strcpy(settings->filename,
-                     settings->sub_context[settings->call_level].filename);
-          }
+		      strcpy(settings->filename,
+			     settings->sub_context[settings->call_level].filename);
+		  }
           
-	  fseek(settings->file_pointer,
-		settings->sub_context[settings->call_level].position,
-		SEEK_SET);
+	      fseek(settings->file_pointer,
+		    settings->sub_context[settings->call_level].position,
+		    SEEK_SET);
 
-	  settings->sequence_number =
-	    settings->sub_context[settings->call_level].sequence_number;
-
+	      settings->sequence_number =
+		  settings->sub_context[settings->call_level].sequence_number;
+	  }
 	  if(settings->sub_name)
 	    {
 	      free(settings->sub_name);
@@ -540,23 +547,27 @@ int Interp::convert_control_functions( /* ARGUMENTS           */
 
 	}
 
-        if(settings->file_pointer == NULL)
-          {
-            ERS(NCE_FILE_NOT_OPEN);
-          }
-        settings->sub_context[settings->call_level].position =
-	    ftell(settings->file_pointer);
-        if(settings->sub_context[settings->call_level].filename)
+      // if the previous file was NULL, mark positon as -1 so as not to
+      // reopen it on return.
+      if (settings->file_pointer == NULL) {
+	  settings->sub_context[settings->call_level].position = -1;
+	  // MSG("---- marking as return to 'no file' - level=%d filename='%s' seqno=%d\n",settings->call_level,settings->filename,settings->sequence_number);
+      } else {
+	  // MSG("---- marking as return to valid file level=%d filename='%s' seqno=%d\n",settings->call_level,settings->filename,settings->sequence_number);
+	  settings->sub_context[settings->call_level].position = ftell(settings->file_pointer); 
+      }
+
+      if(settings->sub_context[settings->call_level].filename)
           {
               // if there is a string here, free it
               free(settings->sub_context[settings->call_level].filename);
           }
-        // save the previous filename
-	logDebug("Duping |%s|", settings->filename);
-        settings->sub_context[settings->call_level].filename =
+      // save the previous filename
+      logDebug("Duping |%s|", settings->filename);
+      settings->sub_context[settings->call_level].filename =
               strdup(settings->filename);
 
-	settings->sub_context[settings->call_level].sequence_number
+      settings->sub_context[settings->call_level].sequence_number
 	    = settings->sequence_number;
 
       logDebug("(in call)set params[%d] return file:%s offset:%ld",
@@ -894,19 +905,25 @@ int Interp::convert_control_functions( /* ARGUMENTS           */
       // in a call -- must do a return
      unwind(settings); // common code for return and endsub
 
-      logDebug("seeking to: %ld",
+     logDebug("seeking to: %ld",
 	       settings->sub_context[settings->call_level].position);
 
-      if(settings->file_pointer == NULL)
-      {
-        ERS(NCE_FILE_NOT_OPEN);
-      }
-
       //!!!KL must open the new file, if changed
-      fseek(settings->file_pointer,
-	    settings->sub_context[settings->call_level].position, SEEK_SET);
 
-      settings->sequence_number =
+      // only seek if not marked as closed.
+     if (settings->sub_context[settings->call_level].position != -1) {
+	 if (settings->file_pointer == NULL)  {
+	     ERS(NCE_FILE_NOT_OPEN);
+	 } else {
+	     fseek(settings->file_pointer,
+		   settings->sub_context[settings->call_level].position, SEEK_SET);
+	 }
+     } else {
+	 settings->file_pointer = NULL;
+	 strcpy(settings->filename,"");
+     }
+
+     settings->sequence_number =
 	settings->sub_context[settings->call_level].sequence_number;
 
       break;
