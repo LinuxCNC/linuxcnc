5b912ebb7e8ed07ff8310f5e28483e7603dfba0a
Chris Radek <chris@timeguy.com>
2011-04-13 09:33:20 -0500

    disallow the trouble case of G4+arc both using P

--- src/emc/rs274ngc/interp_check.cc
+++ src/emc/rs274ngc/interp_check.cc
@@ -86,6 +86,7 @@ int Interp::check_g_codes(block_pointer block,   //!< pointer to a block to be c
   if (mode0 == -1) {
   } else if (mode0 == G_4) {
     CHKS((block->p_number == -1.0), NCE_DWELL_TIME_MISSING_WITH_G4);
+    CHKS((mode1 == G_2 || mode1 == G_3), _("G4 not allowed with G2 or G3 because they both use P"));
   } else if (mode0 == G_10) {
     (block->p_number >= 0) ? p_int = (int) (block->p_number +0.5) :p_int = (int) (block->p_number -0.5);
     CHKS((block->l_number != 2 && block->l_number != 1 && block->l_number != 20 && block->l_number != 10 && block->l_number != 11), _("Line with G10 does not have L1, L10, L11, L2, or L20"));
