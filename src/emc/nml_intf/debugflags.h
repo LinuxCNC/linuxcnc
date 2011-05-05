a35b126d3a359e868cd70d6bb2f7d6c9f5208985
Michael Haberler <git@mah.priv.at>
2011-05-05 23:15:02 +0200

    debugging:  factor out flags into debugflags.h
    purpose: so non-task related subsystems may share [EMC]DEBUG flags

---
+++ src/emc/nml_intf/debugflags.h
@@ -0,0 +1,25 @@
+// factored out from emcglb.h so subsystems not requiring the
+// emcglb.h defines may include them as well
+
+
+
+#define EMC_DEBUG_CONFIG            0x00000002
+#define EMC_DEBUG_VERSIONS          0x00000008
+#define EMC_DEBUG_TASK_ISSUE        0x00000010
+#define EMC_DEBUG_NML               0x00000040
+#define EMC_DEBUG_MOTION_TIME       0x00000080
+#define EMC_DEBUG_INTERP            0x00000100
+#define EMC_DEBUG_RCS               0x00000200
+#define EMC_DEBUG_INTERP_LIST       0x00000800
+#define EMC_DEBUG_IOCONTROL         0x00001000
+#define EMC_DEBUG_OWORD             0x00002000
+#define EMC_DEBUG_REMAP             0x00004000
+#define EMC_DEBUG_PYTHON            0x00008000
+
+#define EMC_DEBUG_UNCONDITIONAL     0x40000000  // always logged
+#define EMC_DEBUG_ALL               0x7FFFFFFF	/* it's an int for %i to work
+						 */
+// debug prefix flags
+#define LOG_TIME  1
+#define LOG_PID   2
+#define LOG_FILENAME 4 // and line
