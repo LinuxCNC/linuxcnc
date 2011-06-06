0a6f064e58cf98360d1d9e920cb4621978a8f027
Michael Haberler <git@mah.priv.at>
2011-06-06 21:29:54 +0200

    adapt t/m6 ngc tc to new scheme, including glue

--- configs/sim/pysubs/pythonsubs.py
+++ configs/sim/pysubs/pythonsubs.py
@@ -9,57 +9,54 @@ from InterpMod import *
 # PYIMPORT=pythonsubs.py
 # any callables in this module will be available as
 # o<callable_name> call or handler names.
-#
+
+
+
 # to remap Tx (prepare) to an NGC file 'prepare.ngc', incantate like so:
 #
-# REMAP=T argspec=T-  prolog=prepare_prolog epilog=prepare_epilog ngc=prepare
+# REMAP=T  prolog=prepare_prolog epilog=prepare_epilog ngc=prepare
 # This means:
-#
-# argspec=T- :
-#     for T, a mandatory parameter word is required, others are ignored
-#
-# ngc=prepare
-#     your O-word procedure goes to prepare.ngc
 #
 # prolog=prepare_prolog
 #     before calling prepare.ngc, execute the Python function 'prepare_prolog'
-#
+# ngc=prepare
+#     your O-word procedure goes to prepare.ngc
 # epilog=prepare_epilog
 #     after calling prepare.ngc, execute the Python function 'prepare_epilog'
 #
 def prepare_prolog(userdata,**words):
-	toolno = words['t']
-	(status,pocket) = interp.find_tool_pocket(toolno)
+	tool = int(words['t'])
+	(status,pocket) = interp.find_tool_pocket(tool)
 	if status != INTERP_OK:
-		interp.set_errormsg("T%d: pocket not found" % (toolno))
+		interp.set_errormsg("T%d: pocket not found" % (tool))
 		return (status,)
-
 	# these variables will be visible in the following ngc oword sub
 	# as #<tool> and #<pocket> as local variables
-	interp.params["tool"] = toolno
+	interp.params["tool"] = tool
 	interp.params["pocket"] = pocket
 	return (INTERP_OK,)
 
-# The actual ngc procedure looks like so:
+# The minimal ngc prepare procedure looks like so:
 #
 # o<r_prepare> sub
-# (debug, r_prepare call_level= #<_call_level> remap_level=#<_remap_level> tool=#<tool> pocket=#<pocket>)
+# (debug, r_prepare tool=#<tool> pocket=#<pocket>)
 #
-# 	; show aborting a prepare on tool 2 by returning a negative value
-# 	o<testabort> if [#<tool> EQ 2]
-# 		     o<testabort> return [-1]
-# 	o<testabort> endif
-#
+# returning a negative value fails the prepare command
+# returning a positive value sets this value as the new pocket:
 # o<r_prepare> endsub [#<pocket>]
 # m2
+
+# the prepare epilog looks at the return value from the NGC procedure
+# and does the right thing:
 def prepare_epilog(userdata,**words):
 	retval = interp.return_value
 	if retval > 0:
 		interp.selected_pocket = int(retval)
 		CanonMod.SELECT_POCKET(int(retval))
+		return (INTERP_OK,)
 	else:
-		CanonMod.INTERP_ABORT(int(retval),"T%d: aborted (return code %.4f)" % (words['t'],retval))
-	return (INTERP_OK,)
+		interp.set_errormsg("T%d: aborted (return code %.4f)" % (int(words['t']),retval))
+		return (INTERP_ERROR,)
 
 
 #
@@ -83,7 +80,7 @@ def change_prolog(userdata,**words):
 	## 	    block->t_number,
 	#interp.params["prepared" ] = 2
 
-	interp.params["pocket" ] = interp.selected_pocket
+	interp.params["selected_pocket" ] = interp.selected_pocket
 	return (INTERP_OK,)
 
 def change_epilog(userdata,**words):
@@ -95,10 +92,10 @@ def change_epilog(userdata,**words):
 		# cause a sync()
 		interp.tool_change_flag = True
 		interp.set_tool_parameters()
+		return (INTERP_OK,)
 	else:
-		# abort
-		CanonMod.INTERP_ABORT(int(retval),"M6 aborted (return code %.4f)" % (retval))
-	return (INTERP_OK,)
+		interp.set_errormsg("M6 aborted (return code %.4f)" % (retval))
+		return (INTERP_ERROR,)
 
 
 # M61 remapped to an all-Python handler
@@ -106,7 +103,7 @@ def change_epilog(userdata,**words):
 #
 # Incantation:
 #
-# REMAP=M61  modalgroup=6  argspec=Q-  python=set_tool_number
+# REMAP=M61    python=set_tool_number
 #
 # This means:
 #
@@ -124,9 +121,12 @@ def set_tool_number(userdata,**words):
 		return (status,)
 	if words['q'] > -TOLERANCE_EQUAL:
 		interp.current_pocket = pocket
+
 		CanonMod.CHANGE_TOOL_NUMBER(pocket)
+		interp.tool_table[0].offset.tran.z = interp.tool_table[pocket].offset.tran.z
 		# cause a sync()
 		interp.tool_change_flag = True
+		interp.set_tool_parameters()
 		return (INTERP_OK,)
 	else:
 		interp.set_errormsg("M61 failed: Q=%4" % (toolno))
