f2d767e5bcaf51962277bfc1601fe409b30ef8bc
Michael Haberler <git@mah.priv.at>
2011-06-09 15:06:37 +0200

    note on Python oword sub parameter passing

--- configs/sim/pysubs/pythonsubs.py
+++ configs/sim/pysubs/pythonsubs.py
@@ -152,15 +152,13 @@ def test_reschedule(userdata,**words):
 # Demo Python O-word subroutine - call as:
 # o<square> [5]
 # (debug, #<_value>)
+#
+# len(args) reflects the number of actual parameters passed
 def square(args):
 	return args[0]*args[0]
 
 
 #----------------  debugging fluff ----------
-
-
-
-
 
 # tool table access
 def print_tool(userdata, **words):
