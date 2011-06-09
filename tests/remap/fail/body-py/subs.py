0d93f31399cd030b06494f0b12600b6e4e23ec6e
Michael Haberler <git@mah.priv.at>
2011-06-09 13:06:28 +0200

    commit all remap regression tests

---
+++ tests/remap/fail/body-py/subs.py
@@ -0,0 +1,5 @@
+from InterpMod import *
+
+def interp_error(userdata, **words):
+    interp.set_errormsg("A failed Python remap handler returning INTERP_ERROR")
+    return (INTERP_ERROR,)
---
+++ tests/remap/fail/prolog/subs.py
@@ -0,0 +1,5 @@
+from InterpMod import *
+
+def failingprolog(userdata, **words):
+    interp.set_errormsg("A failed Python prolog returning INTERP_ERROR")
+    return (INTERP_ERROR,)
