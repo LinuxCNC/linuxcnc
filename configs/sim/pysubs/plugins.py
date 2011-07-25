f63c2c713800936762173d9dc38415817ca79d82
Michael Haberler <git@mah.priv.at>
2011-07-26 01:48:15 +0200

    plugins.py: better test for running under task

--- configs/sim/pysubs/plugins.py
+++ configs/sim/pysubs/plugins.py
@@ -1,4 +1,9 @@
-# sys,interpreter,canon automatically imported
+import interpreter
+import canon
+have_emctask = False
+if 'emctask' in sys.builtin_module_names:
+    import emctask
+    have_task = True
 
 import task
 import oword
