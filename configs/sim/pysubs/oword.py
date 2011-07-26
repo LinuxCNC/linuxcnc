22599267855503f8446808ad481a3f120ec2b22a
Michael Haberler <git@mah.priv.at>
2011-07-26 22:58:27 +0200

    python/oword: add set_named_pin example

--- configs/sim/pysubs/oword.py
+++ configs/sim/pysubs/oword.py
@@ -1,6 +1,7 @@
 import sys
 import interpreter
 
+
 have_emctask = False
 if 'emctask' in sys.builtin_module_names:
     import emctask
@@ -8,28 +9,52 @@ if 'emctask' in sys.builtin_module_names:
 
 import task
 
+#define EMC_DEBUG_CONFIG            0x00000002
+#define EMC_DEBUG_VERSIONS          0x00000008
+#define EMC_DEBUG_TASK_ISSUE        0x00000010
+#define EMC_DEBUG_NML               0x00000040
+#define EMC_DEBUG_MOTION_TIME       0x00000080
+#define EMC_DEBUG_INTERP            0x00000100
+#define EMC_DEBUG_RCS               0x00000200
+#define EMC_DEBUG_INTERP_LIST       0x00000800
+#define EMC_DEBUG_IOCONTROL         0x00001000
+#define EMC_DEBUG_OWORD             0x00002000
+#define EMC_DEBUG_REMAP             0x00004000
+#define EMC_DEBUG_PYTHON            0x00008000
+#define EMC_DEBUG_NAMEDPARAM        0x00010000
+#define EMC_DEBUG_GDBONSIGNAL       0x00020000
+
+def debug():
+    return interpreter.this.debugmask &  0x00008000
+
 # Demo Python O-word subroutine - call as:
 # o<square> [5]
 # (debug, #<_value>)
 #
 # len(args) reflects the number of actual parameters passed
 def square(args):
-	return args[0]*args[0]
+    return args[0]*args[0]
 
 
 #----------------  queue a call for task-time execution ------------
 def qdemo(args):
     print "in oword.qdemo() - enqueueing demo",args
-    try:
-        task.enqueue.demo(args)
-    except Exception,e:
-        print "uh,oh",e
-        pass
+    task.enqueue.demo(args)
+
+
+def set_named_pin(args):
+
+    if (len(args) != 1):
+        interpreter.this.set_errormsg("set_named_pin takes a single argument and a comment")
+    if len(interpreter.this.blocks[0].comment) == 0:
+            interpreter.this.set_errormsg("set_named_pin takes  a comment, which is the HAL pin name")
+
+    task.enqueue.set_named_pin(args[0], interpreter.this.blocks[0].comment)
 
 def notify(args):
     try:
         c = interpreter.this.blocks[0].comment
-        print "in oword.notify() comment=",c
+        if debug(): print "in oword.notify() comment=",c
         task.enqueue.notify(c)
     except Exception,e:
         print "notify: ",e
--- configs/sim/pysubs/task.py
+++ configs/sim/pysubs/task.py
@@ -7,33 +7,41 @@ try:
 except ImportError:
     import pickle
 
-h = None
+
+def debug():
+    return interpreter.this.debugmask &  0x00008000 #  EMC_DEBUG_PYTHON
+
 
 # this is called by task once after startup
 # task_init() is NOT called when running in the UI
 def task_init():
-	global h
-        print "Python: task_init() called"
-	h = hal.component("iocontrol-task")
-	h.newpin("coolant-flood", hal.HAL_BIT, hal.HAL_OUT)
-	h.newpin("coolant-mist", hal.HAL_BIT, hal.HAL_OUT)
+    global h
+    if debug(): print "Python: task_init() called"
 
-	h.newpin("lube-level", hal.HAL_BIT, hal.HAL_OUT)
-	h.newpin("lube", hal.HAL_BIT, hal.HAL_OUT)
+    h = hal.component("iocontrol-task")
+    h.newpin("coolant-flood", hal.HAL_BIT, hal.HAL_OUT)
+    h.newpin("coolant-mist", hal.HAL_BIT, hal.HAL_OUT)
 
-	h.newpin("emc-enable-in", hal.HAL_BIT, hal.HAL_IN)
-	h.newpin("user-enable-out", hal.HAL_BIT, hal.HAL_OUT)
-	h.newpin("user-request-enable", hal.HAL_BIT, hal.HAL_OUT)
+    h.newpin("lube-level", hal.HAL_BIT, hal.HAL_OUT)
+    h.newpin("lube", hal.HAL_BIT, hal.HAL_OUT)
 
-	h.newpin("tool-changed", hal.HAL_BIT, hal.HAL_IN)
-	h.newpin("tool-prep-number", hal.HAL_S32, hal.HAL_OUT)
-	h.newpin("tool-prep-pocket", hal.HAL_S32, hal.HAL_OUT)
-	h.newpin("tool-prepare", hal.HAL_BIT, hal.HAL_OUT)
-	h.newpin("tool-prepared", hal.HAL_BIT, hal.HAL_IN)
+    h.newpin("emc-enable-in", hal.HAL_BIT, hal.HAL_IN)
+    h.newpin("user-enable-out", hal.HAL_BIT, hal.HAL_OUT)
+    h.newpin("user-request-enable", hal.HAL_BIT, hal.HAL_OUT)
 
-	h.newpin("bit", hal.HAL_BIT, hal.HAL_OUT)
-	h.ready()
-	return 1
+    h.newpin("tool-changed", hal.HAL_BIT, hal.HAL_IN)
+    h.newpin("tool-prep-number", hal.HAL_S32, hal.HAL_OUT)
+    h.newpin("tool-prep-pocket", hal.HAL_S32, hal.HAL_OUT)
+    h.newpin("tool-prepare", hal.HAL_BIT, hal.HAL_OUT)
+    h.newpin("tool-prepared", hal.HAL_BIT, hal.HAL_IN)
+
+    h.newpin("bit", hal.HAL_BIT, hal.HAL_OUT)
+
+    h.newpin("float", hal.HAL_FLOAT, hal.HAL_OUT)
+    h.newpin("int", hal.HAL_S32, hal.HAL_OUT)
+    h.ready()
+
+    return 1
 
 #----------------  exec-time plugin support ----------
 
@@ -42,15 +50,31 @@ def task_init():
 class Execute(object):
 
     def demo(self,s):
-	    global h
-	    if h:
-                print "TASK: demo(%s)" % s
-                for i in range(int(s[0])):
-                    h['bit'] = not  h['bit']
-            return 0
-            # return -1 to fail execution
-            #interpreter.interp.set_errormsg("really bad")
-	    #return -1
+        global h
+        if debug(): print "TASK: demo(%s)" % s
+        try:
+            for i in range(int(s[0])):
+                h['bit'] = not  h['bit']
+        except Exception,e:
+            print "bad:",e
+            pass
+        return 0
+    # return -1 to fail execution
+    #interpreter.this.set_errormsg("really bad")
+    #return -1
+
+    def set_named_pin(self,value,name):
+        global h
+        if debug(): print "set_named_pin ",value,name
+        if type(h[name]).__name__ == 'float':
+            h[name] = value
+
+        if type(h[name]).__name__ == 'int':
+            h[name] = int(value)
+
+        if type(h[name]).__name__ == 'bool':
+            h[name] = bool(int(value))
+        return 0
 
     def notify(self, s):
         print "notify s=",s
