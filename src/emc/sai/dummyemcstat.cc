746858967a9d9190005d72fb7ce5e527a097e97a
Michael Haberler <git@mah.priv.at>
2011-07-29 09:57:43 +0200

    rs274: fixup linking

--- src/emc/sai/dummyemcstat.cc
+++ src/emc/sai/dummyemcstat.cc
@@ -8,3 +8,9 @@
 EMC_STAT *emcStatus = new EMC_STAT;
 
 EMC_IO_STAT *emcIoStatus = new EMC_IO_STAT;
+
+int emcOperatorDisplay(int, char const*, ...) {return 0;};
+
+int emcOperatorText(int, char const*, ...) {return 0;}
+
+// int emcOperatorError(int, char const*, ...) {return 0;}
