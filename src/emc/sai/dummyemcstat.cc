03696723a80c81251ffffd5c9c66e5922cb2df59
Michael Haberler <git@mah.priv.at>
2011-08-03 16:41:34 +0200

    add dummyemcstat.cc

--- src/emc/sai/dummyemcstat.cc
+++ src/emc/sai/dummyemcstat.cc
@@ -3,14 +3,22 @@
 #include "rcs.hh"		// NML classes, nmlErrorFormat()
 #include "emc.hh"		// EMC NML
 #include "emc_nml.hh"
-
+#include "rs274ngc.hh"
+#include "rs274ngc_interp.hh"
 
 EMC_STAT *emcStatus = new EMC_STAT;
 
-EMC_IO_STAT *emcIoStatus = new EMC_IO_STAT;
+// EMC_IO_STAT *emcIoStatus = new EMC_IO_STAT;
 
 int emcOperatorDisplay(int, char const*, ...) {return 0;};
 
 int emcOperatorText(int, char const*, ...) {return 0;}
 
 // int emcOperatorError(int, char const*, ...) {return 0;}
+
+
+int emcAbortCleanup(int reason, const char *message)
+{
+    printf("on_abort: [%d] %s\n", reason,message);
+    return 0;
+}
