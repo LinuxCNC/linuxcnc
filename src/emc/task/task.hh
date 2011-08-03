5fdbb8ee4c0441a826af3fa69ee0d736e9468fef
Michael Haberler <git@mah.priv.at>
2011-08-03 16:47:26 +0200

    task.hh: declare emcTaskOnce, emcRunHalFiles

--- src/emc/task/task.hh
+++ src/emc/task/task.hh
@@ -6,7 +6,8 @@ extern int stepping;
 extern int steppingWait;
 extern int emcTaskQueueCommand(NMLmsg *cmd);
 extern int emcPluginCall(EMC_EXEC_PLUGIN_CALL *call_msg);
-extern int emcTaskOnce(const char *python_taskinit);
+extern int emcTaskOnce(const char *inifile, const char *python_taskinit);
+extern int emcRunHalFiles(const char *filename);
 
 #endif
 
