68d49695775736635b4b36e4db70553c04ec163f
Michael Haberler <git@mah.priv.at>
2011-08-03 17:00:39 +0200

    test/remap/fail/epilog: adapt canon->emccanon

---
+++ tests/remap/fail/epilog/remap.py
@@ -0,0 +1,7 @@
+import emccanon
+import interpreter
+
+def failingepilog(userdata, **words):
+    emccanon.MESSAGE("failing_epilog returning INTERP_ERROR")
+    interpreter.this.set_errormsg("A failed Python epilog returning INTERP_ERROR")
+    return interpreter.INTERP_ERROR
