0dee1e544399e9f946df84aa82ca946d75dec52f
Michael Haberler <git@mah.priv.at>
2011-08-08 14:19:22 +0200

    python: more de-bloat

--- configs/sim/pysubs/plugins.py
+++ configs/sim/pysubs/plugins.py
@@ -1,10 +1,9 @@
 import os
 import interpreter
 import emccanon
-have_emctask = False
+
 try:
     import emctask
-    have_task = True
     print "plugins: milltask=",os.getpid()
 except ImportError:
     print "plugins: axis=",os.getpid()
@@ -14,8 +13,5 @@ except ImportError:
 import task
 import oword
 import remap
-
-
-print "plugins MAIN"
 
 
