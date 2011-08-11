9fe9a451e021d50be212876b336c86719a1a5530
Michael Haberler <git@mah.priv.at>
2011-08-11 15:18:59 +0200

    python/task: implement options for Task behaviour
    1 - no iocontrol toolchanger pins - just commit prepare and change:
    this is for usage with toolchange with remapped codes, where pin-wiggling
    is all done in gcode. prepare and change are basically just commit ops.
    
    2. iocontrol-v1 pins
    3. iocontrol-v2 pins
    Task emmulates these in Python

--- configs/sim/pysubs/customtask.py
+++ configs/sim/pysubs/customtask.py
@@ -5,6 +5,7 @@ import emccanon
 import interpreter
 import hal
 import tooltable
+import emc  # ini only
 
 try:
     import cPickle as pickle
@@ -19,10 +20,17 @@ except ImportError:
 def debug():
     return interpreter.this.debugmask &  0x00040000 # EMC_DEBUG_PYTHON_TASK
 
-
 class CustomTask(emctask.Task,UserFuncs):
-    def __init__(self):
+    def __init__(self): #, toolchange_pins = False):
         emctask.Task.__init__(self)
+
+        self.inifile = emc.ini(self.ini_filename)
+        yn = self.inifile.find("PYTHON", "TASK_TOOLCHANGE_PINS")
+        self.tcpins = int(yn) if yn else 0
+        yn = self.inifile.find("PYTHON", "TASK_START_CHANGE_PINS")
+        self.startchange_pins = self.tcpins and (int(yn) if yn else 0)
+        yn = self.inifile.find("PYTHON", "TASK_TOOLCHANGE_FAULT_PINS")
+        self.fault_pins = self.tcpins and (int(yn) if yn else 0)
 
         h = hal.component("iocontrol.0")
         h.newpin("coolant-flood", hal.HAL_BIT, hal.HAL_OUT)
@@ -35,13 +43,27 @@ class CustomTask(emctask.Task,UserFuncs):
         h.newpin("user-enable-out", hal.HAL_BIT, hal.HAL_OUT)
         h.newpin("user-request-enable", hal.HAL_BIT, hal.HAL_OUT)
 
-        h.newpin("tool-change", hal.HAL_BIT, hal.HAL_OUT)
-        h.newpin("tool-changed", hal.HAL_BIT, hal.HAL_IN)
-        h.newpin("tool-number", hal.HAL_S32, hal.HAL_OUT)
-        h.newpin("tool-prep-number", hal.HAL_S32, hal.HAL_OUT)
-        h.newpin("tool-prep-pocket", hal.HAL_S32, hal.HAL_OUT)
-        h.newpin("tool-prepare", hal.HAL_BIT, hal.HAL_OUT)
-        h.newpin("tool-prepared", hal.HAL_BIT, hal.HAL_IN)
+        if self.tcpins:
+            h.newpin("tool-change", hal.HAL_BIT, hal.HAL_OUT)
+            h.newpin("tool-changed", hal.HAL_BIT, hal.HAL_IN)
+            h.newpin("tool-number", hal.HAL_S32, hal.HAL_OUT)
+            h.newpin("tool-prep-number", hal.HAL_S32, hal.HAL_OUT)
+            h.newpin("tool-prep-pocket", hal.HAL_S32, hal.HAL_OUT)
+            h.newpin("tool-prepare", hal.HAL_BIT, hal.HAL_OUT)
+            h.newpin("tool-prepared", hal.HAL_BIT, hal.HAL_IN)
+        if self.startchange_pins:
+            h.newpin("start-change", hal.HAL_BIT, hal.HAL_OUT)
+            h.newpin("start-change-ack", hal.HAL_BIT, hal.HAL_IN)
+        if self.fault_pins:
+            h.newpin("emc-abort", hal.HAL_BIT, hal.HAL_OUT)
+            h.newpin("emc-abort-ack", hal.HAL_BIT, hal.HAL_IN)
+            h.newpin("emc-reason", hal.HAL_S32, hal.HAL_OUT)
+            h.newpin("toolchanger-fault", hal.HAL_BIT, hal.HAL_IN)
+            h.newpin("toolchanger-fault-ack", hal.HAL_BIT, hal.HAL_OUT)
+            h.newpin("toolchanger-reason", hal.HAL_S32, hal.HAL_IN)
+            h.newpin("toolchanger-faulted", hal.HAL_BIT, hal.HAL_OUT)
+            h.newpin("toolchanger-clear-fault", hal.HAL_BIT, hal.HAL_IN)
+
         h.ready()
         self.components = dict()
         self.components["iocontrol.0"] = h
@@ -89,7 +111,6 @@ class CustomTask(emctask.Task,UserFuncs):
 
     def emcToolLoadToolTable(self,file):
         print "py:  emcToolLoadToolTable file =",file
-        # FIXME raise in tt.load, handle exception here
         self.comments = dict()
         self.fms = dict()
         try:
@@ -105,7 +126,6 @@ class CustomTask(emctask.Task,UserFuncs):
         print "prepare complete"
         self.e.io.tool.pocketPrepped = self.hal["tool-prep-pocket"]
         self.hal["tool-prepare"] = 0
-        pass
 
     def emcToolPrepare(self,p,tool):
         print "py:   emcToolPrepare p =",p,"tool =",tool
@@ -113,15 +133,22 @@ class CustomTask(emctask.Task,UserFuncs):
             print "it doesn't make sense to prep the spindle pocket"
             return 0
 
-        self.hal["tool-prep-pocket"] = p
-        if not self.random_toolchanger and (p == 0):
-            self.hal["tool-prep-number"] = 0
-        else:
-            self.hal["tool-prep-number"] = self.e.io.tool.toolTable[p].toolno
+        if self.tcpins:
+            if self.fault_pins and self.hal["toolchanger-faulted"]:
+                print "prepare: toolchanger faulted (reason=%d), next M6 will %s" % (self.hal["toolchanger-reason"], "set fault code and reason"  if self.hal["toolchanger-reason"] > 0 else "abort program")
+            self.hal["tool-prep-pocket"] = p
+            if not self.random_toolchanger and (p == 0):
+                self.hal["tool-prep-number"] = 0
+            else:
+                self.hal["tool-prep-number"] = self.e.io.tool.toolTable[p].toolno
 
-        self.hal["tool-prepare"] = 1
-        # and tell task to wait until status changes to RCS_DONE
-        self.e.io.status =  self.wait_for_named_pin(1,"iocontrol.0.tool-prepared",self.prepare_complete)
+                self.hal["tool-prepare"] = 1
+
+                # and tell task to wait until status changes to RCS_DONE
+                self.e.io.status =  self.wait_for_named_pin(1,"iocontrol.0.tool-prepared",self.prepare_complete)
+        else:
+            self.e.io.tool.pocketPrepped = p
+            self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
         return 0
 
     def reload_tool_number(self, toolno):
@@ -140,7 +167,6 @@ class CustomTask(emctask.Task,UserFuncs):
             if pocket == 0:
                 self.e.io.tool.toolTable[0].zero()
             else:
-                # self.e.io.tool.toolTable[0].assign(self.e.io.tool.toolTable[pocket])
                 self.e.io.tool.toolTable[0] = self.e.io.tool.toolTable[pocket]
 
     def change_complete(self):
@@ -155,8 +181,6 @@ class CustomTask(emctask.Task,UserFuncs):
         self.hal["tool-prep-number"] = 0
         self.hal["tool-prep-pocket"] = 0
         self.hal["tool-change"] = 0
-        self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
-        return 0
 
     def emcToolLoad(self):
         print "py:  emcToolLoad"
@@ -169,10 +193,22 @@ class CustomTask(emctask.Task,UserFuncs):
             self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
             return 0
 
-        if self.e.io.tool.pocketPrepped != -1:
-            self.hal["tool-change"] = 1
-            self.e.io.status =  self.wait_for_named_pin(1,"iocontrol.0.tool-changed",self.change_complete)
-            return 0
+        if self.tcpins:
+            if self.fault_pins and self.hal["toolchanger-faulted"]:
+                self.e.io.status  = emctask.RCS_STATUS.RCS_ERROR
+                return 0
+            if self.e.io.tool.pocketPrepped != -1:
+                self.hal["tool-change"] = 1
+                self.e.io.status =  self.wait_for_named_pin(1,"iocontrol.0.tool-changed",self.change_complete)
+                return 0
+        else:
+            if not self.random_toolchanger and (self.e.io.tool.pocketPrepped == 0):
+                self.e.io.tool.toolInSpindle = 0
+            else:
+                self.e.io.tool.toolInSpindle = self.e.io.tool.toolTable[self.e.io.tool.pocketPrepped].toolno
+            self.load_tool(self.e.io.tool.pocketPrepped)
+            self.e.io.tool.pocketPrepped = -1
+            self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
         return 0
 
     def emcToolUnload(self):
@@ -186,7 +222,8 @@ class CustomTask(emctask.Task,UserFuncs):
     def emcToolSetNumber(self,number):
         print "py:   emcToolSetNumber number =",number
         self.e.io.tool.toolInSpindle = number
-        self.hal["tool-number"] = self.e.io.tool.toolInSpindle
+        if self.tcpins:
+            self.hal["tool-number"] = self.e.io.tool.toolInSpindle
         self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
         return 0
 
@@ -226,22 +263,46 @@ class CustomTask(emctask.Task,UserFuncs):
         self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
         return 0
 
+    def emc_abort_acked(self):
+        print "emc_abort_acked"
+        self.hal["emc-abort"] = 0
+
     def emcIoAbort(self,reason):
         print "py:  emcIoAbort reason=",reason,"state=",self.e.task.state
-        self.hal["coolant-mist"] = 0
+        #print "tc fault=",self.e.io.fault, "tc reason=",self.e.io.reason
+
         self.e.io.coolant.mist = 0
-        self.hal["coolant-flood"] = 0
         self.e.io.coolant.flood = 0
-        self.hal["tool-change"] = 0
-        self.hal["tool-prepare"] = 0
+
+        if self.tcpins:
+            self.hal["coolant-mist"] = 0
+            self.hal["coolant-flood"] = 0
+            self.hal["tool-change"] = 0
+            self.hal["tool-prepare"] = 0
+        if self.startchange_pins:
+            self.hal["start-change"] = 0
+        if self.fault_pins:
+            self.hal["emc-reason"] = reason
+            self.hal["emc-abort"] = 1
+            self.e.io.status =  self.wait_for_named_pin(1,"iocontrol.0.emc-abort-ack",self.emc_abort_acked)
+            return 0
+
         if self._callback:
             print "emcIoAbort: cancelling callback to ",self._callback
             self._callback = None
         self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
         return 0
 
+    def start_change_acked(self):
+        print "start_change_acked"
+        self.hal["start-change"] = 0
+
     def emcToolStartChange(self):
-        print "py:  emcToolStartChange NIY"
+        print "py:  emcToolStartChange", "wait for iocontrol.0.start-change-ack" if self.startchange_pins else "noop"
+        if self.startchange_pins:
+                self.hal["start-change"] = 1
+                self.e.io.status =  self.wait_for_named_pin(1,"iocontrol.0.start-change-ack",self.start_change_acked)
+                return 0
         self.e.io.status  = emctask.RCS_STATUS.RCS_DONE
         return 0
 
@@ -309,13 +370,26 @@ class CustomTask(emctask.Task,UserFuncs):
         #        print "py:  emcIoUpdate"
         self.hal["user-request-enable"] = 0
         self.e.io.aux.estop = not self.hal["emc-enable-in"]
+        if self.fault_pins:
+            if self.hal["toolchanger-fault"]:
+                self.e.io.reason = self.hal["toolchanger-reason"]
+                self.hal["toolchanger-fault-ack"] = 1
+                self.hal["toolchanger-faulted"] = 1 # fault indicator latch
+                self.e.io.fault = 1
+                #self.e.io.status  =  emctask.RCS_STATUS.RCS_ERROR
+                return 0
+            else:
+                self.hal["toolchanger-fault-ack"] = 0
+            if self.hal["toolchanger-clear-fault"]:
+                self.hal["toolchanger-faulted"] = 0 # reset fault indicator latch
+                self.e.io.reason = 0
         if self._check:
             self.e.io.status  = self._check()
         return 0
 
     def wait_for_named_pin_callback(self):
         if self._comp[self._pin] == self._value:
-            print "wait CB done"
+            print "pin %s now %d" % (self._pin,  self._value)
             if self._callback: self._callback()
             self._check = None
             self._callback = None
@@ -323,13 +397,14 @@ class CustomTask(emctask.Task,UserFuncs):
         return emctask.RCS_STATUS.RCS_EXEC
 
     def wait_for_named_pin(self,value,name,callback = None):
-        print "wait_for_named_pin ",value,name
         (component, pin) = name.rsplit('.',1)
         comp = self.components[component]
         if comp[pin] == value:
-            print "wait_for_named_pin: already set"
+            print "pin: %s already at %d" % (name,value)
             if callback: callback()
             return emctask.RCS_STATUS.RCS_DONE
+        else:
+            print "waiting for %s to become %d" % (name,value)
         # else set up callback
         self._comp = comp
         self._pin = pin
@@ -347,13 +422,20 @@ class CustomTask(emctask.Task,UserFuncs):
         self.hal["coolant-mist"] = 0
         self.hal["coolant-flood"] = 0
         self.hal["lube"] = 0
-        self.hal["tool-prepare"] = 0
-        self.hal["tool-prepared"] = 0
-        self.hal["tool-prep-number"] = 0
-        self.hal["tool-prep-pocket"] = 0
-        self.hal["tool-change"] = 0
-        self.hal["tool-number"] = 0
-
+        if self.tcpins:
+            self.hal["tool-prepare"] = 0
+            self.hal["tool-prepared"] = 0
+            self.hal["tool-prep-number"] = 0
+            self.hal["tool-prep-pocket"] = 0
+            self.hal["tool-change"] = 0
+            self.hal["tool-number"] = 0
+        if self.startchange_pins:
+            self.hal["start-change"] = 0
+        if self.fault_pins:
+            self.hal["emc-abort"] = 0
+            self.hal["emc-reason"] = 0
+            self.hal["toolchanger-fault-ack"] = 0
+            self.hal["toolchanger-faulted"] = 0
 
 
 # support queuing calls from Interp to Task Python methods:
