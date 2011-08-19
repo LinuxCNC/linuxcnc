5145304be950fcdb31b973780e6c3afc921e40d7
Michael Haberler <git@mah.priv.at>
2011-08-19 09:13:35 +0200

    tests: more cleanup

--- tests/remap/introspect/test.sh
+++ tests/remap/introspect/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rs274 -v test.var -i test.ini -n 0 -g test.ngc 2>&1
+rs274 -i test.ini -n 0 -g test.ngc 2>&1
 exit $?
--- tests/remap/oword-pycall/test.sh
+++ tests/remap/oword-pycall/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rs274 -v test.var -t test.tbl -i test.ini -n 0 -g test.ngc 2>&1
-exit ${PIPESTATUS[0]}
+rs274 -t test.tbl -i test.ini -n 0 -g test.ngc 2>&1
+exit $?
