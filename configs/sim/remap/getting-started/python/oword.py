22706653baa15c6f2679c3fa8b9cb58a63b70059
Michael Haberler <git@mah.priv.at>
2011-08-23 19:11:50 +0200

    add example from manual

--- configs/sim/remap/getting-started/python/oword.py
+++ configs/sim/remap/getting-started/python/oword.py
@@ -7,3 +7,11 @@ def foo(*args):
 # make debugger available as oword procedure
 def pydevd(self,*args):
     call_pydevd()
+
+
+
+# this would be defined in the oword module
+def mysub(self, *args):
+    print "number of parameters passed:", len(args)
+    for a in args:
+	print a
