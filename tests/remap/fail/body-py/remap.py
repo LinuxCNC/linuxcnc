c8ef2374752780bb0e1341121ea04e84d89487cf
Michael Haberler <git@mah.priv.at>
2011-08-28 18:04:40 +0200

    tests/remap/fail/body-py:  drop useless userdata parameter

--- tests/remap/fail/body-py/remap.py
+++ tests/remap/fail/body-py/remap.py
@@ -1,6 +1,6 @@
 import interpreter
 
-def interp_error(self,userdata, **words):
+def interp_error(self, **words):
     self.set_errormsg("A failed Python remap handler returning INTERP_ERROR")
     return interpreter.INTERP_ERROR
 
