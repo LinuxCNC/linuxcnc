1a52a881a537bce627bbe6284e8c10a0da1847bd
Michael Haberler <git@mah.priv.at>
2011-10-08 06:02:58 +0200

    touchup

--- configs/sim/remap/iocontrol-removed/python/toplevel.py
+++ configs/sim/remap/iocontrol-removed/python/toplevel.py
@@ -4,9 +4,9 @@ import emccanon
 
 try:
     import emctask
-    print "plugins: milltask=",os.getpid()
+    print "toplevel: milltask=",os.getpid()
 except ImportError:
-    print "plugins: axis=",os.getpid()
+    print "toplevel: axis=",os.getpid()
     pass
 
 
