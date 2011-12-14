30af27f418786192b852fa5d6887c0df38fbb178
Michael Haberler <git@mah.priv.at>
2011-12-14 22:22:27 +0100

    gladevcp/examples: extend example to accept HAL widget property via HAL pin

--- configs/gladevcp/set-param-from-ini/meter_scale.py
+++ configs/gladevcp/set-param-from-ini/meter_scale.py
@@ -1,18 +1,26 @@
 import emc
 import os
+import hal
+import hal_glib
 
 class HandlerClass:
+
+    def _on_max_value_change(self,hal_pin,data=None):
+        self.meter.max = float(hal_pin.get())
+        self.meter.queue_draw() # force a widget redraw
 
     def __init__(self, halcomp,builder,useropts):
 	self.builder = builder
 
+        # hal pin with change callback.
+        # When the pin's value changes the callback is executed.
+        self.max_value = hal_glib.GPin(halcomp.newpin('max-value',  hal.HAL_FLOAT, hal.HAL_IN))
+        self.max_value.connect('value-changed', self._on_max_value_change)
+
 	inifile = emc.ini(os.getenv("INI_FILE_NAME"))
 	mmin = float(inifile.find("METER", "MIN") or 0.0)
-	mmax = float(inifile.find("METER", "MAX") or 100.0)
-
-        self.builder.get_object('meter').min = mmin
-        self.builder.get_object('meter').max = mmax
-        self.builder.get_object('label').set_label("min = %f\nmax = %f " % (mmin,mmax))
+        self.meter = self.builder.get_object('meter')
+        self.meter.min = mmin
 
 
 def get_handlers(halcomp,builder,useropts):
