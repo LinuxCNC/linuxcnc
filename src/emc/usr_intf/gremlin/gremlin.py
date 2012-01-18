c3b2d11a1ed957a6de2544f2d15736929bc49d59
cmorley <chrisinnanaimo@hotmail.com>
2012-01-17 17:58:39 -0800

    gladevcp -expose more display options to hal_gremlin
    can select such things as displaying the DRO in metric or imperial units.
    If also checks the INI file, under [DISPLAY] for LATHE = 1 (or true or True).
    This will then only allow P and Y views. The y view will be rotated to a more
    common lathe layout and manual rotation will not be allowed. The DRO will
    display radius and diameter for the X axis.

--- src/emc/usr_intf/gremlin/gremlin.py
+++ src/emc/usr_intf/gremlin/gremlin.py
@@ -28,12 +28,13 @@ class DummyProgress:
     def progress(self): pass
 
 class StatCanon(rs274.glcanon.GLCanon, rs274.interpret.StatMixin):
-    def __init__(self, colors, geometry, stat, random):
+    def __init__(self, colors, geometry, lathe_view_option, stat, random):
         rs274.glcanon.GLCanon.__init__(self, colors, geometry)
         rs274.interpret.StatMixin.__init__(self, stat, random)
         self.progress = DummyProgress()
+        self.lathe_view_option = lathe_view_option
 
-    def is_lathe(self): return False
+    def is_lathe(self): return self.lathe_view_option
 
 class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
               rs274.glcanon.GlCanonDraw):
@@ -51,8 +52,7 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
             a = self.colors[s + "_alpha"]
             s = self.colors[s]
             return [int(x * 255) for x in s + (a,)]
-
-
+        self.inifile = inifile
         self.logger = linuxcnc.positionlogger(linuxcnc.stat(),
             C('backplotjog'),
             C('backplottraverse'),
@@ -65,7 +65,6 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
         thread.start_new_thread(self.logger.start, (.01,))
 
         rs274.glcanon.GlCanonDraw.__init__(self, linuxcnc.stat(), self.logger)
-        self.inifile = inifile
 
         self.current_view = 'z'
 
@@ -94,6 +93,22 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
         self.maxlat = 90
 
         self.highlight_line = None
+        self.program_alpha = False
+        self.use_joints_mode = False
+        self.use_commanded = True
+        self.show_limits = True
+        self.show_extents_option = True
+        self.show_live_plot = True
+        self.show_velocity = True
+        self.metric_units = True
+        self.show_program = True
+        self.show_rapids = True
+        self.use_relative = True
+        self.show_tool = True
+        self.show_dtg = True
+        temp = inifile.find("DISPLAY", "LATHE")
+        self.lathe_option = bool(temp == "1" or temp == "True" or temp == "true" )
+        self.show_offsets = False
 
 	self.a_axis_wrapped = inifile.find("AXIS_3", "WRAPPED_ROTARY")
 	self.b_axis_wrapped = inifile.find("AXIS_4", "WRAPPED_ROTARY")
@@ -138,6 +153,9 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
 
     def _redraw(self): self.expose()
 
+    def clear_live_plotter(self):
+        self.logger.clear()
+
     def map(self, *args):
         gobject.timeout_add(50, self.poll)
 
@@ -169,22 +187,26 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
         self.font_charwidth = width
         rs274.glcanon.GlCanonDraw.realize(self)
 
-        if s.file: self.load(s.file)
+        if s.file: self.load()
 
     def set_current_view(self):
         if self.current_view not in ['x', 'y', 'z', 'p']:
             return
         return getattr(self, 'set_view_%s' % self.current_view)()
 
-    def load(self, filename):
+    def load(self,filename = None):
         s = self.stat
         s.poll()
+        if not filename and s.file:
+            filename = s.file
+        elif not filename and not s.file:
+            return
 
         td = tempfile.mkdtemp()
         self._current_file = filename
         try:
             random = int(self.inifile.find("EMCIO", "RANDOM_TOOLCHANGER") or 0)
-            canon = StatCanon(self.colors, self.get_geometry(), s, random)
+            canon = StatCanon(self.colors, self.get_geometry(),self.lathe_option, s, random)
             parameter = self.inifile.find("RS274NGC", "PARAMETER_FILE")
             temp_parameter = os.path.join(td, os.path.basename(parameter or "linuxcnc.var"))
             if parameter:
@@ -199,27 +221,33 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
 
         self.set_current_view()
 
-    def get_program_alpha(self): return False
+    def get_program_alpha(self): return self.program_alpha
     def get_num_joints(self): return self.num_joints
-    def get_geometry(self): return 'XYZ'
-    def get_joints_mode(self): return False
-    def get_show_commanded(self): return True
-    def get_show_extents(self): return True
-    def get_show_limits(self): return True
-    def get_show_live_plot(self): return True
-    def get_show_machine_speed(self): return True
-    def get_show_metric(self): return True
-    def get_show_program(self): return True
-    def get_show_rapids(self): return True
-    def get_show_relative(self): return True
-    def get_show_tool(self): return True
-    def get_show_distance_to_go(self): return True
+    def get_geometry(self):
+        temp = self.inifile.find("DISPLAY", "GEOMETRY")
+        if temp:
+            self.geometry = temp.upper()
+        else:
+            self.geometry = 'XYZ'
+        return self.geometry
+    def get_joints_mode(self): return self.use_joints_mode
+    def get_show_commanded(self): return self.use_commanded
+    def get_show_extents(self): return self.show_extents_option
+    def get_show_limits(self): return self.show_limits
+    def get_show_live_plot(self): return self.show_live_plot
+    def get_show_machine_speed(self): return self.show_velocity
+    def get_show_metric(self): return self.metric_units
+    def get_show_program(self): return self.show_program
+    def get_show_rapids(self): return self.show_rapids
+    def get_show_relative(self): return self.use_relative
+    def get_show_tool(self): return self.show_tool
+    def get_show_distance_to_go(self): return self.show_dtg
 
     def get_view(self):
         view_dict = {'x':0, 'y':1, 'z':2, 'p':3}
         return view_dict.get(self.current_view, 3)
 
-    def is_lathe(self): return False
+    def is_lathe(self): return self.lathe_option
     def get_current_tool(self):
         for i in self.stat.tool_table:
             if i[0] == self.stat.tool_in_spindle:
@@ -233,7 +261,7 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
     def get_font_info(self):
         return self.font_charwidth, self.font_linespace, self.font_base
 
-    def get_show_offsets(self): return True
+    def get_show_offsets(self): return self.show_offsets
 
     def select_prime(self, x, y):
         self.select_primed = x, y
@@ -265,6 +293,7 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
         button2 = event.state & gtk.gdk.BUTTON2_MASK
         button3 = event.state & gtk.gdk.BUTTON3_MASK
         shift = event.state & gtk.gdk.SHIFT_MASK
+        cancel = bool(self.lathe_option and not self.current_view == 'p')
         if button1 and self.select_primed:
             x, y = self.select_primed
             distance = max(abs(event.x - x), abs(event.y - y))
@@ -272,7 +301,7 @@ class Gremlin(gtk.gtkgl.widget.DrawingArea, glnav.GlNavBase,
         if button1 and not self.select_primed:
             if shift:
                 self.translateOrRotate(event.x, event.y)
-            else:
+            elif not cancel:
                 self.rotateOrTranslate(event.x, event.y)
         elif button2:
             self.translateOrRotate(event.x, event.y)
