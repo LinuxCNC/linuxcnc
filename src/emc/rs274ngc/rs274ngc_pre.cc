cc829033bbb93ece1089334764b2b121f6164d75
Chris Radek <chris@timeguy.com>
2012-04-19 09:20:33 -0500

    explicitly set default spindle mode

--- src/emc/rs274ngc/rs274ngc_pre.cc
+++ src/emc/rs274ngc/rs274ngc_pre.cc
@@ -577,6 +577,7 @@ int Interp::init()
   _setup.sequence_number = 0;   /*DOES THIS NEED TO BE AT TOP? */
 //_setup.speed set in Interp::synch
   _setup.speed_feed_mode = CANON_INDEPENDENT;
+  _setup.spindle_mode = CONSTANT_RPM;
 //_setup.speed_override set in Interp::synch
 //_setup.spindle_turning set in Interp::synch
 //_setup.stack does not need initialization
