0c8ad2c418795c5aeefcc45c0b351e12644c9c5a
Michael Haberler <git@mah.priv.at>
2012-04-22 17:42:01 +0200

    interp: firm up O-word handling
    The following statements now cause an error message during parsing:
    1. a 'return' or 'endsub' not within a sub defintion
    2. a label on 'repeat' which is defined elsewhere
    3. a label on 'while' which is defińed elsewhere and not referring to a 'do'
    4. a label on 'if' defined elsewhere
    5. a undefined label on 'else' or 'elseif'
    6. a label on 'else', 'elseif' or 'endif' not pointing to a matching 'if'
    7. a label on 'break' or 'continue' which does not point to a matching 'while' or 'do'
    8. a label on 'endrepeat' or 'endwhile' no referring to a corresponding 'while' or 'repeat'
    
    These situations now abort the interpreter. To make these errors non-fatal
    warnings on stderr, set bit 0x20 in the [RS274NGC]FEATURE= mask. I rather
    recommend you fix your programs though.
    
    see src/emc/rs274ngc/interp_internal.hh
    
    This is mostly patch 7d918c.
    
    It is now ok to reintroduce it because due to commit 1c5aea the interpreter is now the only
    source of linenumber information, and serial execution of the same command always
    results in the same linenumber. This was not the case with the pseudoMdiLineNumber
    workaround, which prevented proper matching of labels during MDI.

--- src/emc/rs274ngc/interp_o_word.cc
+++ src/emc/rs274ngc/interp_o_word.cc
@@ -640,10 +640,7 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 				      setup_pointer settings)  // pointer to machine settings
 {
     int status = INTERP_OK;
-    // block_pointer eblock = &EXECUTING_BLOCK(*settings);
-    // block_pointer cblock = &CONTROLLING_BLOCK(*settings);
     context_pointer current_frame;
-
     offset_pointer op = NULL;
 
     logOword("convert_control_functions %s", o_ops[block->o_type]);
@@ -703,6 +700,16 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 
     case O_endsub:
     case O_return:
+
+	if  ((settings->call_level == 0) && 
+	     (settings->sub_name == NULL)) {
+	    // detect a standalone 'o<label> return|endsub'
+	    OERR(_("%d: not in a subroutine definition: '%s'"),
+		 settings->sequence_number, settings->linetext);
+	} 
+
+	// proper label semantics (only refer to defined sub, within sub defn etc)
+	// is handled in read_o() for return & endsub
 	current_frame = &settings->sub_context[settings->call_level];
 	CHP(execute_return(settings, current_frame,
 			   current_frame->call_type)); 
@@ -732,6 +739,14 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	break;
 
     case O_repeat:
+	if (control_find_oword(block, settings, &op) == INTERP_OK) {
+	    if (settings->sequence_number != (op->sequence_number + 1)) 
+	        OERR(_("%d: duplicate O-word label: '%s' - defined in line %d"),
+		    settings->sequence_number, 
+		    settings->linetext, op->sequence_number + 1);
+	} else 
+	    CHP(control_save_offset(block, settings));
+
 	// if we were skipping, no longer
 	settings->skipping_o = NULL;
 	status = control_find_oword(block, settings, &op); // &index);
@@ -773,6 +788,16 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	break;
 
     case O_while:
+	if (control_find_oword(block, settings, &op) == INTERP_OK) {
+	    if ((op->type != O_do) &&
+		(settings->sequence_number != (op->sequence_number + 1)))
+	        OERR(_("%d: duplicate O-word label: '%s' - defined in line %d"),
+		    settings->sequence_number, 
+		    settings->linetext, op->sequence_number + 1);
+	} else 
+	    // record only if this is a while/endwhile loop
+	    CHP(control_save_offset(block, settings));
+
 	// if we were skipping, no longer
 	settings->skipping_o = NULL;
 	status = control_find_oword(block, settings, &op); //  &index);
@@ -815,7 +840,16 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	}
 	break;
 
+	    
     case O_if:
+	if (control_find_oword(block, settings, &op) == INTERP_OK) {
+	    if (settings->sequence_number != (op->sequence_number + 1)) 
+	        OERR(_("%d: duplicate O-word label - already defined in line %d: '%s'"),
+		    settings->sequence_number, op->sequence_number + 1,
+		    settings->linetext);
+	} else 
+	    CHP(control_save_offset(block, settings));
+
 	if (settings->test_value != 0.0) {
 	    //true
 	    logOword("executing forward: [%s] in 'if'",
@@ -833,6 +867,14 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	break;
 
     case O_elseif:
+	if (control_find_oword(block, settings, &op) != INTERP_OK) 
+	    OERR(_("%d: undefined O-word label: '%s'"),
+		settings->sequence_number, settings->linetext);
+	if (op->type != O_if)
+	    OERR(_("%d: no matching 'if' label: '%s' (found '%s' in line %d)"),
+		settings->sequence_number, settings->linetext, 
+		o_ops[op->type] + 2, op->sequence_number + 1);
+
 	if ((settings->skipping_o) &&
 	    (0 != strcmp(settings->skipping_o, block->o_name)))  {
 
@@ -883,6 +925,14 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	break;
 
     case O_else:
+	if (control_find_oword(block, settings, &op) != INTERP_OK) 
+	    OERR(_("%d: undefined O-word label: '%s'"),
+		settings->sequence_number, settings->linetext);
+	if (op->type != O_if)
+	    OERR(_("%d: no matching 'if' label: '%s' (found '%s' in line %d)"),
+		settings->sequence_number, settings->linetext, 
+		o_ops[op->type] + 2, op->sequence_number + 1);
+
 	// were we ever not skipping
 	if (settings->executed_if) {
 	    // we have already executed, skip
@@ -909,6 +959,14 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	break;
 
     case O_endif:
+	if (control_find_oword(block, settings, &op) != INTERP_OK) 
+	    OERR(_("%d: undefined O-word label: '%s'"),
+		settings->sequence_number, settings->linetext);
+	if (op->type != O_if)
+	    OERR(_("%d: no matching label: '%s' (found '%s' in line %d): '%s'"),
+		settings->sequence_number, block->o_name, 
+		o_ops[op->type] + 2, op->sequence_number + 1, settings->linetext);
+
 	// stop skipping if we were
 	settings->skipping_o = NULL;
 	logOword("stop skipping forward: [%s] in 'endif'",
@@ -919,6 +977,14 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 	break;
 
     case O_break:
+	if (control_find_oword(block, settings, &op) != INTERP_OK) 
+	    OERR(_("%d: undefined O-word label: '%s'"),
+		settings->sequence_number, settings->linetext);
+	if ((op->type != O_while) && (op->type != O_do))
+	    OERR(_("%d: no matching while/do label: '%s' (found '%s' in line %d)"),
+		settings->sequence_number, settings->linetext, 
+		o_ops[op->type] + 2, op->sequence_number + 1);
+
       // start skipping
       settings->skipping_o = block->o_name;
       settings->skipping_start = settings->sequence_number;
@@ -928,6 +994,14 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
       break;
 
     case O_continue:
+	if (control_find_oword(block, settings, &op) != INTERP_OK) 
+	    OERR(_("%d: undefined O-word label: '%s'"),
+		settings->sequence_number, settings->linetext);
+	if ((op->type != O_while) && (op->type != O_do))
+	    OERR(_("%d: no matching while/do label: '%s' (found '%s' in line %d)"),
+		settings->sequence_number, settings->linetext, 
+		o_ops[op->type] + 2, op->sequence_number + 1);
+
 	// if already skipping, do nothing
 	if ((settings->skipping_o) &&
 	    (0 == strcmp(settings->skipping_o, block->o_name))) {
@@ -945,6 +1019,15 @@ int Interp::convert_control_functions(block_pointer block, // pointer to a block
 
     case O_endrepeat:
     case O_endwhile:
+	if (control_find_oword(block, settings, &op) != INTERP_OK) 
+	    OERR(_("%d: undefined O-word label: '%s'"),
+		settings->sequence_number, settings->linetext);
+	if (((block->o_type == O_endrepeat) && (op->type != O_repeat)) ||
+	    ((block->o_type == O_endwhile) && (op->type != O_while)))
+	    OERR(_("%d: no matching label: '%s' (found '%s' in line %d)"),
+		settings->sequence_number, settings->linetext, 
+		o_ops[op->type] + 2, op->sequence_number + 1);
+
 	// end of a while loop
 	logOword("endwhile: skipping_o:%s", settings->skipping_o);
 	if ((settings->skipping_o) &&
