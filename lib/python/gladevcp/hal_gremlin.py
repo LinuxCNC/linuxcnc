5bd2280d684fe5c9dda3605f6ef32e44fe9df9d0
Chris Morley <chrisinnanaimo@hotmail.com>
2012-06-20 21:52:19 -0700

    gremlin -fix error caused from grid size missing
    additions to AXIS to support grids were not added
    to gremlin -so we add grid_size to fix this.

--- lib/python/gladevcp/hal_gremlin.py
+++ lib/python/gladevcp/hal_gremlin.py
@@ -53,6 +53,8 @@ class HAL_Gremlin(gremlin.Gremlin, _EMC_ActionBase):
                     True, gobject.PARAM_READWRITE | gobject.PARAM_CONSTRUCT),
         'show_dtg' : ( gobject.TYPE_BOOLEAN, 'Show DTG', 'Show Distance To Go',
                     True, gobject.PARAM_READWRITE | gobject.PARAM_CONSTRUCT),
+        'grid_size' : ( gobject.TYPE_FLOAT, 'Grid Size', 'Grid Size',
+                    0, 100, 0, gobject.PARAM_READWRITE | gobject.PARAM_CONSTRUCT),
         'use_joints_mode' : ( gobject.TYPE_BOOLEAN, 'Use joints mode', 'Use joints mode',
                     False, gobject.PARAM_READWRITE | gobject.PARAM_CONSTRUCT),
     }
