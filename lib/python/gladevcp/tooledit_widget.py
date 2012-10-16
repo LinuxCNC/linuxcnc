8ef50b16a74e0991cbe8d4645934dc48d731369c
Chris Morley <chrisinnanaimo@hotmail.com>
2012-10-15 21:48:00 -0700

    gladevcp -update tooledit widget
    change some method names for clarity
    add comments

--- lib/python/gladevcp/tooledit_widget.py
+++ lib/python/gladevcp/tooledit_widget.py
@@ -40,38 +40,46 @@ class ToolEdit(gtk.VBox):
         self.font="sans 12"
         self.wTree = gtk.Builder()
         self.wTree.add_from_file(os.path.join(datadir, "tooledit_gtk.glade") )
+        # connect the signals from Glade
         dic = {
             "on_delete_clicked" : self.delete,
             "on_add_clicked" : self.add,
             "on_reload_clicked" : self.reload,
             "on_save_clicked" : self.save,
-            "on_style_clicked" : self.style,
+            "on_style_clicked" : self.display_toggle,
             "cell_toggled" : self.toggled
             }
         self.wTree.connect_signals( dic )
         renderer = self.wTree.get_object("cell_toggle0")
         renderer.set_property('activatable', True)
+        # these signals include column data so must be made here instead of in Glade
+        # for view 2
         temp = "cell_tool#","cell_pos","cell_x","cell_y","cell_z","cell_a","cell_b","cell_c","cell_u","cell_v","cell_w","cell_d", \
                 "cell_front","cell_back","cell_orient","cell_comments"
         for col,name in enumerate(temp):
             #print name,col
             renderer = self.wTree.get_object(name)
             renderer.connect( 'edited', self.col_editted, col+1 )
+        # for view 1
         temp =[ ("cell_tool#1",1),("cell_pos1",2),("cell_x1",3),("cell_z1",5),("cell_d1",12),("cell_front1",13),("cell_back1",14), \
                 ("cell_orient1",15), ("cell_comments1",16)]
         for name,col in temp:
             renderer = self.wTree.get_object(name)
             renderer.connect( 'edited', self.col_editted, col )
+
+        # global references
         self.model = self.wTree.get_object("liststore1")
         self.all_window = self.wTree.get_object("all_window")
         self.lathe_window = self.wTree.get_object("lathe_window")
         self.view1 = self.wTree.get_object("treeview1")
         self.view2 = self.wTree.get_object("treeview2")
+        # reparent tooledit box from Glades tp level window to tooledit's VBox
         window = self.wTree.get_object("tooledit_box")
         window.reparent(self)
+        # If the toolfile was specified when tooledit was created load it
         if toolfile:
             self.reload(None)
-        self.change_display(0)
+        self.set_display(0)
 
     def delete(self,widget):
         liststore  = self.model
@@ -91,14 +99,14 @@ class ToolEdit(gtk.VBox):
         self.model.append(data)
         self.num_of_col +=1
 
+        # this is for adding a filename path after the tooleditor is already loaded.
     def set_filename(self,filename):
         self.toolfile = filename
         self.reload(None)
 
-    def reload(self,widget):
         # Reload the tool file into display
+    def reload(self,widget):
         # clear the current liststore, search the tool file, and add each tool
-        #
         if self.toolfile == None:return
         self.model.clear()
         print "toolfile:",self.toolfile
@@ -114,6 +122,9 @@ class ToolEdit(gtk.VBox):
             line = rawline.rstrip(comment)
             array = [0,0,0,'0','0','0','0','0','0','0','0','0','0','0','0','0',comment]
             # search beginning of each word for keyword letters
+            # offset 0 is the checkbutton so ignore it
+            # if i = ';' that is the comment and we have already added it
+            # offset 1 and 2 are integers the rest floats
             for offset,i in enumerate(KEYWORDS):
                 if offset == 0 or i == ';': continue
                 for word in line.split():
@@ -150,17 +161,27 @@ class ToolEdit(gtk.VBox):
                     line = line + "%s%d "%(KEYWORDS[num], i)
             print >>file,line
             print line
+        # tell linuxcnc we changed the tool table entries
         try:
             linuxcnc.command().load_tool_table()
         except:
             print "Reloading tooltable into linuxcnc failed"
 
-    def style(self,widget):
-        self.display_type = (self.display_type * -1) +1
-        self.change_display(self.display_type)
 
-    def change_display(self,value):
-        if value == 0:#self.all_window.flags() & gtk.VISIBLE:
+        # This is for changing the display after tool editor was loaded using tye style button
+        # note that it toggles the display
+    def display_toggle(self,widget=None):
+        value = (self.display_type * -1) +1
+        self.set_display(value)
+
+        # There is an 'everything' display and a 'lathe' display
+        # the same model is used in each case just the listview is different
+        # does the actual display change by hiding what we don't want
+        # for whatever reason I have to hide/show both the view and it's container
+    def set_display(self,value):
+        self.display_type = value
+        #if self.all_window.flags() & gtk.VISIBLE:
+        if value == 0:
             self.lathe_window.show()
             self.view1.show()
             self.all_window.hide()
@@ -171,9 +192,11 @@ class ToolEdit(gtk.VBox):
             self.all_window.show()
             self.view2.show()
 
+        # not done yet should change the font of the text
     def set_font(self,value):
         pass
 
+        # depending what is editted add the right type of info integer,float or text
     def col_editted(self, widget, path, new_text, col):
         if col in(1,2):
             self.model[path][col] = int(new_text)
@@ -183,10 +206,12 @@ class ToolEdit(gtk.VBox):
             self.model[path][col] = (new_text)
         print new_text, col
 
+        # this makes the checkboxes actually update
     def toggled(self, widget, path):
         model = self.model
         model[path][0] = not model[path][0]
 
+        # standard Gobject method
     def do_get_property(self, property):
         name = property.name.replace('-', '_')
         if name in self.__gproperties.keys():
@@ -194,6 +219,10 @@ class ToolEdit(gtk.VBox):
         else:
             raise AttributeError('unknown property %s' % property.name)
 
+        # standard Gobject method
+        # changing tye Gobject property 'display_type' will actually change the display
+        # This is so the the Glade editor you can change the display
+        # Note this sets the display absolutely vrs display_toggle method that toggles the display
     def do_set_property(self, property, value):
         name = property.name.replace('-', '_')
         if name == 'font':
@@ -201,14 +230,17 @@ class ToolEdit(gtk.VBox):
         if name == 'display_type':
             print value
             try:
-                self.display_type = value
-                self.change_display(value)
+                self.set_display(value)
             except:
                 pass
 
 
 # for testing without glade editor:
-def main(filename):
+# for what ever reason tooledit always shows both display lists,
+# in the glade editor it shows only one at a time (as it should)
+# you can specify a tool table file at the command line
+# or uncomment the line and set the path correctly.
+def main(filename=None):
     window = gtk.Dialog("My dialog",
                    None,
                    gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
@@ -227,7 +259,8 @@ def main(filename):
        print "False"
 
 if __name__ == "__main__":
+    # if there arw two arguments then specify the path
     if len(sys.argv) > 1: main(sys.argv[1])
-    else: main(None)
+    else: main()
     
     
