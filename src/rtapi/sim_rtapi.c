27bbd08cf796f363a5369f07f0c094f9d2475972
Michael Haberler <git@mah.priv.at>
2012-11-01 22:03:48 +0100

    rtapi: carry over xenomai userthread support from xenomai-integration-mah

--- src/rtapi/sim_rtapi.c
+++ src/rtapi/sim_rtapi.c
@@ -14,7 +14,20 @@
 #include <stdio.h>		/* vprintf() */
 #include <stdlib.h>		/* malloc(), sizeof() */
 #include <stdarg.h>		/* va_* */
+#include "config.h"
+
+#if defined(RTAPI_XENOMAI_USER)
+#include <native/task.h>        /* Xenomai task */
+#include <native/timer.h>
+#include <native/mutex.h>
+#include <rtdk.h>
+#include <nucleus/types.h>     /* for XNOBJECT_NAME_LEN */
+
+#include <sys/io.h>             /* inb, outb */
+#endif
+#if defined(RTAPI_POSIX)
 #include <pth.h>		/* pth_uctx_* */
+#endif
 #include <unistd.h>		/* usleep() */
 #include <sys/ipc.h>		/* IPC_* */
 #include <sys/shm.h>		/* shmget() */
@@ -34,7 +47,14 @@ struct rtapi_module {
 struct rtapi_task {
   int magic;			/* to check for valid handle */
   int owner;
+#if defined(RTAPI_XENOMAI)
+  RT_TASK ctx;		/* Xenomai task */
+  char name[XNOBJECT_NAME_LEN];
+#endif
+#if defined(RTAPI_POSIX)
   pth_uctx_t ctx;		/* thread's context */
+#endif
+  int uses_fp;
   size_t stacksize;
   int prio;
   int period;
@@ -44,8 +64,10 @@ struct rtapi_task {
 };
 
 static struct timeval schedule;
+#if !defined(RTAPI_XENOMAI_USER)
 static int base_periods;
 static pth_uctx_t main_ctx, this_ctx;
+#endif
 
 #define MODULE_MAGIC  30812
 #define TASK_MAGIC    21979	/* random numbers used as signatures */
@@ -58,6 +80,43 @@ static pth_uctx_t main_ctx, this_ctx;
 /* data for all tasks */
 static struct rtapi_task task_array[MAX_TASKS] = {{0},};
 
+#if defined(RTAPI_XENOMAI_USER)
+//FIXME do this once for xenomai (see xenomai_*c)
+
+// Xenomai rt_task priorities are 0: lowest .. 99: highest
+int rtapi_prio_highest(void)
+{
+  return 99;
+}
+
+int rtapi_prio_lowest(void)
+{
+  return 0;
+}
+
+int rtapi_prio_next_higher(int prio)
+{
+  /* return a valid priority for out of range arg */
+  if (prio >= rtapi_prio_highest())
+    return rtapi_prio_highest();
+  if (prio <= rtapi_prio_lowest())
+    return rtapi_prio_lowest();
+
+  /* return next higher priority for in-range arg */
+  return prio + 1;
+}
+
+int rtapi_prio_next_lower(int prio)
+{
+  /* return a valid priority for out of range arg */
+  if (prio <= rtapi_prio_lowest())
+    return rtapi_prio_lowest();
+  if (prio >= rtapi_prio_highest())
+    return rtapi_prio_highest();
+  /* return next lower priority for in-range arg */
+  return prio - 1;
+}
+#else
 /* Priority functions.  SIM uses 0 as the highest priority, as the
 number increases, the actual priority of the task decreases. */
 
@@ -93,6 +152,7 @@ int rtapi_prio_next_lower(int prio)
   /* return next lower priority for in-range arg */
   return prio + 1;
 }
+#endif
 
 static int period = 0;
 int rtapi_clock_set_period(unsigned long int nsecs)
@@ -127,8 +187,13 @@ int rtapi_task_new(void (*taskcode) (void*), void *arg,
   task = &(task_array[n]);
 
   /* check requested priority */
+#if defined(RTAPI_XENOMAI_USER)
+  if ((prio < rtapi_prio_lowest()) || (prio > rtapi_prio_highest()))
+    return -EINVAL;
+#if defined(RTAPI_POSIX)
   if ((prio < rtapi_prio_highest()) || (prio > rtapi_prio_lowest()))
     return -EINVAL;
+#endif
 
   /* label as a valid task structure */
   /*! \todo FIXME - end of non-threadsafe window */
@@ -140,6 +205,9 @@ int rtapi_task_new(void (*taskcode) (void*), void *arg,
   task->stacksize = stacksize;
   task->taskcode = taskcode;
   task->prio = prio;
+  task->uses_fp = uses_fp;
+  strncpy(task->name, name, sizeof(task->name));
+  task->name[sizeof(task->name) - 1]= '\0';
 
   /* and return handle to the caller */
 
@@ -149,6 +217,7 @@ int rtapi_task_new(void (*taskcode) (void*), void *arg,
 
 int rtapi_task_delete(int id) {
   struct rtapi_task *task;
+  int retval;
 
   if(id < 0 || id >= MAX_TASKS) return -EINVAL;
 
@@ -157,8 +226,17 @@ int rtapi_task_delete(int id) {
   if (task->magic != TASK_MAGIC)
     return -EINVAL;
 
+  rtapi_print_msg(RTAPI_MSG_DBG, "rt_task_delete %d \"%s\"\n", id, task->name );
+
+#if defined(RTAPI_XENOMAI_USER)
+  if ((retval = rt_task_delete( &task->ctx )) < 0) {
+      rtapi_print_msg(RTAPI_MSG_ERR,"ERROR: rt_task_delete() = %d %s\n", retval, strerror(retval));
+      return retval;
+  }
+#endif
+#if defined(RTAPI_POSIX)
   pth_uctx_destroy(task->ctx);
-  
+#endif
   task->magic = 0;
   return 0;
 }
@@ -167,18 +245,28 @@ int rtapi_task_delete(int id) {
 static void wrapper(void *arg)
 {
   struct rtapi_task *task;
+  int retval;
 
   /* use the argument to point to the task data */
   task = (struct rtapi_task*)arg;
   if(task->period < period) task->period = period;
   task->ratio = task->period / period;
-  rtapi_print_msg(RTAPI_MSG_INFO, "task %p period = %d ratio=%d\n",
-	  task, task->period, task->ratio);
+  rtapi_print_msg(RTAPI_MSG_DBG, "task %p '%s' period=%d prio=%d ratio=%d\n",
+		  task, task->name, task->prio, task->period, task->ratio);
+
+#if  defined(RTAPI_XENOMAI_USER)
+  if ((retval = rt_task_set_mode(T_WARNSW, 0, NULL)) < 0) {
+      rtapi_print_msg(RTAPI_MSG_ERR,"ERROR: rt_task_set_mode() = %d %s\n", retval, strerror(retval));
+  }
+  if ((retval = rt_task_set_periodic(NULL, TM_NOW, task->period * task->ratio)) < 0) {
+      rtapi_print_msg(RTAPI_MSG_ERR,"ERROR: rt_task_set_periodic() = %d %s\n", retval, strerror(retval));
+  }
+#endif
 
   /* call the task function with the task argument */
   (task->taskcode) (task->arg);
 
-  rtapi_print("ERROR: reached end of wrapper for task %d\n", (int)(task - task_array));
+  rtapi_print_msg(RTAPI_MSG_ERR,"ERROR: reached end of wrapper for task %d\n", (int)(task - task_array));
 }
 
 
@@ -201,6 +289,33 @@ int rtapi_task_start(int task_id, unsigned long int period_nsec)
 
   /* create the thread - use the wrapper function, pass it a pointer
      to the task structure so it can call the actual task function */
+#if defined(RTAPI_XENOMAI_USER)
+  rtapi_print_msg(RTAPI_MSG_DBG, "rt_task_create %d \"%s\"\n", task_id, task->name );
+
+  // http://www.xenomai.org/documentation/trunk/html/api/group__task.html#ga03387550693c21d0223f739570ccd992
+  // Passing T_FPU|T_CPU(1) in the mode parameter thus creates a 
+  // task with FPU support enabled and which will be affine to CPU #1
+  // the task will start out dormant; execution begins with rt_task_start()
+
+  // not sure T_CPU(1) is possible - see:
+  // http://www.xenomai.org/pipermail/xenomai-help/2010-09/msg00081.html
+
+  // since this is a usermode RT task, it will be FP anyway
+  retval = rt_task_create (&task->ctx, task->name, task->stacksize, task->prio, T_FPU);
+  if (retval != 0 )
+  {
+    rtapi_print_msg(RTAPI_MSG_ERR, "rt_task_create failed, rc = %d\n", retval );
+    return -ENOMEM;
+  }
+
+  retval = rt_task_start( &task->ctx, wrapper, (void*)task );
+  if( retval )
+  {
+	rtapi_print_msg(RTAPI_MSG_INFO, "rt_task_start failed, rc = %d\n", retval );
+        return -ENOMEM;
+  }
+#endif
+#if defined(RTAPI_POSIX)
   retval = pth_uctx_create(&task->ctx);
   if (retval == FALSE)
     return -ENOMEM;
@@ -208,6 +323,7 @@ int rtapi_task_start(int task_id, unsigned long int period_nsec)
 	  wrapper, (void*)task, 0);
   if (retval == FALSE)
     return -ENOMEM;
+#endif
 
   return 0;
 }
@@ -216,6 +332,7 @@ int rtapi_task_start(int task_id, unsigned long int period_nsec)
 int rtapi_task_stop(int task_id)
 {
   struct rtapi_task *task;
+  int retval;
   if(task_id < 0 || task_id >= MAX_TASKS) return -EINVAL;
     
   task = &task_array[task_id];
@@ -224,8 +341,15 @@ int rtapi_task_stop(int task_id)
   if (task->magic != TASK_MAGIC)
     return -EINVAL;
 
+#if defined(RTAPI_XENOMAI)
+ if ((retval = rt_task_delete( &task->ctx )) < 0) {
+     rtapi_print_msg(RTAPI_MSG_ERR,"rt_task_delete() = %d\n", retval);
+     return retval;
+ }
+#endif
+#if defined(RTAPI_POSIX)
   pth_uctx_destroy(task->ctx);
-
+#endif
   return 0;
 }
 
@@ -240,7 +364,11 @@ int rtapi_task_pause(int task_id)
   if (task->magic != TASK_MAGIC)
     return -EINVAL;
 
+#if defined(RTAPI_XENOMAI_USER)
+  return rt_task_suspend( &task->ctx );
+#else
   return -ENOSYS;
+#endif
 }
 
 int rtapi_task_resume(int task_id)
@@ -254,7 +382,11 @@ int rtapi_task_resume(int task_id)
   if (task->magic != TASK_MAGIC)
     return -EINVAL;
 
+#if defined(RTAPI_XENOMAI_USER)
+  return rt_task_resume( &task->ctx );
+#else
   return -ENOSYS;
+#endif
 }
 
 
@@ -272,24 +404,41 @@ int rtapi_task_set_period(int task_id,
 
   task->period = period_nsec;
 
+#if defined(RTAPI_XENOMAI_USER)
+  return rt_task_set_periodic(&task->ctx, TM_NOW, task->period * task->ratio );
+#else
   return 0;
+#endif
 }
 
 int rtapi_wait(void)
 {
+#if defined(RTAPI_XENOMAI_USER)
+  return rt_task_wait_period(NULL);
+#endif
+#if defined(RTAPI_POSIX)
   pth_uctx_switch(this_ctx, main_ctx);
+#endif
   return 0;
 }
 
 
 void rtapi_outb(unsigned char byte, unsigned int port)
 {
+#if defined(RTAPI_XENOMAI_USER)
+  outb(byte, port);
+#else
   return;
+#endif
 }
 
 unsigned char rtapi_inb(unsigned int port)
 {
+#if defined(RTAPI_XENOMAI_USER)
+  return inb(port);
+#else
   return 0;
+#endif
 }
 
 long int simple_strtol(const char *nptr, char **endptr, int base) {
@@ -298,6 +447,7 @@ long int simple_strtol(const char *nptr, char **endptr, int base) {
 
 #define MIN_RUNS 13
 
+#if ! defined(RTAPI_XENOMAI_USER)
 static int maybe_sleep(int fd) {
     struct timeval now;
     struct timeval interval;
@@ -376,6 +526,19 @@ int sim_rtapi_run_threads(int fd) {
 	}
     }
 }
+#endif
 
+#if defined(RTAPI_XENOMAI_USER)
+long long rtapi_get_time(void)
+{
+    RTIME now = rt_timer_read();
+    return (long long) now;
+}
+
+long long rtapi_get_clocks(void)
+{
+ return (long long) rt_timer_tsc();
+}
+#endif
 
 #include "rtapi/sim_common.h"
