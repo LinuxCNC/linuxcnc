f2ebffdde8f6cb1070e58a12af0456004cee0da3
Chris Morley <chrisinnanaimo@hotmail.com>
2012-11-04 01:35:56 -0700

    glcanon -pull out DRO formatting into it's own method
    This allows HAL_gremlin to override it, so it can show a different
    DRO without channging AXIS's

--- lib/python/rs274/glcanon.py
+++ lib/python/rs274/glcanon.py
@@ -1194,8 +1194,6 @@ class GlCanonDraw:
                 ypos -= linespace
                 i = i + 1
         if self.get_show_offsets():
-            if self.is_lathe():
-                homed.insert(0,homed[0])
             i=0
             for string in droposstrs:
                 maxlen = max(maxlen, len(string))
@@ -1293,6 +1291,8 @@ class GlCanonDraw:
             g5x_offset = self.to_internal_units(s.g5x_offset)
             g92_offset = self.to_internal_units(s.g92_offset)
             tlo_offset = self.to_internal_units(s.tool_offset)
+            dtg = self.to_internal_linear_unit(s.distance_to_go)
+            spd = self.to_internal_linear_unit(s.current_vel)
 
             if self.get_show_metric():
                 positions = self.from_internal_units(positions, 1)
@@ -1300,19 +1300,31 @@ class GlCanonDraw:
                 g5x_offset = self.from_internal_units(g5x_offset, 1)
                 g92_offset = self.from_internal_units(g92_offset, 1)
                 tlo_offset = self.from_internal_units(tlo_offset, 1)
+                dtg *= 25.4
+                spd = spd * 25.4
+            spd = spd * 60
+            limit, homed, posstrs, droposstrs = self.dro_format(self.stat,spd,dtg,limit,homed,positions,axisdtg,g5x_offset,g92_offset,tlo_offset)
+        else:
+            # N.B. no conversion here because joint positions are unitless
+            posstrs = ["  %s:% 9.4f" % i for i in
+                zip(range(self.get_num_joints()), s.joint_actual_position)]
+            droposstrs = posstrs
+        return limit, homed, posstrs, droposstrs
+
+    def dro_format(self,s,spd,dtg,limit,homed,positions,axisdtg,g5x_offset,g92_offset,tlo_offset):
+            if self.get_show_metric():
+                positions = self.from_internal_units(positions, 1)
+                axisdtg = self.from_internal_units(axisdtg, 1)
+                g5x_offset = self.from_internal_units(g5x_offset, 1)
+                g92_offset = self.from_internal_units(g92_offset, 1)
+                tlo_offset = self.from_internal_units(tlo_offset, 1)
                 format = "% 6s:% 9.3f"
-                if self.get_show_distance_to_go():
-                    droformat = " " + format + "  DTG %1s:% 9.3f"
-                else:
-                    droformat = " " + format
+                droformat = " " + format + "  DTG %1s:% 9.3f"
                 offsetformat = "% 5s %1s:% 9.3f  G92 %1s:% 9.3f"
                 rotformat = "% 5s %1s:% 9.3f"
             else:
                 format = "% 6s:% 9.4f"
-                if self.get_show_distance_to_go():
-                    droformat = " " + format + "  DTG %1s:% 9.4f"
-                else:
-                    droformat = " " + format
+                droformat = " " + format + "  DTG %1s:% 9.4f"
                 offsetformat = "% 5s %1s:% 9.4f  G92 %1s:% 9.4f"
                 rotformat = "% 5s %1s:% 9.4f"
             diaformat = " " + format
@@ -1323,10 +1335,8 @@ class GlCanonDraw:
                 a = "XYZABCUVW"[i]
                 if s.axis_mask & (1<<i):
                     posstrs.append(format % (a, positions[i]))
-                    if self.get_show_distance_to_go():
-                        droposstrs.append(droformat % (a, positions[i], a, axisdtg[i]))
-                    else:
-                        droposstrs.append(droformat % (a, positions[i]))
+                    droposstrs.append(droformat % (a, positions[i], a, axisdtg[i]))
+
             droposstrs.append("")
 
             for i in range(9):
@@ -1351,39 +1361,16 @@ class GlCanonDraw:
             if self.is_lathe():
                 posstrs[0] = format % ("Rad", positions[0])
                 posstrs.insert(1, format % ("Dia", positions[0]*2.0))
-                if self.get_show_distance_to_go():
-                    droposstrs[0] = droformat % ("Rad", positions[0], "R", axisdtg[0])
-                    droposstrs.insert(1, droformat % ("Dia", positions[0]*2.0, "D", axisdtg[0]*2.0))
-                else:
-                    droposstrs[0] = droformat % ("Rad", positions[0])
-                    droposstrs.insert(1, diaformat % ("Dia", positions[0]*2.0))
+                droposstrs[0] = droformat % ("Rad", positions[0], "R", axisdtg[0])
+                droposstrs.insert(1, diaformat % ("Dia", positions[0]*2.0))
 
             if self.get_show_machine_speed():
-                spd = self.to_internal_linear_unit(s.current_vel)
-                if self.get_show_metric():
-                    spd = spd * 25.4 * 60
-                else:
-                    spd = spd * 60
                 posstrs.append(format % ("Vel", spd))
-                pos=0
-                for i in range(9):
-                    if s.axis_mask & (1<<i): pos +=1
-                if self.is_lathe():
-                    pos +=1
-                droposstrs.insert(pos, " " + format % ("Vel", spd))
 
             if self.get_show_distance_to_go():
-                dtg = self.to_internal_linear_unit(s.distance_to_go)
-                if self.get_show_metric():
-                    dtg *= 25.4
                 posstrs.append(format % ("DTG", dtg))
-        else:
-            # N.B. no conversion here because joint positions are unitless
-            posstrs = ["  %s:% 9.4f" % i for i in
-                zip(range(self.get_num_joints()), s.joint_actual_position)]
-            droposstrs = posstrs
-        return limit, homed, posstrs, droposstrs
 
+            return limit, homed, posstrs, droposstrs
 
     def draw_small_origin(self, n):
         glNewList(n, GL_COMPILE)
