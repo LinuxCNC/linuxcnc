6f62a5f1a54ba44a172087f66acb0cc6c16775bf
Chris Morley <chrisinnanaimo@hotmail.com>
2012-11-17 11:44:00 -0800

    gscreen config -add a very custom screen
    This was gaxis - a screen created by Pavel.
    Since Pavel was a major force getting gladeVCP to what it is
    this seems fitting.
    It's not a full featured screen but functional

---
+++ configs/sim/gscreen_custom/gaxis_handler.py
@@ -0,0 +1,41 @@
+# this is a handler file for using Gscreen's infastructure
+# to load a completely custom glade screen
+# The only things that really matters is that it's saved as a GTK builder project,
+# the toplevel window is caller window1 (The default name) and you connect A destroy
+# window signal else you can't close down linuxcnc 
+class HandlerClass:
+
+    # this will be pretty standard to gain access to everything
+    # In this case we didn't specify any signals in glade so we
+    # don't need any callbacks here
+    def __init__(self, halcomp,builder,useropts,gscreen):
+            self.nhits = 0
+            self.emc = gscreen.emc
+            self.data = gscreen.data
+            self.widgets = gscreen.widgets
+            self.gscreen = gscreen
+
+# This connects siganals without using glade
+# in this case to destroy the window
+# it calls the method in gscreen
+def connect_signals(gscreen):
+    signal_list = [ ["window1","destroy", "on_window1_destroy"],
+                    ]
+    for i in signal_list:
+        gscreen.widgets[i[0]].connect(i[1], gscreen[i[2]])
+
+
+# you could initialize your widgets here
+# overwise add the pass command so gscreen doesn't try to initialize
+# the regular widgets
+def initialize_widgets(gscreen):
+    pass
+
+# every 50 milli seconds this gets called
+# add pass so gscreen does update it's regular widgets
+def periodic(gscreen):
+    pass
+
+# standard handler call
+def get_handlers(halcomp,builder,useropts,gscreen):
+     return [HandlerClass(halcomp,builder,useropts,gscreen)]
