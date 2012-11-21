438cac9f106186ca8040a223caf1648624790b86
Michael Haberler <git@mah.priv.at>
2012-11-21 22:28:08 +0100

    rtapi: factor out msg code into a single copy for userthreads styles

--- src/rtapi/linux_common.c
+++ src/rtapi/linux_common.c
@@ -6,8 +6,6 @@
 #include "rtapi.h"
 #include "rtapi_common.h"
 
-static int msg_level = RTAPI_MSG_INFO;	/* message printing level */ //XXX
-
 #include <sys/ipc.h>		/* IPC_* */
 #include <sys/shm.h>		/* shmget() */
 #include <pthread.h>
@@ -180,80 +178,6 @@ int rtapi_shmem_delete(int handle, int module_id)
 	return 0;
 }
 
-#define PRINTBUF_LEN	1024
-
-void default_rtapi_msg_handler(msg_level_t level, const char *fmt, va_list ap)
-{
-    if (level == RTAPI_MSG_ALL)
-	vfprintf(stdout, fmt, ap);
-     else
-	 vfprintf(stderr, fmt, ap);
-}
-
-static rtapi_msg_handler_t rtapi_msg_handler = default_rtapi_msg_handler;
-
-rtapi_msg_handler_t rtapi_get_msg_handler(void)
-{
-	return rtapi_msg_handler;
-}
-
-void rtapi_set_msg_handler(rtapi_msg_handler_t handler)
-{
-	if (handler == NULL)
-		rtapi_msg_handler = default_rtapi_msg_handler;
-	else
-		rtapi_msg_handler = handler;
-}
-
-void rtapi_print(const char *fmt, ...)
-{
-	va_list args;
-
-	va_start(args, fmt);
-	rtapi_msg_handler(RTAPI_MSG_ALL, fmt, args);
-	va_end(args);
-}
-
-void rtapi_print_msg(int level, const char *fmt, ...)
-{
-	va_list args;
-
-	if ((level <= msg_level) && (msg_level != RTAPI_MSG_NONE)) {
-		va_start(args, fmt);
-		rtapi_msg_handler(level, fmt, args);
-		va_end(args);
-	}
-}
-
-int rtapi_snprintf(char *buffer, unsigned long int size, const char *msg, ...)
-{
-	va_list args;
-	int result;
-
-	va_start(args, msg);
-	/* call the normal library vnsprintf() */
-	result = vsnprintf(buffer, size, msg, args);
-	va_end(args);
-
-	return result;
-}
-
-int rtapi_vsnprintf(char *buffer, unsigned long int size, const char *fmt,
-		    va_list args)
-{
-	return vsnprintf(buffer, size, fmt, args);
-}
-
-int rtapi_set_msg_level(int level)
-{
-	msg_level = level;
-	return 0;
-}
-
-int rtapi_get_msg_level()
-{
-	return msg_level;
-}
 
 #ifdef MSR_H_USABLE
 #include <asm/msr.h>
