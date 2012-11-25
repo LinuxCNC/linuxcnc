f11f0e8ded4e5bf4945c0fbc2eb025cd3327666b
Sebastian Kuzminsky <seb@highlab.com>
2012-11-24 23:16:29 -0700

    fix a bug in emccanon's GET_EXTERNAL_TOOL_SLOT()
    Before this commit GET_EXTERNAL_TOOL_SLOT() used to return the tool
    number of the currently loaded tool, which is wrong.  It should return
    the *pocket* of the currently loaded tool.
    
    This commit searches the tool table for the currently loaded tool and
    returns its pocket number.

--- src/emc/task/emccanon.cc
+++ src/emc/task/emccanon.cc
@@ -2698,9 +2698,21 @@ int GET_EXTERNAL_QUEUE_EMPTY(void)
     return emcStatus->motion.traj.queue == 0 ? 1 : 0;
 }
 
+// Returns the "home pocket" of the tool currently in the spindle, ie the
+// pocket that the current tool was loaded from.  Returns 0 if there is no
+// tool in the spindle.
 int GET_EXTERNAL_TOOL_SLOT()
 {
-    return emcStatus->io.tool.toolInSpindle;
+    int toolno = emcStatus->io.tool.toolInSpindle;
+    int pocket;
+
+    for (pocket = 1; pocket < CANON_POCKETS_MAX; pocket++) {
+        if (emcStatus->io.tool.toolTable[pocket].toolno == toolno) {
+            return pocket;
+        }
+    }
+
+    return 0;  // no tool in spindle
 }
 
 int GET_EXTERNAL_SELECTED_TOOL_SLOT()
