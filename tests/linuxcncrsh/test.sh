eb1316ded5abf6ef1ab9d689dc03e75bf790d536
Sebastian Kuzminsky <seb@highlab.com>
2012-11-28 21:33:21 -0700

    add a test of sending linuxcncrsh a big message
    This currently crashes linuxcncrsh (Fortify catches a buffer overrun),
    the fix is in the following commit.

---
+++ tests/linuxcncrsh/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -f gcode-output
+
+linuxcnc -v -d linuxcncrsh-test.ini &
+
+# let linuxcnc come up
+sleep 4 
+
+(
+    echo hello EMC mt 1.0
+    echo set enable EMCTOO
+
+    echo set mode manual
+    echo set estop off
+    echo set machine on
+
+    echo set home 0
+    echo set home 1
+    echo set home 2
+    sleep 1
+
+    echo set mode mdi
+    echo set mdi m100 p-1 q-2
+    sleep 1
+
+    # here comes a big blob
+    dd bs=4096 if=lots-of-gcode
+
+    echo set mdi m100 p-3 q-4
+
+    echo shutdown
+) | telnet localhost 5007
+
+
+# wait for linuxcnc to finish
+wait
+
+exit 0
+
