96028a99ecbe91364719155a34ad68775f126653
John Morris <john@zultron.com>
2012-11-30 14:30:43 -0600

    rtai.h:  remove unneeded #ifdef blocks (and formatting)

--- src/rtapi/rtai.h
+++ src/rtapi/rtai.h
@@ -5,22 +5,12 @@
 ********************************************************************/
 
 
-/***********************************************************************
-*                        MESSAGING FUNCTIONS                           *
-************************************************************************/
-#ifdef RTAPI_MSG
 /* Messaging functions settings */
 
 // RTAI uses rt_printk() instead of printk()
 #define PRINTK rt_printk
 
-#endif  /* RTAPI_MSG */
 
-
-/***********************************************************************
-*                           TASK FUNCTIONS                             *
-************************************************************************/
-#ifdef RTAPI_TASK
 /* Priority functions settings */
 
 /* RTAI uses 0 as the highest priority; higher numbers are lower
@@ -29,4 +19,3 @@
 #define PRIO_LOWEST 0xFFF
 #define PRIO_HIGHEST 0
 
-#endif  /* RTAPI_TASK  */
