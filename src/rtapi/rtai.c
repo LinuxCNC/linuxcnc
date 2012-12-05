8752ae4e2c154455ec882ab92ac7f1e948d775b7
John Morris <john@zultron.com>
2012-12-04 22:38:55 -0600

    rtai.c:  add module master shared memory init function

--- src/rtapi/rtai.c
+++ src/rtapi/rtai.c
@@ -1,1 +1,14 @@
 unsigned int rev_code = 1;
+
+
+int rtapi_module_master_shared_memory_init(rtapi_data_t **rtapi_data) {
+    /* get master shared memory block from OS and save its address */
+    rtapi_data* = &(rtai_kmalloc(RTAPI_KEY, sizeof(rtapi_data_t)));
+    if (rtapi_data* == NULL) {
+	rtapi_print_msg(RTAPI_MSG_ERR,
+	    "RTAPI: ERROR: could not open shared memory\n");
+	return -ENOMEM;
+    }
+
+    return 0;
+}
