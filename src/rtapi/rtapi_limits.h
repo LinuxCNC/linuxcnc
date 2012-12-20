eaa7faf46c7cb64100ad533df6d508d07b6c5750
Michael Haberler <git@mah.priv.at>
2012-12-20 23:52:04 +0100

    rtapi_limits.h:  adapt to new configure symbols
    SIM doesnt exist anymore

--- src/rtapi/rtapi_limits.h
+++ src/rtapi/rtapi_limits.h
@@ -1,7 +1,9 @@
 #ifndef RTAPI_LIMITS_H
 #define RTAPI_LIMITS_H
 
-#if defined(RTAPI) && !defined(SIM)
+#include "config.h"
+
+#if defined(RTAPI) && defined(BUILD_SYS_KBUILD)
 #include <linux/kernel.h>
 #else
 #include <limits.h>
