724f26975a2d6e28b1fb7c2e8acc29aef4aa0c4e
Sebastian Kuzminsky <seb@highlab.com>
2013-03-07 16:54:49 -0700

    remove our copy of redis

--- src/redis/examples/python/simple.py
+++
@@ -1,8 +0,0 @@
-
-import redis
-print "connecting to redis server..",
-r_server = redis.Redis("localhost")
-print "connected."
-r_server.set("who", "mhaberler was here")
-print "key/value pair stored"
-print "value retrieved:", r_server.get("who")
