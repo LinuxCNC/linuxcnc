bea8e81dfe7b7d1119a7cc08b6b5a706fe08a1ba
Michael Haberler <git@mah.priv.at>
2013-04-01 19:29:17 +0200

    src/rtapi/rtapi_bitops.h changes

--- src/rtapi/rtapi_bitops.h
+++ src/rtapi/rtapi_bitops.h
@@ -7,6 +7,7 @@
 // unfortunately it is not available through /usr/include
 // therefore replicate from linux/bitops.h and linux/kernel.h
 // and prefix with an '_' to roll our own
+
 #define _DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
 #ifndef _BIT  // /usr/include/pth.h might bring this in too
 #define _BIT(nr)                 (1UL << (nr))
@@ -15,8 +16,13 @@
 #define _BIT_WORD(nr)            ((nr) / _BITS_PER_LONG)
 #define _BITS_PER_BYTE           8
 #define _BITS_TO_LONGS(nr)       _DIV_ROUND_UP(nr, _BITS_PER_BYTE * sizeof(long))
+#define _BIT_SET(a, b)           ((a)[_BIT_WORD(b)] |=   _BIT_MASK(b))
+#define _BIT_CLEAR(a, b)         ((a)[_BIT_WORD(b)] &= ~ _BIT_MASK(b))
+#define _BIT_TEST(a, b)          ((a)[_BIT_WORD(b)] &    _BIT_MASK(b))
+
 #define _DECLARE_BITMAP(name,bits) \
     unsigned long name[_BITS_TO_LONGS(bits)]
+
 
 #if defined( USE_GCC_ATOMIC_OPS)
 
