658805da43ad5d4cc8a6e1f50dd6ab30aeb28874
Michael Haberler <git@mah.priv.at>
2013-04-01 18:57:34 +0200

    src/hal/components/streamer.h changes

--- src/hal/components/streamer.h
+++ src/hal/components/streamer.h
@@ -9,13 +9,13 @@
 * Copyright (c) 2006 All rights reserved.
 *
 ********************************************************************/
+#include "rtapi_shmkeys.h"
 
 #define MAX_STREAMERS		8
 #define MAX_SAMPLERS		8
 #define MAX_PINS 		20
 #define MAX_SHMEM 		128000
-#define STREAMER_SHMEM_KEY 	0x48535430
-#define SAMPLER_SHMEM_KEY	0x48534130
+
 #define FIFO_MAGIC_NUM		0x4649464F
 
 /* These structs live in the shared memory that connects the user
