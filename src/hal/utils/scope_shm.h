c87d3daefaa85b4b0a23a0ecb4fac13f47fa3001
Michael Haberler <git@mah.priv.at>
2013-04-01 19:19:48 +0200

    src/hal/utils/scope_shm.h changes

--- src/hal/utils/scope_shm.h
+++ src/hal/utils/scope_shm.h
@@ -40,8 +40,8 @@
 /***********************************************************************
 *                         TYPEDEFS AND DEFINES                         *
 ************************************************************************/
+#include "rtapi_shmkeys.h"
 
-#define SCOPE_SHM_KEY  0x130CF406
 #define SCOPE_NUM_SAMPLES_DEFAULT 16000
 
 typedef enum {
