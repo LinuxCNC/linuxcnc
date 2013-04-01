2d3b0c1fb36a157520bea9d4b6482f270062247b
Michael Haberler <git@mah.priv.at>
2013-04-01 19:35:59 +0200

    src/rtapi/rtapi_support.c/.h factored out from _msg.c/h and extended

---
+++ src/rtapi/rtapi_support.c
@@ -0,0 +1,212 @@
+/********************************************************************
+* Description:  rtapi_support.c
+*               This file, 'rtapi_support.c', implements the messaging
+*               functions for both kernel and userland thread
+*               systems.  See rtapi_support.h for more info.
+*
+*               Other than the rest of RTAPI, these functions are linked
+*               into the instance module which is loaded before rtapi.so/ko
+*               so they are available and message level set before
+*               RTAPI starts up
+********************************************************************/
+
+#define USE_SYSLOG // FIXME
+
+// TODO: ringbuffer in global
+
+#include "config.h"
+#include "rtapi.h"
+#include "rtapi_support.h"
+
+#ifdef MODULE
+#include "rtapi_app.h"
+
+#    include <stdarg.h>		/* va_* */
+#    include <linux/kernel.h>	/* kernel's vsnprintf */
+#    define RTPRINTBUFFERLEN 1024
+static int rt_msg_level = RTAPI_MSG_INFO; // RT space
+#else  /* user land */
+#    include <stdio.h>		/* libc's vsnprintf() */
+#ifdef USE_SYSLOG
+#include <syslog.h>
+#endif
+static int pp_msg_level = RTAPI_MSG_INFO; // per process only
+#endif
+
+// most RT systems use printk()
+#ifndef RTAPI_PRINTK
+#define RTAPI_PRINTK printk
+#endif
+
+
+#ifdef MODULE
+void default_rtapi_msg_handler(msg_level_t level, const char *fmt,
+			      va_list ap) {
+    char buf[RTPRINTBUFFERLEN];
+    vsnprintf(buf, RTPRINTBUFFERLEN, fmt, ap);
+    RTAPI_PRINTK(buf);
+}
+
+#else /* user land */
+void default_rtapi_msg_handler(msg_level_t level, const char *fmt,
+			       va_list ap) {
+#ifdef USE_SYSLOG
+    vsyslog(rtapi2syslog(level), fmt, ap);
+#else
+    if (rtapi_get_msg_level() == RTAPI_MSG_ALL)
+	vfprintf(stdout, fmt, ap);
+    else
+	vfprintf(stderr, fmt, ap);
+#endif
+}
+#endif
+
+static rtapi_msg_handler_t rtapi_msg_handler = default_rtapi_msg_handler;
+
+rtapi_msg_handler_t rtapi_get_msg_handler(void) {
+    return rtapi_msg_handler;
+}
+
+void rtapi_set_msg_handler(rtapi_msg_handler_t handler) {
+    if (handler == NULL)
+	rtapi_msg_handler = default_rtapi_msg_handler;
+    else
+	rtapi_msg_handler = handler;
+}
+
+
+
+// rtapi_get_msg_level and rtapi_set_msg_level moved here
+// since they access the global segment
+// which might not exist during first use
+// assure we can use message levels before global_data is set up
+
+static int get_msg_level(void)
+{
+#if MODULE
+    if (global_data == 0)
+	return rt_msg_level;
+    else
+	return global_data->rt_msg_level;
+#else
+    return pp_msg_level;
+#endif
+}
+
+static int set_msg_level(int new_level)
+{
+    int old_level;
+
+#if MODULE
+    if (global_data) {
+	old_level = global_data->rt_msg_level;
+	global_data->rt_msg_level = new_level;
+    } else {
+	old_level = rt_msg_level;
+	rt_msg_level = new_level;
+    }
+    return old_level;
+#else
+    old_level = pp_msg_level;
+    pp_msg_level = new_level;
+    return old_level;
+#endif
+}
+
+int rtapi_set_msg_level(int level) {
+    int oldlevel;
+    if ((level < RTAPI_MSG_NONE) || (level > RTAPI_MSG_ALL)) {
+	return -EINVAL;
+    }
+    oldlevel = set_msg_level(level);
+    // FIXME cleanup logging
+#ifdef USE_SYSLOG
+#ifndef MODULE
+    setlogmask(LOG_UPTO (rtapi2syslog(level)));
+#endif
+#endif
+    return oldlevel;
+}
+
+int rtapi_get_msg_level() {
+    return get_msg_level();
+}
+
+void rtapi_print(const char *fmt, ...) {
+    va_list args;
+
+    va_start(args, fmt);
+    rtapi_msg_handler(RTAPI_MSG_ALL, fmt, args);
+    va_end(args);
+}
+
+void rtapi_print_msg(int level, const char *fmt, ...) {
+    va_list args;
+
+    if ((level <= rtapi_get_msg_level()) && (rtapi_get_msg_level() != RTAPI_MSG_NONE)) {
+	va_start(args, fmt);
+	rtapi_msg_handler(level, fmt, args);
+	va_end(args);
+    }
+}
+
+int rtapi_snprintf(char *buf, unsigned long int size,
+		   const char *fmt, ...) {
+    va_list args;
+    int result;
+
+    va_start(args, fmt);
+    result = vsnprintf(buf, size, fmt, args);
+    va_end(args);
+    return result;
+}
+
+int rtapi_vsnprintf(char *buf, unsigned long int size, const char *fmt,
+		    va_list ap) {
+    return vsnprintf(buf, size, fmt, ap);
+}
+
+#ifdef MODULE
+
+int rtapi_openlog(const char *tag, int level)
+{
+    return 0;
+}
+
+int rtapi_closelog()
+{
+    return 0;
+}
+
+#else
+
+int rtapi_closelog()
+{
+#ifdef USE_SYSLOG
+    closelog();
+#endif
+    return 0;
+}
+
+int rtapi_openlog(const char *tag, int level)
+{
+#ifdef USE_SYSLOG
+    int option = LOG_PID | LOG_NDELAY;
+    if (level > RTAPI_MSG_INFO)
+	option |= LOG_PERROR;
+    openlog (tag, option , LOG_LOCAL1);
+#endif
+    return 0;
+}
+#endif
+
+EXPORT_SYMBOL(rtapi_get_msg_handler);
+EXPORT_SYMBOL(rtapi_set_msg_handler);
+EXPORT_SYMBOL(rtapi_print_msg);
+EXPORT_SYMBOL(rtapi_print);
+EXPORT_SYMBOL(rtapi_snprintf);
+EXPORT_SYMBOL(rtapi_vsnprintf);
+EXPORT_SYMBOL(rtapi_openlog);
+EXPORT_SYMBOL(rtapi_closelog);
+EXPORT_SYMBOL(rtapi_set_msg_level);
+EXPORT_SYMBOL(rtapi_get_msg_level);
---
+++ src/rtapi/rtapi_support.h
@@ -0,0 +1,178 @@
+#ifndef RTAPI_SUPPORT_H
+#define RTAPI_SUPPORT_H
+
+#include "rtapi.h"
+
+RTAPI_BEGIN_DECLS
+
+/***********************************************************************
+*                      MESSAGING FUNCTIONS                             *
+************************************************************************/
+/* implemented in rtapi_support.c */
+
+#include <stdarg.h>		/* va_start and va_end macros */
+
+/** 'rtapi_snprintf()' works like 'snprintf()' from the normal
+    C library, except that it may not handle long longs.
+    It is provided here because some RTOS kernels don't provide
+    a realtime safe version of the function, and those that do don't provide
+    support for printing doubles.  On systems with a
+    good kernel snprintf(), or in user space, this function
+    simply calls the normal snprintf().  May be called from user,
+    init/cleanup, and realtime code.
+*/
+extern int rtapi_snprintf(char *buf, unsigned long int size,
+			   const char *fmt, ...)
+    __attribute__((format(printf,3,4)));
+
+/** 'rtapi_vsnprintf()' works like 'vsnprintf()' from the normal
+    C library, except that it doesn't handle long longs.
+    It is provided here because some RTOS kernels don't provide
+    a realtime safe version of the function, and those that do don't provide
+    support for printing doubles.  On systems with a
+    good kernel vsnprintf(), or in user space, this function
+    simply calls the normal vsnrintf().  May be called from user,
+    init/cleanup, and realtime code.
+*/
+extern int rtapi_vsnprintf(char *buf, unsigned long size,
+			    const char *fmt, va_list ap);
+
+/** 'rtapi_print()' prints a printf style message.  Depending on the
+    RTOS and whether the program is being compiled for user space
+    or realtime, the message may be printed to stdout, stderr, or
+    to a kernel message log, etc.  The calling syntax and format
+    string is similar to printf except that floating point and
+    longlongs are NOT supported in realtime and may not be supported
+    in user space.  For some RTOS's, a 80 byte buffer is used, so the
+    format line and arguments should not produce a line more than
+    80 bytes long.  (The buffer is protected against overflow.)
+    Does not block, but  can take a fairly long time, depending on
+    the format string and OS.  May be called from user, init/cleanup,
+    and realtime code.
+*/
+extern void rtapi_print(const char *fmt, ...)
+    __attribute__((format(printf,1,2)));
+
+/** 'rtapi_print_msg()' prints a printf-style message when the level
+    is less than or equal to the current message level set by
+    rtapi_set_msg_level().  May be called from user, init/cleanup,
+    and realtime code.
+*/
+    typedef enum {
+	RTAPI_MSG_NONE = 0,
+	RTAPI_MSG_ERR,
+	RTAPI_MSG_WARN,
+	RTAPI_MSG_INFO,
+	RTAPI_MSG_DBG,
+	RTAPI_MSG_ALL
+    } msg_level_t;
+
+extern void rtapi_print_msg(int level, const char *fmt, ...)
+    __attribute__((format(printf,2,3)));
+
+/** Set the maximum level of message to print.  In userspace code,
+    each component has its own independent message level.  In realtime
+    code, all components share a single message level.  Returns 0 for
+    success or -EINVAL if the level is out of range. */
+extern int rtapi_set_msg_level(int level);
+
+/** Retrieve the message level set by the last call to rtapi_set_msg_level */
+extern int rtapi_get_msg_level(void);
+
+/** 'rtapi_get_msg_handler' and 'rtapi_set_msg_handler' access the function
+    pointer used by rtapi_print and rtapi_print_msg.  By default, messages
+    appear in the kernel log, but by replacing the handler a user of the rtapi
+    library can send the messages to another destination.  Calling
+    rtapi_set_msg_handler with NULL restores the default handler. Call from
+    real-time init/cleanup code only.  When called from rtapi_print(),
+    'level' is RTAPI_MSG_ALL, a level which should not normally be used
+    with rtapi_print_msg().
+*/
+typedef void(*rtapi_msg_handler_t)(msg_level_t level, const char *fmt,
+				   va_list ap);
+#ifdef RTAPI
+typedef void (*rtapi_set_msg_handler_t)(rtapi_msg_handler_t);
+
+extern void rtapi_set_msg_handler(rtapi_msg_handler_t handler);
+
+typedef rtapi_msg_handler_t (*rtapi_get_msg_handler_t)(void);
+
+extern rtapi_msg_handler_t rtapi_get_msg_handler(void);
+#endif // RTAPI
+
+#define rtapi2syslog(level) (level+2)
+
+#if defined(ULAPI)
+extern int rtapi_openlog(const char *tag, int level);
+extern int rtapi_closelog(void);
+
+/* make sure a given kernel module is loaded.
+   might be needed for some usermode PCI drivers
+*/
+int rtapi_assure_module_loaded(const char *module);
+
+extern long int simple_strtol(const char *nptr, char **endptr, int base);
+
+// kernel tests in rtapi_compat.c
+extern int kernel_is_xenomai();
+extern int kernel_is_rtai();
+extern int kernel_is_rtpreempt();
+
+#endif // BUILD_SYS_USER_DSO
+
+enum flavor_flags {
+    FLAVOR_DOES_IO=1,
+    FLAVOR_KERNEL_BUILD=2,
+};
+
+//#ifdef ULAPI
+typedef struct {
+    const char *name;
+    const char *mod_ext;
+    const char *so_ext;
+    int id;
+    unsigned long flags;
+
+} flavor_t, *flavor_ptr;
+
+extern flavor_t flavors[];
+
+extern flavor_ptr flavor_byname(const char *flavorname);
+extern flavor_ptr flavor_byid(int flavor_id);
+extern flavor_ptr default_flavor(void);
+
+/*
+ * given a flavor reference, result buffer of PATH__MAX size,
+ * a basename and extension, find the path to a module/shared library
+ * as follows:
+ * if LIBPATH/FLAVORNAME/KERNELRELEASE/BASENAME.EXTENSION exists, succeed
+ * else
+ * if LIBPATH/FLAVORNAME/BASENAME.EXTENSION exists, succeed
+ * else
+ * if LIBPATH/BASENAME.EXTENSION exists, succeed
+ * else fail
+ *
+ * With this scheme it is possible to support any number of flavors nad
+ * kernel versions, and specifically override default choices in case of
+ * problems:
+ *
+ * assume you have a xenomai-user build which has mostly flavor-dependent but
+ * release-independent modules;
+ * however, there is a HAL driver for kernel version foo which is not needed on
+ * other kernel versions.
+ * In this case, place modules like so:
+ *
+ * LIBPATH/xenomai-user/    # standard modules go here
+ * LIBPATH/xenomai-user/foo/hal_driver.so   # more specific driver goes here
+ *
+ */
+extern int module_path(flavor_ptr f,
+		       char *result,
+		       const char *libpath,
+		       const char *basename,
+		       const char *ext);
+//#endif // ULAPI
+
+RTAPI_END_DECLS
+
+#endif /* RTAPI_SUPPORT_H */
