5dabf5020e79d0b21e7a56344328bc16cb876cd4
Jeff Epler <jepler@unpythonic.net>
2013-04-01 21:13:45 -0500

    sim: don't export symbols from sim_rtapi_app to components
    this is unintended and undesirable

--- src/rtapi/sim_rtapi_app.cc
+++ src/rtapi/sim_rtapi_app.cc
@@ -332,7 +332,6 @@ static int slave(int fd, vector<string> args) {
 }
 
 static int master(int fd, vector<string> args) {
-    dlopen(NULL, RTLD_GLOBAL);
     do_load_cmd("hal_lib", vector<string>()); instance_count = 0;
     if(args.size()) { 
         int result = handle_command(args);
