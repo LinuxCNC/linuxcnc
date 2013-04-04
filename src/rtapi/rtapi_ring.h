ff0084d865ad23ca38bbe3c8b5c4b7e667d43ecd
Michael Haberler <git@mah.priv.at>
2013-04-04 12:51:42 +0200

    kernel modes: use proper int types

--- src/rtapi/rtapi_ring.h
+++ src/rtapi/rtapi_ring.h
@@ -4,15 +4,15 @@
 //RTAPI_BEGIN_DECLS
 
 // record mode: Negative numbers are needed for skips
-typedef s32 ring_size_t;
+typedef __s32 ring_size_t;
 
 // the ringbuffer shared data as laid out in
 // the RTAPI shared memory segment
 // defaults: record mode, no rmutex/wmutex use
 typedef struct {
-    u8 is_stream;      // record or stream mode
-    u8 use_rmutex;     // hint to using code - use ringheader_t.rmutex
-    u8 use_wmutex;     // hint to using code - use ringheader_t.wmutex
+    __u8 is_stream;      // record or stream mode
+    __u8 use_rmutex;     // hint to using code - use ringheader_t.rmutex
+    __u8 use_wmutex;     // hint to using code - use ringheader_t.wmutex
 
     int reader, writer;  // HAL module id's - informational // MOVE TO HAL LAYER FIXME
     unsigned long rmutex, wmutex; // optional use - if used by multiple readers/writers
@@ -20,7 +20,7 @@ typedef struct {
     size_t size_mask;    // stream mode only
     size_t size;         // common to stream and record mode
     size_t head __attribute__((aligned(16)));
-     u64    generation;
+    __u64    generation;
     size_t tail __attribute__((aligned(16)));
     char buf[0];  // actual storage, plus scratchpad if any
 } ringheader_t;
@@ -38,7 +38,7 @@ typedef struct {
 // using layer data structures
 typedef struct {
     const ringbuffer_t *ring;
-    u64   generation;
+    __u64   generation;
     size_t offset;
 } ringiter_t;
 
