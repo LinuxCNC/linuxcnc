d2b4137f0dc068314ec75718ba6654509a652ac0
Charles Steinkuehler <charles@steinkuehler.net>
2013-04-24 08:58:19 -0500

    Fix goof in mount command syntax

--- configs/pru-examples/setup.sh
+++ configs/pru-examples/setup.sh
@@ -8,7 +8,7 @@ fi
 # Check to see if omap_mux debug directory is available
 if [ ! -e /sys/kernel/debug/omap_mux/ ] ; then
         echo "Trying to mount debugfs"
-        mount -t debugfs /sys/kernel/debug || {
+        mount -t debugfs debugfs /sys/kernel/debug || {
                 echo "Mounting debugfs failed!"
                 exit 1
         }
