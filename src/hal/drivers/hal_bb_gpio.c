bf0bf38d768266704d51f92782ff37b1265a77d2
Joe Chipcutter (Beagleboard) <joe@chips.com>
2013-05-06 05:38:28 +0000

    starting to set up shared memory structures; not currently working

--- src/hal/drivers/hal_bb_gpio.c
+++ src/hal/drivers/hal_bb_gpio.c
@@ -27,8 +27,11 @@ MODULE_AUTHOR("Ian McMahon");
 MODULE_DESCRIPTION("Driver for BeagleBone GPIO pins");
 MODULE_LICENSE("GPL");
 
+#define PORTS 			 2
+#define PINS_PER_PORT 46
+
 typedef struct {
-	hal_u32_t *data_out;
+	hal_bit_t* pins[PINS_PER_PORT]; // array of pointers to bits
 } port_data_t;
 
 static port_data_t *port_data;
@@ -36,14 +39,14 @@ static port_data_t *port_data;
 static const char *modname = MODNAME;
 
 static void write_port(void *arg, long period);
-static void read_port(void *arg, long period);
+//static void read_port(void *arg, long period);
 
 static int comp_id; 
 static int num_ports;
 
 int rtapi_app_main(void) {
 	char name[HAL_NAME_LEN + 1];
-	int n, retval;
+	int i, n, retval;
 
 	num_ports = 1;
 	n = 0; // port number... only one for now
@@ -63,7 +66,26 @@ int rtapi_app_main(void) {
 		return -1;
 	}
 
+	for(i=0; i<PINS_PER_PORT; i++) {
+		port_data[n].pins[i] = hal_malloc(sizeof(hal_bit_t));
+		if(port_data[n].pins[i] == 0) {
+			rtapi_print_msg(RTAPI_MSG_ERR, "%s: ERROR: hal_malloc() failed\n", modname);
+			hal_exit(comp_id);
+			return -1;
+		}
+	}
+
 	// export pins
+	for(i=0; i<PINS_PER_PORT; i++) {
+		rtapi_print("%s: pointer to the pin: %p\n", modname, (port_data[n].pins[i]));
+		rtapi_print("%s: pointer to the pointer to the pin: %p\n", modname, &(port_data[n].pins[i]));
+		retval = hal_pin_bit_newf(HAL_IN, &(port_data[n].pins[i]), comp_id, "bb_gpio.%d.in-%02d", n, i);
+		if(retval < 0) {
+			rtapi_print_msg(RTAPI_MSG_ERR, "%s: ERROR: port %d, pin %02d could not export pin, err: %d\n", modname, n, i, retval);
+			hal_exit(comp_id);
+			return -1;
+		}
+	}
 
 	// export functions
 	rtapi_snprintf(name, sizeof(name), "bb_gpio.%d.write", n);
@@ -89,6 +111,9 @@ static void write_port(void *arg, long period) {
 
 }
 
+
+/*
 static void read_port(void *arg, long period) {
 
 }
+*/
