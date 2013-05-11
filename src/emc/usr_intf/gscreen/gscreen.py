4357cd229d1ee7e4022e940a04a038e95068d26d
Chris Morley <chrisinnanaimo@hotmail.com>
2013-05-10 17:56:45 -0700

    gscreen -add a look for 'timer_interupt' in the handler file
    This is so one can completely override error handling in Gscreen.

--- src/emc/usr_intf/gscreen/gscreen.py
+++ src/emc/usr_intf/gscreen/gscreen.py
@@ -688,7 +688,10 @@ class Gscreen:
             self.add_alarm_entry(_("CYCLE_TIME in [DISPLAY] of INI file is too small: defaulting to 100ms"))
             temp = 100
         print _("timeout %d" % int(temp))
-        gobject.timeout_add(int(temp), self.periodic_status)
+        if "timer_interupt" in dir(self.handler_instance):
+            gobject.timeout_add(int(temp), self.handler_instance.timer_interupt)
+        else:
+            gobject.timeout_add(int(temp), self.timer_interupt)
 
     def initialize_keybindings(self):
         self.widgets.window1.connect('key_press_event', self.on_key_event,1)
@@ -3000,7 +3003,7 @@ class Gscreen:
         settings.set_string_property("gtk-theme-name", theme, "")
 
     # check linuxcnc for status, error and then update the readout
-    def periodic_status(self):
+    def timer_interupt(self):
         self.emc.mask()
         self.emcstat = linuxcnc.stat()
         self.emcerror = linuxcnc.error_channel()
