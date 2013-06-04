ddb69eb3f71bf24a244d06f35bdfdf6893cc57ed
Charles Steinkuehler <charles@steinkuehler.net>
2013-06-04 16:43:34 +0000

    Added new devcie tree files missed in last commit

---
+++ configs/pru-examples/dtc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+/home/linuxcnc/linuxcnc-master/src/hal/support/dtc/dtc -O dtb -o BB-LCNC-BEBOPR-00A0.dtbo -b 0 -@ BB-LCNC-BEBOPR-00A0.dts && \
+cp BB-LCNC-BEBOPR-00A0.dtbo /lib/firmware/
+
