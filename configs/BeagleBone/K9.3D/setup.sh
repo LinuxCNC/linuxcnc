6ae0d981abdaa507c4ef58945a24eb5e6544cc44
Charles Steinkuehler <charles@steinkuehler.net>
2013-06-13 18:44:38 +0000

    configs/K9.3D : Migrated setup script to K9.3D config and updated README

--- configs/BeagleBone/K9.3D/setup.sh
+++ configs/BeagleBone/K9.3D/setup.sh
@@ -1,21 +1,58 @@
 #!/bin/bash
 
-if [ "$UID" -ne "0" ] ; then
-        echo "Script must be run as root!"
-        exit 1
+dtbo_err () {
+	echo "Error loading device tree overlay file: $DTBO" >&2
+	exit 1
+}
+
+pin_err () {
+	echo "Error exporting pin:$PIN" >&2
+	exit 1
+}
+
+dir_err () {
+	echo "Error setting direction:$DIR on pin:$PIN" >&2
+	exit 1
+}
+
+SLOTS=/sys/devices/bone_capemgr.*/slots
+
+# Make sure required device tree overlay(s) are loaded
+for DTBO in BB-LCNC-K93D ; do
+
+	if grep -q $DTBO $SLOTS ; then
+		echo $DTBO overlay found
+	else
+		echo Loading $DTBO overlay
+		sudo su -c "echo $DTBO > $SLOTS" || dtbo_err
+	fi
+done;
+
+if [ ! -r /sys/devices/ocp.*/helper.*/AIN0 ] ; then
+	echo Analog input files not found in /sys/devices/ocp.*/helper.* >&2
+	exit 1;
 fi
 
+if [ ! -r /sys/class/uio/uio0 ] ; then
+	echo PRU control files not found in /sys/class/uio/uio0 >&2
+	exit 1;
+fi
+
+# Export GPIO pins
+# This really only needs to be done to enable the low-level clocks for the GPIO
+# modules.  There is probably a better way to do this...
 while read PIN DIR JUNK ; do
         case "$PIN" in
         ""|\#*)	
 		continue ;;
         *)
-                echo "$PIN" > "/sys/class/gpio/export"
-		echo "$DIR" > "/sys/class/gpio/gpio$PIN/direction"
+		[ -r /sys/class/gpio/gpio$PIN ] && continue
+                sudo su -c "echo $PIN > /sys/class/gpio/export" || pin_err
+		sudo su -c "echo $DIR > /sys/class/gpio/gpio$PIN/direction" || dir_err
                 ;;
         esac
 
-done <<- "EOF"
+done <<- EOF
 	66	in	# J8.7  gpio2_2 
 	67	in	# J8.8  gpio2_3 
 	69	in	# J8.9  gpio2_5 
