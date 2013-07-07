14d60e8d73a6fa9ab4d26ce82c7b96908b36fa4e
Charles Steinkuehler <charles@steinkuehler.net>
2013-07-07 05:05:02 +0000

    config: BeagleBone Update BeBoPr configuration files Add K9 configuration that runs with HDMI enabled

--- configs/BeagleBone/K9.3D/dtc.sh
+++ configs/BeagleBone/K9.3D/dtc.sh
@@ -3,3 +3,6 @@
 dtc -O dtb -o BB-LCNC-K93D-00A0.dtbo -b 0 -@ BB-LCNC-K93D-00A0.dts && \
 cp BB-LCNC-K93D-00A0.dtbo /lib/firmware/
 
+dtc -O dtb -o BB-LCNC-K9wHDMI-00A0.dtbo -b 0 -@ BB-LCNC-K9wHDMI-00A0.dts && \
+cp BB-LCNC-K9wHDMI-00A0.dtbo /lib/firmware/
+
