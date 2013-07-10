d70d57c878b777c4adb663b6aca1b73d0567744f
Charles Steinkuehler <charles@steinkuehler.net>
2013-07-09 21:22:42 -0500

    configs/BeagleBone ; UNTESTED - Add configuration for BeBoPr + Bridge Board

--- configs/BeagleBone/BeBoPr/dtc.sh
+++ configs/BeagleBone/BeBoPr/dtc.sh
@@ -3,3 +3,6 @@
 dtc -O dtb -o BB-LCNC-BEBOPR-00A0.dtbo -b 0 -@ BB-LCNC-BEBOPR-00A0.dts && \
 cp BB-LCNC-BEBOPR-00A0.dtbo /lib/firmware/
 
+dtc -O dtb -o BB-LCNC-BEBOPRBR-00A0.dtbo -b 0 -@ BB-LCNC-BEBOPRBR-00A0.dts && \
+cp BB-LCNC-BEBOPRBR-00A0.dtbo /lib/firmware/
+
