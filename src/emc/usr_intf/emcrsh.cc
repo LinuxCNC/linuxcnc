7c18ad3ee8c28ae39f71aa33f9c67adad9dd82fa
Sebastian Kuzminsky <seb@highlab.com>
2013-07-13 12:54:52 -0600

    linuxcncrsh: use the non-deprecated way to specify signal handlers

--- src/emc/usr_intf/emcrsh.cc
+++ src/emc/usr_intf/emcrsh.cc
@@ -555,7 +555,16 @@ static int initSockets()
   server_len = sizeof(server_address);
   bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
   listen(server_sockfd, 5);
-  signal(SIGCHLD, SIG_IGN);
+
+  // ignore SIGCHLD
+  {
+    struct sigaction act;
+    act.sa_handler = SIG_IGN;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, NULL);
+  }
+
   return 0;
 }
 
@@ -2808,7 +2817,13 @@ int main(int argc, char *argv[])
     saveEmcCommandSerialNumber = emcStatus->echo_serial_number;
 
     // attach our quit function to SIGINT
-    signal(SIGINT, sigQuit);
+    {
+        struct sigaction act;
+        act.sa_handler = sigQuit;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags = 0;
+        sigaction(SIGINT, &act, NULL);
+    }
 
     if (useSockets) sockMain();
 
