f146fd9fdd4033cc2cf0d802f57974a304908e68
Sebastian Kuzminsky <seb@highlab.com>
2013-08-01 00:42:48 -0600

    io: update the spindle "pocket" on M61
    This commit teaches io to update pocket 0 correctly on M61, which
    communicates to the rest of linuxcnc what the currently loaded tool is
    like.

--- src/emc/iotask/ioControl.cc
+++ src/emc/iotask/ioControl.cc
@@ -950,6 +950,7 @@ int main(int argc, char *argv[])
 		rtapi_print_msg(RTAPI_MSG_DBG, "EMC_TOOL_SET_NUMBER old_loaded_tool=%d new_pocket_number=%d new_tool=%d\n", emcioStatus.tool.toolInSpindle, pocket_number, emcioStatus.tool.toolTable[pocket_number].toolno);
 		emcioStatus.tool.toolInSpindle = emcioStatus.tool.toolTable[pocket_number].toolno;
 		*(iocontrol_data->tool_number) = emcioStatus.tool.toolInSpindle; //likewise in HAL
+                load_tool(pocket_number);
 	    }
 	    break;
 
