6018a79e518efef15adca2c9dd580deaeab75600
Jeff Epler <jepler@unpythonic.net>
2013-09-08 08:44:01 -0500

    rtapi_math64: signed versions. (Seem to work, ap)

--- src/rtapi/rtapi_math64.h
+++ src/rtapi/rtapi_math64.h
@@ -5,6 +5,8 @@
 #include <linux/math64.h>
 #define rtapi_div_u64_rem div_u64_rem
 #define rtapi_div_u64 div_u64
+#define rtapi_div_s64_rem div_s64_rem
+#define rtapi_div_s64 div_s64
 #else
 static inline __u64 rtapi_div_u64_rem(__u64 dividend, __u32 divisor, __u32 *remainder)
 {
@@ -13,6 +15,15 @@ static inline __u64 rtapi_div_u64_rem(__u64 dividend, __u32 divisor, __u32 *rema
 }
 
 static inline __u64 rtapi_div_u64(__u64 dividend, __u32 divisor) {
+	return dividend / divisor;
+}
+static inline __s64 rtapi_div_s64_rem(__s64 dividend, __s32 divisor, __s32 *remainder)
+{
+	*remainder = dividend % divisor;
+	return dividend / divisor;
+}
+
+static inline __s64 rtapi_div_s64(__s64 dividend, __s32 divisor) {
 	return dividend / divisor;
 }
 #endif
