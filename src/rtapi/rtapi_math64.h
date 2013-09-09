0159801b6a58b0812c59daf71fa75dfa0337bbbe
Jeff Epler <jepler@unpythonic.net>
2013-09-08 21:50:56 -0500

    math64: version for hardy kernel
    this is compile-tested but not yet real-world tested

--- src/rtapi/rtapi_math64.h
+++ src/rtapi/rtapi_math64.h
@@ -2,11 +2,40 @@
 #define RTAPI_MATH64_H
 
 #ifdef __KERNEL__
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26)
+#include <asm/div64.h>
+static inline __u64 rtapi_div_u64_rem(__u64 dividend, __u32 divisor, __u32 *remainder)
+{
+    *remainder = do_div(dividend, divisor);
+    return dividend;
+}
+static inline __u64 rtapi_div_u64(__u64 dividend, __u32 divisor)
+{
+    __u32 remainder;
+    return rtapi_div_u64_rem(dividend, divisor, &remainder);
+}
+static inline __s64 rtapi_div_s64_rem(__s64 dividend, __s32 divisor, __s32 *remainder)
+{
+    int sgn_dividend = (dividend < 0) ^ (divisor < 0);
+    int sgn_remainder = (dividend < 0);
+    if(dividend < 0) dividend = -dividend;
+    if(divisor < 0) divisor = -divisor;
+    *remainder = sgn_remainder
+        ? -do_div(dividend, divisor) : do_div(dividend, divisor);
+    return sgn_dividend ? -dividend : dividend;
+}
+static inline __s64 rtapi_div_s64(__s64 dividend, __s32 divisor)
+{
+	__s32 remainder;
+	return rtapi_div_s64_rem(dividend, divisor, &remainder);
+}
+#else
 #include <linux/math64.h>
 #define rtapi_div_u64_rem div_u64_rem
 #define rtapi_div_u64 div_u64
 #define rtapi_div_s64_rem div_s64_rem
 #define rtapi_div_s64 div_s64
+#endif
 #else
 static inline __u64 rtapi_div_u64_rem(__u64 dividend, __u32 divisor, __u32 *remainder)
 {
