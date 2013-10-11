e6cc58d143ac74d6a73947fde4f693298949f27f
Michael Haberler <git@mah.priv.at>
2013-10-11 09:15:03 +0200

    emcmotcfg.h: size the alternate queue

--- src/emc/motion/emcmotcfg.h
+++ src/emc/motion/emcmotcfg.h
@@ -61,6 +61,7 @@
  * a TC_STRUCT is about 512 bytes so this queue is
  * about a megabyte.  */
 #define DEFAULT_TC_QUEUE_SIZE 2000
+#define DEFAULT_ALT_TC_QUEUE_SIZE 100   // size of secondary motion queue
 
 /* max following error */
 #define DEFAULT_MAX_FERROR 100
