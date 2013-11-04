f8dfc9848d7f43ad039441d780391529b9d4979b
Robert W. Ellenberg <rwe24g@gmail.com>
2013-11-03 23:56:53 -0500

    Cosmetic changes for consistent style, comments

--- src/emc/kinematics/tp_debug.h
+++ src/emc/kinematics/tp_debug.h
@@ -14,9 +14,15 @@
 #define TP_DEBUG_H
 
 #ifdef TP_DEBUG
+
 #define tp_debug_print(...) rtapi_print(__VA_ARGS__)
+#define STATIC
+
 #else
+
 #define tp_debug_print(...) 
+#define STATIC static
+
 #endif
 
 #endif
