fbc1274cde7425a5236333e41995b9067d65c85f
Michael Haberler <git@mah.priv.at>
2013-11-04 07:59:07 +0100

    tests/header-sanity: exclude rtapi_common.h
    This include expects THREADS_HEADER to be passed in as a filename.

--- tests/build/header-sanity/test.sh
+++ tests/build/header-sanity/test.sh
@@ -4,12 +4,14 @@ HEADERS=$(readlink -f ../../../include)
 for i in $HEADERS/*.h; do
     case $i in
     */rtapi_app.h) continue ;;
+    */rtapi_common.h) continue ;;
     esac
     gcc -DULAPI -I$HEADERS -E -x c $i > /dev/null
 done
 for i in $HEADERS/*.h $HEADERS/*.hh; do
     case $i in
     */rtapi_app.h) continue ;;
+    */rtapi_common.h) continue ;;
     */interp_internal.hh) continue ;;
     esac
     g++ -DULAPI -I$HEADERS -E -x c++ $i > /dev/null
