a63f3e0e9990ad17f377e3d8864c5b3734b1c645
Robert W. Ellenberg <rwe24g@gmail.com>
2013-11-07 12:30:02 -0500

    Reverted to copy by value in some places
    tpAddLine, tpAddCircle, and tpAddRigidTap were reverted to pass the
    input data by value. This is mostly to keep the pointer passing within
    tp.c, so that we don't have to go digging through multiple files to
    figure out where the memory is originally allocated.

--- src/emc/kinematics/tp.h
+++ src/emc/kinematics/tp.h
@@ -97,12 +97,12 @@ extern int tpSetId(TP_STRUCT * tp, int id);
 extern int tpGetExecId(TP_STRUCT * tp);
 extern int tpSetTermCond(TP_STRUCT * tp, int cond, double tolerance);
 extern int tpSetPos(TP_STRUCT * tp, EmcPose pos);
-extern int tpAddRigidTap(TP_STRUCT * tp, EmcPose const * end, double vel, double
+extern int tpAddRigidTap(TP_STRUCT * tp, EmcPose end, double vel, double
         ini_maxvel, double acc, unsigned char enables);
-extern int tpAddLine(TP_STRUCT * tp, EmcPose const * end, int type, double vel, double
+extern int tpAddLine(TP_STRUCT * tp, EmcPose end, int type, double vel, double
                      ini_maxvel, double acc, unsigned char enables, char atspeed, int indexrotary);
-extern int tpAddCircle(TP_STRUCT * tp, EmcPose const * end, PmCartesian const * const center,
-        PmCartesian const * const normal, int turn, int type, double vel, double ini_maxvel,
+extern int tpAddCircle(TP_STRUCT * tp, EmcPose end, PmCartesian center,
+        PmCartesian normal, int turn, int type, double vel, double ini_maxvel,
                        double acc, unsigned char enables, char atspeed);
 extern int tpRunCycle(TP_STRUCT * tp, long period);
 extern int tpPause(TP_STRUCT * tp);
