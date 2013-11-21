4cb86325d47d3e0ec1e72ea22541c3f8e87a4872
Michael Haberler <git@mah.priv.at>
2013-11-21 12:56:43 +0100

    tests/header-sanity: exclude kernel RT includes
    ULAPI components have no business in including those

--- tests/build/header-sanity/test.sh
+++ tests/build/header-sanity/test.sh
@@ -5,6 +5,8 @@ for i in $HEADERS/*.h; do
     case $i in
     */rtapi_app.h) continue ;;
     */rtapi_common.h) continue ;;
+    */rtai-kernel.h) continue ;;
+    */xenomai-kernel.h) continue ;;
     esac
     gcc -DULAPI -I$HEADERS -E -x c $i > /dev/null
 done
@@ -13,6 +15,8 @@ for i in $HEADERS/*.h $HEADERS/*.hh; do
     */rtapi_app.h) continue ;;
     */rtapi_common.h) continue ;;
     */interp_internal.hh) continue ;;
+    */rtai-kernel.h) continue ;;
+    */xenomai-kernel.h) continue ;;
     esac
     g++ -DULAPI -I$HEADERS -E -x c++ $i > /dev/null
 done
