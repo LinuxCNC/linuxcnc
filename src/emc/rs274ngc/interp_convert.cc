96762c7f3caa1365392a4049b8f82ea7c9b09714
Dewey Garrett <dgarrett@panix.com>
2013-11-21 13:07:55 -0700

    interp: show number of erroneous user m code

--- src/emc/rs274ngc/interp_convert.cc
+++ src/emc/rs274ngc/interp_convert.cc
@@ -3240,7 +3240,7 @@ if (IS_USER_MCODE(block,settings,10) && ONCE_M(10)) {
      /* user-defined M codes */
     int index = block->m_modes[10];
     if (USER_DEFINED_FUNCTION[index - 100] == 0) {
-      CHKS(1, NCE_UNKNOWN_M_CODE_USED);
+      CHKS(1, NCE_UNKNOWN_M_CODE_USED,index);
     }
     enqueue_M_USER_COMMAND(index,block->p_number,block->q_number);
   }
