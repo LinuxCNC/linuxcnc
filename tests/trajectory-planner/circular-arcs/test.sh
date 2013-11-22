d5e621ac54c49f3afe12214d07b1d3a56e8e6838
Robert W. Ellenberg <rwe24g@gmail.com>
2013-11-22 15:52:56 -0500

    Updated test.sh to auto-run a program

--- tests/trajectory-planner/circular-arcs/test.sh
+++ tests/trajectory-planner/circular-arcs/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+set -o monitor
 cp position.blank position.txt
-linuxcnc -r circular_arcs.ini > lcnc-runlog.txt
-#exit $?
-awk '/total/ {print $2,$6}' lcnc-runlog.txt > movement.txt 
-octave --persist ./octave/plot_movement.m
+linuxcnc -r circular_arcs.ini &
+python test-ui.py nc_files/spiral-in.ngc
+fg
+exit $1
