dd5796d15a76d07ca806e4205d5411924f15bcbb
Robert W. Ellenberg <rwe24g@gmail.com>
2013-11-25 03:15:13 -0500

    Updated test runs

--- tests/trajectory-planner/circular-arcs/profile-run.sh
+++ tests/trajectory-planner/circular-arcs/profile-run.sh
@@ -3,7 +3,7 @@ set -o monitor
 cp position.blank position.txt
 operf rtapi_app &
 linuxcnc -r circular_arcs.ini &
-python test-ui.py nc_files/square.ngc
+python machine_setup.py nc_files/square.ngc
 fg
 #End profiling
 exit $1
--- tests/trajectory-planner/circular-arcs/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -o monitor
-cp position.blank position.txt
-linuxcnc -r circular_arcs.ini &
-python test-ui.py nc_files/spiral-in.ngc
-fg
-exit $1
