7b8d27b392d52d7db1f10f86b782ee3a9c9fa294
Robert W. Ellenberg <rwe24g@gmail.com>
2013-11-27 02:10:53 -0500

    Added constants for acc ratio

--- src/emc/kinematics/tp.h
+++ src/emc/kinematics/tp.h
@@ -25,6 +25,9 @@
 //TODO Assign by config file
 #define TP_MAX_FEED_SCALE 2.0
 #define TP_MIN_SEGMENT_CYCLES 2.0
+#define TP_ACC_RATIO_NORMAL (pmSqrt(3.0)/2.0)
+#define TP_ACC_RATIO_TANGENTIAL 0.5
+
 
 /* closeness to zero, for determining if a move is pure rotation */
 #define TP_PURE_ROTATION_EPSILON 1e-6
