4f159f52c8d1800c1c14f0f181424f8deb45ed69
Michael Haberler <git@mah.priv.at>
2013-12-03 11:54:09 +0100

    rtapi/ring.h: clarify Pavel's license, copy in Portable Audio license

--- src/rtapi/ring.h
+++ src/rtapi/ring.h
@@ -28,12 +28,31 @@
 *
 * The original record-oriented ring code is by Pavel Shramov, 1/2013,
 * http://psha.org.ru/cgit/psha/ring/
+* License: MIT
+*
+* LinuxCNC integration by Michael Haberler, 2/2013.
 *
 * The stream-oriented ring code is adapted from:
 * Portable Audio I/O Library Ring Buffer utility.
 * Author: Phil Burk, http://www.softsynth.com
-*
-* LinuxCNC integration by Michael Haberler, 2/2013.
+* License:
+* PortAudio Portable Real-Time Audio Library 
+* Copyright (c) 1999-2011 Ross Bencina and Phil Burk
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this
+* software and associated documentation files (the "Software"), to deal in the Software
+* without restriction, including without limitation the rights to use, copy, modify, merge,
+* publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+* persons to whom the Software is furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all copies or
+* substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+* PURPOSE AND NONINFRINGEMENT. 
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ********************************************************************/
 
 #ifndef RING_H
