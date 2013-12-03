969883024275ef87130375679aa6cc90455dc76e
Michael Haberler <git@mah.priv.at>
2013-12-03 12:22:02 +0100

    rtapi/libm.c: change ownership to Michael Haberler
    this was a complete rewrite anyway

--- src/rtapi/xeno_math/libm.c
+++ src/rtapi/xeno_math/libm.c
@@ -1,20 +1,22 @@
 /*
-rtai/libm/libm.c - module wrapper for SunSoft/FreeBSD/MacOX/uclibc libm
-RTAI - Real-Time Application Interface
-Copyright (C) 2001   David A. Schleef <ds@schleef.org>
+   wrapper for xeno_math exported functions
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 as
-published by the Free Software Foundation.
+   Copyright (C) 2013 Michael Haberler <license@mah.priv.at>
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 */
 
 #include <linux/kernel.h>
