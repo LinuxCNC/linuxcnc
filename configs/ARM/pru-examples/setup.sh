37e3cb70f92e9e4ca39dae8a5a0848dbf071156a
Charles Steinkuehler <charles@steinkuehler.net>
2013-12-03 10:28:05 -0600

    Add missing license declarations Signed-off-by: Charles Steinkuehler <charles@steinkuehler.net>

--- configs/ARM/pru-examples/setup.sh
+++ configs/ARM/pru-examples/setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Copyright 2013
+# Charles Steinkuehler <charles@steinkuehler.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 if [ "$UID" -ne "0" ] ; then
         echo "Script must be run as root!"
