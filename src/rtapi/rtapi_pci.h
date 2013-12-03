4d2d5ba8f7f05c0541b6168b1f377218390207bc
Michael Haberler <git@mah.priv.at>
2013-12-03 19:37:49 +0100

    rtap/rt-preempt.*, rtapi_pci.c : add as LGPL 2.1+
    as agreed with original author

--- src/rtapi/rtapi_pci.h
+++ src/rtapi/rtapi_pci.h
@@ -1,3 +1,27 @@
+/********************************************************************
+* Description:  rtapi_pci.h
+*               This file, 'rtapi_pci.h', exports the 
+*               usermode PCI functions 
+*
+*
+* Copyright (C) 2012 - 2013 Charles Steinkuehler <charles AT steinkuehler DOT net>
+*                           Michael Haberler <license AT mah DOT priv DOT at>
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+********************************************************************/
+
 #ifndef RTAPI_PCI_H
 #define RTAPI_PCI_H
 
