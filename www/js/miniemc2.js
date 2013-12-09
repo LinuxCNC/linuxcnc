d4fc92f6a733665964b1519f64604bc594bc6055
Michael Haberler <git@mah.priv.at>
2013-12-09 21:07:38 +0100

    emcweb/js: update according to SergeyK rev 93
    ..
    Updated SVN, repository at  https://miniemc2.googlecode.com/svn/trunk Rev.93,
    diffs are here: https://code.google.com/p/miniemc2/source/detail?r=93
    
    Sergey

--- www/js/miniemc2.js
+++ www/js/miniemc2.js
@@ -3,7 +3,7 @@
  * http://miniemc2.googlecode.com
  *
  * Copyright 2012, Sergey Kaydalov, kayserg@gmail.com
- * Licensed under the GPL Version 2 or later.
+ * Licensed under the GPL Version 2 licenses.
  */
 function miniemc2Init() {
     miniemc2Init.filelist_inited = false;
