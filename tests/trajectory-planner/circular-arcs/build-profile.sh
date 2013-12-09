6bd0e4ab289b3399ad644e8fefa3cd4566aea876
Robert W. Ellenberg <rwe24g@gmail.com>
2013-12-08 19:29:15 -0500

    Completed optimization on blend arc creation

--- tests/trajectory-planner/circular-arcs/build-profile.sh
+++ tests/trajectory-planner/circular-arcs/build-profile.sh
@@ -2,6 +2,5 @@
 cd ../../../src
 #Ugly way to force rebuild of kinematics, which assumes that tp_debug isn't
 #used anywhere else...
-touch emc/kinematics/t*.[c]
-touch emc/motion/c*.c
-make EXTRA_DEBUG='-DTP_PROFILE'
+touch emc/kinematics/t*.[ch]
+make EXTRA_DEBUG='-DTP_PROFILE -DTP_DEBUG -DTC_DEBUG'
