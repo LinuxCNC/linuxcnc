eb5e0a153b4f14220b784966cb5a4ccbe63b1272
Robert W. Ellenberg <rwe24g@gmail.com>
2014-01-07 14:17:45 -0500

    Automatic testing updates

--- tests/trajectory-planner/circular-arcs/test-all.sh
+++ tests/trajectory-planner/circular-arcs/test-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -o monitor
 cp position.blank position.txt
+rm constraints.log
 #Assume build without TP debug logging enabled
-linuxcnc -r circular_arcs.ini > test.log &
+linuxcnc circular_arcs_rt.ini > test.log &
 python run_all_tests.py
 fg
 exit $1
