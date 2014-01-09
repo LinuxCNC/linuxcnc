4def3f629fda71f54e646235b3932fc57ae76217
Robert W. Ellenberg <rwe24g@gmail.com>
2014-01-09 15:19:42 -0500

    removed extraneous externs and added missing functions

--- src/emc/nml_intf/emcpose.h
+++ src/emc/nml_intf/emcpose.h
@@ -28,15 +28,22 @@ void emcPoseZero(EmcPose * const pos);
 int emcPoseAdd(EmcPose const * const p1, EmcPose const * const p2, EmcPose * const out);
 int emcPoseSub(EmcPose const * const p1, EmcPose const * const p2, EmcPose * const out);
 
-extern int emcPoseToPmCartesian(EmcPose const * const pose,
+int emcPoseToPmCartesian(EmcPose const * const pose,
         PmCartesian * const xyz, PmCartesian * const abc, PmCartesian * const uvw);
 int pmCartesianToEmcPose(PmCartesian const * const xyz,
         PmCartesian const * const abc, PmCartesian const * const uvw, EmcPose * const pose);
 
+int emcPoseSelfAdd(EmcPose * const self, EmcPose const * const p2);
+int emcPoseSelfSub(EmcPose * const self, EmcPose const * const p2);
+
+int emcPoseSetXYZ(PmCartesian const * const xyz, EmcPose * const pose);
 int emcPoseSetABC(PmCartesian const * const abc, EmcPose * const pose);
 int emcPoseSetUVW(PmCartesian const * const uvw, EmcPose * const pose);
+
+int emcPoseGetXYZ(EmcPose const * const pose, PmCartesian * const xyz);
 int emcPoseGetABC(EmcPose const * const pose, PmCartesian * const abc);
 int emcPoseGetUVW(EmcPose const * const pose, PmCartesian * const uvw);
+
 int emcPoseMagnitude(EmcPose const * const pose, double * const out);
 
 #endif
