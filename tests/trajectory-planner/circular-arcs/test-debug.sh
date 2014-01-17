fec771379e7e5cb26d9484abc995580e646d4f8f
Robert W. Ellenberg <rwe24g@gmail.com>
2014-01-16 19:10:37 -0500

    Added proper finalization check for segments

--- tests/trajectory-planner/circular-arcs/test-debug.sh
+++ tests/trajectory-planner/circular-arcs/test-debug.sh
@@ -2,7 +2,7 @@
 set -o monitor
 ./build-debug.sh
 cp position.blank position.txt
-linuxcnc -r circular_arcs.ini > test.log &
+linuxcnc configs/circular_arcs_medium.ini > test.log &
 python machine_setup.py $1
 fg
 ./process_runlog.sh test.log
