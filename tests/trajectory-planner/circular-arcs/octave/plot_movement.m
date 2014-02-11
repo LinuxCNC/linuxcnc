4860e4753b8ed787bdc038d42028c07be5c0d7c2
Robert W. Ellenberg <rwe24g@gmail.com>
2014-02-10 19:22:15 -0500

    Fixed exact stop error in tpCheckEndCondition

--- tests/trajectory-planner/circular-arcs/octave/plot_movement.m
+++ tests/trajectory-planner/circular-arcs/octave/plot_movement.m
@@ -1,8 +1,9 @@
 #! /usr/bin/octave --persist
 load movement.log
 %Assume 1kHz
+t=movement(:,1);
 dt=diff(movement(:,1));
 v_nominal=movement(:,3);
 dx = [0;diff(movement(:,2))./dt];
 dv = [0;diff(dx)./dt];
-plot(dv)
+plot(t,dv)
