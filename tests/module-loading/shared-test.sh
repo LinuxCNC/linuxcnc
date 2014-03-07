e43c98022c90451f4b004994fc883f88d9ae35f6
John Morris <john@zultron.com>
2014-03-06 22:27:00 -0600

    tests/module-loading:  Print debug output on failure
    Recent intermittent test failures in tests/module-loading/*, but no
    debug output.
    
    Print contents of output files to give us a clue.
    
    Fixes #89

--- tests/module-loading/shared-test.sh
+++ tests/module-loading/shared-test.sh
@@ -6,10 +6,15 @@ RESULT=$?
 NUM_PINS=$(cat hal-output | egrep $(cat PIN_NAME_REGEX) | wc -l)
 
 if [ $RESULT -ne $(cat RESULT) ]; then
+    echo "Test exited with status $RESULT; output:"
+    cat hal-output
     exit 1
 fi
 
 if [ "$NUM_PINS" -ne $(cat NUM_PINS) ]; then
+    echo "Error:  number of pins found != number expected"
+    echo "Pins found: '$NUM_PINS'"
+    echo "Pins expected:  $(cat RESULT)"
     exit 1
 fi
 
