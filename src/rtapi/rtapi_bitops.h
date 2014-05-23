f83af6e940b1e40a6a50024cf4010635d2075bf1
bryant <bryant@defrag.in>
2014-05-23 16:05:12 -0400

    fix gcc version detection.

--- src/rtapi/rtapi_bitops.h
+++ src/rtapi/rtapi_bitops.h
@@ -33,7 +33,7 @@
 #ifndef __ATOMIC_SEQ_CST
 #define __ATOMIC_SEQ_CST 5
 #endif
-#elif defined(__GNUC__) && (__GNUC__ > 4) && (__GNUC_MINOR__ >= 7)
+#elif defined(__GNUC__) && (__GNUC__ >= 4) && (__GNUC_MINOR__ >= 7)
 #define RTAPI_USE_ATOMIC 1
 #else
 #define RTAPI_USE_ATOMIC 0
