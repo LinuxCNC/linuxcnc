c99d3bef90503d5ad88be90d63337fa31b18969c
Chris Radek <chris@timeguy.com>
2014-06-18 10:06:36 -0500

    Fix many prototypes

--- src/emc/tp/tp.h
+++ src/emc/tp/tp.h
@@ -19,41 +19,41 @@
 #include "tp_types.h"
 #include "tcq.h"
 
-int tpCreate(TP_STRUCT * tp, int _queueSize, TC_STRUCT * tcSpace);
-int tpClear(TP_STRUCT * tp);
-int tpInit(TP_STRUCT * tp);
+int tpCreate(TP_STRUCT * const tp, int _queueSize, TC_STRUCT * const tcSpace);
+int tpClear(TP_STRUCT * const tp);
+int tpInit(TP_STRUCT * const tp);
 int tpClearDIOs(TP_STRUCT * const tp);
-int tpSetCycleTime(TP_STRUCT * tp, double secs);
-int tpSetVmax(TP_STRUCT * tp, double vmax, double ini_maxvel);
-int tpSetVlimit(TP_STRUCT * tp, double limit);
-int tpSetAmax(TP_STRUCT * tp, double amax);
-int tpSetId(TP_STRUCT * tp, int id);
-int tpGetExecId(TP_STRUCT * tp);
-int tpSetTermCond(TP_STRUCT * tp, int cond, double tolerance);
-int tpSetPos(TP_STRUCT * tp, EmcPose const * const pos);
+int tpSetCycleTime(TP_STRUCT * const tp, double secs);
+int tpSetVmax(TP_STRUCT * const tp, double vmax, double ini_maxvel);
+int tpSetVlimit(TP_STRUCT * const tp, double vLimit);
+int tpSetAmax(TP_STRUCT * const tp, double aMax);
+int tpSetId(TP_STRUCT * const tp, int id);
+int tpGetExecId(TP_STRUCT * const tp);
+int tpSetTermCond(TP_STRUCT * const tp, int cond, double tolerance);
+int tpSetPos(TP_STRUCT * const tp, EmcPose const * const pos);
 int tpAddCurrentPos(TP_STRUCT * const tp, EmcPose const * const disp);
 int tpSetCurrentPos(TP_STRUCT * const tp, EmcPose const * const pos);
-int tpAddRigidTap(TP_STRUCT * tp, EmcPose end, double vel, double
+int tpAddRigidTap(TP_STRUCT * const tp, EmcPose end, double vel, double
         ini_maxvel, double acc, unsigned char enables);
-int tpAddLine(TP_STRUCT * tp, EmcPose end, int type, double vel, double
+int tpAddLine(TP_STRUCT * const tp, EmcPose end, int canon_motion_type, double vel, double
                      ini_maxvel, double acc, unsigned char enables, char atspeed, int indexrotary);
-int tpAddCircle(TP_STRUCT * tp, EmcPose end, PmCartesian center,
-        PmCartesian normal, int turn, int type, double vel, double ini_maxvel,
+int tpAddCircle(TP_STRUCT * const tp, EmcPose end, PmCartesian center,
+        PmCartesian normal, int turn, int canon_motion_type, double vel, double ini_maxvel,
                        double acc, unsigned char enables, char atspeed);
-int tpRunCycle(TP_STRUCT * tp, long period);
-int tpPause(TP_STRUCT * tp);
-int tpResume(TP_STRUCT * tp);
-int tpAbort(TP_STRUCT * tp);
+int tpRunCycle(TP_STRUCT * const tp, long period);
+int tpPause(TP_STRUCT * const tp);
+int tpResume(TP_STRUCT * const tp);
+int tpAbort(TP_STRUCT * const tp);
 int tpGetPos(TP_STRUCT const  * const tp, EmcPose * const pos);
-int tpIsDone(TP_STRUCT * tp);
-int tpQueueDepth(TP_STRUCT * tp);
-int tpActiveDepth(TP_STRUCT * tp);
-int tpGetMotionType(TP_STRUCT * tp);
-int tpSetSpindleSync(TP_STRUCT * tp, double sync, int wait);
-void tpToggleDIOs(TC_STRUCT * tc); //gets called when a new tc is taken from the queue. it checks and toggles all needed DIO's
+int tpIsDone(TP_STRUCT * const tp);
+int tpQueueDepth(TP_STRUCT * const tp);
+int tpActiveDepth(TP_STRUCT * const tp);
+int tpGetMotionType(TP_STRUCT * const tp);
+int tpSetSpindleSync(TP_STRUCT * const tp, double sync, int wait);
+void tpToggleDIOs(TC_STRUCT * const tc); //gets called when a new tc is taken from the queue. it checks and toggles all needed DIO's
 
-int tpSetAout(TP_STRUCT * tp, unsigned char index, double start, double end);
-int tpSetDout(TP_STRUCT * tp, int index, unsigned char start, unsigned char end); //gets called to place DIO toggles on the TC queue
+int tpSetAout(TP_STRUCT * const tp, unsigned char index, double start, double end);
+int tpSetDout(TP_STRUCT * const tp, int index, unsigned char start, unsigned char end); //gets called to place DIO toggles on the TC queue
 
 //Misc utlity functions for motion
 int tpFindIntersectionAngle(PmCartesian const * const u1,
