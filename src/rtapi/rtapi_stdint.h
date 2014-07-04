4c2375793c62408d7c429323a41be33be04301f8
Jeff Epler <jepler@unpythonic.net>
2014-07-03 19:41:54 -0500

    rtapi: provide additional portability headers
    .. this set is sufficient to allow modified hostmot2
    and hm2_test components to build.

---
+++ src/rtapi/rtapi_stdint.h
@@ -0,0 +1,41 @@
+//    Copyright 2014 Jeff Epler
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#ifndef RTAPI_STDINT_H
+#define RTAPI_STDINT_H
+
+#ifdef __KERNEL__
+#include <asm/types.h>
+typedef s8 rtapi_s8;
+typedef s16 rtapi_s16;
+typedef s32 rtapi_s32;
+typedef s64 rtapi_s64;
+typedef u8 rtapi_u8;
+typedef u16 rtapi_u16;
+typedef u32 rtapi_u32;
+typedef u64 rtapi_u64;
+#else
+#include <inttypes.h>
+typedef int8_t rtapi_s8;
+typedef int16_t rtapi_s16;
+typedef int32_t rtapi_s32;
+typedef int64_t rtapi_s64;
+typedef uint8_t rtapi_u8;
+typedef uint16_t rtapi_u16;
+typedef uint32_t rtapi_u32;
+typedef uint64_t rtapi_u64;
+#endif
+
+#endif
