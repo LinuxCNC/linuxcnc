363af46a6fd336daf7bb68ab8c86d9c00967fcf1
John Morris <john@zultron.com>
2014-07-12 16:00:10 -0500

    Fix `check-system-configuration` script
    - Instead of imprecise, English language instructions for how to fix
      problems in the udev/shmdrv/rsyslogd checks, print English language
      descriptions of the problems with actual copy-and-paste shell
      commands.  Fixes #112 and #113.
    
    - Check for multiple definitions of `memlock` that may clobber each
      other.  In this case, manual intervention is required; no
      copy-and-paste solution.
    
    - Rename `/etc/limits.d/linuxcnc.conf` to
      `/etc/limits.d/machinekit.conf`.  This could cause warnings for
      folks both running RIP builds (previous this commit) and installing
      packages (when this file is packaged), since it could trigger the
      above 'multiple definitions' issue.  I think this is unlikely.
    
    - Rename `/etc/udev/rules.d/50-LINUXCNC-shmdrv.rules` to
      `/etc/udev/rules.d/50-shmdrv.rules`.  Neither LinuxCNC nor
      Machinekit belong in the config file name for this generic driver.
      This should cause no warnings, since multiple entries are not
      detected.
    
    - Check for the `rsyslogd` `SystemLogRateLimitBurst` directive in more
      than just `/etc/rsyslog.d/linuxcnc.conf`, but also `rsyslog.conf`
      and `rsyslog.d/*.conf`.  In this case, renaming
      `/var/log/linuxcnc.log` might cause confusion, so it is not renamed.

--- scripts/check-system-configuration.sh
+++ scripts/check-system-configuration.sh
@@ -8,37 +8,64 @@
 #
 # This assumes rsyslogd.
 #
-# Rsyslogd can drop debug messages from LinuxCNC when coming in a too
+# Rsyslogd can drop debug messages from Machinekit when coming in a too
 # high a rate.  This is controlled by the SystemLogRateLimitBurst
 # parameter.
+get-conf-files() {
+    conf_files="$*"
+    local conflist=""
+    for i in $conf_files; do
+	conflist+=" $(readlink -e $i)"
+    done
+    echo $conflist
+}
+
 check-rsyslog() {
     local logfile=/var/log/linuxcnc.log
-    local logconf=/etc/rsyslog.d/linuxcnc.conf
+    local logconfigs="$(get-conf-files /etc/rsyslog.conf /etc/rsyslog.d/*.conf)"
 
     local res=0
     local nologfile=false
     if test ! -f $logfile
     then
 	echo "Warning:  Logfile '$logfile' does not exist."
-	echo "          Hint:  Be sure the (possibly empty) logfile exists;"
-	echo "          and restart rsyslogd; rsyslogd will not create"
-	echo "          missing files."
+	echo
+	echo "          rsyslogd does not create non-existent log files; be"
+	echo "          sure the (possibly empty) logfile exists and"
+	echo "          restart rsyslogd"
+	echo
+	echo "          Hint:"
+	echo "            $ sudo touch /var/log/linuxcnc.log"
+	echo "            $ sudo service rsyslog restart"
+	echo
+	echo "          Gurus:  If you intentionally log to another file,"
+	echo "          please be aware of where Machinekit logs go for"
+	echo "          debugging purposes"
 	echo
 	res=1
 	nologfile=true
     fi
 
-    if test ! -f $logconf || ! grep -q SystemLogRateLimitBurst $logconf; then
+    if test -z "$logconfigs"; then
+	echo "Warning:  No rsyslog.conf found; system log daemon not rsyslogd?"
+	echo
+	echo "          Please check your syslog configuration for"
+	echo "          rate limiting; an example for rsyslogd can be found in"
+	echo "          src/rtapi/rsyslogd-linuxcnc.conf"
+	echo
+    elif ! grep -q SystemLogRateLimitBurst $logconfigs; then
 	res=1
 	echo "Warning:  No rate limit in rsyslogd is set."
-	echo "          The rsyslogd drops logs when incoming at higher than"
-	echo "          the rate configured by 'SystemLogRateLimitBurst'.  The"
-	echo "          default rate is lower than LinuxCNC requires when"
-	echo "          running in debug mode."
-	echo "          Hint:  Put src/rtapi/rsyslogd-linuxcnc.conf into"
-	echo "          /etc/rsyslog.d/linuxcnc.conf for a reasonable default,"
-	echo "          and restart rsyslogd."
-	echo "          "
+	echo
+	echo "          The 'rsyslogd' daemon drops logs when incoming at"
+	echo "          higher than the rate configured by"
+	echo "          'SystemLogRateLimitBurst'.  The default rate is lower"
+	echo "          than Machinekit requires when running in debug mode."
+	echo
+	echo "          Hint:"
+	echo "            $ sudo cp rtapi/rsyslogd-linuxcnc.conf" \
+			     "/etc/rsyslog.d/linuxcnc.conf"
+	echo "            $ sudo service rsyslogd restart"
 	echo
     fi
     test $res -ne 0 && return 1
@@ -52,10 +79,11 @@ check-rsyslog() {
     # logging works
 	return 0
     else
-	echo "Warning:  Logging appears to be not working:"
-	echo "          Both $logconf and"
-	echo "          $logfile exist, but a test"
-	echo "          did not appear in the log.  Please investigate."
+	echo "Warning:  Logging appears to be not working"
+	echo
+	echo "          Found rsyslogd configuration and $logfile"
+	echo "          exists, but a test did not appear in the log."
+	echo "          Please investigate."
 	echo
 	return 1
     fi
@@ -67,35 +95,51 @@ check-rsyslog() {
 # Xenomai needs higher memlock ulimit than the default 64k
 #
 check-ulimits() {
-    local ulimit_conf=/etc/security/limits.d/linuxcnc.conf
+    local ulimit_configs="$(get-conf-files \
+	/etc/security/limits.conf /etc/security/limits.d/*.conf)"
     # a guess at what a reasonable memlock value should be
     local reasonable_memlock=32767
 
-    # If $ulimit_conf exists, assume that the contents are correct
-    if test -f $ulimit_conf; then
-	return 0
-    fi
-
-    # Otherwise, look for a reasonable setting for memlock.
+    # Look for a reasonable setting for memlock
     #
     # Assumption: the 'memlock' setting comes from the last entry in
     # these files, and <domain> and <type> fields are correct
-    export LC_COLLATE=C
-    memlock=0
-    for f in /etc/security/limits.conf /etc/security/limits.d/*.conf; do
-	test -f $f || continue
-	mtmp=$(awk '/^[^#]/ && $3=="memlock" {m=$4} END {print m}' $f)
-	test -n "$mtmp" && memlock=$mtmp
+
+    # First, look for 'memlock' entries in the configuration
+    local memlock=0
+    local last_ulimit_config
+    local memlock_found=false
+    for f in $ulimit_configs; do
+	mtmp=$(awk '/^\*[ \t]+(-|soft)[ \t]+memlock/ {m=$4} END {print m}' $f)
+	if test -n "$mtmp"; then
+	    if test $memlock != 0; then
+		echo "Warning:  Multiple configurations for 'memlock' setting"
+		echo
+		echo "          Please check configuration in these files:"
+		echo "            $last_ulimit_config"
+		echo "            $f"
+		echo
+		return 1
+	    fi
+	    memlock=$mtmp
+	    memlock_found=true
+	    last_ulimit_config=$f
+	fi
     done
+    if ! $memlock_found; then
+	echo "Warning:  no configuration for 'memlock' found"
+	echo
+	echo "          Hint:"
+	echo "            $ sudo cp rtapi/shmdrv/limits.d-machinekit.conf ${ulimit_conf}"
+	return 1
+    fi
     
-    if ! test $memlock = unlimited -o \
-	$(($memlock>$reasonable_memlock)) = 1; then
-	echo "Warning:  $ulimit_conf does not exist, and a reasonable"
-	echo "          'memlock' value not found in configuration."
-	echo "          Please check the system configuration and correct."
-	echo "          Hint:  src/rtapi/shmdrv/limits.d-linuxcnc.conf may"
-	echo "          be a reasonable example to install in"
-	echo "          $ulimit_conf."
+    # Next, check the 'memlock' value looks sane
+    if test $memlock != unlimited && test $memlock -lt $reasonable_memlock; then
+	echo "Warning:  Config 'memlock' value $memlock too small"
+	echo
+	echo "          Value for 'memlock' in file $last_ulimit_config"
+	echo "          should be raised to $reasonable_memlock or greater"
 	echo
 	return 1
     fi
@@ -108,7 +152,8 @@ check-ulimits() {
 # The shmdrv converged shared memory driver for kthreads flavors must
 # be accessible.  Look for a 'shmdrv.rules' file.
 check-shmdrv() {
-    local udev_conf=/etc/udev/rules.d/50-LINUXCNC-shmdrv.rules
+    local udev_conf=/etc/udev/rules.d/50-shmdrv.rules
+    local udev_configs="$(get-conf-files /etc/udev/rules.d/*.rules)"
     
     # If $udev_conf exists, assume the contents are correct
     if test -f $udev_conf; then
@@ -116,7 +161,7 @@ check-shmdrv() {
     fi
 
     # Otherwise, assume any KERNEL=="shmdrv" setting is correct
-    for f in /etc/udev/rules.d/*.rules; do
+    for f in $udev_configs; do
 	if grep -q 'KERNEL=="shmdrv"' $f; then
 	    return 0
 	fi
@@ -124,11 +169,13 @@ check-shmdrv() {
     
     # If we're here, we found no sign of udev configuration.
     echo "Warning:  No udev configuration for shmdrv was found."
+    echo
     echo "          The user running LinuxCNC must have write access to"
-    echo "          /dev/shmdrv when running kernel threads.  This may"
+    echo "          /dev/shmdrv when running kernel threads, which may"
     echo "          be configured in /etc/udev/rules.d."
-    echo "          Hint:  see src/rtapi/shmdrv/shmdrv.rules for a"
-    echo "          reasonable default."
+    echo
+    echo "          Hint:"
+    echo "            $ sudo cp rtapi/shmdrv/shmdrv.rules $udev_conf"
     echo
     return 1
 }
