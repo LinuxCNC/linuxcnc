abdae920061a92148a1dbea6144a3dbb969d866a
Jeff Epler <jepler@unpythonic.net>
2014-07-24 09:44:58 -0500

    uspace: Factor RtapiApp class definition to header
    .. this enables an RtapiApp implementation besides Posix to be
    implemented in a separate translation unit.

--- src/rtapi/uspace_rtapi_app.cc
+++ src/rtapi/uspace_rtapi_app.cc
@@ -485,30 +485,6 @@ struct rtapi_task {
 
 namespace
 {
-struct RtapiApp
-{
-
-    RtapiApp(int policy = SCHED_OTHER) : policy(policy), period(0) {}
-
-    int prio_highest();
-    int prio_lowest();
-    int prio_next_higher(int prio);
-    int prio_next_lower(int prio);
-    long clock_set_period(long int period_nsec);
-    int task_new(void (*taskcode)(void*), void *arg,
-            int prio, int owner, unsigned long int stacksize, int uses_fp);
-    virtual int task_delete(int id) = 0;
-    virtual int task_start(int task_id, unsigned long period_nsec) = 0;
-    virtual int task_pause(int task_id) = 0;
-    virtual int task_resume(int task_id) = 0;
-    virtual void wait() = 0;
-    virtual unsigned char do_inb(unsigned int port) = 0;
-    virtual void do_outb(unsigned char value, unsigned int port) = 0;
-    virtual int run_threads(int fd, int (*callback)(int fd)) = 0;
-    int policy;
-    long period;
-};
-
 struct Posix : RtapiApp
 {
     Posix(int policy = SCHED_FIFO) : RtapiApp(policy), do_thread_lock(policy != SCHED_FIFO) {
