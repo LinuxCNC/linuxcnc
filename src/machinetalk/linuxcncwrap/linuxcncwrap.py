33ee5cb90a7eee89482c1da003c2f25a424541a1
Alexander Rössler <mail.aroessler@gmail.com>
2014-08-18 18:01:22 +0200

    machinetalk/linuxcncwrap: add task and interp update functions, added send message functions

--- src/machinetalk/linuxcncwrap/linuxcncwrap.py
+++ src/machinetalk/linuxcncwrap/linuxcncwrap.py
@@ -228,22 +228,22 @@ class LinuxCNCWrapper:
             return False, None
 
     def update_config(self, stat):
-        configModified = False
+        modified = False
 
         if (self.status.config.acceleration != stat.acceleration):
             self.status.config.acceleration = stat.acceleration
             self.txStatus.config.acceleration = stat.acceleration
-            configModified = True
+            modified = True
 
         if (self.status.config.angular_units != stat.angular_units):
             self.status.config.angular_units = stat.angular_units
             self.txStatus.config.angular_units = stat.angular_units
-            configModified = True
+            modified = True
 
         if (self.status.config.axes != stat.axes):
             self.status.config.axes = stat.axes
             self.txStatus.config.axes = stat.axes
-            configModified = True
+            modified = True
 
         txAxis = EmcStatusConfigAxis()
         for index, axis in enumerate(stat.axis):
@@ -291,87 +291,90 @@ class LinuxCNCWrapper:
             if axisModified:
                 txAxis.index = index
                 self.txStatus.config.axis.add().CopyFrom(txAxis)
-                configModified = True
+                modified = True
 
         del txAxis
 
         if (self.status.config.axis_mask != stat.axis_mask):
             self.status.config.axis_mask = stat.axis_mask
             self.txStatus.config.axis_mask = stat.axis_mask
-            configModified = True
+            modified = True
 
         if (self.status.config.cycle_time != stat.cycle_time):
             self.status.config.cycle_time = stat.cycle_time
             self.txStatus.config.cycle_time = stat.cycle_time
-            configModified = True
+            modified = True
 
         if (self.status.config.debug != stat.debug):
             self.status.config.debug = stat.debug
             self.txStatus.config.debug = stat.debug
-            configModified = True
+            modified = True
 
         if (self.status.config.kinematics_type != stat.kinematics_type):
             self.status.config.kinematics_type = stat.kinematics_type
             self.txStatus.config.kinematics_type = stat.kinematics_type
-            configModified = True
+            modified = True
 
         if (self.status.config.linear_units != stat.linear_units):
             self.status.config.linear_units = stat.linear_units
             self.txStatus.config.linear_units = stat.linear_units
-            configModified = True
+            modified = True
 
         if (self.status.config.max_acceleration != stat.max_acceleration):
             self.status.config.max_acceleration = stat.max_acceleration
             self.txStatus.config.max_acceleration = stat.max_acceleration
-            configModified = True
+            modified = True
 
         if (self.status.config.program_units != stat.program_units):
             self.status.config.program_units = stat.program_units
             self.txStatus.config.program_units = stat.program_units
-            configModified = True
+            modified = True
 
         if (self.status.config.velocity != stat.velocity):
             self.status.config.velocity = stat.velocity
             self.txStatus.config.velocity = stat.velocity
-            configModified = True
+            modified = True
+
+        if modified:
+            self.send_config(self.txStatus.config)
 
     def update_io(self, stat):
-        ioModified = False
+        modified = False
 
         if (self.status.io.estop != stat.estop):
             self.status.io.estop = stat.estop
             self.txStatus.io.estop = stat.estop
-            ioModified = True
+            modified = True
 
         if (self.status.io.flood != stat.flood):
             self.status.io.flood = stat.flood
             self.txStatus.io.flood = stat.flood
-            ioModified = True
+            modified = True
 
         if (self.status.io.lube != stat.lube):
             self.status.io.lube = stat.lube
             self.txStatus.io.lube = stat.lube
-            ioModified = True
+            modified = True
 
         if (self.status.io.lube_level != stat.lube_level):
             self.status.io.lube_level = stat.lube_level
             self.txStatus.io.lube_level = stat.lube_level
-            ioModified = True
+            modified = True
 
         if (self.status.io.mist != stat.mist):
             self.status.io.mist = stat.mist
             self.txStatus.io.mist = stat.mist
-            ioModified = True
+            modified = True
 
         if (self.status.io.pocket_prepped != stat.pocket_prepped):
             self.status.io.pocket_prepped = stat.pocket_prepped
             self.txStatus.io.pocket_prepped = stat.pocket_prepped
-            ioModified = True
+            modified = True
 
         if (self.status.io.tool_in_spindle != stat.tool_in_spindle):
             self.status.io.tool_in_spindle = stat.tool_in_spindle
             self.txStatus.io.tool_in_spindle = stat.tool_in_spindle
-            ioModified = True
+            modified = True
 
         positionModified = False
         txPosition = None
@@ -379,7 +382,7 @@ class LinuxCNCWrapper:
         if positionModified:
             self.status.io.tool_offset.CopyFrom(txPosition)
             self.txStatus.io.tool_offset = txPosition
-            ioModified = True
+            modified = True
 
         txToolResult = EmcToolResult()
         for index, toolResult in enumerate(stat.tool_table):
@@ -463,14 +466,196 @@ class LinuxCNCWrapper:
             if toolResultModified:
                 txToolResult.index = index
                 self.txStatus.io.tool_table.add().CopyFrom(txToolResult)
-                ioModified = True
+                modified = True
 
         del txToolResult
+
+        if modified:
+            self.send_io(self.txStatus.io)
+
+    def update_task(self, stat):
+        modified = False
+
+        if (self.status.task.echo_serial_number != stat.echo_serial_number):
+            self.status.task.echo_serial_number = stat.echo_serial_number
+            self.txStatus.task.echo_serial_number = stat.echo_serial_number
+            modified = True
+
+        if (self.status.task.exec_state != stat.exec_state):
+            self.status.task.exec_state = stat.exec_state
+            self.txStatus.task.exec_state = stat.exec_state
+            modified = True
+
+        if (self.status.task.file != stat.file):
+            self.status.task.file = stat.file
+            self.txStatus.task.file = stat.file
+            modified = True
+
+        if (self.status.task.input_timeout != stat.input_timeout):
+            self.status.task.input_timeout = stat.input_timeout
+            self.txStatus.task.input_timeout = stat.input_timeout
+            modified = True
+
+        if (self.status.task.optional_stop != stat.optional_stop):
+            self.status.task.optional_stop = stat.optional_stop
+            self.txStatus.task.optional_stop = stat.optional_stop
+            modified = True
+
+        if (self.status.task.read_line != stat.read_line):
+            self.status.task.read_line = stat.read_line
+            self.txStatus.task.read_line = stat.read_line
+            modified = True
+
+        if (self.status.task.task_mode != stat.task_mode):
+            self.status.task.task_mode = stat.task_mode
+            self.txStatus.task.task_mode = stat.task_mode
+            modified = True
+
+        if (self.status.task.task_paused != stat.task_paused):
+            self.status.task.task_paused = stat.task_paused
+            self.txStatus.task.task_paused = stat.task_paused
+            modified = True
+
+        if (self.status.task.task_state != stat.task_state):
+            self.status.task.task_state = stat.task_state
+            self.txStatus.task.task_state = stat.task_state
+            modified = True
+
+        if modified:
+            self.send_task(self.txStatus.task)
+
+    def update_interp(self, stat):
+        modified = False
+
+        if (self.status.interp.command != stat.command):
+            self.status.interp.command = stat.command
+            self.txStatus.interp.command = stat.command
+            modified = True
+
+        txStatusGCode = EmcStatusGCode()
+        for index, gcode in enumerate(stat.gcodes):
+            txStatusGCode.Clear()
+            gcodeModified = False
+
+            if len(self.status.interp.gcodes) == index:
+                self.status.interp.gcodes.add()
+                self.status.interp.gcodes[index].index = index
+
+            if self.status.interp.gcodes[index].value != gcode:
+                self.status.interp.gcodes[index].value = gcode
+                txStatusGCode.value = gcode
+                gcodeModified = True
+
+            if gcodeModified:
+                txStatusGCode.index = index
+                self.txStatus.interp.gcodes.add().CopyFrom(txStatusGCode)
+                modified = True
+
+        del txStatusGCode
+
+        if (self.status.interp.interp_state != stat.interp_state):
+            self.status.interp.interp_state = stat.interp_state
+            self.txStatus.interp.interp_state = stat.interp_state
+            modified = True
+
+        if (self.status.interp.interpreter_errcode != stat.interpreter_errcode):
+            self.status.interp.interpreter_errcode = stat.interpreter_errcode
+            self.txStatus.interp.interpreter_errcode = stat.interpreter_errcode
+            modified = True
+
+        txStatusMCode = EmcStatusMCode()
+        for index, mcode in enumerate(stat.mcodes):
+            txStatusMCode.Clear()
+            mcodeModified = False
+
+            if len(self.status.interp.mcodes) == index:
+                self.status.interp.mcodes.add()
+                self.status.interp.mcodes[index].index = index
+
+            if self.status.interp.mcodes[index].value != mcode:
+                self.status.interp.mcodes[index].value = mcode
+                txStatusMCode.value = mcode
+                mcodeModified = True
+
+            if mcodeModified:
+                txStatusMCode.index = index
+                self.txStatus.interp.mcodes.add().CopyFrom(txStatusMCode)
+                modified = True
+
+        del txStatusMCode
+
+        txStatusSetting = EmcStatusSetting()
+        for index, setting in enumerate(stat.settings):
+            txStatusSetting.Clear()
+            settingModified = False
+
+            if len(self.status.interp.settings) == index:
+                self.status.interp.settings.add()
+                self.status.interp.settings[index].index = index
+
+            if self.status.interp.settings[index].value != setting:
+                self.status.interp.settings[index].value = setting
+                txStatusSetting.value = setting
+                settingModified = True
+
+            if settingModified:
+                txStatusSetting.index = index
+                self.txStatus.interp.settings.add().CopyFrom(txStatusSetting)
+                modified = True
+
+        del txStatusSetting
+
+        if modified:
+            self.send_interp(self.txStatus.interp)
 
     def update_status(self, stat):
         self.txStatus.clear()
         self.update_config(stat)
         self.update_io(stat)
+        self.update_task(stat)
+        self.update_interp(stat)
+
+    def send_config(self, data):
+        self.tx.type = MT_EMCSTAT_CONFIG
+        self.tx.emc_status_config.MergeFrom(data)
+        txBuffer = self.tx.SerializeToString()
+        print("sending config message")
+        self.tx.Clear()
+        self.statusSocket.send_multipart(['status', txBuffer])
+
+    def send_io(self, data):
+        self.tx.type = MT_EMCSTAT_IO
+        self.tx.emc_status_io.MergeFrom(data)
+        txBuffer = self.tx.SerializeToString()
+        print("sending io message")
+        self.tx.Clear()
+        self.statusSocket.send_multipart(['io', txBuffer])
+
+    def send_task(self, data):
+        self.tx.type = MT_EMCSTAT_TASK
+        self.tx.emc_status_task.MergeFrom(data)
+        txBuffer = self.tx.SerializeToString()
+        print("sending task message")
+        self.tx.Clear()
+        self.statusSocket.send_multipart(['task', txBuffer])
+
+    def send_motion(self, data):
+        self.tx.type = MT_EMCSTAT_MOTION
+        self.tx.emc_status_motion.MergeFrom(data)
+        txBuffer = self.tx.SerializeToString()
+        print("sending motion message")
+        self.tx.Clear()
+        self.statusSocket.send_multipart(['motion', txBuffer])
+
+    def send_interp(self, data):
+        self.tx.type = MT_EMCSTAT_INTERP
+        self.tx.emc_status_interp.MergeFrom(data)
+        txBuffer = self.tx.SerializeToString()
+        print("sending interp message")
+        self.tx.Clear()
+        self.statusSocket.send_multipart(['interp', txBuffer])
+
+    #def send_io(self, data):
 
     def full_read(self):
         pass
@@ -485,14 +670,16 @@ class LinuxCNCWrapper:
 
                 #print text_format.MessageToString(self.status.config)
                 #print text_format.MessageToString(self.txStatus.config)
-                print self.txStatus.io.SerializeToString()
+                #print self.txStatus.io.SerializeToString()
 
             except linuxcnc.error as detail:
                 print(("error", detail))
             time.sleep(self.poll_interval)
 
     def full_update(self):
-        pass
+        self.send_config(self.status.config)
+        self.send_io(self.status.io)
+        #TODO other updates
 
     def processStatus(self, socket):
         print("process status called")
