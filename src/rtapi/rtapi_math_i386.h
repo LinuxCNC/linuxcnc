5112199f587281f155f252209a17aaaef4a182f0
Jeff Epler <jepler@unpythonic.net>
2014-08-20 07:19:42 -0500

    rtapi_math: Move inlines out of i386.h
    .. otherwise the implementations are not available e.g., on x86_64 in kernel
    spcae

--- src/rtapi/rtapi_math_i386.h
+++ src/rtapi/rtapi_math_i386.h
@@ -25,23 +25,4 @@ extern __inline double atan2 (double __y, double __x) {
     __asm __volatile__ ("fpatan" : "=t" (__value) : "0" (__x), "u" (__y) : "st(1)");
     return __value;
 }
-
-extern __inline double atan (double __y) {
-    return atan2(__y, 1.);
-}
-
-extern __inline double asin (double __x) {
-    return atan2(__x, sqrt (1.0 - __x * __x));
-}
-
-extern __inline double acos (double __x) {
-    return atan2(sqrt(1.0 - __x * __x), __x);
-}
-
-extern __inline double fmax(double __y, double __x) {
-    return __y > __x ? __y : __x;
-}
-extern __inline double fmin(double __y, double __x) {
-    return __y < __x ? __y : __x;
-}
 #endif /* RTAPI_MATH_I386_H */
