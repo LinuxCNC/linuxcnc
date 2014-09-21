259fc06ddce60a2b2923f7c38b3a6077754dcf62
Jeff Epler <jepler@unpythonic.net>
2014-09-21 10:15:22 -0500

    rtapi_math64: drop unneeded workaround for old kernels

--- src/rtapi/rtapi_math64.h
+++ src/rtapi/rtapi_math64.h
@@ -21,40 +21,11 @@
 #include <rtapi_stdint.h>
 
 #ifdef __KERNEL__
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26)
-#include <asm/div64.h>
-static inline rtapi_u64 rtapi_div_u64_rem(rtapi_u64 dividend, rtapi_u32 divisor, rtapi_u32 *remainder)
-{
-    *remainder = do_div(dividend, divisor);
-    return dividend;
-}
-static inline rtapi_u64 rtapi_div_u64(rtapi_u64 dividend, rtapi_u32 divisor)
-{
-    rtapi_u32 remainder;
-    return rtapi_div_u64_rem(dividend, divisor, &remainder);
-}
-static inline rtapi_s64 rtapi_div_s64_rem(rtapi_s64 dividend, rtapi_s32 divisor, rtapi_s32 *remainder)
-{
-    int sgn_dividend = (dividend < 0) ^ (divisor < 0);
-    int sgn_remainder = (dividend < 0);
-    if(dividend < 0) dividend = -dividend;
-    if(divisor < 0) divisor = -divisor;
-    *remainder = sgn_remainder
-        ? -do_div(dividend, divisor) : do_div(dividend, divisor);
-    return sgn_dividend ? -dividend : dividend;
-}
-static inline rtapi_s64 rtapi_div_s64(rtapi_s64 dividend, rtapi_s32 divisor)
-{
-	rtapi_s32 remainder;
-	return rtapi_div_s64_rem(dividend, divisor, &remainder);
-}
-#else
 #include <linux/math64.h>
 #define rtapi_div_u64_rem div_u64_rem
 #define rtapi_div_u64 div_u64
 #define rtapi_div_s64_rem div_s64_rem
 #define rtapi_div_s64 div_s64
-#endif
 #else
 static inline rtapi_u64 rtapi_div_u64_rem(rtapi_u64 dividend, rtapi_u32 divisor, rtapi_u32 *remainder)
 {
