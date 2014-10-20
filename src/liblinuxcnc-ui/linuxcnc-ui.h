4dcd04b02179b6f428b62c2afa9730943c304df4
Jeff Epler <jepler@unpythonic.net>
2014-10-19 22:16:15 -0500

    lui: additional typedefs
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- src/liblinuxcnc-ui/linuxcnc-ui.h
+++ src/liblinuxcnc-ui/linuxcnc-ui.h
@@ -26,7 +26,7 @@ typedef struct lui lui_t;
 
 
 // types for EMC_TASK_SET_STATE
-// these must match EMC_TASK_SET_STATE_ENUM from emc/nml_intf/emc.hh
+// these must match EMC_TASK_STATE_ENUM from emc/nml_intf/emc.hh
 typedef enum {
     lui_task_state_estop = 1,
     lui_task_state_estop_reset = 2,
@@ -34,6 +34,67 @@ typedef enum {
     lui_task_state_on = 4
 } lui_task_state_t;
 
+// these must match EMC_TASK_MODE_ENUM from emc/nml_intf/emc.hh
+typedef enum {
+    lui_task_mode_manual = 1,
+    lui_task_mode_auto = 2,
+    lui_task_mode_mdi = 3,
+} lui_task_mode_t;
+
+// these must match EMC_TASK_EXEC_ENUM
+typedef enum {
+    lui_task_exec_error = 1,
+    lui_task_exec_done = 2,
+    lui_task_exec_waiting_for_motion = 3,
+    lui_task_exec_waiting_for_motion_queue = 4,
+    lui_task_exec_waiting_for_io = 5,
+    lui_task_exec_waiting_for_motion_and_io = 7,
+    lui_task_exec_waiting_for_delay = 8,
+    lui_task_exec_waiting_for_system_cmd = 9,
+    lui_task_exec_waiting_for_spindle_oriented = 10
+} lui_exec_state_t;
+
+// these must match EMC_TASK_INTERP_ENUM
+typedef enum {
+    lui_task_interp_idle = 1,
+    lui_task_interp_reading = 2,
+    lui_task_interp_paused = 3,
+    lui_task_interp_waiting = 4
+} lui_interp_state_t;
+
+// these must match CANON_LINEAR_UNITS
+typedef enum {
+    lui_linear_units_inch = 1,
+    lui_linear_units_mm = 2,
+    // cm = 3,
+} lui_linear_units_t;
+
+// must match KINEMATICS_TYPE
+typedef enum {
+    lui_kinematics_identity = 1,
+    lui_kinematics_forward_only,
+    lui_kinematics_inverse_only,
+    lui_kinematics_both,
+} lui_kinematics_type_t;
+
+// must match CANON_MOTION_MODE
+typedef enum {
+    lui_motion_mode_exact_stop = 1,
+    lui_motion_mode_exact_path = 2,
+    lui_motion_mode_continuous = 3,
+} lui_motion_mode_t;
+
+// must match (in memory layout) EmcPose
+typedef struct {
+    double x, y, z, a, b, c, u, v, w;
+} lui_position_t;
+
+typedef struct {
+    int toolno;
+    lui_position_t offset;
+    double diameter, frontangle, backangle;
+    int orientation;
+} lui_tool_info_t;
 
 #ifdef __cplusplus
 extern "C" {
