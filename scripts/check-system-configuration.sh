25c95dc41a38c43cf14f283ce93b5f8863ac8f42
Kent Reed <kentallanreed@gmail.com>
2014-11-07 09:47:08 -0500

    corrects a typo, fixes #344

--- scripts/check-system-configuration.sh
+++ scripts/check-system-configuration.sh
@@ -65,7 +65,7 @@ check-rsyslog() {
 	echo "          Hint:"
 	echo "            $ sudo cp rtapi/rsyslogd-linuxcnc.conf" \
 			     "/etc/rsyslog.d/linuxcnc.conf"
-	echo "            $ sudo service rsyslogd restart"
+	echo "            $ sudo service rsyslog restart"
 	echo
     fi
     test $res -ne 0 && return 1
