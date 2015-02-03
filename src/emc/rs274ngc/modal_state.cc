3e26a13e2499597d89db7f0bd0e0089c85ff0f66
Robert W. Ellenberg <rwe24g@gmail.com>
2015-02-03 17:18:36 -0500

    statetag: fixes to prevent potential errors
    1) Add error checks to prevent using an empty state tag as a source of stat information
    2) Create virtual function for restore_from_tag to eliminate dangerous
    pointer cast
    
    Signed-off-by: Robert W. Ellenberg <rwe24g@gmail.com>

--- src/emc/rs274ngc/modal_state.cc
+++ src/emc/rs274ngc/modal_state.cc
@@ -30,3 +30,26 @@ StateTag::StateTag(struct state_tag_t const & basetag): flags(basetag.packed_fla
     speed = basetag.speed;
 }
 
+
+/**
+ * Return true if the tag is a valid state, and false if not
+ */
+int StateTag::is_valid(void) const
+{
+
+    if (fields[GM_FIELD_LINE_NUMBER] <= 1) {
+        return false;
+    }
+
+    //TODO magic numbers
+    if (fields[GM_FIELD_ORIGIN] < 540) {
+        return false;
+    }
+
+    if (fields[GM_FIELD_PLANE] < 170 ) {
+        return false;
+    }
+
+    return true;
+}
+
