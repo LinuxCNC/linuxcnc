c309e5e2459c233b32e1dd783d901e7510e9f465
Michael Haberler <git@mah.priv.at>
2015-03-22 15:40:22 +0100

    pbmsgs: emit array including message type id, name, npb descriptor
    this will be the driving data for all lookups:
    
    msg type id -> nanopb descriptor, name, maximum deserialized size
    msg type name -> type id, .. as above..
    nanopb descriptor -> type id, name

--- src/machinetalk/msgcomponents/pbmsgs.h
+++ src/machinetalk/msgcomponents/pbmsgs.h
@@ -5,6 +5,31 @@
 #include <machinetalk/nanopb/pb_decode.h>
 #include <machinetalk/nanopb/pb_encode.h>
 
+// the message type descriptor
+typedef struct {
+    uint32_t msgid;
+    int32_t encoded_size;
+    const char *name;
+    size_t size;
+    const pb_field_t *fields;
+    void *user;
+} msginfo_t;
+
+#define PB_MSG(id, size, name) {				\
+	id,							\
+	    size,						\
+	    # name,						\
+	    sizeof(PB_MSG_ ## id),				\
+	    name ## _fields,					\
+	    NULL,						\
+	    },
+
+#define PB_MSGINFO_DELIMITER {0, -1, NULL, 0, NULL, NULL}
+#define PB_MSGID
+
 #include <machinetalk/generated/message.npb.h>
+#include <machinetalk/generated/value.npb.h>
+#include <machinetalk/generated/test.npb.h>
+
 
 #endif // _PBMSGS_H
