a6d9c67aa1ea19de54d21e69f5bb48810ef76c25
Robert W. Ellenberg <rwe24g@gmail.com>
2015-03-30 11:24:41 -0400

    task: added command type for reverse run / task plan
    Signed-off-by: Robert W. Ellenberg <rwe24g@gmail.com>

--- src/emc/nml_intf/emc.hh
+++ src/emc/nml_intf/emc.hh
@@ -167,6 +167,7 @@ struct PM_CARTESIAN;
 #define EMC_TASK_PLAN_SET_OPTIONAL_STOP_TYPE         ((NMLTYPE) 517)
 #define EMC_TASK_PLAN_SET_BLOCK_DELETE_TYPE          ((NMLTYPE) 518)
 #define EMC_TASK_PLAN_OPTIONAL_STOP_TYPE             ((NMLTYPE) 519)
+#define EMC_TASK_PLAN_REVERSE_TYPE                   ((NMLTYPE) 520)
 
 #define EMC_TASK_STAT_TYPE                           ((NMLTYPE) 599)
 
