6b52f681be300ff7e0ba02c5f9299ee6c5994863
Jeff Epler <jepler@unpythonic.net>
2015-04-18 08:45:30 -0500

    hm2_eth: in realtime context, queue all writes
    .. these writes will take place before hm2_write returns, so it will make
    no difference to the caller.
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- src/hal/drivers/mesa-hostmot2/hm2_eth.c
+++ src/hal/drivers/mesa-hostmot2/hm2_eth.c
@@ -437,7 +437,12 @@ static int hm2_eth_enqueue_read(hm2_lowlevel_io_t *this, rtapi_u32 addr, void *b
     return 1;
 }
 
+static int hm2_eth_enqueue_write(hm2_lowlevel_io_t *this, rtapi_u32 addr, void *buffer, int size);
+
 static int hm2_eth_write(hm2_lowlevel_io_t *this, rtapi_u32 addr, void *buffer, int size) {
+    if(rtapi_task_self() >= 0)
+        return hm2_eth_enqueue_write(this, addr, buffer, size);
+
     int send;
     static struct {
         lbp16_cmd_addr wr_packet;
