8fed5d8cff186ab5241e534592c560c87fb443a5
Sebastian Kuzminsky <seb@highlab.com>
2015-04-25 11:47:02 -0600

    hy-vfd: add copyright info to hy_vfd.c
    Signed-off-by: Sebastian Kuzminsky <seb@highlab.com>

--- src/hal/user_comps/huanyang-vfd/hy_vfd.c
+++ src/hal/user_comps/huanyang-vfd/hy_vfd.c
@@ -1,4 +1,23 @@
 
+//
+// Copyright (C) 2009 scotta at CNCZone.com, alan_3301 at CNCZone.com
+//     2014 Benjamin Brockhaus
+//     2015 Sebastian Kuzminsky <seb@highlab.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+//
 
 
 #ifndef ULAPI
