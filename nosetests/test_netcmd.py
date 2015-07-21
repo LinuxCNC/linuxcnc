91213d3a643b7f167f9ad0d6e84bb42a3a53573c
Alexander Rössler <mail.aroessler@gmail.com>
2015-07-21 09:15:20 +0200

    nosetests: cleanup linter errors

--- nosetests/test_netcmd.py
+++ nosetests/test_netcmd.py
@@ -5,28 +5,29 @@ from machinekit.nosetests.realtime import setup_module,teardown_module
 from machinekit.nosetests.support import fnear
 
 from machinekit import hal
-import os
+
 
 def test_component_creation():
-    global c1,c2
+    global c1, c2
     c1 = hal.Component("c1")
     c1.newpin("s32out", hal.HAL_S32, hal.HAL_OUT, init=42)
-    c1.newpin("s32in",  hal.HAL_S32, hal.HAL_IN)
-    c1.newpin("s32io",  hal.HAL_S32, hal.HAL_IO)
+    c1.newpin("s32in", hal.HAL_S32, hal.HAL_IN)
+    c1.newpin("s32io", hal.HAL_S32, hal.HAL_IO)
     c1.newpin("floatout", hal.HAL_FLOAT, hal.HAL_OUT, init=42)
-    c1.newpin("floatin",  hal.HAL_FLOAT, hal.HAL_IN)
-    c1.newpin("floatio",  hal.HAL_FLOAT, hal.HAL_IO)
+    c1.newpin("floatin", hal.HAL_FLOAT, hal.HAL_IN)
+    c1.newpin("floatio", hal.HAL_FLOAT, hal.HAL_IO)
 
     c1.ready()
 
     c2 = hal.Component("c2")
     c2.newpin("s32out", hal.HAL_S32, hal.HAL_OUT, init=4711)
-    c2.newpin("s32in",  hal.HAL_S32, hal.HAL_IN)
-    c2.newpin("s32io",  hal.HAL_S32, hal.HAL_IO)
+    c2.newpin("s32in", hal.HAL_S32, hal.HAL_IN)
+    c2.newpin("s32io", hal.HAL_S32, hal.HAL_IO)
     c2.newpin("floatout", hal.HAL_FLOAT, hal.HAL_OUT, init=4711)
-    c2.newpin("floatin",  hal.HAL_FLOAT, hal.HAL_IN)
-    c2.newpin("floatio",  hal.HAL_FLOAT, hal.HAL_IO)
+    c2.newpin("floatin", hal.HAL_FLOAT, hal.HAL_IN)
+    c2.newpin("floatio", hal.HAL_FLOAT, hal.HAL_IO)
     c2.ready()
+
 
 def test_net_existing_signal_with_bad_type():
     hal.newsig("f", hal.HAL_FLOAT)
@@ -37,16 +38,18 @@ def test_net_existing_signal_with_bad_type():
         pass
     del hal.signals["f"]
 
+
 def test_net_match_nonexistant_signals():
     try:
-        hal.net("nosuchsig", "c1.s32out","c2.s32out")
+        hal.net("nosuchsig", "c1.s32out", "c2.s32out")
         raise "should not happen"
     except TypeError:
         pass
 
+
 def test_net_pin2pin():
     try:
-        hal.net("c1.s32out","c2.s32out")
+        hal.net("c1.s32out", "c2.s32out")
         #TypeError: net: 'c1.s32out' is a pin - first argument must be a signal name
         raise "should not happen"
     except TypeError:
@@ -56,9 +59,9 @@ def test_net_pin2pin():
 def test_net_existing_signal():
     hal.newsig("s32", hal.HAL_S32)
 
-    assert hal.pins["c1.s32out"].linked == False
+    assert hal.pins["c1.s32out"].linked is False
     hal.net("s32", "c1.s32out")
-    assert hal.pins["c1.s32out"].linked == True
+    assert hal.pins["c1.s32out"].linked is True
 
     hal.newsig("s32too", hal.HAL_S32)
     try:
@@ -69,8 +72,9 @@ def test_net_existing_signal():
 
     del hal.signals["s32"]
 
+
 def test_newsig():
-    floatsig1 = hal.newsig("floatsig1", hal.HAL_FLOAT)
+    hal.newsig("floatsig1", hal.HAL_FLOAT)
     try:
         hal.newsig("floatsig1", hal.HAL_FLOAT)
         # RuntimeError: Failed to create signal floatsig1: HAL: ERROR: duplicate signal 'floatsig1'
@@ -78,7 +82,7 @@ def test_newsig():
     except RuntimeError:
         pass
     try:
-        hal.newsig(32423 *32432, hal.HAL_FLOAT)
+        hal.newsig(32423 * 32432, hal.HAL_FLOAT)
         raise "should not happen"
     except TypeError:
         pass
@@ -130,4 +134,4 @@ def test_check_net_args():
         pass
 
 (lambda s=__import__('signal'):
-     s.signal(s.SIGTERM, s.SIG_IGN))()
+    s.signal(s.SIGTERM, s.SIG_IGN))()
--- nosetests/test_rtapi.py
+++ nosetests/test_rtapi.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python
-import os,time
+import os
+import time
+import sys
 
 from nose import with_setup
 from machinekit.nosetests.realtime import setup_module ,teardown_module
 
-from machinekit import rtapi,hal
+from machinekit import rtapi
+from machinekit import hal
 
-import ConfigParser
+if sys.version_info >= (3, 0):
+    import configparser
+else:
+    import ConfigParser as configparser
+
+
+uuid = None  # initialized by test_get_uuid
+rt = None
+
 
 def test_get_uuid():
     global uuid, rt
-    cfg = ConfigParser.ConfigParser()
+    cfg = configparser.ConfigParser()
     cfg.read(os.getenv("MACHINEKIT_INI"))
     uuid = cfg.get("MACHINEKIT", "MKUUID")
+
 
 def test_rtapi_connect():
     global rt
     rt = rtapi.RTAPIcommand(uuid=uuid)
 
+
 def test_loadrt_or2():
     global rt
     rt.loadrt("or2")
-    rt.newthread("servo-thread",1000000,fp=True)
-    hal.addf("or2.0","servo-thread")
+    rt.newthread("servo-thread", 1000000, fp=True)
+    hal.addf("or2.0", "servo-thread")
     hal.start_threads()
     time.sleep(0.2)
+
 
 def test_unloadrt_or2():
     hal.stop_threads()
@@ -32,4 +46,4 @@ def test_unloadrt_or2():
     rt.unloadrt("or2")
 
 (lambda s=__import__('signal'):
-     s.signal(s.SIGTERM, s.SIG_IGN))()
+    s.signal(s.SIGTERM, s.SIG_IGN))()
