e63a15d3e85945bf428f63f823faff0688d44f7e
Jeff Epler <jepler@unpythonic.net>
2015-07-21 20:20:56 -0500

    hm2_eth: reset errno before socket recv
    .. so that a subesquent error reported is not from some
    other syscall
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- src/hal/drivers/mesa-hostmot2/hm2_eth.c
+++ src/hal/drivers/mesa-hostmot2/hm2_eth.c
@@ -438,6 +438,7 @@ static int hm2_eth_read(hm2_lowlevel_io_t *this, rtapi_u32 addr, void *buffer, i
       read_packet.addr_lo, read_packet.addr_hi, size);
     t1 = rtapi_get_time();
     do {
+        errno = 0;
         recv = eth_socket_recv(board->sockfd, (void*) &tmp_buffer, size, 0);
         if(recv < 0) rtapi_delay(READ_PCK_DELAY_NS);
         t2 = rtapi_get_time();
