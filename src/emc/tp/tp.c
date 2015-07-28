207bf754a31704d587367783b654fe69ec22ac89
Robert W. Ellenberg <rwe24g@gmail.com>
2015-07-27 22:09:13 -0400

    tp: purge a few lines of redundant code
    Signed-off-by: Robert W. Ellenberg <rwe24g@gmail.com>

--- src/emc/tp/tp.c
+++ src/emc/tp/tp.c
@@ -1959,7 +1959,6 @@ int tpAddCircle(TP_STRUCT * const tp,
 
     // Update tc target with existing circular segment
     tc.target = pmCircle9Target(&tc.coords.circle);
-    tcClampVelocityByLength(&tc);
     if (tc.target < TP_POS_EPSILON) {
         return TP_ERR_ZERO_LENGTH;
     }
@@ -1967,8 +1966,7 @@ int tpAddCircle(TP_STRUCT * const tp,
     tc.nominal_length = tc.target;
 
     //Reduce max velocity to match sample rate
-    double sample_maxvel = tc.target / (tp->cycleTime * TP_MIN_SEGMENT_CYCLES);
-    tc.maxvel = fmin(tc.maxvel, sample_maxvel);
+    tcClampVelocityByLength(&tc);
 
     double v_max_actual = pmCircleActualMaxVel(&tc.coords.circle.xyz, &tc.acc_ratio_tan, ini_maxvel, acc, false);
     tp_debug_print("tc.acc_ratio_tan = %f\n",tc.acc_ratio_tan);
