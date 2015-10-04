b050cbfc78e465aaebda254043fd3a75528ddb76
Robert W. Ellenberg <rwe24g@gmail.com>
2015-10-04 11:51:40 -0400

    test: fix for release build script
    Signed-off-by: Robert W. Ellenberg <rwe24g@gmail.com>

--- tests/trajectory-planner/circular-arcs/build-release.sh
+++ tests/trajectory-planner/circular-arcs/build-release.sh
@@ -3,4 +3,4 @@ cd ../../../src
 #Ugly way to force rebuild of kinematics, which assumes that tp_debug isn't
 #used anywhere else...
 touch emc/tp/t[cp]*.[ch]
-make EXTRA_DEBUG=''
+make -j4 EXTRA_DEBUG=''
