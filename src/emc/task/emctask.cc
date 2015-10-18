441231c4f71dd3ba6df980517e8151d213ddf07c
Jeff Epler <jepler@unpythonic.net>
2015-10-18 11:50:35 -0500

    task: enable and disable the right number of joints
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- src/emc/task/emctask.cc
+++ src/emc/task/emctask.cc
@@ -255,7 +255,7 @@ int emcTaskSetState(int state)
         emcMotionAbort();
 	// turn the machine servos off-- go into READY state
         emcSpindleAbort();
-	for (t = 0; t < emcStatus->motion.traj.axes; t++) {
+	for (t = 0; t < emcStatus->motion.traj.joints; t++) {
 	    emcJointDisable(t);
 	}
 	emcTrajDisable();
@@ -271,7 +271,7 @@ int emcTaskSetState(int state)
     case EMC_TASK_STATE_ON:
 	// turn the machine servos on
 	emcTrajEnable();
-	for (t = 0; t < emcStatus->motion.traj.axes; t++) {
+	for (t = 0; t < emcStatus->motion.traj.joints; t++) {
 	    emcJointEnable(t);
 	}
 	emcLubeOn();
@@ -293,7 +293,7 @@ int emcTaskSetState(int state)
         emcSpindleAbort();
 	// go into estop-- do both IO estop and machine servos off
 	emcAuxEstopOn();
-	for (t = 0; t < emcStatus->motion.traj.axes; t++) {
+	for (t = 0; t < emcStatus->motion.traj.joints; t++) {
 	    emcJointDisable(t);
 	}
 	emcTrajDisable();
