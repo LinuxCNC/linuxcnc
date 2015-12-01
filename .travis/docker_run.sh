eadf006b0755f26e1bf2973f6e40545d712edee2
GP Orcullo <kinsamanka@gmail.com>
2015-12-01 18:03:24 +0800

    Added travis-ci.org and packagecloud.io integration

--- .travis/docker_run.sh
+++ .travis/docker_run.sh
@@ -10,6 +10,7 @@ COMMITTER_EMAIL="$(git log -1 --pretty=format:%ae)"
 COMMIT_TIMESTAMP="$(git log -1 --pretty=format:%at)"
 DISTRO=${TAG%-*}
 MARCH=${TAG#*-}
+test ${TRAVIS_PULL_REQUEST} = false && IS_PR=false || IS_PR=true
 # Verbose RIP build output:  "true" or "false"
 MK_BUILD_VERBOSE=${MK_BUILD_VERBOSE:-"false"}
 # Verbose package build output:  "true" or "false"
@@ -39,6 +40,7 @@ docker run \
     -e COMMIT_TIMESTAMP=${COMMIT_TIMESTAMP} \
     -e MK_BUILD_VERBOSE="${MK_BUILD_VERBOSE}" \
     -e MK_PACKAGE_VERBOSE="${MK_PACKAGE_VERBOSE}" \
+    -e IS_PR="${IS_PR}" \
     -e MAJOR_MINOR_VERSION \
     -e PKGSOURCE \
     -e DISTRO \
@@ -52,17 +54,33 @@ docker run \
     ${DOCKER_CONTAINER}:${TAG} \
     ${CHROOT_PATH}${TRAVIS_PATH}/${cmd}.sh
 
-# tests are run under a new container instead of chrooting
-# this will allow us to run docker without using privileged mode
-if [ ${CMD} == "run_tests" ];
-then
-    # create container using RIP rootfs
-    docker build -t mk_runtest .travis/mk_runtests
-    
-    # run regressions
-    docker run \
-        -e MACHINEKIT_PATH=${MACHINEKIT_PATH} \
-        -e MK_DEBUG_TESTS=${MK_DEBUG_TESTS} \
-        -e LC_ALL="POSIX" \
-        --rm=true mk_runtest /run_tests.sh
+if ${IS_PR}; then
+    if test ${cmd} = build_rip; then
+	# PR:  Run regression tests
+	#
+	# tests are run under a new container instead of chrooting
+	# this will allow us to run docker without using privileged mode
+
+	# create container using RIP rootfs
+	docker build -t mk_runtest .travis/mk_runtests
+
+	# run regressions
+	docker run \
+	    -e MACHINEKIT_PATH=${MACHINEKIT_PATH} \
+	    -e MK_DEBUG_TESTS=${MK_DEBUG_TESTS} \
+	    -e LC_ALL="POSIX" \
+	    --rm=true mk_runtest /run_tests.sh
+    fi
+else
+    if test ${cmd} != build_rip; then
+	# Merge:  Upload packages to packagecloud, if applicable
+	if test -n "${PACKAGECLOUD_USER}"; then
+	    PACKAGECLOUD_REPO=${PACKAGECLOUD_REPO:-machinekit}
+	    repo=${PACKAGECLOUD_USER}/${PACKAGECLOUD_REPO}/debian/${DISTRO}
+	    package_cloud push ${repo} ${TRAVIS_BUILD_DIR}/deploy/*deb
+	    if test ${MARCH} = 64; then
+		package_cloud push ${repo} ${TRAVIS_BUILD_DIR}/deploy/*dsc
+	    fi
+	fi	
+    fi
 fi
