ikcalB/linuxcnc-mirror

hal: factor out streamer/sampler
.. so that hypothetically these can be used to stream to or sample from
a special-purpose component like a serial device.

Since the runtests use sampler and streamer extensively, I think that's
a good sign that the code is right.

Signed-off-by: Jeff Epler <jepler@unpythonic.net>

src/hal/hal_lib.c
@@ -70,6 +70,7 @@ MODULE_LICENSE("GPL");
 #if defined(ULAPI)
 #include <sys/types.h>		/* pid_t */
 #include <unistd.h>		/* getpid() */
+#include <time.h>
 #endif
 
 char *hal_shmem_base = 0;
@@ -3419,6 +3420,259 @@ static void free_thread_struct(hal_thread_t * thread)
 }
 #endif /* RTAPI */
 
+static char *halpr_type_string(int type, char *buf, size_t nbuf) {
+    switch(type) {
+        case HAL_BIT: return "bit";
+        case HAL_FLOAT: return "float";
+        case HAL_S32: return "s32";
+        case HAL_U32: return "u32";
+        default:
+            rtapi_snprintf(buf, nbuf, "UNK#%d", type);
+            return buf;
+    }
+}
+
+int halpr_parse_types(hal_type_t type[HAL_STREAM_MAX_PINS], const char *cfg)
+{
+    const char *c;
+    int n;
+
+    c = cfg;
+    n = 0;
+    while (( n < HAL_STREAM_MAX_PINS ) && ( *c != '\0' )) {
+	switch (*c) {
+	case 'f':
+	case 'F':
+	    type[n++] = HAL_FLOAT;
+	    c++;
+	    break;
+	case 'b':
+	case 'B':
+	    type[n++] = HAL_BIT;
+	    c++;
+	    break;
+	case 'u':
+	case 'U':
+	    type[n++] = HAL_U32;
+	    c++;
+	    break;
+	case 's':
+	case 'S':
+	    type[n++] = HAL_S32;
+	    c++;
+	    break;
+	default:
+	    rtapi_print_msg(RTAPI_MSG_ERR,
+		"stream: ERROR: unknown type '%c', must be F, B, U, or S\n", *c);
+	    return 0;
+	}
+    }
+    if ( *c != '\0' ) {
+	/* didn't reach end of cfg string */
+	rtapi_print_msg(RTAPI_MSG_ERR,
+	    "stream: ERROR: more than %d items\n", HAL_STREAM_MAX_PINS);
+	return 0;
+    }
+    return n;
+}
+
+int hal_stream_create(hal_stream_t *stream, int comp, int key, int depth, const char *typestring)
+{
+    int result = 0;
+    hal_type_t type[HAL_STREAM_MAX_PINS];
+    result = halpr_parse_types(type, typestring);
+    if(result < 0) return result;
+    int pin_count = result;
+
+    size_t size = sizeof(struct hal_stream_shm) + sizeof(union hal_stream_data) * depth * (1+pin_count);
+    result = rtapi_shmem_new(key, comp, size);
+    if(result < 0) return result;
+    stream->shmem_id = result;
+
+    result = rtapi_shmem_getptr(stream->shmem_id, (void**)&stream->fifo);
+    if(result < 0) {
+        rtapi_shmem_delete(key, comp);
+        return result;
+    }
+    memset(stream->fifo, 0, sizeof(*stream->fifo));
+
+    stream->fifo->depth = depth;
+    stream->fifo->num_pins = pin_count;
+    memcpy(stream->fifo->type, type, sizeof(type));
+    stream->comp_id = comp;
+    stream->fifo->magic = HAL_STREAM_MAGIC_NUM;
+    return 0;
+}
+
+extern void hal_stream_destroy(hal_stream_t *stream)
+{
+    hal_stream_detach(stream);
+}
+
+static int hal_stream_advance(hal_stream_t *stream, int n) {
+    n = n + 1;
+    if(n >= stream->fifo->depth) n = 0;
+    return n;
+}
+
+static int hal_stream_newin(hal_stream_t *stream) {
+    return hal_stream_advance(stream, stream->fifo->in);
+}
+
+bool hal_stream_writable(hal_stream_t *stream) {
+    return hal_stream_newin(stream) != stream->fifo->out;
+}
+
+bool hal_stream_readable(hal_stream_t *stream) {
+    return stream->fifo->in != stream->fifo->out;
+}
+
+int hal_stream_depth(hal_stream_t *stream) {
+    int out = stream->fifo->out;
+    int in = stream->fifo->in;
+    int result = in - out;
+    if(result < 0) result += stream->fifo->depth - 1;
+    return result;
+}
+
+int hal_stream_maxdepth(hal_stream_t *stream) {
+    return stream->fifo->depth;
+}
+
+#ifdef ULAPI
+void hal_stream_wait_writable(hal_stream_t *stream, sig_atomic_t *stop) {
+    while(!hal_stream_writable(stream) && (!stop || !*stop)) {
+        /* fifo full, sleep for 10mS */
+        struct timespec delay = { .tv_sec = 0, .tv_nsec = 10000000};
+        clock_nanosleep(CLOCK_MONOTONIC, 0, &delay, NULL);
+    }
+}
+
+void hal_stream_wait_readable(hal_stream_t *stream, sig_atomic_t *stop) {
+    while(!hal_stream_readable(stream) && (!stop || !*stop)) {
+        /* fifo full, sleep for 10mS */
+        struct timespec delay = { .tv_sec = 0, .tv_nsec = 10000000};
+        nanosleep(&delay, NULL);
+    }
+}
+#endif
+
+int hal_stream_write(hal_stream_t *stream, union hal_stream_data *buf) {
+    if(!hal_stream_writable(stream)) {
+        stream->fifo->num_overruns++;
+        return -ENOSPC;
+    }
+    int in = stream->fifo->in, newin = hal_stream_advance(stream, in);
+    int num_pins = stream->fifo->num_pins;
+    int stride = num_pins + 1;
+    union hal_stream_data *dptr = &stream->fifo->data[in * stride];
+    memcpy(dptr, buf, sizeof(union hal_stream_data) * num_pins);
+    dptr[num_pins].s = ++stream->fifo->this_sample;
+    stream->fifo->in = newin;
+    return 0;
+}
+
+int hal_stream_read(hal_stream_t *stream, union hal_stream_data *buf, unsigned *this_sample) {
+    if(!hal_stream_readable(stream)) {
+        stream->fifo->num_underruns ++;
+        return -ENOSPC;
+    }
+    int out = stream->fifo->out, newout = hal_stream_advance(stream, out);
+    int num_pins = stream->fifo->num_pins;
+    int stride = num_pins + 1;
+    union hal_stream_data *dptr = &stream->fifo->data[out * stride];
+    memcpy(buf, dptr, sizeof(union hal_stream_data) * num_pins);
+    if(this_sample) *this_sample = dptr[num_pins].s;
+    stream->fifo->out = newout;
+    return 0;
+}
+
+int hal_stream_attach(hal_stream_t *stream, int comp_id, int key, const char *typestring) {
+    int i;
+
+    stream->shmem_id = stream->comp_id = -1;
+    stream->fifo = NULL;
+    memset(stream, 0, sizeof(*stream));
+    int result = rtapi_shmem_new(key, comp_id, sizeof(struct hal_stream_shm));
+    int shmem_id = result;
+    if ( result < 0 ) goto fail;
+
+    void *shmem_ptr;
+    result = rtapi_shmem_getptr(shmem_id, &shmem_ptr);
+    if ( result < 0 ) goto fail;
+
+    struct hal_stream_shm *fifo = shmem_ptr;
+    if ( fifo->magic != HAL_STREAM_MAGIC_NUM ) {
+        result = -EINVAL;
+        goto fail;
+    }
+    if(typestring) {
+        hal_type_t type[HAL_STREAM_MAX_PINS];
+        result = halpr_parse_types(type, typestring);
+        if(!result) { result = -EINVAL; goto fail; }
+        for(i=0; i<result; i++) {
+            if(type[i] != fifo->type[i]) {
+                char typename0[8], typename1[8];
+                rtapi_print_msg(RTAPI_MSG_ERR,
+                    "Type mismatch: types[%d] = %s vs %s\n", i,
+                        halpr_type_string(fifo->type[i],
+                            typename0, sizeof(typename0)),
+                        halpr_type_string(type[i],
+                            typename1, sizeof(typename1)));
+                result = -EINVAL; goto fail;
+            }
+        }
+    }
+    /* now use data in fifo structure to calculate proper shmem size */
+    int depth = fifo->depth;
+    int pin_count = fifo->num_pins;
+    size_t size = sizeof(struct hal_stream_shm) + sizeof(union hal_stream_data) * depth * (1+pin_count);
+    /* close shmem, re-open with proper size */
+    rtapi_shmem_delete(shmem_id, comp_id);
+    shmem_id = result = rtapi_shmem_new(key, comp_id, size);
+    if ( result < 0 ) goto fail;
+    result = rtapi_shmem_getptr(shmem_id, &shmem_ptr);
+    if ( result < 0 ) goto fail;
+
+    stream->shmem_id = shmem_id;
+    stream->comp_id = comp_id;
+    stream->fifo = fifo;
+    return 0;
+
+fail:
+    if(shmem_id >= 0)
+        rtapi_shmem_delete(shmem_id, comp_id);
+    return result;
+}
+
+int hal_stream_detach(hal_stream_t *stream) {
+    if(stream->shmem_id >= 0) {
+        int res = rtapi_shmem_delete(stream->shmem_id, stream->comp_id);
+        if(res < 0)
+            rtapi_print_msg(RTAPI_MSG_ERR,
+                "hal_stream_detach: rtapi_shmem_delete: failed with code %d\n",
+                res);
+    }
+    stream->shmem_id = stream->comp_id = -1;
+    stream->fifo = NULL;
+    return 0;
+}
+
+int hal_stream_element_count(hal_stream_t *stream) {
+    return stream->fifo->num_pins;
+}
+
+hal_type_t hal_stream_element_type(hal_stream_t *stream, int idx) {
+    return stream->fifo->type[idx];
+}
+
+int hal_stream_num_overruns(hal_stream_t *stream) {
+    return stream->fifo->num_overruns;
+}
+
+int hal_stream_num_underruns(hal_stream_t *stream) {
+    return stream->fifo->num_underruns;
+}
 
 #ifdef RTAPI
 /* only export symbols when we're building a kernel module */
@@ -3490,4 +3744,17 @@ EXPORT_SYMBOL(halpr_find_funct_by_owner);
 
 EXPORT_SYMBOL(halpr_find_pin_by_sig);
 
+EXPORT_SYMBOL(hal_stream_create);
+EXPORT_SYMBOL(hal_stream_destroy);
+EXPORT_SYMBOL(hal_stream_readable);
+EXPORT_SYMBOL(hal_stream_depth);
+EXPORT_SYMBOL(hal_stream_maxdepth);
+EXPORT_SYMBOL(hal_stream_write);
+EXPORT_SYMBOL(hal_stream_read);
+EXPORT_SYMBOL(hal_stream_attach);
+EXPORT_SYMBOL(hal_stream_detach);
+EXPORT_SYMBOL(hal_stream_element_count);
+EXPORT_SYMBOL(hal_stream_element_type);
+EXPORT_SYMBOL(hal_stream_num_overruns);
+EXPORT_SYMBOL(hal_stream_num_underruns);
 #endif /* rtapi */

LinuxCNC/linuxcnc

src/hal/hal_lib.c
@@ -58,7 +58,7 @@
 #include "hal_priv.h"		/* HAL private decls */
 
 #include "rtapi_string.h"
-#include <rtapi_mutex.h>
+#include "rtapi_atomic.h"
 
 #ifdef RTAPI
 #include "rtapi_app.h"
@@ -71,6 +71,7 @@ MODULE_LICENSE("GPL");
 #if defined(ULAPI)
 #include <sys/types.h>		/* pid_t */
 #include <unistd.h>		/* getpid() */
+#include <time.h>
 #endif
 
 char *hal_shmem_base = 0;
@@ -3425,6 +3426,282 @@ static void free_thread_struct(hal_thread_t * thread)
 }
 #endif /* RTAPI */
 
+static char *halpr_type_string(int type, char *buf, size_t nbuf) {
+    switch(type) {
+        case HAL_BIT: return "bit";
+        case HAL_FLOAT: return "float";
+        case HAL_S32: return "s32";
+        case HAL_U32: return "u32";
+        default:
+            rtapi_snprintf(buf, nbuf, "UNK#%d", type);
+            return buf;
+    }
+}
+
+int halpr_parse_types(hal_type_t type[HAL_STREAM_MAX_PINS], const char *cfg)
+{
+    const char *c;
+    int n;
+
+    c = cfg;
+    n = 0;
+    while (( n < HAL_STREAM_MAX_PINS ) && ( *c != '\0' )) {
+	switch (*c) {
+	case 'f':
+	case 'F':
+	    type[n++] = HAL_FLOAT;
+	    c++;
+	    break;
+	case 'b':
+	case 'B':
+	    type[n++] = HAL_BIT;
+	    c++;
+	    break;
+	case 'u':
+	case 'U':
+	    type[n++] = HAL_U32;
+	    c++;
+	    break;
+	case 's':
+	case 'S':
+	    type[n++] = HAL_S32;
+	    c++;
+	    break;
+	default:
+	    rtapi_print_msg(RTAPI_MSG_ERR,
+		"stream: ERROR: unknown type '%c', must be F, B, U, or S\n", *c);
+	    return 0;
+	}
+    }
+    if ( *c != '\0' ) {
+	/* didn't reach end of cfg string */
+	rtapi_print_msg(RTAPI_MSG_ERR,
+	    "stream: ERROR: more than %d items\n", HAL_STREAM_MAX_PINS);
+	return 0;
+    }
+    return n;
+}
+
+int hal_stream_create(hal_stream_t *stream, int comp, int key, int depth, const char *typestring)
+{
+    int result = 0;
+    hal_type_t type[HAL_STREAM_MAX_PINS];
+    result = halpr_parse_types(type, typestring);
+    if(result < 0) return result;
+    int pin_count = result;
+
+    size_t size = sizeof(struct hal_stream_shm) + sizeof(union hal_stream_data) * depth * (1+pin_count);
+    result = rtapi_shmem_new(key, comp, size);
+    if(result < 0) return result;
+    stream->shmem_id = result;
+
+    result = rtapi_shmem_getptr(stream->shmem_id, (void**)&stream->fifo);
+    if(result < 0) {
+        rtapi_shmem_delete(key, comp);
+        return result;
+    }
+    memset(stream->fifo, 0, sizeof(*stream->fifo));
+
+    stream->fifo->depth = depth;
+    stream->fifo->num_pins = pin_count;
+    memcpy(stream->fifo->type, type, sizeof(type));
+    stream->comp_id = comp;
+    stream->fifo->magic = HAL_STREAM_MAGIC_NUM;
+    return 0;
+}
+
+extern void hal_stream_destroy(hal_stream_t *stream)
+{
+    hal_stream_detach(stream);
+}
+
+static int hal_stream_advance(hal_stream_t *stream, int n) {
+    n = n + 1;
+    if(n >= stream->fifo->depth) n = 0;
+    return n;
+}
+
+static int hal_stream_newin(hal_stream_t *stream) {
+    return hal_stream_advance(stream, stream->fifo->in);
+}
+
+bool hal_stream_writable(hal_stream_t *stream) {
+    return hal_stream_newin(stream) != stream->fifo->out;
+}
+
+bool hal_stream_readable(hal_stream_t *stream) {
+    return stream->fifo->in != stream->fifo->out;
+}
+
+int hal_stream_depth(hal_stream_t *stream) {
+    int out = stream->fifo->out;
+    int in = stream->fifo->in;
+    int result = in - out;
+    if(result < 0) result += stream->fifo->depth - 1;
+    return result;
+}
+
+int hal_stream_maxdepth(hal_stream_t *stream) {
+    return stream->fifo->depth;
+}
+
+#ifdef ULAPI
+void hal_stream_wait_writable(hal_stream_t *stream, sig_atomic_t *stop) {
+    while(!hal_stream_writable(stream) && (!stop || !*stop)) {
+        /* fifo full, sleep for 10mS */
+        struct timespec delay = { .tv_sec = 0, .tv_nsec = 10000000};
+        clock_nanosleep(CLOCK_MONOTONIC, 0, &delay, NULL);
+    }
+}
+
+void hal_stream_wait_readable(hal_stream_t *stream, sig_atomic_t *stop) {
+    while(!hal_stream_readable(stream) && (!stop || !*stop)) {
+        /* fifo full, sleep for 10mS */
+        struct timespec delay = { .tv_sec = 0, .tv_nsec = 10000000};
+        nanosleep(&delay, NULL);
+    }
+}
+#endif
+
+static int hal_stream_atomic_load_in(hal_stream_t *stream)
+{
+    return atomic_load_explicit(&stream->fifo->in, memory_order_acquire);
+}
+
+static int hal_stream_atomic_load_out(hal_stream_t *stream)
+{
+    return atomic_load_explicit(&stream->fifo->out, memory_order_acquire);
+}
+
+
+static void hal_stream_atomic_store_in(hal_stream_t *stream, int newin)
+{
+    atomic_store_explicit(&stream->fifo->in, newin, memory_order_release);
+}
+
+static void hal_stream_atomic_store_out(hal_stream_t *stream, int newout)
+{
+    atomic_store_explicit(&stream->fifo->out, newout, memory_order_release);
+}
+
+int hal_stream_write(hal_stream_t *stream, union hal_stream_data *buf) {
+    if(!hal_stream_writable(stream)) {
+        stream->fifo->num_overruns++;
+        return -ENOSPC;
+    }
+    int in = hal_stream_atomic_load_in(stream),
+        newin = hal_stream_advance(stream, in);
+    int num_pins = stream->fifo->num_pins;
+    int stride = num_pins + 1;
+    union hal_stream_data *dptr = &stream->fifo->data[in * stride];
+    memcpy(dptr, buf, sizeof(union hal_stream_data) * num_pins);
+    dptr[num_pins].s = ++stream->fifo->this_sample;
+    hal_stream_atomic_store_in(stream, newin);
+    return 0;
+}
+
+int hal_stream_read(hal_stream_t *stream, union hal_stream_data *buf, unsigned *this_sample) {
+    if(!hal_stream_readable(stream)) {
+        stream->fifo->num_underruns ++;
+        return -ENOSPC;
+    }
+    int out = hal_stream_atomic_load_out(stream),
+        newout = hal_stream_advance(stream, out);
+    int num_pins = stream->fifo->num_pins;
+    int stride = num_pins + 1;
+    union hal_stream_data *dptr = &stream->fifo->data[out * stride];
+    memcpy(buf, dptr, sizeof(union hal_stream_data) * num_pins);
+    if(this_sample) *this_sample = dptr[num_pins].s;
+    hal_stream_atomic_store_out(stream, newout);
+    return 0;
+}
+
+int hal_stream_attach(hal_stream_t *stream, int comp_id, int key, const char *typestring) {
+    int i;
+
+    stream->shmem_id = stream->comp_id = -1;
+    stream->fifo = NULL;
+    memset(stream, 0, sizeof(*stream));
+    int result = rtapi_shmem_new(key, comp_id, sizeof(struct hal_stream_shm));
+    int shmem_id = result;
+    if ( result < 0 ) goto fail;
+
+    void *shmem_ptr;
+    result = rtapi_shmem_getptr(shmem_id, &shmem_ptr);
+    if ( result < 0 ) goto fail;
+
+    struct hal_stream_shm *fifo = shmem_ptr;
+    if ( fifo->magic != HAL_STREAM_MAGIC_NUM ) {
+        result = -EINVAL;
+        goto fail;
+    }
+    if(typestring) {
+        hal_type_t type[HAL_STREAM_MAX_PINS];
+        result = halpr_parse_types(type, typestring);
+        if(!result) { result = -EINVAL; goto fail; }
+        for(i=0; i<result; i++) {
+            if(type[i] != fifo->type[i]) {
+                char typename0[8], typename1[8];
+                rtapi_print_msg(RTAPI_MSG_ERR,
+                    "Type mismatch: types[%d] = %s vs %s\n", i,
+                        halpr_type_string(fifo->type[i],
+                            typename0, sizeof(typename0)),
+                        halpr_type_string(type[i],
+                            typename1, sizeof(typename1)));
+                result = -EINVAL; goto fail;
+            }
+        }
+    }
+    /* now use data in fifo structure to calculate proper shmem size */
+    int depth = fifo->depth;
+    int pin_count = fifo->num_pins;
+    size_t size = sizeof(struct hal_stream_shm) + sizeof(union hal_stream_data) * depth * (1+pin_count);
+    /* close shmem, re-open with proper size */
+    rtapi_shmem_delete(shmem_id, comp_id);
+    shmem_id = result = rtapi_shmem_new(key, comp_id, size);
+    if ( result < 0 ) goto fail;
+    result = rtapi_shmem_getptr(shmem_id, &shmem_ptr);
+    if ( result < 0 ) goto fail;
+
+    stream->shmem_id = shmem_id;
+    stream->comp_id = comp_id;
+    stream->fifo = fifo;
+    return 0;
+
+fail:
+    if(shmem_id >= 0)
+        rtapi_shmem_delete(shmem_id, comp_id);
+    return result;
+}
+
+int hal_stream_detach(hal_stream_t *stream) {
+    if(stream->shmem_id >= 0) {
+        int res = rtapi_shmem_delete(stream->shmem_id, stream->comp_id);
+        if(res < 0)
+            rtapi_print_msg(RTAPI_MSG_ERR,
+                "hal_stream_detach: rtapi_shmem_delete: failed with code %d\n",
+                res);
+    }
+    stream->shmem_id = stream->comp_id = -1;
+    stream->fifo = NULL;
+    return 0;
+}
+
+int hal_stream_element_count(hal_stream_t *stream) {
+    return stream->fifo->num_pins;
+}
+
+hal_type_t hal_stream_element_type(hal_stream_t *stream, int idx) {
+    return stream->fifo->type[idx];
+}
+
+int hal_stream_num_overruns(hal_stream_t *stream) {
+    return stream->fifo->num_overruns;
+}
+
+int hal_stream_num_underruns(hal_stream_t *stream) {
+    return stream->fifo->num_underruns;
+}
 
 #ifdef RTAPI
 /* only export symbols when we're building a kernel module */
@@ -3498,4 +3775,17 @@ EXPORT_SYMBOL(halpr_find_pin_by_sig);
 
 EXPORT_SYMBOL(hal_pin_alias);
 EXPORT_SYMBOL(hal_param_alias);
+EXPORT_SYMBOL_GPL(hal_stream_create);
+EXPORT_SYMBOL_GPL(hal_stream_destroy);
+EXPORT_SYMBOL_GPL(hal_stream_readable);
+EXPORT_SYMBOL_GPL(hal_stream_depth);
+EXPORT_SYMBOL_GPL(hal_stream_maxdepth);
+EXPORT_SYMBOL_GPL(hal_stream_write);
+EXPORT_SYMBOL_GPL(hal_stream_read);
+EXPORT_SYMBOL_GPL(hal_stream_attach);
+EXPORT_SYMBOL_GPL(hal_stream_detach);
+EXPORT_SYMBOL_GPL(hal_stream_element_count);
+EXPORT_SYMBOL_GPL(hal_stream_element_type);
+EXPORT_SYMBOL_GPL(hal_stream_num_overruns);
+EXPORT_SYMBOL_GPL(hal_stream_num_underruns);
 #endif /* rtapi */