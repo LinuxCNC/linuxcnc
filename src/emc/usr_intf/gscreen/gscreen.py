9d12ce61ee1aad3708c38cf9ddc5ead501d8249b
Dewey Garrett <dgarrett@panix.com>
2016-04-05 17:15:58 -0700

    gscreen update for joints_axes +debugging aids  JA
    1) try: init_glcanondraw() for all configs (formerly
       was attempted only for some configs)
    
    2) Add tests to limit error messages that occur on the
       sim config configs/sim/gscreen/gscreen_custom/9axis.ini
       (The problem may be with the sim config not with gscreen.py)
       The messages also may occur in debugging defective configs
       and the rapid and persistent issue of messages seems
       to hang the system attempting to handle unlimited, repeated
       popups.
    
       Tests use new globals:
         excepthook_msg_ct = 0
         excepthook_msg_ct_max = 10
         update_spindle_bar_error_ct = 0
         update_spindle_bar_error_ct_max = 3
    
    Ref: configs/sim/gscreen/gscreen_custom/9axis.ini
      repeatedly issues messages about 'No widget 's_display2'
      same behavior in git master (669f5fe)
    
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- src/emc/usr_intf/gscreen/gscreen.py
+++ src/emc/usr_intf/gscreen/gscreen.py
@@ -60,6 +60,15 @@ import time
 from time import strftime,localtime
 import hal_glib
 
+#--------------------------------------------------------
+# limit number of times err msgs are displayed
+excepthook_msg_ct = 0
+excepthook_msg_ct_max = 10
+
+update_spindle_bar_error_ct = 0
+update_spindle_bar_error_ct_max = 3
+#--------------------------------------------------------
+
 # try to add a notify system so messages use the
 # nice intergrated pop-ups
 # Ubuntu kinda wrecks this be not following the
@@ -150,15 +159,22 @@ def excepthook(exc_type, exc_obj, exc_tb):
     except NameError:
         w = None
     lines = traceback.format_exception(exc_type, exc_obj, exc_tb)
-    m = gtk.MessageDialog(w,
+    global excepthook_msg_ct,excepthook_msg_ct_max
+    excepthook_msg_ct += 1
+    if excepthook_msg_ct < excepthook_msg_ct_max:
+        print "*******************************************************\n",excepthook_msg_ct
+        print "".join(lines)
+        print "*******************************************************\n",excepthook_msg_ct
+    if excepthook_msg_ct < 1:
+        m = gtk.MessageDialog(w,
                 gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                 gtk.MESSAGE_ERROR, gtk.BUTTONS_OK,
                 ("Gscreen encountered an error.  The following "
                 "information may be useful in troubleshooting:\n\n")
                 + "".join(lines))
-    m.show()
-    m.run()
-    m.destroy()
+        m.show()
+        m.run()
+        m.destroy()
 sys.excepthook = excepthook
 
 # constants
@@ -753,6 +769,13 @@ class Gscreen:
         else:
             self.initialize_widgets()
 
+        try:
+            self.widgets.gremlin.init_glcanondraw(
+                trajcoordinates = self.inifile.find("TRAJ", "COORDINATES"),
+                kinstype = self.inifile.find("KINS", "KINEMATICS"))
+        except:
+            print ("**** GSCREEN NOTE: could not init_glcanondraw for gremlin")
+
         # see if there are user messages in the ini file 
         self.message_setup()
 
@@ -1180,12 +1203,9 @@ class Gscreen:
         """ initalizes the plot screen options from the data class
             expects the plot widget to be called gremlin
         """ 
-        self.widgets.gremlin.init_glcanondraw(
-                trajcoordinates = self.inifile.find("TRAJ", "COORDINATES"),
-                kinstype = self.inifile.find("KINS", "KINEMATICS"))
         self.widgets.show_offsets.set_active( self.data.show_offsets )
         self.widgets.gremlin.show_offsets = self.data.show_offsets
-        self.widgets.grid_size.set_value(self.data.grid_size) 
+        self.widgets.grid_size.set_value(self.data.grid_size)
         self.widgets.gremlin.grid_size = self.data.grid_size
         self.widgets.gremlin.set_property('view',self.data.plot_view[0])
         self.widgets.gremlin.set_property('metric_units',(self.data.dro_units == self.data._MM))
@@ -4404,7 +4424,12 @@ class Gscreen:
         try:
             self.widgets.s_display2.set_value(abs(self.data.spindle_speed))
         except:
-            self.show_try_errors()
+            global update_spindle_bar_error_ct,update_spindle_bar_error_ct_max
+            if update_spindle_bar_error_ct < update_spindle_bar_error_ct_max:
+                print "%2d/%2d update_spindle_bar error"%(
+                       update_spindle_bar_error_ct,update_spindle_bar_error_ct_max)
+                self.show_try_errors()
+                update_spindle_bar_error_ct += 1
 
     def update_dro(self):
         # DRO
