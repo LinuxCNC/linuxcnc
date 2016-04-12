4f9d779c62dd0958be22f58eb42c513dc4842894
Dewey Garrett <dgarrett@panix.com>
2016-04-11 17:48:05 -0700

    axis.py restore task_mode after touchoff if    JA
    no open file.  (can occur using [DISPLAY]OPEN_FILE="")
    reload_file() --> get_mode_from_tab() --> ensure_manual
    
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- src/emc/usr_intf/axis/scripts/axis.py
+++ src/emc/usr_intf/axis/scripts/axis.py
@@ -2575,6 +2575,7 @@ class TclCommands(nf.TclCommands):
         else:
             system = vars.touch_off_system.get()
         if new_axis_value is None: return
+        save_task_mode = s.task_mode
         vars.touch_off_system.set(system)
         ensure_mode(linuxcnc.MODE_MDI)
         s.poll()
@@ -2594,6 +2595,7 @@ class TclCommands(nf.TclCommands):
         s.poll()
         o.tkRedraw()
         reload_file(False)
+        ensure_mode(save_task_mode)
 
     def touch_off_tool(event=None, new_axis_value = None):
         global system
@@ -2620,6 +2622,7 @@ class TclCommands(nf.TclCommands):
         else:
             system = vars.touch_off_system.get()
         if new_axis_value is None: return
+        save_task_mode = s.task_mode
         vars.touch_off_system.set(system)
         ensure_mode(linuxcnc.MODE_MDI)
         s.poll()
@@ -2642,6 +2645,7 @@ class TclCommands(nf.TclCommands):
         s.poll()
         o.tkRedraw()
         reload_file(False)
+        ensure_mode(save_task_mode)
 
     def set_axis_offset(event=None):
         commands.touch_off_system(new_axis_value=0.)
