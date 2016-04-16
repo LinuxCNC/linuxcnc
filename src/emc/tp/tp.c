f2439a5ab132e7626483a28a757ddd849abd3042
Robert W. Ellenberg <rwe24g@gmail.com>
2016-04-16 08:55:35 -0400

    tp: Added missing tag in rigid tap initialization
    (cherry picked from commit 5640f2e56684235cd90ef7d769283ba932132572)
    Signed-off-by: John Morris <john@zultron.com>

--- src/emc/tp/tp.c
+++ src/emc/tp/tp.c
@@ -1486,6 +1486,7 @@ int tpAddRigidTap(TP_STRUCT * const tp,
             tp->cycleTime,
             enables,
             1);
+    tc.tag = tag;
 
     // Setup any synced IO for this move
     tpSetupSyncedIO(tp, &tc);
