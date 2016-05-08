57fdb553a1418f39f52caf4774e7758657d1b840
Michael Haberler <git@mah.priv.at>
2016-05-08 21:46:01 +0200

    XYZACkins: adapt to vtable API

--- src/emc/kinematics/XYZACkins.c
+++ src/emc/kinematics/XYZACkins.c
@@ -15,67 +15,69 @@
 #include "rtapi.h"
 #include "rtapi_math.h"
 
+#define VTVERSION VTKINEMATICS_VERSION1
+
 struct haldata {
     hal_float_t *Y_offset;
     hal_float_t *Z_offset;
     hal_float_t *Tool_offset;
 } *haldata;
 
-int kinematicsForward(const double *joints,
-		      EmcPose * pos,
-		      const KINEMATICS_FORWARD_FLAGS * fflags,
-		      KINEMATICS_INVERSE_FLAGS * iflags)
+static int kinematicsForward(const double *joints,
+			     EmcPose * pos,
+			     const KINEMATICS_FORWARD_FLAGS * fflags,
+			     KINEMATICS_INVERSE_FLAGS * iflags)
 {
     double dy = *(haldata->Y_offset);
     double dz = *(haldata->Z_offset);
     double dT = *(haldata->Tool_offset);
-    double a_rad = joints[3]*M_PI/180;
-    double c_rad = joints[5]*M_PI/180;
+    double a_rad = joints[3] * M_PI/180;
+    double c_rad = joints[5] * M_PI/180;
     dz = dz + dT;
-    pos->tran.x = cos(c_rad)*(joints[0]) +
-	sin(c_rad)*cos(a_rad)*(joints[1] - dy) +
-	sin(c_rad)*sin(a_rad)*(joints[2] - dz) +
-	sin(c_rad)*dy;
-    pos->tran.y = -sin(c_rad)*(joints[0]) +
-	cos(c_rad)*cos(a_rad)*(joints[1] - dy) +
-	cos(c_rad)*sin(a_rad)*(joints[2] - dz) +
-	cos(c_rad)*dy;
-    pos->tran.z = -sin(a_rad)*(joints[1] - dy) +
-	cos(a_rad)*(joints[2] - dz) + dz;
+    pos->tran.x = rtapi_cos(c_rad) * (joints[0]) +
+	rtapi_sin(c_rad) * rtapi_cos(a_rad) * (joints[1] - dy) +
+	rtapi_sin(c_rad) * rtapi_sin(a_rad) * (joints[2] - dz) +
+	rtapi_sin(c_rad) * dy;
+    pos->tran.y = -rtapi_sin(c_rad) * (joints[0]) +
+	rtapi_cos(c_rad) * rtapi_cos(a_rad) * (joints[1] - dy) +
+	rtapi_cos(c_rad) * rtapi_sin(a_rad) * (joints[2] - dz) +
+	rtapi_cos(c_rad) * dy;
+    pos->tran.z = -rtapi_sin(a_rad) * (joints[1] - dy) +
+	rtapi_cos(a_rad) * (joints[2] - dz) + dz;
     pos->a = joints[3];
     pos->b = joints[4];
     pos->c = joints[5];
     return 0;
 }
 
-int kinematicsInverse(const EmcPose * pos,
-		      double *joints,
-		      const KINEMATICS_INVERSE_FLAGS * iflags,
-		      KINEMATICS_FORWARD_FLAGS * fflags)
+static int kinematicsInverse(const EmcPose * pos,
+			     double *joints,
+			     const KINEMATICS_INVERSE_FLAGS * iflags,
+			     KINEMATICS_FORWARD_FLAGS * fflags)
 {
     double dy = *(haldata->Y_offset);
     double dz = *(haldata->Z_offset);
     double dT = *(haldata->Tool_offset);
-    double c_rad = pos->c*M_PI/180;
-    double a_rad = pos->a*M_PI/180;
+    double c_rad = pos->c * M_PI/180;
+    double a_rad = pos->a * M_PI/180;
     dz = dz + dT;
-    joints[0] = cos(c_rad)*pos->tran.x -
-	sin(c_rad)*pos->tran.y;
-    joints[1] = sin(c_rad)*cos(a_rad)*pos->tran.x +
-	cos(c_rad)*cos(a_rad)*pos->tran.y -
-	sin(a_rad)*pos->tran.z -
-	cos(a_rad)*dy + sin(a_rad)*dz + dy;
-    joints[2] = sin(c_rad)*sin(a_rad)*pos->tran.x +
-	cos(c_rad)*sin(a_rad)*pos->tran.y +
-	cos(a_rad)*pos->tran.z -
-	sin(a_rad)*dy - cos(a_rad)*dz + dz;
+    joints[0] = rtapi_cos(c_rad) * pos->tran.x -
+	rtapi_sin(c_rad) * pos->tran.y;
+    joints[1] = rtapi_sin(c_rad) * rtapi_cos(a_rad) * pos->tran.x +
+	rtapi_cos(c_rad) * rtapi_cos(a_rad) * pos->tran.y -
+	rtapi_sin(a_rad) * pos->tran.z -
+	rtapi_cos(a_rad) * dy + rtapi_sin(a_rad) * dz + dy;
+    joints[2] = rtapi_sin(c_rad) * rtapi_sin(a_rad) * pos->tran.x +
+	rtapi_cos(c_rad) * rtapi_sin(a_rad) * pos->tran.y +
+	rtapi_cos(a_rad) * pos->tran.z -
+	rtapi_sin(a_rad) * dy - rtapi_cos(a_rad) * dz + dz;
     joints[3] = pos->a;
     joints[4] = pos->b;
     joints[5] = pos->c;
     return 0;
 }
 
-KINEMATICS_TYPE kinematicsType()
+static KINEMATICS_TYPE kinematicsType()
 {
     return KINEMATICS_BOTH;
 }
@@ -83,27 +85,44 @@ KINEMATICS_TYPE kinematicsType()
 #include "rtapi.h" /* RTAPI realtime OS API */
 #include "rtapi_app.h" /* RTAPI realtime module decls */
 #include "hal.h"
-EXPORT_SYMBOL(kinematicsType);
-EXPORT_SYMBOL(kinematicsInverse);
-EXPORT_SYMBOL(kinematicsForward);
+
 MODULE_LICENSE("GPL");
 
-int comp_id;
+static vtkins_t vtk = {
+    .kinematicsForward = kinematicsForward,
+    .kinematicsInverse  = kinematicsInverse,
+    // .kinematicsHome = kinematicsHome,
+    .kinematicsType = kinematicsType
+};
 
-int rtapi_app_main(void) {
+static int comp_id, vtable_id;
+static const char *name = "XYZACkins";
+
+int rtapi_app_main(void)
+{
     int res = 0;
-    comp_id = hal_init("XYZACkins");
-    if(comp_id < 0) return comp_id;
+    comp_id = hal_init(name);
+    if (comp_id < 0) return comp_id;
+
     haldata = hal_malloc(sizeof(struct haldata));
-    if((res = hal_pin_float_new("XYZACkins.Tool-offset",
-				HAL_IN, &(haldata->Tool_offset),
-				comp_id)) < 0) goto error;
-    if((res = hal_pin_float_new("XYZACkins.Y-offset",
-				HAL_IO, &(haldata->Y_offset),
-				comp_id)) < 0) goto error;
-    if((res = hal_pin_float_new("XYZACkins.Z-offset",
-				HAL_IO, &(haldata->Z_offset),
-				comp_id)) < 0) goto error;
+
+    if (((res = hal_pin_newf(HAL_FLOAT, HAL_IN, (void **) &(haldata->Tool_offset),
+			     comp_id, "%s.Tool-offset", name)) < 0) ||
+	((res = hal_pin_newf(HAL_FLOAT, HAL_IN, (void **)  &(haldata->Y_offset),
+			     comp_id, "%s.Y-offset", name)) < 0) ||
+	((res = hal_pin_newf(HAL_FLOAT, HAL_IN, (void **)  &(haldata->Z_offset),
+			     comp_id, "%s.Z-offset", name)) < 0))
+	goto error;
+
+    vtable_id = hal_export_vtable(name, VTVERSION, &vtk, comp_id);
+
+    if (vtable_id < 0) {
+	rtapi_print_msg(RTAPI_MSG_ERR,
+			"%s: ERROR: hal_export_vtable(%s,%d,%p) failed: %d\n",
+			name, name, VTVERSION, &vtk, vtable_id );
+	return -ENOENT;
+    }
+
     hal_ready(comp_id);
     return 0;
  error:
