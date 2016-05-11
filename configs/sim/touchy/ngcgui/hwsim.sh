f80d04fa438f5a286b2e281a13f079f15069e979
Dewey Garrett <dgarrett@panix.com>
2016-05-11 13:46:45 -0700

    touchy: jog in teleop mode,fail if not identity JA
    Retains backwards compatibility to existing, supported configs prior
    to joints_axes which use KINEMATICS_IDENTITY
    
    Future work required if anyone wants to use this gui
    for non-identity kinematics or to support switching
    joint/teleop modes within the gui.
    
    Note: For wheel jogging in joint mode, connect corresponding
          (one-to-one) joint and axis letter pins:
    
          joint.N.jog* and axis.L.jog* pins
    
          (see configs/sim/touchy/ngcgui/hwsim.sh for typical connections)
    
    Note: For testing, the examples in configs/sim/touchy/ngcgui/
          provide sim_pin guis to simulate jog buttons and wheels
    
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- configs/sim/touchy/ngcgui/hwsim.sh
+++ configs/sim/touchy/ngcgui/hwsim.sh
@@ -2,19 +2,21 @@
 # simulate hardware buttons and jogwheel for touchy
 # Note: some pins are not used in some configs
 
-halcmd net :xen touchy.jog.wheel.x  joint.0.jog-enable
-halcmd net :yen touchy.jog.wheel.y  joint.1.jog-enable
-halcmd net :zen touchy.jog.wheel.z  joint.2.jog-enable
+# identity kins assumed: connect one-to-one joints & axis letters
+
+halcmd net :xen touchy.jog.wheel.x  joint.0.jog-enable axis.x.jog-enable
+halcmd net :yen touchy.jog.wheel.y  joint.1.jog-enable axis.y.jog-enable
+halcmd net :zen touchy.jog.wheel.z  joint.2.jog-enable axis.z.jog-enable
 
 halcmd net :scale  touchy.jog.wheel.increment
-halcmd net :scale  joint.0.jog-scale
-halcmd net :scale  joint.1.jog-scale
-halcmd net :scale  joint.2.jog-scale
+halcmd net :scale  joint.0.jog-scale axis.x.jog-scale
+halcmd net :scale  joint.1.jog-scale axis.y.jog-scale
+halcmd net :scale  joint.2.jog-scale axis.z.jog-scale
 
 halcmd net :counts touchy.wheel-counts
-halcmd net :counts joint.0.jog-counts
-halcmd net :counts joint.1.jog-counts
-halcmd net :counts joint.2.jog-counts
+halcmd net :counts joint.0.jog-counts axis.x.jog-counts
+halcmd net :counts joint.1.jog-counts axis.y.jog-counts
+halcmd net :counts joint.2.jog-counts axis.z.jog-counts
 
 sim_pin \
 touchy.cycle-start/mode=pulse \
@@ -27,4 +29,5 @@ touchy.cycle-start/mode=pulse \
   touchy.jog.continuous.y.positive/mode=hold \
   touchy.jog.continuous.z.negative/mode=hold \
   touchy.jog.continuous.z.positive/mode=hold \
-  :counts/mode=pulse
+  :counts/mode=pulse \
+  >/dev/null
