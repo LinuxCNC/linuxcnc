9fc5bdefa8df6d7178cbfd57218f950eda8e5e0a
Dewey Garrett <dgarrett@panix.com>
2016-05-14 13:11:19 -0700

    axis.py set_motion_teleop in update() polling   JA
    followup to automated switch-to-teleop in motion merge
    
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- src/emc/usr_intf/axis/scripts/axis.py
+++ src/emc/usr_intf/axis/scripts/axis.py
@@ -744,6 +744,14 @@ class LivePlotter:
             del self.stat
             return
 
+        if  (    all_homed
+            and (self.stat.motion_mode != linuxcnc.TRAJ_MODE_FREE)
+            and (self.stat.task_mode   != linuxcnc.MODE_AUTO)
+            ):
+            try:
+                set_motion_teleop(1)
+            except: pass
+
         # With joints_axes support and jogging while mdi
         #  (ref: emctaskmain.cc:allow_while_idle_type())
         # behavior is better without changing tabs upon detecting
