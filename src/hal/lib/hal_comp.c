7755638c2b565af44d10ce8694dddbe4e37decbc
Michael Haberler <git@mah.priv.at>
2016-06-13 22:20:53 +0200

    HAL/newinst: generalize and dicument argument passing
    API change, but negligable fallout.
    
    Upside - argument parsing becomes easier - one can use getopt(3).

--- src/hal/lib/hal_comp.c
+++ src/hal/lib/hal_comp.c
@@ -613,7 +613,7 @@ static int create_instance(const hal_funct_args_t *fa)
 	HALERR("instance '%s' already exists", iname);
 	return -EBUSY;
     }
-    return comp->ctor(iname, argc - 2, &argv[2]);
+    return comp->ctor(argc, argv);
 }
 
 static int delete_instance(const hal_funct_args_t *fa)
