4bb283d776509a953d8cbdc4cdd25e97cf50e5a5
Jeff Epler <jepler@unpythonic.net>
2016-06-28 19:55:59 -0500

    halmodule: better check for HAL being initialized
    .. the old check, SHMPTR(0), will become a compile error
    soon since this file is C++ and 0 carries no type information.
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- src/hal/halmodule.cc
+++ src/hal/halmodule.cc
@@ -760,7 +760,7 @@ static PyObject * pyhal_pin_new(halitem * pin, const char * name) {
 PyObject *pin_has_writer(PyObject *self, PyObject *args) {
     char *name;
     if(!PyArg_ParseTuple(args, "s", &name)) return NULL;
-    if(!SHMPTR(0)) {
+    if(!hal_shmem_base) {
 	PyErr_Format(PyExc_RuntimeError,
 		"Cannot call before creating component");
 	return NULL;
@@ -784,7 +784,7 @@ PyObject *pin_has_writer(PyObject *self, PyObject *args) {
 PyObject *component_exists(PyObject *self, PyObject *args) {
     char *name;
     if(!PyArg_ParseTuple(args, "s", &name)) return NULL;
-    if(!SHMPTR(0)) {
+    if(!hal_shmem_base) {
 	PyErr_Format(PyExc_RuntimeError,
 		"Cannot call before creating component");
 	return NULL;
@@ -796,7 +796,7 @@ PyObject *component_exists(PyObject *self, PyObject *args) {
 PyObject *component_is_ready(PyObject *self, PyObject *args) {
     char *name;
     if(!PyArg_ParseTuple(args, "s", &name)) return NULL;
-    if(!SHMPTR(0)) {
+    if(!hal_shmem_base) {
 	PyErr_Format(PyExc_RuntimeError,
 		"Cannot call before creating component");
 	return NULL;
@@ -809,7 +809,7 @@ PyObject *new_sig(PyObject *self, PyObject *args) {
     char *name;
     int type,retval;
     if(!PyArg_ParseTuple(args, "si", &name,&type)) return NULL;
-    if(!SHMPTR(0)) {
+    if(!hal_shmem_base) {
 	PyErr_Format(PyExc_RuntimeError,
 		"Cannot call before creating component");
 	return NULL;
@@ -838,7 +838,7 @@ PyObject *new_sig(PyObject *self, PyObject *args) {
 PyObject *connect(PyObject *self, PyObject *args) {
     char *signame,*pinname;
     if(!PyArg_ParseTuple(args, "ss", &pinname,&signame)) return NULL;
-    if(!SHMPTR(0)) {
+    if(!hal_shmem_base) {
 	PyErr_Format(PyExc_RuntimeError,
 		"Cannot call before creating component");
 	return NULL;
@@ -914,7 +914,7 @@ PyObject *set_p(PyObject *self, PyObject *args) {
     void *d_ptr;
     
     if(!PyArg_ParseTuple(args, "ss", &name,&value)) return NULL;
-    if(!SHMPTR(0)) {
+    if(!hal_shmem_base) {
 	PyErr_Format(PyExc_RuntimeError,
 		"Cannot call before creating component");
 	return NULL;
