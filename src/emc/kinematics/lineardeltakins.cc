b0a683dc177c199afc23ccc17a9fdcfc10ee3748
Jeff Epler <jepler@unpythonic.net>
2016-07-01 20:59:02 -0400

    lineardeltakins: fix compile error on Ubuntu 16.04
    Closes #101.
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- src/emc/kinematics/lineardeltakins.cc
+++ src/emc/kinematics/lineardeltakins.cc
@@ -15,6 +15,7 @@
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <cmath>
+#define isnan(x) std::isnan(x)
 #include "lineardeltakins-common.h"
 #include <boost/python.hpp>
 using namespace boost::python;
