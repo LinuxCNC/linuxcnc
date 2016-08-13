3948c8340faabc760a761bb5a423678820c5e80c
John Morris <john@zultron.com>
2016-08-13 17:18:05 -0500

    Change licenses to GPLv2+
    Change licenses to GPLv2+ for the following files, as approved by the
    author Rob Ellenberg in LinuxCNC GitHub issue #134:
    
      src/emc/motion/state_tag.h
      src/emc/rs274ngc/modal_state.cc
      src/emc/rs274ngc/modal_state.hh
    
    [1]: https://github.com/LinuxCNC/linuxcnc/issues/134
    
    Signed-off-by: John Morris <john@zultron.com>

--- src/emc/motion/state_tag.h
+++ src/emc/motion/state_tag.h
@@ -5,11 +5,21 @@
 * a given motion line. This state info isn't actually used by motion
 * directly, but indicates the motion state.
 *
-* Author: Robert W. Ellenberg
-* License: GPL Version 2
-* System: Linux
+* Copyright © 2015 Robert W. Ellenberg
 *
-* Copyright (c) 2004 All rights reserved
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ********************************************************************/
 
 
