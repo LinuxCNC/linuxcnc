c7e4c36a1ac1e88f07cf9b6fcd2e418438a6d89d
Sebastian Kuzminsky <seb@highlab.com>
2016-09-02 17:08:34 -0600

    rs274ngc-startup test: wait for Task to start up
    Before this patch, sluggish startup of Task & Interp could cause false
    failures of this test.
    
    Wait for Task to finish initializing the Stat buffer, and for Interp to
    be Idle, before asserting that the RS274NGC_STARTUP_CODE ran successfully.
    
    Verified by (temporarily) changing the tests's startup code to "G4 P15",
    the wait code timed out and threw an exception.
    
    Signed-off-by: Sebastian Kuzminsky <seb@highlab.com>

--- tests/rs274ngc-startup/test-ui.py
+++ tests/rs274ngc-startup/test-ui.py
@@ -14,12 +14,44 @@ import re
 
 retval = 0
 
+
+def wait_for_linuxcnc_startup(status, timeout=10.0):
+
+    """Poll the Status buffer waiting for it to look initialized,
+    rather than just allocated (all-zero).  Returns on success, throws
+    RuntimeError on failure."""
+
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        status.poll()
+        if (status.angular_units == 0.0) \
+            or (status.axes == 0) \
+            or (status.axis_mask == 0) \
+            or (status.cycle_time == 0.0) \
+            or (status.exec_state != linuxcnc.EXEC_DONE) \
+            or (s.interp_state != linuxcnc.INTERP_IDLE) \
+            or (status.inpos == False) \
+            or (status.linear_units == 0.0) \
+            or (status.max_acceleration == 0.0) \
+            or (status.max_velocity == 0.0) \
+            or (status.program_units == 0.0) \
+            or (status.rapidrate == 0.0) \
+            or (status.state != linuxcnc.STATE_ESTOP) \
+            or (status.task_state != linuxcnc.STATE_ESTOP):
+            time.sleep(0.1)
+        else:
+            # looks good
+            return
+
+    # timeout, throw an exception
+    raise RuntimeError
+
+
 c = linuxcnc.command()
 s = linuxcnc.stat()
 e = linuxcnc.error_channel()
 
-# FIXME: this is lame
-time.sleep(1)
+wait_for_linuxcnc_startup(s)
 
 s.poll()
 
