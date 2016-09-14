0316a08fc145af20e4cd098d46c7219481b19b84
Jeff Epler <jepler@unpythonic.net>
2016-09-14 07:04:41 -0500

    testsuite: specify var filename
    closes #162.
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- tests/interp/compile/use-rs274.cc
+++ tests/interp/compile/use-rs274.cc
@@ -162,7 +162,9 @@ double GET_EXTERNAL_ANGLE_UNITS() {}
 int GET_EXTERNAL_MIST() {}
 CANON_MOTION_MODE GET_EXTERNAL_MOTION_CONTROL_MODE() {}
 double GET_EXTERNAL_MOTION_CONTROL_TOLERANCE() {}
-void GET_EXTERNAL_PARAMETER_FILE_NAME(char *filename, int max_size) {}
+void GET_EXTERNAL_PARAMETER_FILE_NAME(char *filename, int max_size) {
+    snprintf(filename, max_size, "%s", "rs274ngc.var");
+}
 CANON_PLANE GET_EXTERNAL_PLANE() {}
 double GET_EXTERNAL_POSITION_A() {}
 double GET_EXTERNAL_POSITION_B() {}
