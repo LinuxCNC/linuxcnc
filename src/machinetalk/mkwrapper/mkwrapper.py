c86d7c8559f9bd888bfe22e6f02f7fb817d0ba9d
Mick <arceye@mgware.co.uk>
2016-10-18 16:30:28 +0100

    Minor edit to mkwrapper.py
    Signed-off-by: Mick <arceye@mgware.co.uk>

--- src/machinetalk/mkwrapper/mkwrapper.py
+++ src/machinetalk/mkwrapper/mkwrapper.py
@@ -1060,7 +1060,7 @@ class LinuxCNCWrapper():
                                                         'increments', value)
 
             axis = self.status.config.axis[index]
-            axisModified |= self.update_proto_value(axis, txAxis, 'axis_type', statAxis['axisType'])
+            axisModified |= self.update_proto_value(axis, txAxis, 'axis_type', statAxis['axis_type'])
 
             for name in ['backlash', 'max_ferror', 'max_position_limit',
                          'min_ferror', 'min_position_limit']:
