285e69ace5b682e7e2aec40b5158c152b1d9d3b0
tseufl <t_seufl@gmx.de>
2016-10-22 13:49:08 +0200

    bugfix 2.7: gscreen.py,-userthemedir+local_theme
    - the "not" produces an error and program abort -> see 2.7 / 2.8 specifically "My Configurations / sim.gscreen.gscreen.custom / gscreen_gaxis"
    - with os.symlink is a link from src: "localtheme" -> dest: "userthemedir+'/%s'%self.data.local_theme" set, by the "not" before a nonexistent directory is trying to link -> program abort
    Signed-off-by: Thoren Seufl <t_seufl@gmx.de>

--- src/emc/usr_intf/gscreen/gscreen.py
+++ src/emc/usr_intf/gscreen/gscreen.py
@@ -619,7 +619,7 @@ class Gscreen:
         if os.path.exists(localtheme):
             self.data.local_theme = 'Link to %s_theme'% self.skinname
             # create systemlink because one can't store themes in an arbitrary folder.
-            if not os.path.exists(userthemedir+'/%s'%self.data.local_theme):
+            if os.path.exists(userthemedir+'/%s'%self.data.local_theme):
                 os.symlink(localtheme,userthemedir+'/%s'%self.data.local_theme)
             settings = gtk.settings_get_default()
             settings.set_string_property("gtk-theme-name", self.data.local_theme, "")
