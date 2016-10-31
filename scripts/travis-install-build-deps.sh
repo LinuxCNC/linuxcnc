e1f23f5060ec84ce201e0ac2816075cda32e4129
Jeff Epler <jepler@unpythonic.net>
2016-10-31 10:48:50 -0500

    trying to trace why apache2 is even installed..

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -3,3 +3,4 @@ sudo apt-get update -qq
 sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
 debian/configure uspace
 mk-build-deps -i -r -s sudo -t 'apt-get --no-install-recommends --no-install-suggests'
+sudo apt-get remove -y apache2
