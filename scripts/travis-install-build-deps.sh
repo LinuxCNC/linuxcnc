318b3e9eabe990077cffd3ec39bf7fcc79f5b444
Jeff Epler <jepler@unpythonic.net>
2016-10-31 10:43:40 -0500

    travis: ask apt-get to install less stuff

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -2,4 +2,4 @@
 sudo apt-get update -qq
 sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
 debian/configure uspace
-mk-build-deps -i -r -s sudo
+mk-build-deps -i -r -s sudo -t 'apt-get --no-install-recommends --no-install-suggests'
