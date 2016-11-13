73e68603e9dfc92349bea52af195dc73979b8c58
andypugh <andy@bodgesoc.org>
2016-11-13 20:48:14 +0000

    Hostmot2 Absolute Encoders: Add a flag to inhibit encoder wrapping.
    Multi-turn encoders should never wrap, and a false detection of a wrap may
    lead to permanent offsets that can not be removed without a restart
    This commit adds an extra flag for SSI BiSS and Fanuc encoders.
    
    
    Signed-off-by: andypugh <andy@bodgesoc.org>

--- src/hal/drivers/mesa-hostmot2/sserial.h
+++ src/hal/drivers/mesa-hostmot2/sserial.h
@@ -90,6 +90,7 @@ typedef struct {
     short ParmAddr;
     char UnitString[HM2_SSERIAL_MAX_STRING_LENGTH+1];
     char NameString[HM2_SSERIAL_MAX_STRING_LENGTH+1];
+    char Flags; //0x01 = graycode 0x02 = nowrap
 }hm2_sserial_data_t;
 
 static const hm2_sserial_data_t hm2_8i20_params[] = {
@@ -177,6 +178,7 @@ typedef struct {
     hal_float_t fullscale;
     hal_u32_t u32_param;
     hal_bit_t graycode;
+    hal_bit_t nowrap;
     rtapi_s64 oldval; // not pins, but this way every pin can have one
     rtapi_s64 accum; // these two are only currently used by encoders
     rtapi_s64 offset;
