22c47a112ffdc3b44616c93b171cbb56f5d3b507
Mick <arceye@mgware.co.uk>
2017-01-07 15:26:23 +0000

    Fix haltalk assert error
    Implement some of changes in 01d8c5d machinekit/master
    `Added broker support`
    
    This changes the handling of messages and prevents an assert error
    experienced in original code when the size of the message was tested.
    
    Thanks to user @zhivko for spotting where error originated.
    
    Signed-off-by: Mick <arceye@mgware.co.uk>

--- src/machinetalk/haltalk/haltalk_command.cc
+++ src/machinetalk/haltalk/haltalk_command.cc
@@ -45,29 +45,22 @@ handle_command_input(zloop_t *loop, zmq_pollitem_t *poller, void *arg)
     zframe_t *f = zmsg_last(msg);
     zmsg_remove(msg, f);
 
-    size_t nframes = zmsg_size( msg);
 
-    for(size_t i = 0; i < nframes; i++) {
-	zframe_t *f = zmsg_pop (msg);
-
-	if (!self->rx.ParseFromArray(zframe_data(f), zframe_size(f))) {
-	    zframe_t *o = zmsg_first (msg);  // freed with msg
-	    std::string origin( (const char *) zframe_data(o), zframe_size(o));
-	    rtapi_print_hex_dump(RTAPI_MSG_ALL, RTAPI_DUMP_PREFIX_OFFSET,
-				 16, 1, zframe_data(f), zframe_size(f),
-				 true, NULL,
-				 "%s: invalid pb ", origin.c_str());
-	} else {
-	    if (self->cfg->debug) {
-		std::string s;
-		gpb::TextFormat::PrintToString(self->rx, &s);
-		fprintf(stderr,"%s: req=%s\n",__func__,s.c_str());
+    if (!self->rx.ParseFromArray(zframe_data(f), zframe_size(f))) {
+       zframe_t *o = zmsg_first (msg);  // freed with msg
+        std::string origin( (const char *) zframe_data(o), zframe_size(o));
+        rtapi_print_hex_dump(RTAPI_MSG_ALL, RTAPI_DUMP_PREFIX_OFFSET, 16, 1, 
+	    zframe_data(f), zframe_size(f), true, NULL, "%s: invalid pb ", origin.c_str());
+	} 
+    else {
+        if (self->cfg->debug) {
+            std::string s;
+	    gpb::TextFormat::PrintToString(self->rx, &s);
+    	    fprintf(stderr,"%s: req=%s\n",__func__,s.c_str());
 	    }
-	    // a valid protobuf. Interpret and reply as needed.
-	    dispatch_request(self, msg, poller->socket);
+	// a valid protobuf. Interpret and reply as needed.
+        dispatch_request(self, msg, poller->socket);
 	}
-	zframe_destroy(&f);
-    }
     zframe_destroy(&f);
     zmsg_destroy(&msg);
 
