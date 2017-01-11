3af5a23b9d1ce8bf65573aff4f2197b7ca444a45
Chris Morley <chrisinnanaimo@hotmail.com>
2017-01-11 01:41:30 -0800

    qtvcp -add a MDI single line entry widget
    One can load aux programs such as halmeter and halshow by
    typing them in in capital leters, otherwise it's treated as a MDI
    command
    
    Signed-off-by: Chris Morley <chrisinnanaimo@hotmail.com>
    Signed-off-by: chris morley <chrisinnanaimo@hotmail.com>

--- lib/python/qtvcp_widgets/widget_plugins/lcnc_widgetplugin.py
+++ lib/python/qtvcp_widgets/widget_plugins/lcnc_widgetplugin.py
@@ -3,6 +3,7 @@
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtDesigner import QPyDesignerCustomWidgetPlugin
 from qtvcp_widgets.drowidget import Lcnc_DROLabel
+from qtvcp_widgets.mdi_line import Lcnc_MDILine
 
 from qtvcp_widgets.qtvcp_icons import Icon
 ICON = Icon()
@@ -38,3 +39,35 @@ class LcncDROLabelPlugin(QPyDesignerCustomWidgetPlugin):
         return '<widget class="Lcnc_DROLabel" name="lcnc_drolabel" />\n'
     def includeFile(self):
         return "qtvcp_widgets.drowidget"
+
+####################################
+# MDI edit line
+####################################
+class Lcnc_MDIlinePlugin(QPyDesignerCustomWidgetPlugin):
+    def __init__(self, parent = None):
+        QPyDesignerCustomWidgetPlugin.__init__(self)
+        self.initialized = False
+    def initialize(self, formEditor):
+        if self.initialized:
+            return
+        self.initialized = True
+    def isInitialized(self):
+        return self.initialized
+    def createWidget(self, parent):
+        return Lcnc_MDILine(parent)
+    def name(self):
+        return "Lcnc_MDILine"
+    def group(self):
+        return "Linuxcnc"
+    def icon(self):
+        return QtGui.QIcon(QtGui.QPixmap(ICON.get_path('lcnc_mdiLine')))
+    def toolTip(self):
+        return "MDI edit line Widget"
+    def whatsThis(self):
+        return ""
+    def isContainer(self):
+        return True
+    def domXml(self):
+        return '<widget class="Lcnc_MDILine" name="lcnc_mdiline" />\n'
+    def includeFile(self):
+        return "qtvcp_widgets.mdi_line"
