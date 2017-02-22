5ce31592ef904e7f9b37691357cb86e62c667100
Jeff Epler <jepler@unpythonic.net>
2017-02-22 15:47:05 -0600

    travis: manually uninstal gpl3 readline
    In the past, the apt resolver automatically uninstalled the gpl3
    readline (libreadline-dev, libreadline6-dev) as needed to satisfy
    the dependencies of linuxcnc-build-deps.  At some point around
    2017-02-10, this changed on travis-ci.org and instead it preferred
    to remove linuxcnc-build-deps and leave a bunch of deps (such as
    libudev-dev, the first one configure hit) in a non-usable state.
    Work around this by manually removing the packages that seem to be
    causing the problems.
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 sudo apt-get update -qq
 sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
+sudo apt-get remove -f libreadline6-dev || true
+sudo apt-get remove -f libreadline-dev || true
 debian/configure uspace
 mk-build-deps
 sudo dpkg -i linuxcnc-*.deb || true
