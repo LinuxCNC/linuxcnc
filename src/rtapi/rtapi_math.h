0aa1e2fadacba26fad931fdf3ab68f0a2ebe97a8
Dewey Garrett <dgarrett@panix.com>
2017-03-06 17:56:08 -0700

    rtapi_math.h include fmod()
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- src/rtapi/rtapi_math.h
+++ src/rtapi/rtapi_math.h
@@ -43,6 +43,7 @@ extern double exp(double);
 extern double pow(double, double);
 extern double fmin(double, double);
 extern double fmax(double, double);
+extern double fmod(double, double);
 
 extern double round(double);
 extern double ceil(double);
