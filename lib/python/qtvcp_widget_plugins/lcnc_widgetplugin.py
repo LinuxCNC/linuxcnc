3f568c142f7c3eade7c320c58b72eb9dadbd0ace
Chris Morley <chrisinnanaimo@hotmail.com>
2017-07-14 15:39:24 -0600

    qtscreen - fix mised QT4-5 import statements
    Signed-off-by: Chris Morley <chrisinnanaimo@hotmail.com>

--- lib/python/qtvcp_widget_plugins/lcnc_widgetplugin.py
+++ lib/python/qtvcp_widget_plugins/lcnc_widgetplugin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from PyQt4 import QtCore, QtGui
-from PyQt4.QtDesigner import QPyDesignerCustomWidgetPlugin
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtDesigner import QPyDesignerCustomWidgetPlugin
 from qtvcp_widgets.drowidget import Lcnc_DROLabel
 from qtvcp_widgets.mdi_line import Lcnc_MDILine
 from qtvcp_widgets.gcode_widget import GcodeEditor
