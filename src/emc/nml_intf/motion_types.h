a63ec765cbc36694b3835be31d7fe050d616cff3
Lubomir Rintel <lkundrak@v3.sk>
2017-09-15 13:04:31 +0200

    all: update FSF address in GPL 2.0 notices
    If you write to 59 Temple Place you're unlikely to get a response.
    Let's realign the address with a more up-to-date one from [1].
    
      [1] https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
    
    This is purely a cosmetic change, doesn't affect the meaning of the
    license. Done to make rpmlint happy.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

--- src/emc/nml_intf/motion_types.h
+++ src/emc/nml_intf/motion_types.h
@@ -12,7 +12,7 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define EMC_MOTION_TYPE_TRAVERSE 1
 #define EMC_MOTION_TYPE_FEED 2
 #define EMC_MOTION_TYPE_ARC 3
--- src/emc/usr_intf/axis/scripts/image-to-gcode.py
+++ src/emc/usr_intf/axis/scripts/image-to-gcode.py
@@ -8,8 +8,8 @@
 ## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
 ## the GNU General Public License for more details.  You should have
 ## received a copy of the GNU General Public License along with image-to-gcode;
-## if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-## Suite 330, Boston, MA 02111-1307 USA
+## if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+## Fifth Floor, Boston, MA 02110-1301 USA.
 ## 
 ## image-to-gcode.py is Copyright (C) 2005 Chris Radek
 ## chris@timeguy.com
