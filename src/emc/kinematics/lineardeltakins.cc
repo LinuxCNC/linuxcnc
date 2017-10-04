bb2d088668604e41b9e1ab2312bd462110c4d015
Anton Midyukov <antohami@altlinux.org>
2017-10-04 19:43:18 +0700

    Fix issues 335 Fix build error: emc/kinematics/lineardeltakins.cc:18:18: error: 'std::std' has not been declared

--- src/emc/kinematics/lineardeltakins.cc
+++ src/emc/kinematics/lineardeltakins.cc
@@ -15,10 +15,10 @@
 //    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include <cmath>
-#define isnan(x) std::isnan(x)
-#include "lineardeltakins-common.h"
 #include <boost/python.hpp>
 using namespace boost::python;
+#define isnan(x) std::isnan(x)
+#include "lineardeltakins-common.h"
 
 static object forward(double j0, double j1, double j2)
 {
