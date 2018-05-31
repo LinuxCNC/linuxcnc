552b4304c5debb6a1b0529a997d0715cbf9bc906
Chris Morley <chrisinnanaimo@hotmail.com>
2018-05-31 12:07:43 -0700

    stepconf: fix stepgen number wrong in lathe config
    This was caused by a goof in fix 6a1265 fr all limits + home
     option.

--- src/emc/usr_intf/stepconf/build_HAL.py
+++ src/emc/usr_intf/stepconf/build_HAL.py
@@ -256,7 +256,7 @@ class HAL:
 
         if self.d.axes == 2:
             self.connect_axis(file, 0, 'x')
-            self.connect_axis(file, 2, 'z')
+            self.connect_axis(file, 1, 'z')
         elif self.d.axes == 0:
             self.connect_axis(file, 0, 'x')
             self.connect_axis(file, 1, 'y')
