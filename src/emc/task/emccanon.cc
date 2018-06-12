4b9972a75cfee68019bf5599800a51d80476237a
Sebastian Kuzminsky <seb@highlab.com>
2018-06-12 12:58:45 -0600

    Canon: make EMCCANON_DEBUG work again
    We don't usually build with the EMCCANON_DEBUG #define turned on, and
    some of the code conditionally compiled in had bit-rotted.  This commit
    fixes those problems.

--- src/emc/task/emccanon.cc
+++ src/emc/task/emccanon.cc
@@ -1456,9 +1456,9 @@ void ARC_FEED(int line_number,
 
 
     canon_debug("start = %f %f %f\n",
-            canonEndPoint.x,
-            canonEndPoint.y,
-            canonEndPoint.z);
+            canon.endPoint.x,
+            canon.endPoint.y,
+            canon.endPoint.z);
     canon_debug("end = %f %f %f\n",
             end_cart.x,
             end_cart.y,
@@ -1704,7 +1704,7 @@ void ARC_FEED(int line_number,
 
     // Limit velocity by maximum
     double vel = MIN(canon.linearFeedRate, v_max);
-    canon_debug("current F = %f\n",currentLinearFeedRate);
+    canon_debug("current F = %f\n",canon.linearFeedRate);
     canon_debug("vel = %f\n",vel);
 
     canon_debug("v_max = %f\n",v_max);
