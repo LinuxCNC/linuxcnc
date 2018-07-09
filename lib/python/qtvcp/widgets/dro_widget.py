2a86be3d0b60a6be8640c896d91644ac9def778e
Chris Morley <chrisinnanaimo@hotmail.com>
2018-07-09 01:51:11 -0700

    qtvcp -revamp dro widget using enums
    Incorperate Kurt Jacobson's enums work into dro widget.
    Also add ability to display in a static unit rather
    then follow the current mode unit.
    Enums allowdesigner to display options as text in a combobox rather then
    numbers in a spin box. Unfortunately this doesn't work for designer 5.5
    It reverts back to numbers in a spinbox.

--- lib/python/qtvcp/widgets/dro_widget.py
+++ lib/python/qtvcp/widgets/dro_widget.py
@@ -14,13 +14,14 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-import sys
 import os
 
 from PyQt5 import QtCore, QtWidgets
 
 from qtvcp.widgets.widget_baseclass import _HalWidgetBase
 from qtvcp.core import Status, Info
+from qtvcp.enums import AXIS, REFERENCE, UNITS
+
 from qtvcp import logger
 
 # Instantiate the libraries with global reference
@@ -35,15 +36,20 @@ LOG = logger.getLogger(__name__)
 # LOG.setLevel(logger.INFO) # One of DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 
-class DROLabel(QtWidgets.QLabel, _HalWidgetBase):
+class DROLabel(QtWidgets.QLabel, AXIS, REFERENCE, UNITS, _HalWidgetBase):
+    # used for Designer comboboxes
+    QtCore.Q_ENUMS(AXIS)
+    QtCore.Q_ENUMS(REFERENCE)
+    QtCore.Q_ENUMS(UNITS)
     def __init__(self, parent=None):
         super(DROLabel, self).__init__(parent)
-        self.diameter = False
-        self.reference_type = 0
-        self.joint_number = 0
-        self.display_units_mm = 0
-        self.mm_text_template = '%10.3f'
-        self.imperial_text_template = '%9.4f'
+        self._axis = AXIS.Y
+        self._units = UNITS.ProgrammedMode
+        self._diameter_mode = False
+        self._reference_type = REFERENCE.Absolute
+        self._g21_is_active = 0
+        self._mm_text_template = '%10.3f'
+        self._inch_text_template = '%9.4f'
         self.setText('--------------')
 
     def _hal_init(self):
@@ -53,73 +59,105 @@ class DROLabel(QtWidgets.QLabel, _HalWidgetBase):
         STATUS.connect('diameter-mode', self._switch_modes)
 
     def update(self, widget, absolute, relative, dtg):
-        if self.display_units_mm != INFO.MACHINE_IS_METRIC:
-            absolute = INFO.convert_units_9(absolute)
-            relative = INFO.convert_units_9(relative)
-            dtg = INFO.convert_units_9(dtg)
+        def convert( a, r, d):
+            ab = INFO.convert_units_9(a)
+            re = INFO.convert_units_9(r)
+            dt = INFO.convert_units_9(d)
+            return ab, re, dt
 
-        if self.display_units_mm:
-            tmpl = lambda s: self.mm_text_template % s
+        # we need to convert the units if the display units
+        # does not match the machine based units
+        # if we are following programmed units then this can change
+        if (self._units == UNITS.ProgrammedMode and
+            bool(self._g21_is_active) != INFO.MACHINE_IS_METRIC):
+            absolute, relative, dtg = convert(absolute, relative, dtg)
+        elif self._units == UNITS.Inch and INFO.MACHINE_IS_METRIC:
+            absolute, relative, dtg = convert(absolute, relative, dtg)
+        elif (self._units == UNITS.Metric and not INFO.MACHINE_IS_METRIC):
+            absolute, relative, dtg = convert(absolute, relative, dtg)
+
+        # are we following the Gcode mode or 
+        # displaying a static unit
+        if self._units == UNITS.ProgrammedMode:
+            is_metric = bool(self._g21_is_active)
         else:
-            tmpl = lambda s: self.imperial_text_template % s
-        # only joint 0 (X) can use diameter mode
-        if self.diameter and self.joint_number == 0:
+            is_metric = bool(self._units == UNITS.Metric)
+        # set the units template accordingly
+        if is_metric:
+            tmpl = lambda s: self._mm_text_template % s
+        else:
+            tmpl = lambda s: self._inch_text_template % s
+
+        # only the X axis can use diameter mode
+        if self._diameter_mode and self._axis == AXIS.X:
             scale = 2.0
         else:
             scale = 1
+        # set text by reference type and scale
         try:
-            if self.reference_type == 0:
-                self.setText(tmpl(absolute[self.joint_number]*scale))
-            elif self.reference_type == 1:
-                self.setText(tmpl(relative[self.joint_number]*scale))
-            elif self.reference_type == 2:
-                self.setText(tmpl(dtg[self.joint_number]*scale))
+            if self._reference_type == REFERENCE.Absolute:
+                self.setText(tmpl(absolute[self._axis]*scale))
+            elif self._reference_type == REFERENCE.Relative:
+                self.setText(tmpl(relative[self._axis]*scale))
+            elif self._reference_type == REFERENCE.DistanceToGo:
+                self.setText(tmpl(dtg[self._axis]*scale))
         except:
             pass
-        return True
 
     def _switch_units(self, widget, data):
-        self.display_units_mm = data
+        self._g21_is_active = data
 
     def _switch_modes(self, w, mode):
-        self.diameter = mode
-
-    def set_to_inch(self):
-        self.display_units_mm = 0
-
-    def set_to_mm(self):
-        self.display_units_mm = 1
-
-    def set_to_diameter(self):
-        self.diameter = True
-
-    def set_to_radius(self):
-        self.diameter = False
+        self._diameter_mode = mode
 
 # property getter/setters
 
-    # JOINT Number
-    def setjoint_number(self, data):
-        self.joint_number = data
-    def getjoint_number(self):
-        return self.joint_number
-    def resetjoint_number(self):
-        self.joint_number = 0
-    Qjoint_number = QtCore.pyqtProperty(int, getjoint_number, setjoint_number, resetjoint_number)
-
     # user system Number
+    @QtCore.pyqtSlot(REFERENCE)
     def setreference_type(self, data):
-        self.reference_type = data
+        self._reference_type = data
     def getreference_type(self):
-        return self.reference_type
+        return self._reference_type
     def resetreference_type(self):
-        self.reference_type = 0
-    Qreference_type = QtCore.pyqtProperty(int, getreference_type, setreference_type, resetreference_type)
+        self._reference_type = REFERENCE.Relative
+    reference_type = QtCore.pyqtProperty(REFERENCE, getreference_type, setreference_type, resetreference_type)
+
+    def getAxis(self):
+        return self._axis
+    @QtCore.pyqtSlot(AXIS)
+    def setAxis(self, axis):
+        self._axis = axis
+    axis = QtCore.pyqtProperty(AXIS, getAxis, setAxis)
+
+    def getUnits(self):
+        return self._units
+    @QtCore.pyqtSlot(UNITS)
+    def setUnits(self, units):
+        self._units = units
+    units = QtCore.pyqtProperty(UNITS, getUnits, setUnits)
+
+    def setMMTextTemplate(self, data):
+        self._mm_text_template = data
+    def getMMTextTemplate(self):
+        return self._mm_text_template
+    def resetMMTextTemplate(self):
+        self._mm_text_template = '%9.4f'
+
+    def setInchTextTemplate(self, data):
+        self._inch_text_template = data
+    def getInchTextTemplate(self):
+        return self._inch_text_template
+    def resetInchTextTemplate(self):
+        self._inch_text_template = '%10.3f'
+
+    mm_template = QtCore.pyqtProperty(str, getMMTextTemplate, setMMTextTemplate, resetMMTextTemplate)
+    inch_template = QtCore.pyqtProperty(str, getInchTextTemplate, setInchTextTemplate, resetInchTextTemplate)
+
 
 # for testing without editor:
 def main():
     import sys
-    from PyQt4.QtGui import QApplication
+    from PyQt5.QtWidgets import QApplication
 
     app = QApplication(sys.argv)
     widget = DROLabel()
