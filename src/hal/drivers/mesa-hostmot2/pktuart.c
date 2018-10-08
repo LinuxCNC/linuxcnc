59a85b8ce0b1bd41a64d64cadd4e8446aa247fc4
John Morris <john@zultron.com>
2018-10-08 13:46:30 +0800

    mesa-hostmot2/pktuart.c:  fix compiler warning
        Compiling realtime hal/drivers/mesa-hostmot2/pktuart.c
        In file included from hal/drivers/mesa-hostmot2/pktuart.c:26:0:
        hal/drivers/mesa-hostmot2/pktuart.c: In function 'hm2_pktuart_read':
        hal/drivers/mesa-hostmot2/hostmot2.h:50:21: warning: format '%s' expects a matching 'char *' argument [-Wformat=]
         #define HM2_NAME    "hm2"
    			 ^
        hal/drivers/mesa-hostmot2/hostmot2.h:67:66: note: in expansion of macro 'HM2_NAME'
         #define HM2_ERR(fmt, args...)    rtapi_print_msg(RTAPI_MSG_ERR,  HM2_NAME "/%s: " fmt, hm2->llio->name, ## args)
    								      ^~~~~~~~
        hal/drivers/mesa-hostmot2/pktuart.c:403:9: note: in expansion of macro 'HM2_ERR'
    	     HM2_ERR("%s read: hm2->llio->write failure %s\n", name);
    	     ^~~~~~~

--- src/hal/drivers/mesa-hostmot2/pktuart.c
+++ src/hal/drivers/mesa-hostmot2/pktuart.c
@@ -400,7 +400,7 @@ int hm2_pktuart_read(char *name, unsigned char data[], u8 *num_frames, u16 *max_
     r = hm2->llio->read(hm2->llio, hm2->pktuart.instance[inst].rx_mode_addr,
                         &buff, sizeof(u32));
     if (r < 0) {
-        HM2_ERR("%s read: hm2->llio->write failure %s\n", name);
+        HM2_ERR("%s read: hm2->llio->write failure\n", name);
                 return -1; // make the error message more detailed
     }
     countp = (buff >> 16)  & 0x1f; 
