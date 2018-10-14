4634c29afa7b26198e07f75c1fe352c9316cd339
Chris Morley <chrisinnanaimo@hotmail.com>
2018-10-13 20:09:12 -0700

    cython -update HAL test file
    add s32 and float pins

--- src/hal/cython/test.py
+++ src/hal/cython/test.py
@@ -3,17 +3,31 @@ import py3hal as hal
 import time
 c = hal.component('component1')
 try:
-    c.new_signal('connection1', hal.BIT)
-    c.new_pin('test-pin-in',hal.BIT,hal.IN)
-    c.set_prefix('qwerty')
-    c.new_pin('test-pin-out',hal.BIT,hal.OUT)
+    c.new_signal('connection-bit', hal.BIT)
+    c.new_pin('test-pin-bit-in',hal.BIT,hal.IN)
+    c.new_pin('test-pin-bit-out',hal.BIT,hal.OUT)
+    c.connect('component1.test-pin-bit-in', 'connection-bit')
 
-    c.connect('component1.test-pin-in', 'connection1')
-    c.connect('qwerty.test-pin-out', 'connection1')
+    c.new_signal('connection-s32', hal.S32)
+    c.new_pin('test-pin-s32-in',hal.S32,hal.IN)
+    c.new_pin('test-pin-s32-out',hal.S32,hal.OUT)
+    c.connect('component1.test-pin-s32-in', 'connection-s32')
+
+    c.new_signal('connection-float', hal.FLOAT)
+    c.new_pin('test-pin-float-in',hal.FLOAT,hal.IN)
+    c.new_pin('test-pin-float-out',hal.FLOAT,hal.OUT)
+    c.connect('component1.test-pin-float-in', 'connection-float')
 
     c.ready()
+    print ('start')
     while 1:
-        time.sleep(1)
+        time.sleep(.5)
+        c['test-pin-bit-out'].set(c['test-pin-bit-in'].get())
+        c['test-pin-s32-out'].set(c['test-pin-s32-in'].get())
+        c['test-pin-float-out'].set(c['test-pin-float-in'].get())
+        print('test-pin-bit-out = ',c['test-pin-bit-out'].get())
+        print('test-pin-s32-out = ',c['test-pin-s32-out'].get())
+        print('test-pin-float-out = ',c['test-pin-float-out'].get())
 except KeyboardInterrupt:
     c.exit()
     raise SystemExit
