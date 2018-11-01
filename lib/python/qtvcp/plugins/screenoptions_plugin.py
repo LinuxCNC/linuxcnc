9a8e9762185166734997420707d950185a2ff243
Chris Morley <chrisinnanaimo@hotmail.com>
2018-10-31 23:56:22 -0700

    qtvcp -fix screenoptions task extention
    Right click shows a dialog for property setting again

--- lib/python/qtvcp/plugins/screenoptions_plugin.py
+++ lib/python/qtvcp/plugins/screenoptions_plugin.py
@@ -16,17 +16,17 @@ class LcncScreenOptionsPlugin(QPyDesignerCustomWidgetPlugin):
     def __init__(self, parent = None):
         super(LcncScreenOptionsPlugin, self).__init__(parent)
         self.initialized = False
+
     def initialize(self, formEditor):
         if self.initialized:
             return
         manager = formEditor.extensionManager()
         if manager:
           self.factory = \
-              GeoLocationTaskMenuFactory(manager)
+              screenOptionsTaskMenuFactory(manager)
           manager.registerExtensions(
               self.factory,
-              "com.trolltech.Qt.Designer.TaskMenu")
-
+              "org.qt-project.Qt.Designer.TaskMenu")
         self.initialized = True
     def isInitialized(self):
         return self.initialized
@@ -49,7 +49,7 @@ class LcncScreenOptionsPlugin(QPyDesignerCustomWidgetPlugin):
     def includeFile(self):
         return "qtvcp.widgets.screen_options"
 
-class GeoLocationDialog(QtWidgets.QDialog):
+class screenOptionsDialog(QtWidgets.QDialog):
 
    def __init__(self, widget, parent = None):
 
@@ -59,17 +59,14 @@ class GeoLocationDialog(QtWidgets.QDialog):
 
       self.previewWidget = ScreenOptions()
       self.previewWidget.notify_option = widget.notify_option
-      #self.previewWidget.longitude = widget.longitude
 
       buttonBox = QtWidgets.QDialogButtonBox()
       okButton = buttonBox.addButton(buttonBox.Ok)
       cancelButton = \
          buttonBox.addButton(buttonBox.Cancel)
 
-      self.connect(okButton, QtCore.SIGNAL("clicked()"),
-                   self.updateWidget)
-      self.connect(cancelButton, QtCore.SIGNAL("clicked()"),
-                   self, QtCore.SLOT("reject()"))
+      okButton.clicked.connect(self.updateWidget)
+      cancelButton.clicked.connect(self.reject)
 
       layout = QtWidgets.QGridLayout()
       self.c_notify = QtWidgets.QCheckBox("Desktop Notify Errors")
@@ -78,14 +75,20 @@ class GeoLocationDialog(QtWidgets.QDialog):
       self.c_errors.setChecked(widget.catch_errors)
       self.c_close = QtWidgets.QCheckBox("Catch close Event")
       self.c_close.setChecked(widget.close_event)
+      self.c_play_sounds = QtWidgets.QCheckBox("Play Sounds")
+      self.c_play_sounds.setChecked(widget.play_sounds)
+      self.c_use_pref_file = QtWidgets.QCheckBox("Set up a Preference File")
+      self.c_use_pref_file.setChecked(widget.use_pref_file)
 
       layout.addWidget(self.c_notify)
       layout.addWidget(self.c_errors)
       layout.addWidget(self.c_close)
-      layout.addWidget(buttonBox, 3, 0, 1, 2)
+      layout.addWidget(self.c_play_sounds)
+      layout.addWidget(self.c_use_pref_file)
+      layout.addWidget(buttonBox, 5, 0, 1, 2)
       self.setLayout(layout)
 
-      self.setWindowTitle(self.tr("Select Options"))
+      self.setWindowTitle(self.tr("Set Options"))
 
    def updateWidget(self):
 
@@ -100,10 +103,14 @@ class GeoLocationDialog(QtWidgets.QDialog):
               QtCore.QVariant(self.c_errors.isChecked()))
           formWindow.cursor().setProperty("catch_close_option",
               QtCore.QVariant(self.c_close.isChecked()))
+          formWindow.cursor().setProperty("play_sounds_option",
+              QtCore.QVariant(self.c_play_sounds.isChecked()))
+          formWindow.cursor().setProperty("use_pref_file_option",
+              QtCore.QVariant(self.c_use_pref_file.isChecked()))
 
       self.accept()
 
-class GeoLocationMenuEntry(QPyDesignerTaskMenuExtension):
+class screenOptionsMenuEntry(QPyDesignerTaskMenuExtension):
 
   def __init__(self, widget, parent):
 
@@ -111,9 +118,8 @@ class GeoLocationMenuEntry(QPyDesignerTaskMenuExtension):
 
       self.widget = widget
       self.editStateAction = QtWidgets.QAction(
-          self.tr("Select Options..."), self)
-      self.connect(self.editStateAction,
-          QtCore.SIGNAL("triggered()"), self.updateLocation)
+          self.tr("Set Options..."), self)
+      self.editStateAction.triggered.connect(self.updateLocation)
 
   def preferredEditAction(self):
       return self.editStateAction
@@ -122,21 +128,19 @@ class GeoLocationMenuEntry(QPyDesignerTaskMenuExtension):
       return [self.editStateAction]
 
   def updateLocation(self):
-      dialog = GeoLocationDialog(self.widget)
+      dialog = screenOptionsDialog(self.widget)
       dialog.exec_()
 
-class GeoLocationTaskMenuFactory(QExtensionFactory):
+class screenOptionsTaskMenuFactory(QExtensionFactory):
 
   def __init__(self, parent = None):
-
       QExtensionFactory.__init__(self, parent)
 
   def createExtension(self, obj, iid, parent):
-
-      if iid != "com.trolltech.Qt.Designer.TaskMenu":
+      if iid != "org.qt-project.Qt.Designer.TaskMenu":
           return None
 
       if isinstance(obj, ScreenOptions):
-          return GeoLocationMenuEntry(obj, parent)
+          return screenOptionsMenuEntry(obj, parent)
 
       return None
