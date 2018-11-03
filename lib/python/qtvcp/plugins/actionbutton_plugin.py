36f7561c8d2b7f7e8eed8103d904382d15199d1a
Chris Morley <chrisinnanaimo@hotmail.com>
2018-11-03 00:44:34 -0700

    qtvcp -some work and experiments on a taskmenu
    setting slots and signals in the the taskmenu was a bust

--- lib/python/qtvcp/plugins/actionbutton_plugin.py
+++ lib/python/qtvcp/plugins/actionbutton_plugin.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtDesigner import QPyDesignerCustomWidgetPlugin
+import sip
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtDesigner import QPyDesignerCustomWidgetPlugin, \
+                QPyDesignerTaskMenuExtension, QExtensionFactory, \
+                QDesignerFormWindowInterface, QPyDesignerMemberSheetExtension
 from qtvcp.widgets.action_button import ActionButton
 from qtvcp.widgets.action_button_round import RoundButton
 from qtvcp.widgets.qtvcp_icons import Icon
 ICON = Icon()
+
+Q_TYPEID = {
+    'QDesignerContainerExtension':     'org.qt-project.Qt.Designer.Container',
+    'QDesignerPropertySheetExtension': 'org.qt-project.Qt.Designer.PropertySheet',
+    'QDesignerTaskMenuExtension': 'org.qt-project.Qt.Designer.TaskMenu',
+    'QDesignerMemberSheetExtension': 'org.qt-project.Qt.Designer.MemberSheet'
+}
 
 ####################################
 # ActionBUTTON
@@ -25,7 +35,10 @@ class ActionButtonPlugin(QPyDesignerCustomWidgetPlugin):
 
         if self.initialized:
             return
-
+        manager = formEditor.extensionManager()
+        if manager:
+            self.factory = ActionButtonTaskMenuFactory(manager)
+            manager.registerExtensions(self.factory, Q_TYPEID['QDesignerTaskMenuExtension'])
         self.initialized = True
 
     def isInitialized(self):
@@ -69,6 +82,100 @@ class ActionButtonPlugin(QPyDesignerCustomWidgetPlugin):
     # a module path.
     def includeFile(self):
         return "qtvcp.widgets.action_button"
+
+
+class ActionButtonDialog(QtWidgets.QDialog):
+
+   def __init__(self, widget, parent = None):
+
+      QtWidgets.QDialog.__init__(self, parent)
+
+      self.widget = widget
+
+      self.previewWidget = ActionButton()
+      #self.previewWidget.notify_option = widget.notify_option
+
+      buttonBox = QtWidgets.QDialogButtonBox()
+      okButton = buttonBox.addButton(buttonBox.Ok)
+      cancelButton = buttonBox.addButton(buttonBox.Cancel)
+
+      okButton.clicked.connect(self.updateWidget)
+      cancelButton.clicked.connect(self.reject)
+
+      layout = QtWidgets.QGridLayout()
+      self.c_estop = QtWidgets.QCheckBox("Estop Action")
+      self.c_estop.setChecked(widget.estop )
+      layout.addWidget(self.c_estop)
+
+      layout.addWidget(buttonBox, 5, 0, 1, 2)
+      self.setLayout(layout)
+
+      self.setWindowTitle(self.tr("Set Options"))
+
+      return
+      # well this didn't give me what I wanted
+      form = QDesignerFormWindowInterface.findFormWindow(widget)
+      if form:
+          editor = form.core()
+          manager = editor.extensionManager()
+          w = editor.topLevel()
+          print w
+          sheet = manager.extension(w, Q_TYPEID['QDesignerMemberSheetExtension'])
+          # This explicit cast is necessary here
+          sheet = sip.cast(sheet, QPyDesignerMemberSheetExtension)
+          memberCount = sheet.count()
+          print widget,'count:', memberCount
+          print sheet.indexOf('clicked()')
+          print sheet.indexOf('sender')
+          for i in range(0,memberCount):
+              #sheet.setVisible(i,False)
+              if sheet.isSignal(i) or sheet.isSlot(i):
+                  print i, sheet.signature(i)
+          #print help(sheet)
+          #print dir(widget)
+
+   def updateWidget(self):
+
+      formWindow = QDesignerFormWindowInterface.findFormWindow(self.widget)
+      if formWindow:
+          formWindow.cursor().setProperty("estop_action",
+              QtCore.QVariant(self.c_estop.isChecked()))
+
+
+      self.accept()
+
+class ActionButtonMenuEntry(QPyDesignerTaskMenuExtension):
+
+    def __init__(self, widget, parent):
+        super(QPyDesignerTaskMenuExtension, self).__init__(parent)
+        self.widget = widget
+        self.editStateAction = QtWidgets.QAction(
+          self.tr("Set Options..."), self)
+        self.editStateAction.triggered.connect(self.updateOptions)
+
+    def preferredEditAction(self):
+        return self.editStateAction
+
+    def taskActions(self):
+        return [self.editStateAction]
+
+    def updateOptions(self):
+        dialog = ActionButtonDialog(self.widget)
+        dialog.exec_()
+
+class ActionButtonTaskMenuFactory(QExtensionFactory):
+    def __init__(self, parent = None):
+        QExtensionFactory.__init__(self, parent)
+
+    def createExtension(self, obj, iid, parent):
+
+        if not isinstance(obj, ActionButton):
+            return None
+        if iid == Q_TYPEID['QDesignerTaskMenuExtension']:
+            return ActionButtonMenuEntry(obj, parent)
+        elif iid == Q_TYPEID['QDesignerMemberSheetExtension']:
+            return ActionButtonMemberSheet(obj, parent)
+        return None
 
 ####################################
 # RoundButton
