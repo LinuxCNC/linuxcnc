7626740fe91d991f91f7fc612ff34fa10c21b570
Dewey Garrett <dgarrett@panix.com>
2018-11-13 10:16:52 -0700

    show_errors.tcl provide button to "Create Report"
    Replaces "Select All" button which requires user to
    be familiar with copy/paste provisions of the window
    manager in order to submit the information.
    
    The created report (/tmp/linuxcnc.report) includes
    all of the text widget window text and _additional_
    info about the machine, operating system, etc. as
    generated by the linuxcnc_info script.
    
    NOTE: The linuxcnc script calls show_errors.tcl only
    when there is no tty (terminal) or no [DISPLAY]DISPLAY
    setting.  This often means the user is starting linuxcnc
    by a graphical interface like a desktop shortcut and
    consequently there is no reporting of errors in a terminal.

--- tcl/show_errors.tcl
+++ tcl/show_errors.tcl
@@ -1,4 +1,4 @@
-#    This is a part of EMC2
+#    This is a part of LinuxCNC
 #    Copyright 2006-2009 Jeff Epler <jepler@unpythonic.net>
 #
 #    This program is free software; you can redistribute it and/or modify
@@ -15,15 +15,42 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# Load the emc package, which defines variables for various useful paths
+# Load the Linuxcnc package, which defines variables for various useful paths
 package require Linuxcnc
+
+set ::program    $::argv0
+set ::print_file [lindex $argv 1]
+set ::debug_file [lindex $argv 0]
+
+proc popup_msg {msg} {
+  tk_messageBox \
+    -title "$::program" \
+    -type ok \
+    -message "$msg"
+}
+
+proc create_report {} {
+  set   info_file   /tmp/linuxcnc.info   ;# created by linuxcnc_info
+  set   report_file /tmp/linuxcnc.report ;# created by this script
+  exec  linuxcnc_info -s                 ;# -s: suppress opening by a viewer
+
+  set   txt [.f2.t get 1.0 end]          ;# all text from text widget
+  set   fd [open $report_file w]
+  puts  $fd "Error report created by $::program:\n"
+  puts  $fd $txt
+  puts  $fd "-----------------------------------------------------------------------"
+  puts  $fd "Info report created by linuxcnc_info:"
+  close $fd
+  exec  cat $info_file >> $report_file
+  popup_msg "Report file created by linuxcnc_info:\n\n$report_file"
+}
 
 proc insert_file {w title f {p {}}} {
     if [catch {set fd [open $f r]
                set t [read $fd]
                close $fd
               } msg] {
-       set t "$::argv0:\n   $msg" ;# info on failed open
+       set t "$::program:\n   $msg" ;# info on failed open
        if {[string first dmesg $f] >= 0} {
           set sys ""
           catch {set sys [exec lsb_release -d]}
@@ -50,7 +77,7 @@ proc insert_file {w title f {p {}}} {
 wm ti . [msgcat::mc "LinuxCNC Errors"]
 frame .f
 label .f.b -bitmap error
-label .f.l -justify l -wraplength 400 -text [msgcat::mc "LinuxCNC terminated with an error.  When reporting problems, please include all the information below in your message."]
+label .f.l -justify l -wraplength 400 -text [msgcat::mc "LinuxCNC terminated with an error.  When reporting problems, please create a report file and include in your message."]
 pack .f.b -side left -padx 8 -pady 8
 pack .f.l -side left
 pack .f -side top -fill x -anchor w
@@ -65,8 +92,8 @@ grid .f2.s -row 0 -column 1 -sticky ns
 .f2.t tag configure title -font {Helvetica 16 bold}
 pack .f2 -fill both -expand 1
 
-insert_file .f2.t "Print file information:" [lindex $argv 1]
-insert_file .f2.t "Debug file information:" [lindex $argv 0]
+insert_file .f2.t "Print file information:" $::print_file
+insert_file .f2.t "Debug file information:" $::debug_file
 if {$linuxcnc::SIMULATOR != "yes"} {
     insert_file .f2.t "Kernel message information:" {|dmesg} \
 	"^.*Adeos: Pipelining started\.|^.*I-pipe: Domain RTAI registered\."
@@ -74,8 +101,12 @@ if {$linuxcnc::SIMULATOR != "yes"} {
 .f2.t configure -state disabled
 
 frame .f3
-button .f3.b1 -text [msgcat::mc "Select All"] -command {.f2.t tag add sel 0.0 end; tk_textCopy .f2.t} -width 15 -padx 4 -pady 1
+#button .f3.b1 -text [msgcat::mc "Select All"] -command {.f2.t tag add sel 0.0 end; tk_textCopy .f2.t} -width 15 -padx 4 -pady 1
+
+button .f3.b1a -text [msgcat::mc "Create Report File"] -command {create_report} -width 15 -padx 4 -pady 1
+
 button .f3.b2 -text [msgcat::mc "Close"] -command {destroy .} -width 15 -padx 4 -pady 1
-pack .f3.b1 -side left -anchor e -padx 4 -pady 4
+#pack .f3.b1 -side left -anchor e -padx 4 -pady 4
+pack .f3.b1a -side left -anchor e -padx 4 -pady 4 -fill x -expand 1
 pack .f3.b2 -side left -anchor e -padx 4 -pady 4
-pack .f3 -side top -anchor e
+pack .f3 -side top -anchor e -fill x -expand 1
