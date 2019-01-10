8563c31fbc01a6fc03cfacedef7ccbdbd19224f6
Robert W. Ellenberg <rwe24g@gmail.com>
2019-01-10 12:46:08 -0500

    Fix pedantic return type mismatch

--- src/emc/tp/tp.c
+++ src/emc/tp/tp.c
@@ -1867,11 +1867,11 @@ STATIC tc_blend_type_t tpHandleBlendArc(TP_STRUCT * const tp, TC_STRUCT * const 
     // 1 timestep's worth of distance in prev_tc
     if ( !prev_tc) {
         tp_debug_print(" queue empty\n");
-        return TP_ERR_FAIL;
+        return NO_BLEND;
     }
     if (prev_tc->progress > prev_tc->target / 2.0) {
         tp_debug_print(" prev_tc progress (%f) is too large, aborting blend arc\n", prev_tc->progress);
-        return TP_ERR_FAIL;
+        return NO_BLEND;
     }
 
     // Check for tangency between segments and handle any errors
