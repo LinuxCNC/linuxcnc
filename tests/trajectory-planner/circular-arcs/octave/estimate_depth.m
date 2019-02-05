67508fecc1c1e96b6eadc5925cf98d55e77b9806
Robert W. Ellenberg <rwe24g@gmail.com>
2019-02-05 10:32:40 -0500

    test: clean up old octave scripts and fix up broken / missing functions in test code.
    This includes some octave scratchwork to verify behavior of split cycles
    and circular segment velocity / acceleration tradeoffs.

--- tests/trajectory-planner/circular-arcs/octave/estimate_depth.m
+++ tests/trajectory-planner/circular-arcs/octave/estimate_depth.m
@@ -1,5 +1,8 @@
+%% -- estimate_depth(a_limit, v_limit, L_min, dt)
+%    
+%   For the given a_limit (max acceleration), v_limit (max velocity), estimate the queue depth 
+%   to reach peak velocity, for a series of worst-case moves of length L_min
 function depth = estimate_depth(a_limit,v_limit,L_min,dt)
-
     v_sample= L_min/(2.0*dt);
     v_max = min(v_sample,v_limit);
     v = 0;
@@ -8,5 +11,4 @@ function depth = estimate_depth(a_limit,v_limit,L_min,dt)
         v = sqrt(v^2 + 2*a_limit*L_min);
         depth+=1;
     end
-    
 end
