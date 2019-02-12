8b1ea56e3126d3c3fa0be23d14223a07a6c1ed20
Chris Morley <chrisinnanaimo@hotmail.com>
2019-02-11 23:23:26 -0800

    qtvcp -allow shift and control as keypresses

--- lib/python/qtvcp/qt_makegui.py
+++ lib/python/qtvcp/qt_makegui.py
@@ -48,7 +48,6 @@ class MyEventFilter(QtCore.QObject):
                 handled = self.w.handler_instance.keypress_event__(receiver,event)
             elif self.has_process_key_handler:
                 if event.isAutoRepeat():return True
-                if event.key() in(16777249,16777248): return True
                 p,k,c,s,ctrl = self.process_event(event,True)
                 handled = self.w.handler_instance.processed_key_event__(receiver,event,p,k,c,s,ctrl)
             if handled: return True
@@ -59,7 +58,6 @@ class MyEventFilter(QtCore.QObject):
                 handled = self.w.handler_instance.keyrelease_event__(event)
             elif self.has_process_key_handler:
                 if event.isAutoRepeat():return True
-                if event.key() in(16777249,16777248): return True
                 p,k,c,s,ctrl = self.process_event(event,False)
                 handled = self.w.handler_instance.processed_key_event__(receiver,event,p,k,c,s,ctrl)
             if handled: return True
