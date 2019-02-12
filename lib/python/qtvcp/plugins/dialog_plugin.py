6c6db78384f3016defbd1b1c9fcfeeb8edbf4bff
Chris Morley <chrisinnanaimo@hotmail.com>
2019-02-12 02:08:07 -0800

    qtvcp -add entry dialog to designer

--- lib/python/qtvcp/plugins/dialog_plugin.py
+++ lib/python/qtvcp/plugins/dialog_plugin.py
@@ -394,3 +394,40 @@ class VersaProbeDialogPlugin(QPyDesignerCustomWidgetPlugin):
     def includeFile(self):
         return "qtvcp.widgets.dialog_widget"
 
+###############################################################################
+# Entry Dialog
+###############################################################################
+class EntryDialogPlugin(QPyDesignerCustomWidgetPlugin):
+
+    def __init__(self, parent=None):
+        super(EntryDialogPlugin, self).__init__(parent)
+        self.initialized = False
+
+    def initialize(self, core):
+        if self.initialized:
+            return
+        self.initialized = True
+    def isInitialized(self):
+        return self.initialized
+    def createWidget(self, parent):
+        return EntryDialog(parent)
+    def name(self):
+        return "EntryDialog"
+    def group(self):
+        return "Linuxcnc - Dialogs"
+    def icon(self):
+        return QIcon(QPixmap(ICON.get_path('entrydialog')))
+    def toolTip(self):
+        return "Basic Entry Dialog"
+    def whatsThis(self):
+        return ""
+    def isContainer(self):
+        return False
+    # Returns an XML description of a custom widget instance that describes
+    # default values for its properties. Each custom widget created by this
+    # plugin will be configured using this description.
+    def domXml(self):
+        return '<widget class="EntryDialog" name="entrydialog" />\n'
+    def includeFile(self):
+        return "qtvcp.widgets.dialog_widget"
+
