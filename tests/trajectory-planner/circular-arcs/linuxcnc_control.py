78e2c0cea602bff87f372b83f4ec35f08ef39987
Robert W. Ellenberg <rwe24g@gmail.com>
2019-02-28 23:16:06 -0500

    tp: refactor TP functional tests to run headless
    * Make TP batch test script run headless (instead of using axis GUI).
    Along with the "nogui" versions of test configs, this makes it possible
    to execute a batch test, which will log TP errors to stderr, and return
    with a nonzero exit code if any errors are found.

--- tests/trajectory-planner/circular-arcs/linuxcnc_control.py
+++ tests/trajectory-planner/circular-arcs/linuxcnc_control.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-'''Copied from m61-test'''
+"""Copied from m61-test"""
 
 import linuxcnc
 import os
+import re
 from time import sleep
 
 
-# this is how long we wait for linuxcnc to do our bidding
-
 class LinuxcncError(Exception):
-    pass
-#    def __init__(self, value):
-#        self.value = value
-#    def __str__(self):
-#        return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class LinuxcncControl:
-    '''
+    """
     issue G-Code commands
     make sure important modes are saved and restored
     mode is saved only once, and can be restored only once
@@ -27,29 +27,57 @@ class LinuxcncControl:
             any internal sub using e.g("G0.....")
         e.finish_mdi()
 
-    '''
+    """
 
-    def __init__(self,timeout=2):
-        self.c = linuxcnc.command()
-        self.e = linuxcnc.error_channel()
-        self.s = linuxcnc.stat()
+    def __init__(self, timeout=2, throw_exceptions=False):
+        self.c = None
+        self.e = None
+        self.s = None
         self.timeout = timeout
+        self.error_list = []
+        self.raise_on_error = throw_exceptions
+        # Ignore blank errors by default
+        self.err_ignore_mask = '^$'
+        self.try_init(ignore_error=True)
+
+    def try_init(self, ignore_error=False):
+        try:
+            self.c = linuxcnc.command()
+            self.e = linuxcnc.error_channel()
+            self.s = linuxcnc.stat()
+            self.s.poll()
+        except linuxcnc.error as e:
+            if ignore_error:
+                return False
+
+        return True
+
+    def wait_for_backend(self, timeout_sec):
+        k = 0
+        print('Waiting for LinuxCNC...')
+        while k < 25:
+            k += 1
+            sleep(1)
+            if self.try_init(ignore_error=True):
+                return True
+
+        return False
 
     def running(self, do_poll=True):
-        '''
-        check wether interpreter is running.
+        """
+        check if interpreter is running.
         If so, cant switch to MDI mode.
-        '''
+        """
         if do_poll:
             self.s.poll()
         return (self.s.task_mode == linuxcnc.MODE_AUTO and
                 self.s.interp_state != linuxcnc.INTERP_IDLE)
 
-    def set_mode(self,m):
-        '''
+    def set_mode(self, m):
+        """
         set EMC mode if possible, else throw LinuxcncError
         return current mode
-        '''
+        """
         self.s.poll()
         if self.s.task_mode == m :
             return m
@@ -60,11 +88,11 @@ class LinuxcncControl:
 
         return m
 
-    def set_state(self,m):
-        '''
+    def set_state(self, m):
+        """
         set EMC mode if possible, else throw LinuxcncError
         return current mode
-        '''
+        """
         self.s.poll()
         if self.s.task_mode == m :
             return m
@@ -72,25 +100,24 @@ class LinuxcncControl:
         self.c.wait_complete(self.timeout)
         return m
 
-    def do_home(self,axismask):
+    def do_home(self, axismask):
         self.s.poll()
         self.c.home(axismask)
         self.c.wait_complete(self.timeout)
 
-
     def ok_for_mdi(self):
-        '''
+        """
         check wether ok to run MDI commands.
-        '''
+        """
         self.s.poll()
         return not self.s.estop and self.s.enabled and self.s.homed
 
     def prepare_for_mdi(self):
-        '''
+        """
         check wether ok to run MDI commands.
         throw  LinuxcncError if told so.
         return current mode
-        '''
+        """
 
         self.s.poll()
         if self.s.estop:
@@ -109,11 +136,11 @@ class LinuxcncControl:
 
     g_raise_except = True
 
-    def g(self,code,wait=False):
-        '''
+    def g(self, code, wait=False):
+        """
         issue G-Code as MDI command.
         wait for completion if reqested
-        '''
+        """
 
         self.c.mdi(code)
         if wait:
@@ -122,19 +149,10 @@ class LinuxcncControl:
                     pass
                 return True
             except KeyboardInterrupt:
-                print "interrupted by keyboard in c.wait_complete(self.timeout)"
+                self.error_list.append("warning: interrupted by keyboard in c.wait_complete(self.timeout)")
                 return False
 
-        self.error = self.e.poll()
-        if self.error:
-            kind, text = self.error
-            if kind in (linuxcnc.NML_ERROR, linuxcnc.OPERATOR_ERROR):
-                if self.g_raise_except:
-                    raise LinuxcncError(text)
-                else:
-                    print ("error " + text)
-            else:
-                print ("info " + text)
+        self.poll_and_log_error(code)
         return False
 
     def get_current_tool(self):
@@ -142,7 +160,7 @@ class LinuxcncControl:
         return self.s.tool_in_spindle
 
     def active_codes(self):
-        self.e.poll()
+        self.s.poll()
         return self.s.gcodes
 
     def get_current_system(self):
@@ -154,48 +172,80 @@ class LinuxcncControl:
                         return i - 584
         return 1
 
-    def open_program(self,filename):
-        '''Open an nc file'''
+    def open_program(self, filename):
+        """Open an nc file"""
         self.s.poll()
         self.set_mode(linuxcnc.MODE_AUTO)
         self.c.wait_complete()
         sleep(.25)
-        self.c.program_open(filename)
+        full_file = os.path.join(os.getcwd(), filename)
+        self.c.program_open(full_file)
         self.c.wait_complete()
 
     def run_full_program(self):
-        '''Start a loaded program'''
+        """Start a loaded program"""
         self.s.poll()
         self.c.auto(linuxcnc.AUTO_RUN, 0)
-        self.c.wait_complete(self.timeout)
-        return self.check_rcs_error()
+        return self.c.wait_complete(self.timeout)
 
-    def set_feed_scale(self,scale):
-        '''Assign a feed scale'''
+    def set_feed_scale(self, scale):
+        """Assign a feed scale"""
 
         self.s.poll()
         self.c.feedrate(scale)
         self.c.wait_complete(self.timeout)
 
-    def wait_on_program(self):
+    def wait_on_program(self, f):
         self.s.poll()
         while self.s.exec_state != linuxcnc.EXEC_DONE or self.s.state != linuxcnc.RCS_DONE and self.s.task_state == linuxcnc.STATE_ON:
-            sleep(.25)
+            sleep(.01)
             self.s.poll()
+            self.flush_errors(f)
             if self.s.task_state != linuxcnc.STATE_ON:
+                self.error_list.append('{}: error: motion disabled'.format(f))
                 return False
-            if self.check_rcs_error():
-                print "Found RCS error while waiting, running again"
-                self.run_full_program()
 
         return True
+
+    def run_until_done(self, f):
+        self.set_mode(linuxcnc.MODE_AUTO)
+        self.open_program(f)
+        self.set_mode(linuxcnc.MODE_AUTO)
+        self.run_full_program()
+        return self.wait_on_program(f)
 
     def check_rcs_error(self):
         self.s.poll()
         if self.s.state == linuxcnc.RCS_ERROR:
-            print "detected RCS error"
             return True
         return False
 
-def introspect():
-    os.system("halcmd show pin python-ui")
+    def set_error_ignore_pattern(self, pattern):
+        self.err_ignore_mask = pattern
+
+    def poll_and_log_error(self, f):
+        error = self.e.poll()
+        if error is None:
+            return False
+
+        err_type, msg_text = error
+        err_type_str = "error" if err_type in (linuxcnc.NML_ERROR, linuxcnc.OPERATOR_ERROR) else "info"
+        err_str = "{}: {}: {}".format(f, err_type_str, msg_text)
+        self.error_list.append(err_str)
+        if self.raise_on_error:
+            raise LinuxcncError(err_str)
+        return True
+
+    def flush_errors(self, filename=""):
+        while self.poll_and_log_error(filename):
+            sleep(0.001)
+            continue
+
+    def has_error(self):
+        for msg in self.error_list:
+            if re.search(self.err_ignore_mask, msg):
+                continue
+            if re.search('error: ', msg):
+                return True
+        else:
+            return False
--- tests/trajectory-planner/circular-arcs/run_all_tests.py
+++ tests/trajectory-planner/circular-arcs/run_all_tests.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-'''Copied from m61-test'''
+"""
+Based on the control script in m61_test
+"""
 
 import linuxcnc
 from linuxcnc_control import  LinuxcncControl
-import hal
+#import hal
 import sys
 
 from time import sleep
@@ -11,7 +13,8 @@ from time import sleep
 from os import listdir
 from os.path import isfile, join
 import re
-import Tkinter, os
+import glob
+
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
@@ -45,71 +48,58 @@ def query_yes_no(question, default="yes"):
             sys.stdout.write("Please respond with 'yes' or 'no' "\
                              "(or 'y' or 'n').\n")
 
+
 def find_test_nc_files(testpath='nc_files', show=False):
     files = [ testpath + '/' + f for f in listdir(testpath) if isfile(join(testpath,f)) and re.search('.ngc$',f) is not None ]
     if show:
         print "In folder {0}, found {1} files:".format(testpath,len(files))
-        for f in files:
-            print f
+        for fname in files:
+            print fname
     return files
 
-def axis_open_program(t,f):
-    return t.tk.call("send", "axis", ("remote","open_file_name", os.path.abspath(f)))
 
-t = Tkinter.Tk()
-t.wm_withdraw()
+if __name__ == '__main__':
+    """ Run batch tests (user specifies which ones interactively) """
+    run_sts = 0
 
-"""Run the test"""
+    # raw_input("Press any key when the LinuxCNC GUI has loaded")
+    # KLUDGE this lists all subfolders in the auto-test directory
+    # this dir should be populated with symlinks to any folders of test files to
+    # run
+    test_folders = glob.glob('nc_files/auto-test/*')
 
-raw_input("Press any key when the LinuxCNC GUI has loaded")
+    test_files = []
 
-import glob
-#KLUDGE this lists all subfolders in the auto-test directory
-# this dir should be populated with symlinks to any folders of test files to
-# run
-test_folders=glob.glob('nc_files/auto-test/*')
+    for f in test_folders:
+        test_files.extend(find_test_nc_files(f, True))
 
-test_files = []
+    e = LinuxcncControl(1)
 
+    load_timeout = 5
+    if not e.wait_for_backend(load_timeout):
+        sys.stderr.write("error: LinuxCNC failed to load in {} seconds".format(load_timeout))
+        sys.exit(2)
 
-for f in test_folders:
-    run_quick = query_yes_no("Run test cases in %s?" % f )
-    if run_quick:
-        test_files.extend(find_test_nc_files(f,True))
+    e.g_raise_except = False
+    e.set_mode(linuxcnc.MODE_MANUAL)
+    e.set_state(linuxcnc.STATE_ESTOP_RESET)
+    e.set_state(linuxcnc.STATE_ON)
+    e.do_home(-1)
+    sleep(0.5)
+    e.set_mode(linuxcnc.MODE_AUTO)
+    sleep(0.5)
 
+    error_list = []
+    for f in test_files:
+        if re.search('.ngc$|.TAP$|.nc$', f) is not None:
+            print "Loading program {0}".format(f)
+            e.set_mode(linuxcnc.MODE_AUTO)
+            e.open_program(f)
+            e.set_mode(linuxcnc.MODE_AUTO)
+            e.run_full_program()
+            e.wait_on_program(f)
 
-h = hal.component("python-ui")
-h.ready() # mark the component as 'ready'
-
-#Don't need this with axis?
-#os.system("halcmd source ./postgui.hal")
-
-#
-# connect to LinuxCNC
-#
-
-e = LinuxcncControl(1)
-e.g_raise_except = False
-e.set_mode(linuxcnc.MODE_MANUAL)
-e.set_state(linuxcnc.STATE_ESTOP_RESET)
-e.set_state(linuxcnc.STATE_ON)
-e.do_home(-1)
-sleep(1)
-e.set_mode(linuxcnc.MODE_AUTO)
-sleep(1)
-for f in test_files:
-    if re.search('\.ngc$',f) is not None:
-        print "Loading program {0}".format(f)
-        e.set_mode(linuxcnc.MODE_AUTO)
-        sleep(1)
-        axis_open_program(t,f)
-        sleep(1)
-        e.set_mode(linuxcnc.MODE_AUTO)
-        while e.run_full_program():
-            sleep(.5)
-        res = e.wait_on_program()
-        if res == False:
-            print "Program {0} failed to complete!".format(f)
-            sys.exit(1)
-
-print "All tests completed!"
+    # KLUDGE just in case any others creep in
+    e.flush_errors()
+    sys.stderr.writelines(e.error_list)
+    sys.exit(e.has_error())
