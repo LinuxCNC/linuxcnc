1b8d53a25e05bf49172a568c3066d6844eaae0bd
Robert W. Ellenberg <rwe24g@gmail.com>
2019-04-17 16:43:39 -0400

    Redirect unit test canon output to a log file (not currently used for tests results)

--- unit_tests/interp/tests_main.cc
+++ unit_tests/interp/tests_main.cc
@@ -5,6 +5,7 @@
 #include <rs274ngc_interp.hh>
 extern Interp *pinterp;
 Interp dummy_interp;
+#include <stdio.h>
 
 // KLUDGE fix missing symbol the ugly way
 struct _inittab builtin_modules[] = {
@@ -15,6 +16,7 @@ int main (int argc, char * argv[]) {
     // KLUDGE just to satisfy saicanon dependencies, not used in tests
     dummy_interp = Interp();
     pinterp = &dummy_interp;
+    _outfile = fopen("test_interp_canon.log", "w");
     PythonPlugin::instantiate(builtin_modules);
     return Catch::Session().run( argc, argv );
 }
