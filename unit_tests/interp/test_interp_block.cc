0f6f3688e383da98edc4e3a9c23bee5338a8f003
Robert W. Ellenberg <rwe24g@gmail.com>
2019-04-23 10:04:24 -0400

    unit_test: parse tests for G0 and G2 moves

--- unit_tests/interp/test_interp_block.cc
+++ unit_tests/interp/test_interp_block.cc
@@ -35,16 +35,15 @@ static block_struct read_and_parse_command(Interp &interp, const char *command)
 
 SCENARIO("Movement commands")
 {
-  GIVEN("Linear Feed along all axes ")
+  GIVEN("Linear Feed along multiple axes ")
   {
     DECL_INIT_TEST_INTERP();
-    REQUIRE_INTERP_OK(test_interp.execute("g20"));
-    REQUIRE(settings->length_units == CANON_UNITS_INCHES);
 
     THEN("Block should have G1 word and axis words") {
       const char *command = "G1 X1 Y2 Z3 A4 C6";
 
       block_struct eblock = read_and_parse_command(test_interp, command);
+      REQUIRE(eblock.g_modes[GM_MOTION] == G_1);
       REQUIRE(eblock.x_flag);
       REQUIRE(eblock.y_flag);
       REQUIRE(eblock.z_flag);
@@ -57,6 +56,52 @@ SCENARIO("Movement commands")
       REQUIRE_FUZZ(eblock.z_number, 3);
       REQUIRE_FUZZ(eblock.a_number, 4);
       REQUIRE_FUZZ(eblock.c_number, 6);
+    }
+  }
+  GIVEN("Rapid along multiple axes ")
+  {
+    DECL_INIT_TEST_INTERP();
+
+    THEN("Block should have G0 word and axis words") {
+      const char *command = "G0 X1 Y2 Z3 A4 C6";
+
+      block_struct eblock = read_and_parse_command(test_interp, command);
+      REQUIRE(eblock.g_modes[GM_MOTION] == G_0);
+      REQUIRE(eblock.x_flag);
+      REQUIRE(eblock.y_flag);
+      REQUIRE(eblock.z_flag);
+      REQUIRE(eblock.a_flag);
+      REQUIRE(!eblock.b_flag);
+      REQUIRE(eblock.c_flag);
+
+      REQUIRE_FUZZ(eblock.x_number, 1);
+      REQUIRE_FUZZ(eblock.y_number, 2);
+      REQUIRE_FUZZ(eblock.z_number, 3);
+      REQUIRE_FUZZ(eblock.a_number, 4);
+      REQUIRE_FUZZ(eblock.c_number, 6);
+    }
+  }
+  GIVEN("Clockwise circular motion commanded")
+  {
+    DECL_INIT_TEST_INTERP();
+    const char *command = "G2 X1 Y2 Z3 R1";
+    block_struct eblock = read_and_parse_command(test_interp, command);
+
+    THEN("Block should have G2 / G3word and axis words") {
+
+      REQUIRE(eblock.g_modes[GM_MOTION] == G_2);
+      REQUIRE(eblock.x_flag);
+      REQUIRE(eblock.y_flag);
+      REQUIRE(eblock.z_flag);
+      REQUIRE(eblock.r_flag);
+      REQUIRE(!eblock.a_flag);
+      REQUIRE(!eblock.b_flag);
+      REQUIRE(!eblock.c_flag);
+
+      REQUIRE_FUZZ(eblock.x_number, 1);
+      REQUIRE_FUZZ(eblock.y_number, 2);
+      REQUIRE_FUZZ(eblock.z_number, 3);
+      REQUIRE_FUZZ(eblock.r_number, 1);
     }
   }
 }
