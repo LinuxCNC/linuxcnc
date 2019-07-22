5cc0f8b3454866a0c8244204de40a72befd0c853
TurBoss <j.l.toledano.l@gmail.com>
2019-07-22 05:47:37 +0200

    bump travis to bionic so we have python 3.6

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 cd src
 ./autogen.sh
-./configure --with-realtime=uspace --disable-check-runtime-deps
+./configure --with-realtime=uspace --disable-check-runtime-deps --with-python=python3 --with-boost-python=boost_python36
 make -j2  # cores: ~2, bursted
 ../scripts/rip-environment runtests
