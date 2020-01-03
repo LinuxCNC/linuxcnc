108b18f140620cf7c567c0c1a1eaa62f278adca7
Mark <mark.vandoesburg@hetnet.nl>
2020-01-03 22:36:03 +0100

    We're no longer parsing so don't include <regex>

--- src/emc/rs274ngc/interp_g7x.cc
+++ src/emc/rs274ngc/interp_g7x.cc
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <deque>
-#include <regex>
+#include <memory>
 #include <complex>
 
 ////////////////////////////////////////////////////////////////////////////////
