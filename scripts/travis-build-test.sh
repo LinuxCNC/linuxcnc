31653f599b458ab449db71c03cee2e16a6fe90fb
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-04-21 22:20:21 +0200

    Make build process more verbose
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 cd src
 ./autogen.sh
 ./configure --with-realtime=uspace --disable-check-runtime-deps --enable-build-documentation
-make -O -j$(nproc)
+make -O -j$(nproc) V=1
 ../scripts/rip-environment runtests
