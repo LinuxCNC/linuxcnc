3b0e411fc84c0383ec3941476c51bd938b75b4ac
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-04-24 00:06:26 +0200

    Fix tests/interp/python-self
    Fixes:
     File "./interp/python-self/subs.py", line 24
        print "__init__: self not is this"
                                         ^
    SyntaxError: Missing parentheses in call to 'print'. Did you mean print("__init__: self not is this")?
    
     File "./interp/python-self/oword.py", line 25
        print "param1 was retained, value = ", self.param1
                                            ^
    SyntaxError: Missing parentheses in call to 'print'. Did you mean print("param1 was retained, value = ", self.param1)?
    
    Sorry: TabError: inconsistent use of tabs and spaces in indentation (oword.py, line 31)
    
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- tests/interp/python-self/oword.py
+++ tests/interp/python-self/oword.py
@@ -22,13 +22,13 @@ def retainparam1(self,x):
 
 def checkparam1_retained(self):
     if hasattr(self,'param1'):
-        print "param1 was retained, value = ", self.param1
+        print("param1 was retained, value =  {}".format(self.param1))
     else:
         return "param1 was not retained across invocations"
 
     # test object identity
     if hasattr(interpreter,'this'):
-	print "this is self:", self is interpreter.this
+        print("this is self: {}".format(self is interpreter.this))
     else:
-        print "module interpreter: no 'this' attribute"
+        print("module interpreter: no 'this' attribute")
         
