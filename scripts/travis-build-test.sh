e9290b7f036b88cc79f6ba54ce68d44853828994
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2020-04-24 19:15:38 +0200

    Add pycheck (Python check) Makefile target
    It is designed to be used for checking compatibility of python
    scripts (currently only from the 'tests' subdirectory) with
    both python2 and python3 interpreters.
    
    Usage:
      $ make pycheck
    
    Successfull output:
      $ make pycheck
      Reading 200/200 dependency files
      Done reading dependencies
      Reading 212/212 realtime dependency files
      Done reading realtime dependencies
      Checking *.py files for python2 and python3 compatibility...
      Checking "text/x-python" (by MIME type) files for python2 and python3 compatibility...
    
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -2,5 +2,5 @@
 cd src
 ./autogen.sh
 ./configure --with-realtime=uspace --disable-check-runtime-deps --enable-build-documentation
-make -O -j$(nproc) V=1
+make -O -j$(getconf _NPROCESSORS_ONLN) default pycheck V=1
 ../scripts/rip-environment runtests
