e2eaf6ad81b40ed6bb6fe30adb07edd3961e341d
Rene Hopf <renehopf@mac.com>
2020-05-03 13:40:19 +0200

    debuglevel.py: python3 compatibility

--- src/emc/usr_intf/axis/scripts/debuglevel.py
+++ src/emc/usr_intf/axis/scripts/debuglevel.py
@@ -2,7 +2,10 @@
 import sys, os
 import gettext
 BASE = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), ".."))
-gettext.install("linuxcnc", localedir=os.path.join(BASE, "share", "locale"), unicode=True)
+if sys.version_info[0] == 3:
+    gettext.install("linuxcnc", localedir=os.path.join(BASE, "share", "locale"))
+else:
+    gettext.install("linuxcnc", localedir=os.path.join(BASE, "share", "locale"), unicode=True)
 
 import linuxcnc
 import tkinter
