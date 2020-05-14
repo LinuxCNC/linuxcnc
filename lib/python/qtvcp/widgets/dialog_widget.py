aee3fb8a6b1fc600e99634024b82cfb978fac4a9
Chris Morley <chrisinnanaimo@hotmail.com>
2020-05-14 13:13:20 -0700

    qtvcp -dialog_widgets: fix hiding of overly if file loading cancelled.

--- lib/python/qtvcp/widgets/dialog_widget.py
+++ lib/python/qtvcp/widgets/dialog_widget.py
@@ -538,8 +538,9 @@ class FileDialog(QFileDialog, GeometryMixin):
                 self.PREFS_.putpref('RecentPath_0', fname, str, 'BOOK_KEEPING')
             ACTION.OPEN_PROGRAM(fname)
             STATUS.emit('update-machine-log', 'Loaded: ' + fname, 'TIME')
-        # overlay hides it's self after loading
-        #STATUS.emit('focus-overlay-changed', False, None, None)
+            # overlay hides it's self after loading
+        else:
+            STATUS.emit('focus-overlay-changed', False, None, None)
         return fname
 
     def save_dialog(self, extensions = None, preselect = None, directory = None):
