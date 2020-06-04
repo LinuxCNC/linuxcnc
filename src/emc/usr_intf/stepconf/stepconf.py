14e58bc43dcea2f5213ca4103c65b6290a864008
Nicola Quargentan <nicola@quargentan.com>
2020-06-04 10:40:17 +0200

    Removed reading of the /proc/ioports file. Now the search for parallel ports only watch /proc/sys/dev/parport.
    Signed-off-by: Nicola Quargentan <nicola@quargentan.com>

--- src/emc/usr_intf/stepconf/stepconf.py
+++ src/emc/usr_intf/stepconf/stepconf.py
@@ -917,71 +917,42 @@ class StepconfApp:
     def find_parport(self):
         # Try to find parallel port
         lparport=[]
-        # open file.
-        """
-        in_file = open("/proc/ioports","r")
-        for line in in_file:
-            if "parport" in line:
-                tmprow = line.strip()
-                lrow = tmprow.split(":")
-                address_range = lrow[0].strip()
-                init_address = address_range.split("-")[0].strip()
-                if(init_address != "0000"):
-                    lparport.append("0x" + init_address)
-        in_file.close()
-        """
-        try:
-            in_file = open("/proc/ioports","r")
-            try:
-                for line in in_file:
-                    if "parport" in line:
-                        tmprow = line.strip()
-                        lrow = tmprow.split(":")
-                        address_range = lrow[0].strip()
-                        init_address = address_range.split("-")[0].strip()
-                        if(init_address != "0000"):
-                            lparport.append("0x" + init_address)
-                in_file.close()
-                # if I have found ports then return
-                if (lparport != []):
-                    return(lparport)
-            except err:
-                print ("Error read parport in /proc/ioports")
-                in_file.close()
-        except:
-            print ("Unable to open /proc/ioports")
-
-        if lparport == []:
-            # Try /proc/sys/dev/parport/parport#/base-addr
-            parport_path="/proc/sys/dev/parport"
-            if(os.path.isdir(parport_path)):
-                parport_list=os.listdir(parport_path)
-                for current_parport in parport_list:
-                    if(current_parport == "default"):
+        # Try /proc/sys/dev/parport/parport#/base-addr
+        parport_path="/proc/sys/dev/parport"
+        if(os.path.isdir(parport_path)):
+            parport_list=os.listdir(parport_path)
+            for current_parport in parport_list:
+                if(current_parport == "default"):
+                    continue
+                # find port number
+                find_string="parport"
+                if(current_parport.find(find_string) == 0):
+                    try:
+                        port_number=current_parport.split(find_string)[1]
+                        lparport.append(port_number)
+                    except:
                         continue
-                    # TODO
-                    # find base-addr file
-                    baseaddr=os.path.join(parport_path, current_parport, "base-addr")
-                    if(os.path.exists(baseaddr) == True):
-                        try:
-                            in_file = open(baseaddr,"r")
-                        except:
-                            print ("Unable to open %s" % baseaddr )
-                            continue
-                        # read base-addr file
-                        try:
-                            for line in in_file:
-                                # get init_address and number of port
-                                lline=line.split()
-                                dec_address=lline[0].strip()
-                                port_number=lline[1].strip()
-                                init_address=hex(int(dec_address))
-                                lparport.append(port_number)
-                        except:
-                            print ("Error read %s" % baseaddr)
-                            in_file.close()
-                            continue
+                # find base-addr file
+                # Not used, but I want to be sure there is a real parport
+                baseaddr=os.path.join(parport_path, current_parport, "base-addr")
+                if(os.path.exists(baseaddr) == True):
+                    try:
+                        in_file = open(baseaddr,"r")
+                    except:
+                        print ("Unable to open %s" % baseaddr )
+                        continue
+                    # read base-addr file
+                    try:
+                        for line in in_file:
+                            # get init_address
+                            lline=line.split()
+                            dec_address=lline[0].strip()
+                            init_address=hex(int(dec_address))
+                    except:
+                        print ("Error read %s" % baseaddr)
                         in_file.close()
+                        continue
+                    in_file.close()
         if lparport == []:
             print ("No parport found")
             return([])
