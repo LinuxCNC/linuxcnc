209902da30c9dd68a775819177d44ab695e3220b
Phillip Carter <phillcarter54@gmail.com>
2020-06-09 17:52:20 +1000

    plasmac: fix float probe kangaroo

--- configs/by_machine/plasmac/plasmac_config.py
+++ configs/by_machine/plasmac/plasmac_config.py
@@ -321,7 +321,7 @@ class HandlerClass:
 
     def __init__(self, halcomp,builder,useropts):
 
-        self.plasmacVersion = 'PlasmaC v0.133'
+        self.plasmacVersion = 'PlasmaC v0.134'
 
         self.halcomp = halcomp
         self.builder = builder
