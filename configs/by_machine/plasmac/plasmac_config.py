251b993c61f339506edf276d6fad3866e526dd94
Phillip Carter <phillcarter54@gmail.com>
2020-08-26 21:06:26 +1000

    plasmac: fix offset clear for low home height

--- configs/by_machine/plasmac/plasmac_config.py
+++ configs/by_machine/plasmac/plasmac_config.py
@@ -321,7 +321,7 @@ class HandlerClass:
 
     def __init__(self, halcomp,builder,useropts):
 
-        self.plasmacVersion = 'PlasmaC v0.165'
+        self.plasmacVersion = 'PlasmaC v0.166'
 
         self.halcomp = halcomp
         self.builder = builder
