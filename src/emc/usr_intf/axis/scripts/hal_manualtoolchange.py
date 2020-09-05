063274274032e78e356f94a4df6d2961fde6b75a
htasta <fuckallmailregisterations@mail.ch>
2020-09-05 20:10:53 +0200

    hal_manualtoolchange: additional information in tool change dialog
    Added diameter and comment from the tool table to tool change dialog
    to make it easier to find the right tool without having to look up
    the tool number.

--- src/emc/usr_intf/axis/scripts/hal_manualtoolchange.py
+++ src/emc/usr_intf/axis/scripts/hal_manualtoolchange.py
@@ -34,7 +34,7 @@ def stop_polling_hal_in_background():
 
 def do_change(n):
     if n:
-        message = _("Insert tool %d and click continue when ready") % n
+        message = get_tool_change_message(n)
     else:
         message = _("Remove the tool and click continue when ready")
     app.wm_withdraw()
@@ -49,6 +49,49 @@ def do_change(n):
     if r == 0:
         h.changed = True
     app.update()
+
+def get_tool_change_message(n):
+    try:
+        linuxcnc_stat = linuxcnc.stat()
+        linuxcnc_stat.poll()
+        inidir = os.path.dirname(linuxcnc_stat.inifile_path)
+        inifile = linuxcnc.ini(linuxcnc_stat.inifile_path)
+        tooltable_file = inifile.find("EMCIO", "TOOL_TABLE")
+        machine_units = inifile.find("TRAJ", "LINEAR_UNITS")
+        # make sure we get an absolute path to the tool table
+        if (tooltable_file != ""):
+            if (not os.path.isabs(tooltable_file)):
+                tooltable_file = os.path.join(inidir, tooltable_file)
+        # load the tool table file
+        tool_info = get_tool_info(tooltable_file, n)
+        diameter = ("%f" % tool_info["diameter"]).rstrip("0").rstrip(".")
+        return "".join((_("Insert tool and click continue when ready\n\n"),
+                        _("Tool number: %d\n") % n,
+                        _("Diameter: %s%s\n") % (diameter, machine_units),
+                        _("Comment: %s") % tool_info["comment"]))
+    except Exception as error:
+        # old style message with just tool number and the error message
+        return "".join((_("Insert tool %d and click continue when ready.") % n,
+                        _("\n\nError: %s") % error))
+
+def get_tool_info(file, n):
+    with open(file, "r") as f:
+        for i, line in enumerate(f):
+            tool_data = parse_line(line)
+            if tool_data["number"] == n:
+                return tool_data
+    raise Exception(_("Tool not found."))
+
+def parse_line(line):
+    data = {}
+    parts = line.partition(";")
+    data["comment"] = parts[2]
+    for token in parts[0].upper().split(" "):
+        if (token.startswith("T")):
+            data["number"] = int(token[1:])
+        if (token.startswith("D")):
+            data["diameter"] = float(token[1:])
+    return data
 
 h = hal.component("hal_manualtoolchange")
 h.newpin("number", hal.HAL_S32, hal.HAL_IN)
