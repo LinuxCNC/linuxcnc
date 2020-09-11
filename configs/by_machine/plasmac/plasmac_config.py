1ffce156e1855bb251cd5c3a117edeacc1356061
Phillip Carter <phillcarter54@gmail.com>
2020-09-11 15:18:11 +1000

    plasmac: use dbounce component for new configs

--- configs/by_machine/plasmac/plasmac_config.py
+++ configs/by_machine/plasmac/plasmac_config.py
@@ -350,7 +350,7 @@ class HandlerClass:
 
     def __init__(self, halcomp,builder,useropts):
 
-        self.plasmacVersion = 'PlasmaC v0.172'
+        self.plasmacVersion = 'PlasmaC v0.173'
 
         self.W = gtk.Window()
         self.halcomp = halcomp
