fa4abe05690e858071a2204578fbc4c602eb87be
Phillip Carter <phillcarter54@gmail.com>
2020-09-16 14:52:48 +1000

    plasmac: fix cut-recovery direction buttons

--- configs/by_machine/plasmac/plasmac_config.py
+++ configs/by_machine/plasmac/plasmac_config.py
@@ -350,7 +350,7 @@ class HandlerClass:
 
     def __init__(self, halcomp,builder,useropts):
 
-        self.plasmacVersion = 'PlasmaC v0.174'
+        self.plasmacVersion = 'PlasmaC v0.175'
 
         self.W = gtk.Window()
         self.halcomp = halcomp
