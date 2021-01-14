6e0c97869bc2b6bf7dd6e0dba4ab5fb33b4cf7c8
Dewey Garrett <dgarrett@panix.com>
2021-01-14 06:28:44 -0700

    motion-logger.c print format was: %.6f, is: %.6g
    to reduce long lines occuring for items like 1e+99
    example:
    
    -SET_ACC acc=999999999999999967336168804116 ...
    +SET_ACC acc=1e+99

--- tests/motion-logger/basic/expected.s
+++ tests/motion-logger/basic/expected.s
@@ -1,15 +1,15 @@
-SET_LINE x=1.000000, y=0.000000, z=0.000000, a=0.000000, b=0.000000, c=0.000000, u=0.000000, v=0.000000, w=0.000000, id=2, motion_type=1, vel=4.000000, ini_maxvel=4.000000, acc=1000.000000, turn=-1
+SET_LINE x=1, y=0, z=0, a=0, b=0, c=0, u=0, v=0, w=0, id=2, motion_type=1, vel=4, ini_maxvel=4, acc=1000, turn=-1
 SPINDLE_ON speed=0.000000, css_factor=0.000000, xoffset=0.000000
-SET_LINE x=1.500000, y=0.000000, z=0.000000, a=0.000000, b=0.000000, c=0.000000, u=0.000000, v=0.000000, w=0.000000, id=6, motion_type=1, vel=4.000000, ini_maxvel=4.000000, acc=1000.000000, turn=-1
+SET_LINE x=1.5, y=0, z=0, a=0, b=0, c=0, u=0, v=0, w=0, id=6, motion_type=1, vel=4, ini_maxvel=4, acc=1000, turn=-1
 SPINDLE_ON speed=0.000000, css_factor=0.000000, xoffset=0.000000
-SET_LINE x=2.000000, y=0.000000, z=0.000000, a=0.000000, b=0.000000, c=0.000000, u=0.000000, v=0.000000, w=0.000000, id=10, motion_type=1, vel=4.000000, ini_maxvel=4.000000, acc=1000.000000, turn=-1
+SET_LINE x=2, y=0, z=0, a=0, b=0, c=0, u=0, v=0, w=0, id=10, motion_type=1, vel=4, ini_maxvel=4, acc=1000, turn=-1
 SPINDLE_ON speed=234.000000, css_factor=0.000000, xoffset=0.000000
-SET_LINE x=3.000000, y=0.000000, z=0.000000, a=0.000000, b=0.000000, c=0.000000, u=0.000000, v=0.000000, w=0.000000, id=14, motion_type=1, vel=4.000000, ini_maxvel=4.000000, acc=1000.000000, turn=-1
+SET_LINE x=3, y=0, z=0, a=0, b=0, c=0, u=0, v=0, w=0, id=14, motion_type=1, vel=4, ini_maxvel=4, acc=1000, turn=-1
 SPINDLE_ON speed=321.000000, css_factor=0.000000, xoffset=0.000000
 SPINDLE_ON speed=432.000000, css_factor=0.000000, xoffset=0.000000
-SET_LINE x=4.000000, y=0.000000, z=0.000000, a=0.000000, b=0.000000, c=0.000000, u=0.000000, v=0.000000, w=0.000000, id=19, motion_type=1, vel=4.000000, ini_maxvel=4.000000, acc=1000.000000, turn=-1
+SET_LINE x=4, y=0, z=0, a=0, b=0, c=0, u=0, v=0, w=0, id=19, motion_type=1, vel=4, ini_maxvel=4, acc=1000, turn=-1
 SPINDLE_OFF
 SPINDLE_ON speed=0.000000, css_factor=0.000000, xoffset=0.000000
-SET_LINE x=5.000000, y=0.000000, z=0.000000, a=0.000000, b=0.000000, c=0.000000, u=0.000000, v=0.000000, w=0.000000, id=26, motion_type=1, vel=4.000000, ini_maxvel=4.000000, acc=1000.000000, turn=-1
+SET_LINE x=5, y=0, z=0, a=0, b=0, c=0, u=0, v=0, w=0, id=26, motion_type=1, vel=4, ini_maxvel=4, acc=1000, turn=-1
 SET_SPINDLESYNC sync=0.000000, flags=0x00000000
 SPINDLE_OFF
