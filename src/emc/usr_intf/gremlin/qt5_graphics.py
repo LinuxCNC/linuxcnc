6b01fc6ce964a8ccfc535e169a57ae0d20a0188d
Chris Morley <chrisinnanaimo@hotmail.com>
2021-01-26 22:39:40 -0800

    qtvcp -gcode_display: fix broken display when using DRO and lathe
    After fixing the insert DRO display awhile back i guess I never tested
    a lathe config that used the DRO.
    I forgot to add a needed attibutes. With openGL code, it doesn't crash
    it just does weird things.

--- src/emc/usr_intf/gremlin/qt5_graphics.py
+++ src/emc/usr_intf/gremlin/qt5_graphics.py
@@ -251,6 +251,7 @@ class Lcnc_3dGraphics(QGLWidget,  glcanon.GlCanonDraw, glnav.GlNavBase):
         self.show_rapids = True
         self.use_relative = True
         self.show_tool = True
+        self.show_lathe_radius = False
         self.show_dtg = True
         self.grid_size = 0.0
         temp = self.inifile.find("DISPLAY", "LATHE")
