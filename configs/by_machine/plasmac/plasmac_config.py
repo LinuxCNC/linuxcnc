1ed191f4d9805232e0c45ebefc23564822c273e9
Phillip Carter <phillcarter54@gmail.com>
2021-02-22 13:48:02 +1100

    plasmac: fix materialverter for sheetcam update

--- configs/by_machine/plasmac/plasmac_config.py
+++ configs/by_machine/plasmac/plasmac_config.py
@@ -350,7 +350,7 @@ class HandlerClass:
 
     def __init__(self, halcomp,builder,useropts):
 
-        self.plasmacVersion = 'PlasmaC v0.190'
+        self.plasmacVersion = 'PlasmaC v0.191'
 
         self.W = gtk.Window()
         self.halcomp = halcomp
