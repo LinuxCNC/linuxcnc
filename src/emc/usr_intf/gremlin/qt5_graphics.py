fa5bce58d522ff8e735eab0168e867c7589dc31f
Chris Morley <chrisinnanaimo@hotmail.com>
2021-03-05 11:43:51 -0800

    qtvcp -qt5_graphics: fix spelling mistakes in Gcode properties

--- src/emc/usr_intf/gremlin/qt5_graphics.py
+++ src/emc/usr_intf/gremlin/qt5_graphics.py
@@ -443,11 +443,11 @@ class Lcnc_3dGraphics(QGLWidget,  glcanon.GlCanonDraw, glnav.GlNavBase):
                 )
  
             props['G0'] = "%f %s".replace("%f", fmt) % (from_internal_linear_unit(g0, conv), units)
-            props['gG1'] = "%f %s".replace("%f", fmt) % (from_internal_linear_unit(g1, conv), units)
+            props['G1'] = "%f %s".replace("%f", fmt) % (from_internal_linear_unit(g1, conv), units)
             if gt > 120:
                 props['Run'] = _("%.1f Minutes") % (gt/60)
             else:
-                props['Run'] = _("%d Ceconds") % (int(gt))
+                props['Run'] = _("%d Seconds") % (int(gt))
 
             min_extents = from_internal_units(canon.min_extents, conv)
             max_extents = from_internal_units(canon.max_extents, conv)
