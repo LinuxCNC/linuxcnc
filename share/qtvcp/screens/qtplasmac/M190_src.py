81b848db7c7cb99922b26353116b352c148b0b81
Phillip Carter <phillcarter54@gmail.com>
2021-05-15 16:46:23 +1000

    qtplasmac: make helper files use build python version

--- share/qtvcp/screens/qtplasmac/M190_src.py
+++ share/qtvcp/screens/qtplasmac/M190_src.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 '''
 M190
@@ -22,26 +21,27 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 import sys
 import time
-
+import hal
 from subprocess import Popen,PIPE
 
+h = hal.component('dummy')
 materialNum = int(float(sys.argv[1]))
 timeout = 0.5
 
 def get_material():
-    return int(Popen(['halcmd getp qtplasmac.material_change_number'], stdout=PIPE, shell=True).communicate()[0].decode().strip())
+    return int(hal.get_value('qtplasmac.material_change_number'))
 
 def set_material(material):
-    Popen(['halcmd setp qtplasmac.material_change_number %d' %(material)], shell=True)
+    hal.set_p('qtplasmac.material_change_number', '{}'.format(material))
 
 def get_change():
-    return int(Popen(['halcmd getp qtplasmac.material_change'], stdout=PIPE, shell=True).communicate()[0].decode().strip())
+    return int(hal.get_value('qtplasmac.material_change'))
 
 def set_change(value):
-    Popen(['halcmd setp qtplasmac.material_change %d' %(value)], shell=True)
+    hal.set_p('qtplasmac.material_change', '{}'.format(value))
 
 def set_timeout():
-    Popen(['halcmd setp qtplasmac.material_change_timeout 1'], shell=True)
+    hal.set_p('qtplasmac.material_change_timeout', '1')
 
 try:
     if materialNum != get_material():
