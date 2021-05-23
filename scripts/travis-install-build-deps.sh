c8074d7a002850eb550e27cfd4cd80f494c8a5ea
Rene Hopf <renehopf@mac.com>
2021-05-23 17:06:19 +0200

    add debug output to mk-build-deps

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -6,5 +6,5 @@ sudo apt-get install -y devscripts equivs build-essential --no-install-recommend
 sudo apt-get remove -f libreadline6-dev || true
 sudo apt-get remove -f libreadline-dev || true
 debian/configure uspace noauto
-mk-build-deps -i -r -s sudo -t 'apt-get --no-install-recommends --no-install-suggests'
+mk-build-deps -i -r -s sudo -t 'apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --no-install-suggests'
 sudo apt install -y lintian
