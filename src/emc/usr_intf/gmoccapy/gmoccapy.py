7c42be9e435d201fbf6344c16316422035a43bd4
Norbert Schechner <nieson@web.de>
2021-06-06 21:11:18 +0200

    gmoccapy_3_1_3_9 - fix issue 1177 - stop button in auto mode
    ver 3.1.3.9
    - added missing hal status signals to glade file to set
      button sensitive states in AUTO mode correct
      This fixes issue #1177
    
    Signed-off-by: Norbert Schechner <nieson@web.de>

--- src/emc/usr_intf/gmoccapy/gmoccapy.py
+++ src/emc/usr_intf/gmoccapy/gmoccapy.py
@@ -83,7 +83,7 @@ sys.excepthook = excepthook
 
 # constants
 #         # gmoccapy  #"
-_RELEASE = " 3.1.3.8"
+_RELEASE = " 3.1.3.9"
 _INCH = 0                         # imperial units are active
 _MM = 1                           # metric units are active
 
