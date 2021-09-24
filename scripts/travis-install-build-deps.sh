7b875bec414f50638650785ac77990f53ccba404
Steffen <moeller@debian.org>
2021-09-24 17:26:49 +0200

    simplifying debian build, manual patch application

--- scripts/travis-install-build-deps.sh
+++ scripts/travis-install-build-deps.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo deb-src http://us.archive.ubuntu.com/ubuntu/ xenial main universe >> /etc/apt/sources.list'
 grep . /etc/apt/sources.list /etc/apt/sources.list.d/* || true
 sudo apt-get update -qq
-sudo apt-get install -y devscripts equivs build-essential --no-install-recommends
+sudo apt-get install -y devscripts equivs build-essential quilt --no-install-recommends
 sudo apt-get remove -f libreadline6-dev || true
 sudo apt-get remove -f libreadline-dev || true
 debian/configure
