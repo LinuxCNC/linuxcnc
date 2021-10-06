38c4d4b21502ff70ec0403f7e535b872294a9ed7
Chris Morley <chrisinnanaimo@hotmail.com>
2021-10-05 19:09:56 -0700

    qtvcp -simplewidgets: fix default template of LCDNumber
    This will make it work just as before the upgrade.
    Also remove some debug prints

--- lib/python/qtvcp/widgets/simple_widgets.py
+++ lib/python/qtvcp/widgets/simple_widgets.py
@@ -35,7 +35,7 @@ class LCDNumber(QtWidgets.QLCDNumber, _HalWidgetBase):
     def __init__(self, parent=None):
         super(LCDNumber, self).__init__(parent)
         self._pin_name = ''
-        self._floatTemplate = '{:.4f}'
+        self._floatTemplate = ''
         self._bit_pin_type = False
         self._s32_pin_type = False
         self._float_pin_type = True
@@ -61,10 +61,8 @@ class LCDNumber(QtWidgets.QLCDNumber, _HalWidgetBase):
     def updateFloatDisplay(self, data):
         try:
             if self._floatTemplate == '':
-                print ('None')
                 self.updateDisplay(data)
                 return
-            print(':',self._floatTemplate)
             t = self._floatTemplate.format
             self.display(t(data))
         except:
@@ -124,7 +122,7 @@ class LCDNumber(QtWidgets.QLCDNumber, _HalWidgetBase):
     def get_floatTemplate(self):
         return self._floatTemplate
     def reset_floatTemplate(self):
-        self._floatTemplate = '{:.4f}'
+        self._floatTemplate = ''
 
     # designer will show these properties in this order:
     pin_name = QtCore.pyqtProperty(str, get_pin_name, set_pin_name, reset_pin_name)
