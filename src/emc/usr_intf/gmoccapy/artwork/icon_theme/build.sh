8994101a310a6e0256627aa3a3b5650f85ae0dcb
Peter Müller <3819198+PeterMue@users.noreply.github.com>
2022-01-15 20:00:02 +0100

    Add material theme scaffold
    * Build script for material theme
    * Add material build to parent build.sh

--- src/emc/usr_intf/gmoccapy/artwork/icon_theme/build.sh
+++ src/emc/usr_intf/gmoccapy/artwork/icon_theme/build.sh
@@ -2,4 +2,5 @@
 pushd "$(dirname "$(realpath "$0")")"
 # build each icon theme like this:
 # python3 -m ${THEME_DIR}.build $@
+python3 -m material.build $@
 popd
---
+++ src/emc/usr_intf/gmoccapy/artwork/icon_theme/material/build.py
@@ -0,0 +1,59 @@
+import sys
+import os
+from shutil import rmtree, copyfile
+from pathlib import Path
+from _build_helper import Composer, Dimension
+
+BASE_PATH = Path(__file__).parent.absolute()
+TEMP_DIR = Path(BASE_PATH).joinpath("temp")
+TARGET_DIR = Path(BASE_PATH).joinpath("../../../../../../../share/gmoccapy/icons/material").resolve()
+
+DIMENSIONS = list(Dimension(s) for s in [8, 16, 24, 48, 128])
+
+
+def clean(dirs):
+    """Delete each directory"""
+    for dir in dirs:
+        if dir.exists():
+            rmtree(dir)
+            print(f"deleted {dir}")
+
+
+def compose_images():
+    """Compose images"""
+
+    composer = Composer(TARGET_DIR, TEMP_DIR)
+
+def create_theme_index():
+    """Create theme index file"""
+
+    os.makedirs(TARGET_DIR, exist_ok=True)
+
+    index = "index.theme"
+    src = Path(BASE_PATH).joinpath(index)
+    dest = copyfile(src, Path(TARGET_DIR).joinpath(index) )
+    if dest:
+        print(f"copied {src} -> {dest}")
+
+
+def build():
+    """Default build steps"""
+
+    clean([TARGET_DIR, TEMP_DIR])
+    compose_images()
+    clean([TEMP_DIR])
+    create_theme_index()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        build()
+    else:
+        if "clean" in sys.argv:
+            clean([TARGET_DIR, TEMP_DIR])
+
+        if "compose" in sys.argv:
+            compose_images()
+
+        if "index" in sys.argv:
+            create_theme_index()
