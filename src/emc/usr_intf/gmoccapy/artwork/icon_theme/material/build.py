cb1f7f80c7b386e169d2bb2a57c49ff590406a3f
Peter Müller <3819198+PeterMue@users.noreply.github.com>
2022-01-15 22:29:20 +0100

    Add a bunch of icon to the material theme
    * icon: back_to_app
    * icons: coolant flood and mist
    * icons: gremlin controls (zoom in/out, toolpath, clear, dimension, views)
    * icons: estop on/off
    * icons: main power on/off
    * icons: mode buttons (manual, mdi, auto, settings, user_tabs) on/off
    * icons: settings on/off
    * icons: ref home/all/axis/numeric
    * icons: spindle (forward/stop/reverse) on/off
    * icons: translate axis (touch-off)

--- src/emc/usr_intf/gmoccapy/artwork/icon_theme/material/build.py
+++ src/emc/usr_intf/gmoccapy/artwork/icon_theme/material/build.py
@@ -24,6 +24,157 @@ def compose_images():
 
     composer = Composer(TARGET_DIR, TEMP_DIR)
 
+    def svg(name):
+        return Path(BASE_PATH).joinpath("svg", name)
+
+    # ref
+    ref_icons_svg = svg("ref_icons.inkscape.svg")
+    composer.add("ref_all.symbolic.png",
+                 src_file=ref_icons_svg,
+                 context="actions",
+                 layers=["origin", "all-arrows"]
+                 )
+    composer.add("unhome.symbolic.png",
+                 src_file=ref_icons_svg,
+                 context="actions",
+                 layers=["unhome"]
+                 )
+    for axis in ["x", "y", "z", "a", "b", "c", "u", "v", "w"] + [str(i) for i in range(8)]:
+        composer.add(f"ref_{axis}.symbolic.png",
+                     src_file=ref_icons_svg,
+                     context="actions",
+                     layers=["origin", "up-right-arrow", axis]
+                     )
+
+    # translate
+    translate_icons_svg = svg("translate_icons.inkscape.svg")
+    for axis in ["x", "y", "z", "a", "b", "c", "u", "v", "w"]:
+        composer.add(f"translate_{axis}.symbolic.png",
+                     src_file=translate_icons_svg,
+                     context="actions",
+                     layers=["background", "question", axis]
+                     )
+
+    # power button & main switch
+    power_icons_svg = svg("power_icons.inkscape.svg")
+    main_switch_icons_svg = svg("main_switch_icons.inkscape.svg")
+    for state in ["on", "off"]:
+        composer.add(f"power_{state}.symbolic.png",
+                     src_file=power_icons_svg,
+                     context="actions",
+                     layers=["background", state]
+                     )
+        composer.add(f"main_switch_{state}.symbolic.png",
+                     src_file=main_switch_icons_svg,
+                     context="actions",
+                     layers=[state]
+                     )
+
+    # mode buttons
+    mode_icons_svg = svg("mode_icons.inkscape.svg")
+    for active in ["active", "inactive"]:
+        for mode in ["manual", "mdi", "auto", "settings", "user_tabs"]:
+            composer.add(f"mode_{mode}_{active}.symbolic.png",
+                         src_file=mode_icons_svg,
+                         context="actions",
+                         layers=[active, mode]
+                         )
+
+    # back to app
+    back_to_app_svg = svg("back_to_app.inkscape.svg")
+    composer.add("back_to_app.symbolic.png",
+                 src_file=back_to_app_svg,
+                 context="actions",
+                 layers=["base"]
+                 )
+
+    # gremlin controls
+    gremlin_control_icons_svg = svg("gremlin_control_icons.inkscape.svg")
+    composer.add("toolpath.symbolic.png",
+                 src_file=gremlin_control_icons_svg,
+                 context="actions",
+                 layers=["toolpath"]
+                 )
+    composer.add("clear.symbolic.png",
+                 src_file=gremlin_control_icons_svg,
+                 context="actions",
+                 layers=["clear"]
+                 )
+    composer.add("dimensions.symbolic.png",
+                 src_file=gremlin_control_icons_svg,
+                 context="actions",
+                 layers=["dimensions"]
+                 )
+
+    for zoom in ["zoom_in", "zoom_out"]:
+        composer.add(f"{zoom}.symbolic.png",
+                     src_file=gremlin_control_icons_svg,
+                     context="actions",
+                     layers=[zoom]
+                     )
+
+    composer.add(f"tool_axis_p.symbolic.png",
+                 src_file=gremlin_control_icons_svg,
+                 context="actions",
+                 layers=["perspective_plane"]
+                 )
+
+    for axis in ["x", "y", "z"]:
+        composer.add(f"tool_axis_{axis}.symbolic.png",
+                     src_file=gremlin_control_icons_svg,
+                     context="actions",
+                     layers=["simple_plane", axis]
+                     )
+
+    # coolant controls
+    coolant_icons_svg = svg("coolant_icons.inkscape.svg")
+    for status in ["active", "inactive"]:
+        for coolant in ["mist", "flood"]:
+            composer.add(f"coolant_{coolant}_{status}.symbolic.png",
+                         src_file=coolant_icons_svg,
+                         context="actions",
+                         layers=[f"{coolant}_{status}"]
+                         )
+
+    # spindle control icons
+    spindle_icons_svg = svg("spindle_icons.inkscape.svg")
+    for direction in ["left", "right"]:
+        composer.add(f"spindle_{direction}.symbolic.png",
+                     src_file=spindle_icons_svg,
+                     context="actions",
+                     layers=["spindle", "turn", direction]
+                     )
+        composer.add(f"spindle_{direction}_on.symbolic.png",
+                     src_file=spindle_icons_svg,
+                     context="actions",
+                     layers=["spindle_on", "turn", direction]
+                     )
+
+    # spindle stop
+    composer.add(f"spindle_stop_on.symbolic.png",
+                 src_file=spindle_icons_svg,
+                 context="actions",
+                 layers=["spindle", "stop_on"]
+                 )
+    composer.add(f"spindle_stop.symbolic.png",
+                 src_file=spindle_icons_svg,
+                 context="actions",
+                 layers=["spindle", "stop"]
+                 )
+
+    # jog speed
+    jog_speed_svg = svg("jog_speed_icons.inkscape.svg")
+    for speed in ["slow", "fast"]:
+        composer.add(f"jog_speed_{speed}.symbolic.png",
+                     src_file=jog_speed_svg,
+                     context="actions",
+                     layers=[speed]
+                     )
+
+    # compose all images
+    composer.compose(DIMENSIONS)
+
+
 def create_theme_index():
     """Create theme index file"""
 
