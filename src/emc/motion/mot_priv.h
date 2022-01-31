abd1c8fdfa2c6e0a96f33f2f33f6acef28bac943
Phillip Carter <phillcarter54@gmail.com>
2022-01-31 18:56:21 +1100

    add new halpins motion.jog-inhibit and motion.jog-is-active

--- src/emc/motion/mot_priv.h
+++ src/emc/motion/mot_priv.h
@@ -151,6 +151,8 @@ typedef struct {
     hal_bit_t *feed_hold;	/* RPI: set TRUE to stop motion maskable with g53 P1*/
     hal_bit_t *feed_inhibit;	/* RPI: set TRUE to stop motion (non maskable)*/
     hal_bit_t *homing_inhibit;	/* RPI: set TRUE to inhibit homing*/
+    hal_bit_t *jog_inhibit;	/* RPI: set TRUE to inhibit jogging*/
+    hal_bit_t *jog_is_active;	/* RPI: TRUE if active jogging*/
     hal_bit_t *tp_reverse;	/* Set true if trajectory planner is running in reverse*/
     hal_bit_t *motion_enabled;	/* RPI: motion enable for all joints */
     hal_bit_t *in_position;	/* RPI: all joints are in position */
