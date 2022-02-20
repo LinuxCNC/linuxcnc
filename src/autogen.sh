6235730e12a43712d2363cd9c53e11a862d98338
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2022-02-20 20:06:21 +0100

    Remove autogenerated files (fixes #1618)
    Removes the following files automake can either
    generate or install:
     - src/config.guess
     - src/config.sub
     - src/install-sh
    
    Adjusts the src/.gitignore accordingly.
    
    Switches to use autoreconf in autogen.sh.
    
    Adds Makefile.am stub as this is required by automake and
    renames Makefile to makefile (to keep precedence over
    generated Makefile).
    
    Removes everything related to TRIVIAL_BUILD as in a standard
    automake project it is also not possible to invoke any make
    targets prior to generating configure/Makefile files.
    
    Future improvements of build system could:
     - move things from makefile to Makefile.am and then
       remove makefile,
     - switch to use a different build system.
    
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- src/autogen.sh
+++ src/autogen.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 rm -rf autom4te.cache
-AUTOGEN_TARGET=${AUTOGEN_TARGET-configure:config.h.in}
-set -e
-case :$AUTOGEN_TARGET: in
-*:configure:*) autoconf; touch configure ;;
-esac
-case :$AUTOGEN_TARGET: in
-*:config.h.in:*) autoheader; touch config.h.in ;;
-esac
+autoreconf -fi .
