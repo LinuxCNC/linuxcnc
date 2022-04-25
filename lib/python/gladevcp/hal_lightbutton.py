c016e5e13efdda77f02015abe58dc296758cc095
Hans Unzner <hansunzner@gmail.com>
2022-04-25 09:36:29 +0200

    gladevcp: removed PangoCairo warning

--- lib/python/gladevcp/hal_lightbutton.py
+++ lib/python/gladevcp/hal_lightbutton.py
@@ -17,6 +17,7 @@ gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
+gi.require_version("PangoCairo", "1.0")
 from gi.repository import Pango, PangoCairo
 import cairo
 import math
