7c6be08cb2c16f75dc4eb201ac5d3bcc3d6d34b5
CMorley <chrisinnanaimo@hotmail.com>
2022-06-10 22:07:02 -0700

    qtvcp -hallabel: base widget on scaled label instead of qlabel

--- lib/python/qtvcp/widgets/hal_label.py
+++ lib/python/qtvcp/widgets/hal_label.py
@@ -19,6 +19,7 @@ from PyQt5.QtWidgets import QLabel
 import hal
 from qtvcp.widgets.widget_baseclass import _HalWidgetBase
 from PyQt5.QtCore import pyqtSignal, pyqtProperty, pyqtSlot
+from qtvcp.widgets.simple_widgets import ScaledLabel
 from qtvcp import logger
 
 # Instantiate the libraries with global reference
@@ -30,7 +31,7 @@ LOG = logger.getLogger(__name__)
 
 
 ################################################################
-class HALLabel(QLabel, _HalWidgetBase):
+class HALLabel(ScaledLabel, _HalWidgetBase):
     def __init__(self, parent=None):
         super(HALLabel, self).__init__(parent)
         self._textTemplate = '%f'
@@ -41,6 +42,7 @@ class HALLabel(QLabel, _HalWidgetBase):
         self._float_pin_type = False
 
     def _hal_init(self):
+        super()._hal_init()
         if self._pin_name == '':
             pname = self.HAL_NAME_
         else:
