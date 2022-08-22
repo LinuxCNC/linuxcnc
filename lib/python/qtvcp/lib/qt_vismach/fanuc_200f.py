25f05dc6c0ca3a12586f0a71411bf43d59629235
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2022-08-22 22:38:11 +0200

    Switch to python3 for fanuc_200f.py
    Also adds executable bit.
    
    Signed-off-by: Damian Wrobel <dwrobel@ertelnet.rybnik.pl>

--- lib/python/qtvcp/lib/qt_vismach/fanuc_200f.py
+++ lib/python/qtvcp/lib/qt_vismach/fanuc_200f.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
