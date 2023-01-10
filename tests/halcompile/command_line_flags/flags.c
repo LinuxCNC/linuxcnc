7b1d41b7023730626eeeda3bc294fda636e0bc25
andypugh <andy@bodgesoc.org>
2023-01-10 01:54:49 +0000

    halcompile: Add command line arguments to provide compile and link flags
    It is possible to provide extra compiler and linker flags with "option" lines
    inside a hal component written in the .comp preprocessor format.
    However for files written in straight C this does not work.
    This commit adds --extra-compile-args and --extra-link-args as command-line
    options to halcompile.
    See https://github.com/LinuxCNC/linuxcnc/issues/2204 for more background
    
    
    Signed-off-by: andypugh <andy@bodgesoc.org>

---
+++ tests/halcompile/command_line_flags/flags.c
@@ -0,0 +1,20 @@
+/*
+  flags.c
+  To test command line compiler flags to halcompile
+
+  Copyright (C) 2023 Andy Pugh
+ */
+
+
+#ifndef ULAPI
+#error This is intended as a userspace component only.
+#endif
+
+#include <readline.h>
+
+int main(int argc, char **argv)
+{
+    const char* res;
+    rl_initialize();
+    return 0;
+}
