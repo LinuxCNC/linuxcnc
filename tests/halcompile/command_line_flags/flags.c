a94aa82a2a78d7cfa08f5b8c53b4d10ad85929a9
andypugh <andy@bodgesoc.org>
2023-01-10 00:31:38 +0000

    Tests: Add a test for halcompile command-line flags
    Signed-off-by: andypugh <andy@bodgesoc.org>

---
+++ tests/halcompile/command_line_flags/flags.c
@@ -0,0 +1,21 @@
+/*
+  flags.c
+  To test command line compiler flags to halcompile
+
+  Copyright (C) 2023 Andy Pugh
+ */
+
+
+#ifndef ULAPI
+#error This is intended as a userspace component only.
+#endif
+
+#include <readline.h>
+
+int main(int argc, char **argv)
+{
+    const char* res;
+    rl_ding();
+    return 0;
+}
+
