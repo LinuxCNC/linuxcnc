d6f6ac1cef5a6c45b2fd33ecc8d7c17482a4399f
Sebastian Kuzminsky <seb@highlab.com>
2023-01-22 19:49:54 +0000

    tests: verify that halcompile installs files with correct permissions
    JT reports on IRC that `halcompile --install` installs uspace realtime
    comps (which are .so files) with execute permissions enabled, which
    is wrong.
    
    This commit modifies some existing halcompile tests to verify that
    `halcompile --install` makes userspace (non-rt) comps executable, and
    rt comps (at least in uspace) non-executable.
    
    The second part of that currently fails, fixed in the following commit.
    
    (Note that our debs correctly have no execute bits set for the .so
    realtime comps.)

--- tests/halcompile/personalities_mod/test.sh
+++ tests/halcompile/personalities_mod/test.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
+set -e
+
 DIR=../../../src/hal/components ;# use in-tree components
 halcompile --personalities=2 --install $DIR/lincurve.comp
 halcompile --personalities=2 --install $DIR/logic.comp
 halcompile --personalities=2 --install $DIR/bitslice.comp
+
+# This tells us the expected filename extension ${MODULE_EXT} of realtime
+# modules.
+source "${EMC2_HOME}/scripts/rtapi.conf"
+
+for INSTALLED_FILE in "${EMC2_HOME}"/rtlib/{lincurve,logic,bitslice}"${MODULE_EXT}"; do
+    if [[ ! -f "${INSTALLED_FILE}" ]]; then
+        echo "'halcompile --install' did not install '${INSTALLED_FILE}'"
+        exit 1
+    fi
+
+    MODE=$(stat --printf '%#03a\n' "${INSTALLED_FILE}")
+    if [[ $((MODE & 0111)) != '0' ]]; then
+        echo "installed file '${INSTALLED_FILE}' has incorrect permissions"
+        echo "expected no execute bits, got ${MODE}"
+        exit 1
+    fi
+done
 
 for HAL in *.hal; do
     echo "testing $HAL"
