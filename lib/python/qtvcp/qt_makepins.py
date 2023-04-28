b7559509fee872262d1be95f48f1efdb0989ae60
CMorley <chrisinnanaimo@hotmail.com>
2023-04-28 08:09:05 -0600

    qtvcp -keep track of embedded panels so as to call closing functions
    otherwise preference saving won't work

--- lib/python/qtvcp/qt_makepins.py
+++ lib/python/qtvcp/qt_makepins.py
@@ -43,6 +43,7 @@ class QTPanel():
         self._screenOptions = None
         self._geo_string = ''
         self.PATH = path
+        window._VCPWindowList.append(window)
 
         # see if a screenoptions widget is present
         # if it is then initiate the preference file
@@ -97,6 +98,7 @@ class QTPanel():
                     # search for ui path and load if available
                     uipath = self.PATH.find_embed_panel_path(cmd)
                     window[pName].instance(uipath)
+                    window._VCPWindowList.append(window[pName])
 
                     # initialize handler if available
                     if hndlr is not True and os.path.exists(hndlr):
