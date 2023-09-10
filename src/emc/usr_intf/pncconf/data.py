f04fdafe6be50038c6f97e43b43181818fd83f1b
CMorley <chrisinnanaimo@hotmail.com>
2023-09-09 22:53:38 -0700

    pncconf -fix setting of Touchy preferences
    was fixed in 2.9 but not 2.8

--- src/emc/usr_intf/pncconf/data.py
+++ src/emc/usr_intf/pncconf/data.py
@@ -995,8 +995,10 @@ If you have a REALLY large config that you wish to convert to this newer version
             #print "Setting TOUCHY preferences"
             templist = {"touchyabscolor":"abs_textcolor","touchyrelcolor":"rel_textcolor",
                         "touchydtgcolor":"dtg_textcolor","touchyerrcolor":"err_textcolor"}
-            for key,value in templist.iteritems():
-                prefs.putpref(value, self[key], str)
+
+
+            for key,value in templist.items():
+                _APP.set_touchy_preference(value, self[key], str)
             if self.touchyposition[0] or self.touchysize[0]:
                     pos = size = ""
                     if self.touchyposition[0]:
@@ -1005,9 +1007,9 @@ If you have a REALLY large config that you wish to convert to this newer version
                         size = "%dx%d"% (self.touchysize[1],self.touchysize[2])
                     geo = "%s%s"%(size,pos)
             else: geo = "default"
-            prefs.putpref('window_geometry',geo, str)
-            prefs.putpref('gtk_theme',self.touchytheme, str)
-            prefs.putpref('window_force_max', self.touchyforcemax, bool)
+            _APP.set_touchy_preference('window_geometry',geo, str)
+            _APP.set_touchy_preference('gtk_theme',self.touchytheme, str)
+            _APP.set_touchy_preference('window_force_max', self.touchyforcemax, bool)
 
         # write AXIS rc file for geometry
         if self.frontend == _PD._AXIS and (self.axisposition[0] or self.axissize[0] or self.axisforcemax):
