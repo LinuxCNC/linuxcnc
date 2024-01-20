0d9a475218d3093ea4b16dd8661bba2812fb7ddd
CMorley <chrisinnanaimo@hotmail.com>
2024-01-20 00:08:25 -0800

    qtvcp -cam_view: make sure the image is square
    otherwise things are shown distorted

--- lib/python/qtvcp/widgets/camview_widget.py
+++ lib/python/qtvcp/widgets/camview_widget.py
@@ -324,6 +324,13 @@ class CamView(QtWidgets.QWidget, _HalWidgetBase):
             except:
                 pass
 
+    def resizeEvent(self, event):
+        # Create a square base size of 10x10 and scale it to the new size
+        # maintaining aspect ratio.
+        new_size = QtCore.QSize(10, 10)
+        new_size.scale(event.size(), QtCore.Qt.KeepAspectRatio)
+        self.resize(new_size)
+
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
