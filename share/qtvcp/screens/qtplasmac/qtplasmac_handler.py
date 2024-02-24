d94aa29a01cc519257477743c03d42dec9d6eb8e
Greg Carl <26096779+snowgoer540@users.noreply.github.com>
2024-02-24 09:58:10 -0500

    qtplasmac:  allow Z DRO to display torch height in manual cut

--- share/qtvcp/screens/qtplasmac/qtplasmac_handler.py
+++ share/qtvcp/screens/qtplasmac/qtplasmac_handler.py
@@ -1,4 +1,4 @@
-VERSION = '237.304'
+VERSION = '237.305'
 LCNCVER = '2.9'
 DOCSVER = LCNCVER
 
@@ -106,6 +106,7 @@ def click_signal(widget):
   # the main handler
 class HandlerClass:
     from qtvcp.lib.qtplasmac import conversational as CONV
+
     # when self.w.button_frame changes size
     def eventFilter(self, object, event):
         if (event.type() == QtCore.QEvent.Resize):
@@ -2271,7 +2272,7 @@ class HandlerClass:
                 STATUS.emit('update-machine-log', log, 'TIME')
 
     def plasmac_state_changed(self, state):
-        if ((state > self.PROBE_UP and not STATUS.is_interp_idle()) or state == self.PROBE_TEST) and hal.get_value('axis.z.eoffset-counts'):
+        if (state > self.PROBE_UP or state == self.PROBE_TEST) and hal.get_value('axis.z.eoffset-counts'):
             # set z dro to offset mode
             self.w.dro_z.setProperty('Qreference_type', 10)
         if state == self.IDLE:
