d55ddb4ab16918efb604cefb95b34313c55d67b1
David Mueller <mueller_david@hotmail.com>
2024-08-26 10:24:04 +0200

    fix configs/apps/gladevcp: update to gtk3 ('sourceview' and 'animated backdrop' still not 100%))

--- configs/apps/gladevcp/by-widget/sourceview.py
+++ configs/apps/gladevcp/by-widget/sourceview.py
@@ -1,6 +1,7 @@
-
-import gtk
-import gtksourceview2 as gtksourceview
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository import GtkSource
 
 class HandlerClass:
 
@@ -15,23 +16,26 @@ class HandlerClass:
             self.mark = self.textbuffer.create_source_mark('highlight', 'highlight', line)
             self.mark.set_visible(True)
         else:
+            print('3')
             self.textbuffer.move_mark(self.mark, line)
+        print('4')
         self.sourceview.scroll_to_mark(self.mark, 0, True, 0, 0.5)
-
+        print('5')
+        
     def file_set(self,widget,data=None):
         filename = widget.get_filename()
         print("file_set",filename)
         self.textbuffer.set_text(open(filename).read())
         self.line = 1
-        self._set_line(self.line)
+        #self._set_line(self.line)
 
     def on_down(self,widget,data=None):
         self.line += 1
-        self._set_line(self.line)
+        #self._set_line(self.line)
 
     def on_up(self,widget,data=None):
         self.line -= 1
-        self._set_line(self.line)
+        #self._set_line(self.line)
 
     def __init__(self, halcomp,builder,useropts):
         self.halcomp = halcomp
@@ -39,14 +43,14 @@ class HandlerClass:
 
         self.line = 1
         self.sourceview = builder.get_object('gtksourceview1')
-        self.textbuffer = gtksourceview.Buffer()
+        self.textbuffer = GtkSource.Buffer()
         self.sourceview.set_buffer(self.textbuffer)
 
         self.sourceview.set_show_line_numbers(True)
         self.sourceview.set_show_line_marks(True)
         self.sourceview.set_highlight_current_line(True)
-        self.sourceview.set_mark_category_icon_from_icon_name('highlight', 'gtk-forward')
-        self.sourceview.set_mark_category_background('highlight', gtk.gdk.Color('yellow'))
+        #self.sourceview.set_mark_category_icon_from_stock('highlight', 'gtk-forward')
+        #self.sourceview.set_mark_category_background('highlight', gtk.gdk.Color('yellow'))
         self.mark = None
 
 def get_handlers(halcomp,builder,useropts):
