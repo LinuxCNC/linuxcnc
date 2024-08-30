1af6b67da6bcbee7fe6c554822b801296d036220
Sigma1912 <46067220+Sigma1912@users.noreply.github.com>
2024-08-30 14:54:10 +0200

    Update configs/apps/gladevcp/animated-backdrop/cairodraw.py
    Co-authored-by: Hans Unzner <hansunzner@gmail.com>

--- configs/apps/gladevcp/animated-backdrop/cairodraw.py
+++ configs/apps/gladevcp/animated-backdrop/cairodraw.py
@@ -2,7 +2,7 @@
 import gi
 gi.require_version("Gtk","3.0")
 gi.require_version("Gdk","3.0")
-gi.require_version("GtkSource","3.0")
+gi.require_version("GtkSource","4")
 from gi.repository import Gtk
 import sys, cairo
 from math import pi
