f82dfe8cd18e7497f6f212b11834affd9f02541a
Hans Unzner <hansunzner@gmail.com>
2025-02-09 10:33:30 +0100

    gmoccapy_3_5_0: update release notes, set version number

--- src/emc/usr_intf/gmoccapy/gmoccapy.py
+++ src/emc/usr_intf/gmoccapy/gmoccapy.py
@@ -76,7 +76,7 @@ sys.excepthook = excepthook
 
 # constants
 #         # gmoccapy  #"
-_RELEASE = " 3.4.9"
+_RELEASE = " 3.5.0"
 _INCH = 0                         # imperial units are active
 _MM = 1                           # metric units are active
 
