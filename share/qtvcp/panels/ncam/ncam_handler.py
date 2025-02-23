b8ec4eac87d4a1d81ffba62920e71d63780501de
CMorley <chrisinnanaimo@hotmail.com>
2025-02-23 12:45:26 -0800

    qtvcp -qt ncam panel: add a basic panel for testing
    embeddable in other screens

---
+++ share/qtvcp/panels/ncam/ncam_handler.py
@@ -0,0 +1,79 @@
+############################
+# **** IMPORT SECTION **** #
+############################
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from qtvcp.core import Status
+from qtvcp.lib.native_cam.nccam_window import NCamWindow as NCAM
+
+###########################################
+# **** instantiate libraries section **** #
+###########################################
+STATUS = Status()
+###################################
+# **** HANDLER CLASS SECTION **** #
+###################################
+
+class HandlerClass:
+
+    ########################
+    # **** INITIALIZE **** #
+    ########################
+    # widgets allows access to  widgets from the qtvcp files
+    # at this point the widgets and hal pins are not instantiated
+    def __init__(self, halcomp,widgets,paths):
+        self.w = widgets
+
+
+    ##########################################
+    # Special Functions called from QTVCP
+    ##########################################
+
+    # at this point:
+    # the widgets are instantiated.
+    # the HAL pins are built but HAL is not set ready
+    def initialized__(self):
+        self.object = NCAM()
+        self.w.mainLayout.addWidget(self.object)
+
+
+    ########################
+    # callbacks from STATUS #
+    ########################
+
+    #######################
+    # callbacks from form #
+    #######################
+
+    #####################
+    # general functions #
+    #####################
+
+    #####################
+    # KEY BINDING CALLS #
+    #####################
+
+    ###########################
+    # **** closing event **** #
+    ###########################
+    def closing_cleanup__(self):
+        pass
+
+    ##############################
+    # required class boiler code #
+    ##############################
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+    def __setitem__(self, item, value):
+        return setattr(self, item, value)
+
+
+################################
+# required handler boiler code #
+################################
+
+def get_handlers(halcomp,widgets,paths):
+     return [HandlerClass(halcomp,widgets,paths)]
