8a83e1b6d455ee17951933176ad47f24c8340a2c
Steve Richardson <tangentaudio@gmail.com>
2025-02-25 18:21:13 -0500

    QtVcp: tool chooser, add gpl header

--- lib/python/qtvcp/widgets/tool_chooser.py
+++ lib/python/qtvcp/widgets/tool_chooser.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+# qtvcp
+#
+# Copyright (c) 2025 Steve Richardson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#################################################################################
+
 import sys
 import os
 import operator
