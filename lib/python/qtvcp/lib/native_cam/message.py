bc10785db009318676c56a15b4cd3688b15b5614
CMorley <chrisinnanaimo@hotmail.com>
2025-03-15 12:08:37 -0700

    qtvcp -ncam: add a prebuilt message for status update
    Not used yet but probably useful for embedded ncam

--- lib/python/qtvcp/lib/native_cam/message.py
+++ lib/python/qtvcp/lib/native_cam/message.py
@@ -15,12 +15,27 @@ class ZMQMessage:
         self.topic = b'QtVCP'
 
     def reload(self, fname):
-        print('reload messsge:',fname)
+        print('ZMQ reload messsge:',fname)
         # prebuilt message 1
         # makes a dict of function to call plus any arguments
         x = {                               # <1>
             "FUNCTION": "external_load",
             "ARGS": [fname]
+            }
+        # convert to JSON object
+        m1 = json.dumps(x)
+        try:
+            self.socket.send_multipart([self.topic, bytes((m1).encode('utf-8'))])
+        except:
+            pass
+
+    def addStatus(self,msg, alertLevel = 0, noLog = False):
+        print('ZMQ add status:',msg)
+        # prebuilt message 2
+        # makes a dict of function to call plus any arguments
+        x = {                               # <1>
+            "FUNCTION": "add_status",
+            "ARGS": [msg, alertLevel, noLog]
             }
         # convert to JSON object
         m1 = json.dumps(x)
