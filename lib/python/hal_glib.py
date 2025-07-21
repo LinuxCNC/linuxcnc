b12a396a6229711ad9cf69b59e4dc3f8c3a5e68b
CMorley <chrisinnanaimo@hotmail.com>
2025-07-20 17:47:08 -0700

    hal_glib spring board file - remove dubious comments

--- lib/python/hal_glib.py
+++ lib/python/hal_glib.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-
-# QTVcp Logging Module
-# Provides a consistent and easy to use logging facility.  Log messages printed
-# to the terminal will be colorized for easy identification of log level.
-#
-# Copyright (c) 2017 Kurt Jacobson
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
