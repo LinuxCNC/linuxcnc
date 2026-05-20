8fef3f43c2954f029aeb2414135a5a8642e44684
Bertho Stultiens <bertho@vagrearg.org>
2026-05-20 18:12:20 +0200

    shellcheck: unused variables that can be removed

--- tests/overrun/test.sh
+++ tests/overrun/test.sh
@@ -2,7 +2,6 @@
 TMPDIR=$(mktemp -d /tmp/overrun.XXXXXX)
 trap 'rm -rf "$TMPDIR"' 0 1 2 3 15
 
-TEST_HAL=$TMPDIR/test.hal
 echo loadusr -w echo overrun > "$TMPDIR/test.hal"
 
 ! $RUNTESTS "$TMPDIR" 2>&1
