81adbbca1a21f14c4fd5b035db5c780cfae4bcfa
Bertho Stultiens <bertho@vagrearg.org>
2026-05-20 18:15:54 +0200

    shellcheck: mostly ignore required constructs that generate diagnostics

--- tests/inifile/missing_values/test.sh
+++ tests/inifile/missing_values/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# We don't want a single redirect because the output must be very specific.
+# shellcheck disable=SC2129
 true > result
 inivar -ini missing_section.ini -sec section1 -var key1 >> result 2>&1
 inivar -ini missing_value.ini -var key1 >> result 2>&1
--- tests/interp/compile/test.sh
+++ tests/interp/compile/test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -xe
 
+# PYTHON_CPPFLAGS is supposed to be word split
+# shellcheck disable=SC2086
 g++ -o use-rs274 use-rs274.cc \
     -Wall -Wextra -Wno-return-type -Wno-unused-parameter \
     -I "$HEADERS" $PYTHON_CPPFLAGS -L "$LIBDIR" -Wl,-rpath,"$LIBDIR" $PYTHON_EXTRA_LDFLAGS $PYTHON_LIBS $PYTHON_EXTRA_LIBS -lrs274
