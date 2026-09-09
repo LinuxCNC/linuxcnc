851d6df65b3a0fa300b670ff12ae280755e84eb3
Cmorley <chrisinnanaimo@hotmail.com>
2026-09-09 14:07:17 -0700

    qtvcp -allow setting of window title from designer
    mostly for simpole VCPs

--- src/emc/usr_intf/qtvcp/qtvcp.py
+++ src/emc/usr_intf/qtvcp/qtvcp.py
@@ -335,10 +335,11 @@ Pressing cancel will close linuxcnc.""" % target)
 
             # title
             if SCRN_INIPATH:
-                title ='QTvcp-Screen-%s'% opts.component
+                title ='QtVCP-Screen-%s'% opts.component
             else:
-                title = 'QTvcp-Panel-%s'% opts.component
-            window.setWindowTitle(title)
+                title = 'QtVCP-Panel-%s'% opts.component
+            if 'MainWindow' in window.windowTitle():
+                window.setWindowTitle(title)
 
         if opts.usermod and "pre_hal_init__" in dir(window.handler_instance):
             LOG.debug('''Calling the handler file's pre_hal_init__ function''')
