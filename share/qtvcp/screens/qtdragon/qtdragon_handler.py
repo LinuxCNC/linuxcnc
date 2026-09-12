3f5c935dd8585a0024bbde20099283e9b8aec54f
Rene Hopf <renehopf@mac.com>
2026-09-12 21:47:42 +0200

    change external offset to float

--- share/qtvcp/screens/qtdragon/qtdragon_handler.py
+++ share/qtvcp/screens/qtdragon/qtdragon_handler.py
@@ -359,7 +359,7 @@ class HandlerClass:
         QHAL.newPin("eoffset-enable", QHAL.HAL_BIT, QHAL.HAL_OUT)
         QHAL.newPin("eoffset-clear", QHAL.HAL_BIT, QHAL.HAL_OUT)
         self.h['eoffset-clear'] = True
-        QHAL.newPin("eoffset-spindle-count", QHAL.HAL_S32, QHAL.HAL_OUT)
+        QHAL.newPin("eoffset-spindle-count", QHAL.HAL_FLOAT, QHAL.HAL_OUT)
         pin = QHAL.newPin("eoffset-is-active", QHAL.HAL_BIT, QHAL.HAL_IN)
         pin.pinValueChanged.connect(lambda p,v: self.external_offset_state_changed(v))
 
