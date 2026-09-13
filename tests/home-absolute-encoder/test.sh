6136221d1b3a908f5be12ca4fef5a3f178ae0439
Rene Hopf <renehopf@mac.com>
2026-09-13 19:25:33 +0200

    fix encoder test syntax

--- tests/home-absolute-encoder/test.sh
+++ tests/home-absolute-encoder/test.sh
@@ -3,7 +3,7 @@
 # Seed the position file with a stale position that has nothing to do
 # with the absolute encoder value.
 rm -f position.txt
-for i in $(seq 16); do echo 3.0; done > position.txt
+yes 3.0 | head -n 16 > position.txt
 
 # First run starts from the seeded position file.
 linuxcnc -r test.ini || exit 1
