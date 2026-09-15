56181536464617737878b02d6efc801d89859046
Hans Unzner <hansunzner@gmail.com>
2026-09-15 21:49:14 +0200

    halscope: trigger line optimization
    - change the style of the dotted line
    - reduce the size of the edge indicator arrow-
    - swap colors: the trigger arrow now has the color of the channel instead of the trigger line

--- src/hal/utils/scope_disp.c
+++ src/hal/utils/scope_disp.c
@@ -714,7 +714,7 @@ void draw_triggerline(int chan_num, int highlight) {
         chan->scale * ((chan->position - trig->level) * 10) +
 	chan->vert_offset;
 
-    const double dashes[2] = {2,4};
+    const double dashes[2] = {6,6};
     int ndash = sizeof(dashes) / sizeof(dashes[0]);
 
     int y1 = (fp_level-yfoffset) * yscale + ypoffset;
@@ -728,23 +728,26 @@ void draw_triggerline(int chan_num, int highlight) {
 
     if(ctrl_shm->trig_edge) dy = -dy;
 
-    if(highlight) {
-        gdk_cairo_set_source_rgba(disp->context, &disp->color_selected[chan_num - 1]);
-    } else {
-        gdk_cairo_set_source_rgba(disp->context, &disp->color_normal[chan_num - 1]);
-    }
-    cairo_set_dash(disp->context, dashes, ndash, 0.0);
-    line(chan_num | 0x200, 0, y1, disp->width, y1);
-    /* setting ndash = 0 to disable dashing */
-    cairo_set_dash(disp->context, dashes, 0, 0.0);
+    /* set color for trigger line and edge indicator */
     if(highlight) {
         gdk_cairo_set_source_rgba(disp->context, &disp->color_grid);
     } else {
         gdk_cairo_set_source_rgba(disp->context, &disp->color_baseline);
     }
+    cairo_set_dash(disp->context, dashes, ndash, 0.0);
+    line(chan_num | 0x200, 0, y1, disp->width, y1);
+    /* setting ndash = 0 to disable dashing */
+    cairo_set_dash(disp->context, dashes, 0, 0.0);
+    /* set color for trigger edge indicator */
+    if(highlight) {
+        gdk_cairo_set_source_rgba(disp->context, &disp->color_selected[chan_num - 1]);
+    } else {
+        gdk_cairo_set_source_rgba(disp->context, &disp->color_normal[chan_num - 1]);
+    }
+    /* draw trigger edge indicator */
     line(chan_num | 0x300, 2*dx, y1, 2*dx, y1 + 2*dy);
-    line(chan_num | 0x300, dx, y1+dy, 2*dx, y1 + 2*dy);
-    line(chan_num | 0x300, 3*dx, y1+dy, 2*dx, y1 + 2*dy);
+    line(chan_num | 0x300, (2-0.7)*dx, y1+1.2*dy, 2*dx, y1 + 2*dy);
+    line(chan_num | 0x300, (2+0.7)*dx, y1+1.2*dy, 2*dx, y1 + 2*dy);
 }
 
 
