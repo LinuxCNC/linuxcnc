ff1462cc7790c32c4b218c457916f6a286282ede
Rene Hopf <renehopf@mac.com>
2026-09-17 01:44:52 +0200

    Revert "hal_bridge -add a warning if the write address is already used"
    This reverts commit ee2369c7030babba64de948742b12881abf31536.

--- src/hal/user_comps/hal_bridge.py
+++ src/hal/user_comps/hal_bridge.py
@@ -118,10 +118,7 @@ class Bridge(object):
     def init_write(self):
         context = zmq.Context()
         self.writeSocket = context.socket(zmq.PUB)
-        try:
-            self.writeSocket.bind(self.writeAddress)
-        except zmq.error.ZMQError:
-            LOG.critical(f'Write address already in use? {self.writeAddress}')
+        self.writeSocket.bind(self.writeAddress)
 
     def init_read(self):
         # ZeroMQ Context
