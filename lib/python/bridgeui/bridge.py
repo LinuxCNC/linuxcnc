2c089fc0605530c35367ab53f02e6d6ea4038fc3
Rene Hopf <renehopf@mac.com>
2026-09-17 01:44:50 +0200

    Revert "bridge -Don't break everything if there is no ZMQ library"
    This reverts commit 7836c07209a12294c47671c731e697d88908fc63.

--- lib/python/bridgeui/bridge.py
+++ lib/python/bridgeui/bridge.py
@@ -94,9 +94,6 @@ class Bridge(object):
 
     # callback from ZMQ read socket
     def readMsg(self):
-        # no ZMQ library. return so HALUI works otherwise
-        if not ZMQ: return
-
         if self.readSocket.getsockopt(zmq.EVENTS) & zmq.POLLIN:
             while self.readSocket.getsockopt(zmq.EVENTS) & zmq.POLLIN:
                 # get raw message
