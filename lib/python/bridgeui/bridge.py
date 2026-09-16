0c417593debc56ad94799feeac9952868996112b
Rene Hopf <renehopf@mac.com>
2026-09-17 01:44:51 +0200

    Revert "bridge - send out a key string with macro/mdi name"
    This reverts commit a3dbe9eaa6bc77e81d0b385bd000fefe138b5ca2.

--- lib/python/bridgeui/bridge.py
+++ lib/python/bridgeui/bridge.py
@@ -138,11 +138,11 @@ class Bridge(object):
             #print ('axis state', self.axesSelected,self.currentSelectedAxis)
 
     # send msg to hal_glib
-    def writeMsg(self, msg, data1=()):
+    def writeMsg(self, msg, data=''):
         #print('Write Msg called')
         if ZMQ:
             topic = self.writeTopic
-            message = json.dumps({'FUNCTION':msg,'ARGS':data1})
+            message = json.dumps({'FUNCTION':msg,'ARGS':data})
             LOG.debug('Sending ZMQ Message:{} {}'.format(topic, message))
             self.writeSocket.send_multipart(
                         [bytes(topic.encode('utf-8')),
@@ -179,7 +179,6 @@ class Bridge(object):
     # if the number is bigger then MDI command list
     # then look for MACRO commands
     def getMdiName(self, num):
-        # macro if num beyound length of MDIs
         if num >len(self.INFO.MDI_COMMAND_DICT)-1:
             offset = len(self.INFO.MDI_COMMAND_DICT)
             return self.getMacroName(num-offset)
@@ -196,12 +195,11 @@ class Bridge(object):
         return temp
 
     def runIndexedMacro(self, num):
-        if num <= len(self.INFO.MDI_COMMAND_DICT)-1:
-            # check for any MDI commands first:
-            name = self.getMdiName(num)
-            LOG.debug('Macro name:{} ,index: {}'.format(name, num))
-            if name != 'None':
-                self.writeMsg('request_macro_call', (name, 'MDI'))
+        # check for any MDI commands first:
+        name = self.getMdiName(num)
+        LOG.debug('Macro name:{} ,index: {}'.format(name, num))
+        if name != 'None':
+            self.writeMsg('request_macro_call', name)
 
         # else look for any MACRO commands:
         else:
@@ -209,11 +207,11 @@ class Bridge(object):
             name = self.getMacroName(num-offset)
             LOG.debug('Macro name:{} ,index: {}'.format(name, num))
             if name != 'None':
-                self.writeMsg('request_macro_call',(name, 'MACRO'))
+                self.writeMsg('request_macro_call', name)
 
-        # count of MDI and MACRO commands
+        # cound of MDI and MACRO commands
     def getMdiCount(self):
-        #print('->',len(self.INFO.MDI_COMMAND_DICT) + len(self.INFO.MACRO_COMMAND_DICT))
+        #print('->',len(self.INFO.MDI_COMMAND_DICT),len(self.INFO.MACRO_COMMAND_DICT))
         return len(self.INFO.MDI_COMMAND_DICT) + len(self.INFO.MACRO_COMMAND_DICT)
 
     def getJogRate(self):
