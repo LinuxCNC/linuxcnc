f3a59a99026e6ced32b5c3d7a513745209ad2c64
Rene Hopf <renehopf@mac.com>
2026-09-17 01:44:41 +0200

    Revert "gmoccapy -create a dialog in gmoccapy for system shutdown"
    This reverts commit 2ad3a0b0d688742dd1cac52b8d01e3566e0c03ab.

--- src/emc/usr_intf/gmoccapy/gmoccapy.py
+++ src/emc/usr_intf/gmoccapy/gmoccapy.py
@@ -2957,11 +2957,7 @@ class gmoccapy(object):
         self.widgets.window1.destroy()
 
     def system_shutdown(self, widget):
-        msg = _('Unsaved Data Will Be Lost.\n Are You Sure ?')
-        response = self.dialogs.show_yesno_dialog(self,
-               msg , _('Warning: System Shutdown'),'WARNING')
-        if response:        
-            self.ACTION.SHUT_SYSTEM_DOWN_PROMPT(False)
+        self.ACTION.SHUT_SYSTEM_DOWN_PROMPT()
 
 # button handlers End
 # =========================================================
