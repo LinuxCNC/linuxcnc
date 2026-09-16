e061c50335544794aeb1738ce630cffa5837d750
Rene Hopf <renehopf@mac.com>
2026-09-17 01:44:51 +0200

    Revert "halui -add softkey pins and message"
    This reverts commit 822e4c17f2fcce44b189932f04d542eed78e6bbe.

--- lib/python/bridgeui/bridge.py
+++ lib/python/bridgeui/bridge.py
@@ -168,12 +168,8 @@ class Bridge(object):
     def shutdownController(self):
         self.writeMsg('request_shutdown')
 
-    def softkey(self, index):
-        print(f'Softkey index {index}')
-        self.writeMsg('request_softkey', index)
-
-    # if the number is bigger then MDI command list
-    # then look for MACRO commands
+        # if the number is bigger then MDI command list
+        # then look for MACRO commands
     def getMdiName(self, num):
         if num >len(self.INFO.MDI_COMMAND_DICT)-1:
             offset = len(self.INFO.MDI_COMMAND_DICT)
