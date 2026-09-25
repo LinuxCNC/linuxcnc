2e89ae18bf90933ab878a2387a4e7ef246a41ae7
Hannes Diethelm <hannes.diethelm@gmail.com>
2026-09-25 22:45:54 +0200

    VERSION: wip

--- .github/scripts/build-package-arch.sh
+++ .github/scripts/build-package-arch.sh
@@ -4,8 +4,6 @@ set -eu #Needed so CI fails when anything is wrong
 set -x
 
 debian/configure
-debian/update-dch-from-git
-scripts/get-version-from-git | sed -re 's/^v(.*)$/\1/' >| VERSION; cat VERSION
 git diff
 apt-get --yes build-dep --arch-only .
 debuild -us -uc --build=any
